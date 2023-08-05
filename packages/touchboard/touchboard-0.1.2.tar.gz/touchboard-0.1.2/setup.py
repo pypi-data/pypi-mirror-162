c085459aeee66107919a01e08b0a32e0e9b2a018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package touchboard/0.1.2/touchboard-0.1.2.tar.gz

---
+++ packages/touchboard/touchboard-0.1.2.tar.gz/touchboard-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['touchboard']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyautogui>=0.9.53', 'rubicon-objc>=0.4.2']
+
+setup_kwargs = {
+    'name': 'touchboard',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': "# Installation:\n\nRun ```pip install touchboard ==0.1.0```. Replace the version (0.1.0) with your corresponding version by operating system (0.1.0 for windows, 0.1.1 for linux, or 0.1.2 for macos)\n\n#### Note: This has ONLY been tested on windows.\n# Code:\n\n```py\nimport touchboard\n\ntouchboard.begin('SERIAL PORT HERE')\n```\n",
+    'author': 'Python Nerd',
+    'author_email': 'prajwalmisc@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
