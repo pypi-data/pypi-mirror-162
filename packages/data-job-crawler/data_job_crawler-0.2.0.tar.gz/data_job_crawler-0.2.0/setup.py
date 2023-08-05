b4cc086729380171627e056d8755d401f4a1a034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package data-job-crawler/0.2.0/data_job_crawler-0.2.0.tar.gz

---
+++ packages/data-job-crawler/data_job_crawler-0.2.0.tar.gz/data_job_crawler-0.2.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['data_job_crawler',
+ 'data_job_crawler.crawler',
+ 'data_job_crawler.crawler.spiders']
+
+package_data = \
+{'': ['*'], 'data_job_crawler.crawler.spiders': ['data/*']}
+
+install_requires = \
+['Scrapy>=2.6.2,<3.0.0', 'playwright>=1.24.1,<2.0.0', 'psycopg2>=2.9.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'data-job-crawler',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Donor',
+    'author_email': 'donorfelita@msn.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
