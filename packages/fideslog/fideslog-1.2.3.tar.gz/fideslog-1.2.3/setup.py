e5dbe0c3b8d965d25c1f68a5d87b24dee8d87128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package fideslog/1.2.3/fideslog-1.2.3.tar.gz

---
+++ packages/fideslog/fideslog-1.2.3.tar.gz/fideslog-1.2.3/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+
+from setuptools import setup
+
+import versioneer
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = open("./fideslog/sdk/python/README.md").read()
+
+# Requirements
+install_requires = (
+    open("fideslog/sdk/python/requirements.txt").read().strip().split("\n")
+)
+dev_requires = open("dev-requirements.txt").read().strip().split("\n")
+
+setup(
+    name="fideslog",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="SDK for interacting with fideslog",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ethyca/fideslog",
+    python_requires=">=3.8, <4",
+    package_dir={"fideslog": "fideslog"},
+    packages=["fideslog.sdk.python"],
+    author="Ethyca, Inc.",
+    author_email="fidesteam@ethyca.com",
+    license="Apache License 2.0",
+    install_requires=install_requires,
+    dev_requires=dev_requires,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
