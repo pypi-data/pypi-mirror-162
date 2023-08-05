a75466e5d7ecd265c7a894135785fe36ddc7c2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package ensure-sops/0.1.1/ensure_sops-0.1.1.tar.gz

---
+++ packages/ensure-sops/ensure_sops-0.1.1.tar.gz/ensure_sops-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ensure_sops']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.20.0,<0.21.0', 'ruamel.yaml>=0.17.21,<0.18.0']
+
+entry_points = \
+{'console_scripts': ['ensure_sops = ensure_sops.main:main']}
+
+setup_kwargs = {
+    'name': 'ensure-sops',
+    'version': '0.1.1',
+    'description': 'Check if your files are encrypted with Mozilla SOPS or not. Can act as pre-commit hook as well.',
+    'long_description': None,
+    'author': 'Engineering Team at Tacto Technology GmbH.',
+    'author_email': 'engineering@tacto.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<3.11',
+}
+
+
+setup(**setup_kwargs)
