0754b0a92d6c21536504c7b33c3b4dd66c61da13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package linkding-cli/2022.8.0/linkding_cli-2022.8.0-py3-none-any.whl

---
+++ packages/linkding-cli/linkding_cli-2022.8.0-py3-none-any.whl/linkding_cli/const.py
@@ -0,0 +1,14 @@
+"""Define package constants."""
+import logging
+
+LOGGER = logging.getLogger(__package__)
+
+CONF_LIMIT = "limit"
+CONF_OFFSET = "offset"
+CONF_TOKEN = "token"
+CONF_URL = "url"
+CONF_VERBOSE = "verbose"
+
+ENV_CONFIG = "LINKDING_CONFIG"
+ENV_TOKEN = "LINKDING_TOKEN"
+ENV_URL = "LINKDING_URL"
