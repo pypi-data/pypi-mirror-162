0a5937ccdbba1db815ae8ae47b1e17c11c567b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:44 +0000

    Add package eks-deploy/0.0.3/eks_deploy-0.0.3-py3-none-any.whl

---
+++ packages/eks-deploy/eks_deploy-0.0.3-py3-none-any.whl/eks_deploy/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py for tst1
+
+if __name__ == '__main__':
+    #print('tst1.__init__ run as main')
+
+else:
+    #print('tst1.__init__ run as init')
+    
+    
---
+++ packages/eks-deploy/eks_deploy-0.0.3-py3-none-any.whl/eks_deploy/eks_imgop/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py for pack2
+
+if __name__ == '__main__':
+    print('pack2.__init__ run as main')
+else:
+    print('pack2.__init__ run as init')
+    
