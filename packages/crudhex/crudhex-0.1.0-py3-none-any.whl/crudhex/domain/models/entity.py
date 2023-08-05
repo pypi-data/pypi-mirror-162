7d6ad397776a8bba98facacf9ff37989400ab832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:04 +0000

    Add package crudhex/0.1.0/crudhex-0.1.0-py3-none-any.whl

---
+++ packages/crudhex/crudhex-0.1.0-py3-none-any.whl/crudhex/domain/models/entity.py
@@ -0,0 +1,28 @@
+from .field import Field
+from typing import List, Optional
+
+
+class Entity:
+    name: str
+    meta: Optional['Entity.Meta'] = None
+    fields: List[Field]
+
+    def __init__(self, name: str):
+        self.name = name
+        self.fields = []
+
+    def has_meta(self):
+        return self.meta is not None
+
+    class Meta:
+        table_name: str
+
+        def __init__(self, table_name: str):
+            self.table_name = table_name
+
+        def to_dict(self):
+            return self.__dict__
+
+        @classmethod
+        def default(cls, entity_name: str) -> 'Entity.Meta':
+            return cls(entity_name.lower())
