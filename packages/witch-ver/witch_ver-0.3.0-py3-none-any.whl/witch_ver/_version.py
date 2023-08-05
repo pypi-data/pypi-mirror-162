cd90b4760755206dcb601be83724d36de2d6dd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:01 +0000

    Add package witch-ver/0.3.0/witch_ver-0.3.0-py3-none-any.whl

---
+++ packages/witch-ver/witch_ver-0.3.0-py3-none-any.whl/witch_ver/_version.py
@@ -0,0 +1,15 @@
+"""Static module version information
+"""
+
+version_dict = {
+    "tag": "v0.3.0",
+    "tag_prefix": "v",
+    "sha": "c00b9b9e961a1c3c957991e4527c01454efab2ea",
+    "sha_abbrev": "c00b9b9",
+    "branch": None,
+    "date": "2022-07-29T14:38:37-07:00",
+    "dirty": False,
+    "distance": 0,
+    "pretty_str": "0.3.0",
+    "git_dir": None
+}
