ebbf7ba6bb8ee0f26ed6fc9438dbac052c4659e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:22 +0000

    Add package nawah/2.0.0b30/nawah-2.0.0b30-py3-none-any.whl

---
+++ packages/nawah/nawah-2.0.0b30-py3-none-any.whl/nawah/types/__init__.py
@@ -0,0 +1,25 @@
+"""Provides types used in Nawah"""
+
+from ._types import (AnalyticsEvents, AppPackage, IPQuota, NawahConn,
+                     NawahConnQuota, NawahDoc, NawahEvents, NawahQueryOperAnd,
+                     NawahQueryOperOr, NawahQuerySpecial,
+                     NawahQuerySpecialGroup, NawahQueryStep, NawahSession,
+                     Results, ResultsArgs)
+
+__all__ = [
+    "AnalyticsEvents",
+    "AppPackage",
+    "IPQuota",
+    "NawahConn",
+    "NawahConnQuota",
+    "NawahDoc",
+    "NawahEvents",
+    "NawahQueryOperAnd",
+    "NawahQueryOperOr",
+    "NawahQuerySpecial",
+    "NawahQuerySpecialGroup",
+    "NawahQueryStep",
+    "NawahSession",
+    "Results",
+    "ResultsArgs",
+]
