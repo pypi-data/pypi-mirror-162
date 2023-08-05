b8e82838db561c19ee14403980c671734ad4ce7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:56 +0000

    Add package PyMacApp/3.2.3/PyMacApp-3.2.3.tar.gz

---
+++ packages/PyMacApp/PyMacApp-3.2.3.tar.gz/PyMacApp-3.2.3/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup
+import sys
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+__version__ = "3.2.3"
+
+setup(
+    name="PyMacApp",
+    version=__version__,
+    description="Basic Tools to Build, Package, Sign, and Notarize Python Apps for MacOS",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp",
+    author="The Nicholas R. Barrow Company, LLC",
+    author_email="me@nicholasrbarrow.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["pymacapp", "pymacapp.runtools", "pymacapp.buildtools", "pymacapp.buildtools.app", "pymacapp.buildtools.package"],
+    package_data={'pymacapp': ['entitlements.plist']},
+    include_package_data=True,
+    install_requires=["PyInstaller", "urirouter"],
+    project_urls={
+        'Source': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp',
+        'Tracker': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp/issues',
+    }
+)
