6d07762f6aa2a76bcb3b304e5158664ebb726aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:08 +0000

    Add package phenotastic/0.1.5/phenotastic-0.1.5-py2.py3-none-any.whl

---
+++ packages/phenotastic/phenotastic-0.1.5-py2.py3-none-any.whl/phenotastic/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Phenotastic."""
+import warnings
+
+import os
+import sys
+import vtk
+import numpy as np
+
+__author__ = """Henrik Ahl"""
+__email__ = 'hpa22@cam.ac.uk'
+__version__ = '0.1.5'
