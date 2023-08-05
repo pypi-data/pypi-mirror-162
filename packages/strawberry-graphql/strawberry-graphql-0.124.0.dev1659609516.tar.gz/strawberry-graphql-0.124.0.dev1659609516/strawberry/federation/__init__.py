65cd89fa73dd18e89b396e1a6b48a46c7cc16239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package strawberry-graphql/0.124.0.dev1659609516/strawberry-graphql-0.124.0.dev1659609516.tar.gz

---
+++ packages/strawberry-graphql/strawberry-graphql-0.124.0.dev1659609516.tar.gz/strawberry-graphql-0.124.0.dev1659609516/strawberry/federation/__init__.py
@@ -0,0 +1,12 @@
+from .field import field
+from .object_type import input, interface, type
+from .schema import Schema
+
+
+__all__ = [
+    "field",
+    "input",
+    "interface",
+    "type",
+    "Schema",
+]
