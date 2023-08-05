09391d2afbbc18d3e8b18f2fecef0f9c63174abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:33 +0000

    Add package django-salesforce/4.1/django-salesforce-4.1.tar.gz

---
+++ packages/django-salesforce/django-salesforce-4.1.tar.gz/django-salesforce-4.1/tests/mypy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+.tox/typing/bin/mypy --strict salesforce/*.py
+RET=$?
+MORE_TESTS=tests/inspectdb
+for x in tests/test_* $MORE_TESTS; do
+    if test -e $x/test.sh; then
+        if test -e $x/mypy.ini; then
+            CONFIG=$x/mypy.ini
+        else
+            CONFIG=mypy.ini
+        fi
+	echo "== .tox/typing/bin/mypy --config-file=$CONFIG $x $@ =="
+        .tox/typing/bin/mypy --config-file=$CONFIG $x $@ || RET=$?
+    fi
+done
+test $RET -eq 0
