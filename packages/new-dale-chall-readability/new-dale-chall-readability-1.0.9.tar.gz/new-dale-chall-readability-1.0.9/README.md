35017093f32af56181b2fc7cb619fea69c88bd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:32 +0000

    Add package new-dale-chall-readability/1.0.9/new-dale-chall-readability-1.0.9.tar.gz

---
+++ packages/new-dale-chall-readability/new-dale-chall-readability-1.0.9.tar.gz/new-dale-chall-readability-1.0.9/README.md
@@ -0,0 +1,37 @@
+[![Tests and type-checks](https://github.com/public-law/new-dale-chall-readability/actions/workflows/python-app.yml/badge.svg)](https://github.com/public-law/new-dale-chall-readability/actions/workflows/python-app.yml) [![Maintainability](https://api.codeclimate.com/v1/badges/ef1198fa2d9246aa3c7d/maintainability)](https://codeclimate.com/github/public-law/new-dale-chall-readability/maintainability)
+
+
+# The new Dale-Chall readability formula
+
+In a nutshell:
+
+```bash
+pip install new-dale-chall-readability
+```
+
+```python
+In [1]: from new_dale_chall_readability import cloze_score, reading_level
+
+In [2]: text = (
+   ...:     'Latin for "friend of the court." It is advice formally offered '
+   ...:     'to the court in a brief filed by an entity interested in, but not '
+   ...:     'a party to, the case.'
+   ...:     )
+
+In [3]: cloze_score(text)
+Out[3]: 42.46652
+
+In [4]: reading_level(text)
+Out[4]: '5-6'
+```
+
+
+This implementation closely follows the specification, directly from
+the book's text (Chall & Dale, 1995). The test cases are also directly from the
+book.
+
+
+## References
+
+Chall, J., & Dale, E. (1995). _Readability revisited: The new Dale-Chall readability formula_.
+Brookline Books.
---
+++ packages/new-dale-chall-readability/new-dale-chall-readability-1.0.9.tar.gz/new-dale-chall-readability-1.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['new_dale_chall_readability']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4']
+
+setup_kwargs = {
+    'name': 'new-dale-chall-readability',
+    'version': '1.0.9',
+    'description': 'An implementation of the New Dale-Chall readability formula which closely follows the specification.',
+    'long_description': '[![Tests and type-checks](https://github.com/public-law/new-dale-chall-readability/actions/workflows/python-app.yml/badge.svg)](https://github.com/public-law/new-dale-chall-readability/actions/workflows/python-app.yml) [![Maintainability](https://api.codeclimate.com/v1/badges/ef1198fa2d9246aa3c7d/maintainability)](https://codeclimate.com/github/public-law/new-dale-chall-readability/maintainability)\n\n\n# The new Dale-Chall readability formula\n\nIn a nutshell:\n\n```bash\npip install new-dale-chall-readability\n```\n\n```python\nIn [1]: from new_dale_chall_readability import cloze_score, reading_level\n\nIn [2]: text = (\n   ...:     \'Latin for "friend of the court." It is advice formally offered \'\n   ...:     \'to the court in a brief filed by an entity interested in, but not \'\n   ...:     \'a party to, the case.\'\n   ...:     )\n\nIn [3]: cloze_score(text)\nOut[3]: 42.46652\n\nIn [4]: reading_level(text)\nOut[4]: \'5-6\'\n```\n\n\nThis implementation closely follows the specification, directly from\nthe book\'s text (Chall & Dale, 1995). The test cases are also directly from the\nbook.\n\n\n## References\n\nChall, J., & Dale, E. (1995). _Readability revisited: The new Dale-Chall readability formula_.\nBrookline Books.\n',
+    'author': 'Robb Shecter',
+    'author_email': 'robb@public.law',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/public-law/new-dale-chall-readability',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
