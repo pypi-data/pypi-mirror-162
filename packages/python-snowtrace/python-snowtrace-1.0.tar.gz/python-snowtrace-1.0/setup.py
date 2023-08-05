dc765c8f5e0f8f76a12d67b9449ebf6fbb6d13ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:55 +0000

    Add package python-snowtrace/1.0/python-snowtrace-1.0.tar.gz

---
+++ packages/python-snowtrace/python-snowtrace-1.0.tar.gz/python-snowtrace-1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="python-snowtrace",
+    version="1.0",
+    description="A minimal, yet complete, python API for snowtrace.io.",
+    url="https://github.com/EmperorMew/python-snowtrace",
+    author="EmperorMew",
+    author_email="info@ethmint.com",
+    license="MIT",
+    packages=[
+        "etherscan",
+        "etherscan.configs",
+        "etherscan.enums",
+        "etherscan.modules",
+        "etherscan.utils",
+    ],
+    install_requires=["requests"],
+    include_package_data=True,
+    zip_safe=False,
+)
