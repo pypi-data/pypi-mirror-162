24828446076ea42e2fe910e23f5bcec56c136418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package touchboard/0.1.3/touchboard-0.1.3.tar.gz

---
+++ packages/touchboard/touchboard-0.1.3.tar.gz/touchboard-0.1.3/README.md
@@ -0,0 +1,12 @@
+# Installation:
+
+Run ```pip install touchboard ==0.1.0```. Replace the version (0.1.0) with your corresponding version by operating system (0.1.0 for windows, 0.1.3 for linux, or 0.1.2 for macos)
+
+#### Note: This has ONLY been tested on windows.
+# Code:
+
+```py
+import touchboard
+
+touchboard.begin('SERIAL PORT HERE')
+```
---
+++ packages/touchboard/touchboard-0.1.3.tar.gz/touchboard-0.1.3/touchboard/__init__.py
@@ -0,0 +1,50 @@
+__version__ = '0.1.3'
+
+import serial
+from pynput.keyboard import Key, Controller
+import pyautogui as pag
+
+pag.FAILSAFE = False
+
+keyboard = Controller()
+def begin(port):
+  ser = serial.Serial(port, 115200)
+  
+  print(ser.name)         # check which port was really used
+  
+  
+  while True:
+  	line = ser.readline().decode('UTF-8')
+  	if 'Lst' in line:
+  		drag = 1
+  		continue
+  	elif 'Rst' in line:
+  		drag = 2
+  		continue
+  	elif 'Stop' in line:
+  		drag = 0
+  		continue
+  
+  	if 'left' in line:
+  		pag.click(clicks = int(line.split()[1]))
+  	elif 'right' in line:
+  		pag.click(button = 'right', clicks = int(line.split()[1]))
+  	elif drag == 1:
+  		pos = line.split()
+  		pos[0] = int(pos[0])
+  		pos[1] = int(pos[1])
+  		pag.drag(pos[0], pos[1], 0, button = 'left')
+  	elif drag == 2:
+  		pos = line.split()
+  		pos[0] = int(pos[0])
+  		pos[1] = int(pos[1])
+  		pag.drag(pos[0], pos[1], 0, button = 'left')
+  	else:
+  		pos = line.split()
+  		pos[0] = int(pos[0])
+  		pos[1] = int(pos[1])
+  		pag.moveRel(pos[0], pos[1], 0)
+  		continue
+  
+
+ser.close()
