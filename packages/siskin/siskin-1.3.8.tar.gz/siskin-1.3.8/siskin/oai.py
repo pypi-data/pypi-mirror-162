5fe2164ab549a84952d7e0fed41c66d69ad237bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:29 +0000

    Add package siskin/1.3.8/siskin-1.3.8.tar.gz

---
+++ packages/siskin/siskin-1.3.8.tar.gz/siskin-1.3.8/siskin/oai.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+# pylint: disable=C0301,E1101,C0103
+
+# Copyright 2021 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+from __future__ import print_function
+
+import json
+import logging
+import sys
+import tempfile
+import time
+
+import requests
+
+import xmltodict
+
+logger = logging.getLogger("siskin")
+
+
+def pqdt_harvest(
+    sleep=0,
+    user_agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+    endpoint="https://pqdtoai.proquest.com/OAIHandler",
+):
+    """
+    Harvest PQDT and return path to tempfile with raw response XML.
+
+    Background: Both, metha and sickle report bad resumption tokens.
+
+    Oh, what an ugly endpoint; the interface meant to be machine readable
+    yields a HTTP 500 when requested with a machine user agent. One may even
+    see the conversation that led to this feature.
+
+    Also: user agent header parsing seems case sensitive; recommended
+    reading: https://datatracker.ietf.org/doc/html/rfc2616#section-4.2.
+
+    Also: a data endpoint that needs session management, nice.
+
+    TODO: We need to build a single XML file.
+
+    <Records xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    </Records>
+    """
+    link = "{}?metadataPrefix=oai_dc&verb=ListRecords".format(endpoint)
+    cookies = None
+    prev_cursor, batch_size = None, 100  # assumed, updated while we observe the cursor
+
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as tf:
+        while True:
+            logger.debug(link)
+            resp = requests.get(
+                link, cookies=cookies, headers={"User-Agent": user_agent}
+            )
+            if resp.status_code >= 400:
+                raise RuntimeError(
+                    "harvest failed: {} {}".format(link, resp.status_code)
+                )
+            logger.debug(
+                "retrieved {} {}".format(len(resp.text), resp.text[:50], "...")
+            )
+            dd = xmltodict.parse(resp.text)
+            tf.write(resp.text)
+            tf.write("\n")
+            try:
+                tokenTag = dd["OAI-PMH"]["ListRecords"]["resumptionToken"]
+                if tokenTag is None:
+                    break
+                cursor = int(tokenTag["@cursor"])
+                size = int(tokenTag["@completeListSize"])
+                token = tokenTag["#text"]
+            except KeyError as exc:
+                logger.debug(
+                    json.dumps(
+                        {
+                            "text": resp.text,
+                            "status": resp.status_code,
+                        }
+                    )
+                )
+                raise RuntimeError(
+                    "failed to fetch pqdt, unexpected text: {}".format(resp.text)
+                )
+            else:
+                if not token or int(cursor) >= int(size):
+                    break
+            if resp.cookies:
+                cookies = resp.cookies
+            if prev_cursor is not None:
+                batch_size = cursor - prev_cursor
+            if cursor + batch_size >= size:
+                logger.info("harvest done")
+            else:
+                link = r"{}?resumptionToken={}&verb=ListRecords".format(endpoint, token)
+                logger.debug("{} {} {}".format(cursor, size, token))
+                time.sleep(sleep)
+                prev_cursor = cursor
+
+    return tf.name
+
+
+if __name__ == "__main__":
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(name)s][%(levelname)-8s] %(message)s"
+    )
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
+
+    filename = pqdt_harvest()
+    print(filename, file=sys.stderr)
---
+++ packages/siskin/siskin-1.3.8.tar.gz/siskin-1.3.8/siskin/sources/olc.py
@@ -0,0 +1,161 @@
+# coding: utf-8
+# pylint: disable=C0301,E1101
+
+# Copyright 2019 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+"""
+OLC, refs #16279.
+
+A list of (51) collection names and internal identifiers can be found here:
+https://is.gd/8DFvOo (GBV).
+"""
+
+import datetime
+import glob
+import json
+import os
+
+import luigi
+from gluish.format import Zstd
+from gluish.intervals import monthly
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+from siskin.conversions import olc_to_intermediate_schema
+from siskin.task import DefaultTask
+from siskin.utils import sha1obj
+
+
+class OLCTask(DefaultTask):
+    """
+    OLC base task.
+    """
+
+    TAG = "68"
+
+    # cf. https://is.gd/8DFvOo
+    COLLECTIONS = [
+        "SSG-OLC-ANG",
+        "SSG-OLC-ARC",
+        "SSG-OLC-BUB",
+        "SSG-OLC-FTH",
+        "SSG-OLC-GER",
+        "SSG-OLC-GWK",
+        "SSG-OLC-KPH",
+        "SSG-OLC-MKW",
+        "SSG-OLC-MUS",
+        "SSG-OLC-PHI",
+    ]
+
+    def closest(self):
+        return monthly(date=self.date)
+
+
+class OLCDump(OLCTask):
+    """
+    Fetch a slice (or all of a SOLR), refs #16279.
+
+    Currently, 9/51 collections are requested, but that might change over time.
+    """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def run(self):
+        query = " OR ".join(
+            ["collection_details:{}".format(c) for c in self.COLLECTIONS]
+        )
+        output = shellout(
+            """
+            solrdump -verbose -server {server} -q '{query}' |
+            jq -rc . | zstd -c -T0 > {output}
+            """,
+            query=query,
+            server=self.config.get("olc", "solr"),
+        )
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        filename = "{}-{}.ndj.zst".format(self.date, sha1obj(self.COLLECTIONS))
+        return luigi.LocalTarget(path=self.path(filename=filename), format=Zstd)
+
+
+class OLCIntermediateSchema(OLCTask):
+    """
+    Sample convertion.
+    """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return OLCDump(date=self.date)
+
+    def run(self):
+        with self.input().open() as file:
+            with self.output().open("w") as output:
+                for i, line in enumerate(file):
+                    if i % 100000 == 0:
+                        self.logger.debug("@{}".format(i))
+                    doc = json.loads(line)
+                    result = olc_to_intermediate_schema(doc)
+                    output.write(bytes(json.dumps(result), encoding="utf-8"))
+                    output.write(b"\n")
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="ndj.zst"), format=Zstd)
+
+
+class OLCIntermediateSchemaDeprecated(OLCTask):
+    """
+    Run and collect OLC, 68, standalone script. There is licensing information
+    added in the standalone script and later during AILicening, too. That
+    should be ok.
+    """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def run(self):
+        """
+        Find the most recent output, compress and move into the "siskin" tree.
+        """
+        shellout(
+            "{script} --overwrite --outputformat json",
+            script=self.assets(
+                "68/68-fincjson",
+                ignoremap={1: "most probably ok for skip, due to existing file"},
+            ),
+        )
+
+        # Find the output file.
+        taskdir = os.path.join(self.BASE, self.TAG)
+        outputs = sorted(
+            glob.glob(os.path.join(taskdir, "68-output-*json")), reverse=True
+        )
+        if len(outputs) == 0:
+            raise RuntimeError(
+                "could not find any artifacts for source at {}".format(taskdir)
+            )
+        path = outputs[0]
+
+        # Compress as AIIntermediateSchema requires all artifacts to be gzip compressed.
+        output = shellout("zstd -T0 -c {input} > {output}", input=path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="ndj.zst"))
