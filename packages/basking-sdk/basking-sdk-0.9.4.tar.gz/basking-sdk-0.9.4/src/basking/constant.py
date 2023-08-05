8b4da429fd15eb699d0080605b2fb3055656ca63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package basking-sdk/0.9.4/basking-sdk-0.9.4.tar.gz

---
+++ packages/basking-sdk/basking-sdk-0.9.4.tar.gz/basking-sdk-0.9.4/src/basking/constant.py
@@ -0,0 +1,5 @@
+"""constant variables"""
+
+BASKING_API_URL_PROD = 'https://api.basking.io/api/'
+BASKING_API_URL_LOCAL = 'http://127.0.0.1:3535'
+TIMEOUT_MESSAGE = 'The Basking API returned a timeout. Please try this request again in a few minutes. If the problem persists, please reach out to support under support@basking.io'
