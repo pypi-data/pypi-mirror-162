6f61841f54b0c2c6db3cad6f271d40eec95c88c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package nonebot-plugin-autohelp/0.1.5/nonebot_plugin_autohelp-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-autohelp/nonebot_plugin_autohelp-0.1.5-py3-none-any.whl/nonebot_plugin_autohelp/__init__.py
@@ -0,0 +1,9 @@
+"""Autogenerate response to commands /help !help help ²Ëµ¥ caidan /i.
+
+For more info on usage:
+/help -h
+"""
+__version__ = "0.1.5"
+from .nonebot_plugin_autohelp import nonebot_plugin_autohelp
+
+__all__ = ("nonebot_plugin_autohelp",)
