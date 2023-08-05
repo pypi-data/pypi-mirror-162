6fa3fa5c24908b1aab40558006c76f0af17ca262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:02 +0000

    Add package muvisdk/0.0.7/muvisdk-0.0.7.tar.gz

---
+++ packages/muvisdk/muvisdk-0.0.7.tar.gz/muvisdk-0.0.7/src/muvisdk/mercadopago_sdk/MercadoPagoSDK.py
@@ -0,0 +1,28 @@
+import mercadopago
+from .Customer import Customer
+from .Card import Card
+from .CardToken import CardToken
+from .Payment import Payment
+
+
+class MercadoPagoSDK:
+    def __init__(self, merchant):
+        if 'credentials' in merchant:
+            access_token = merchant['credentials']['mercadopago']['access_token']
+        else:
+            access_token = merchant['keys']['access_token']
+        self.sdk = mercadopago.SDK(access_token)
+        self.merchant_id = merchant['_id']
+        self.merchant_name = merchant['name']
+
+    def customer(self):
+        return Customer(self.sdk)
+    
+    def card(self):
+        return Card(self.sdk)
+
+    def card_token(self):
+        return CardToken(self.sdk)
+    
+    def payment(self):
+        return Payment(self.sdk)
