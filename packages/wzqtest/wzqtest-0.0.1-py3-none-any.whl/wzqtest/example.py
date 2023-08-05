a5c5c4b153e23c3a3d3d61b8514c2554bccb64e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package wzqtest/0.0.1/wzqtest-0.0.1-py3-none-any.whl

---
+++ packages/wzqtest/wzqtest-0.0.1-py3-none-any.whl/wzqtest/example.py
@@ -0,0 +1,12 @@
+# example.py
+class Example()
+
+    def __init__(self, name):
+        print(f'hello {name}!')
+
+def main():
+    example = Example('world')
+
+if __name__ == '__main__':
+    main()
+
