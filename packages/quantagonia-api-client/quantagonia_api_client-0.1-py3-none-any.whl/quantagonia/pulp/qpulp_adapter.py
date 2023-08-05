0b26591f7c57116ce5dbde1c45cf3125f8fc4e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package quantagonia-api-client/0.1/quantagonia_api_client-0.1-py3-none-any.whl

---
+++ packages/quantagonia-api-client/quantagonia_api_client-0.1-py3-none-any.whl/quantagonia/pulp/qpulp_adapter.py
@@ -0,0 +1,15 @@
+from .hybrid_solver_api import QQVM_CMD
+
+from quantagonia.runner import Runner
+from quantagonia.runner_factory import HybridSolverConnectionType, RunnerFactory
+from quantagonia.enums import HybridSolverServers
+
+class QPuLPAdapter:
+
+  @classmethod
+  def getSolver(cls, connection : HybridSolverConnectionType, api_key : str = None, server : HybridSolverServers = HybridSolverServers.PROD):
+    runner : Runner = RunnerFactory.getRunner(connection, api_key, server)
+    solver = QQVM_CMD(runner=runner)
+
+    return solver
+
