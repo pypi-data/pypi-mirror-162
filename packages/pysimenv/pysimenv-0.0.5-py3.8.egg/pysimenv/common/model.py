571ff96a6a0e1d7f0c98b4934a6619eac31b4417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:56 +0000

    Add package pysimenv/0.0.5/pysimenv-0.0.5-py3.8.egg

---
+++ packages/pysimenv/pysimenv-0.0.5-py3.8.egg/pysimenv/common/model.py
@@ -0,0 +1,126 @@
+import numpy as np
+from typing import Union, List, Tuple, Optional
+from pysimenv.core.base import SimObject, StaticObject, ArrayType
+from pysimenv.core.system import DynObject, MultipleSystem, DynSystem
+
+
+class SignalGenerator(StaticObject):
+    def __init__(self, shaping_fun, interval: Union[int, float] = -1):
+        super(SignalGenerator, self).__init__(interval=interval)
+        self.shaping_fun = shaping_fun
+
+    # implementation
+    def _forward(self):
+        return self.shaping_fun(self._sim_clock.time)
+
+
+class FeedbackControl(MultipleSystem):
+    def __init__(self, system: DynObject, control: StaticObject):
+        super(FeedbackControl, self).__init__()
+        self.system = system
+        self.control = control
+
+        self.attach_sim_objects([system, control])
+
+    # implement
+    def _forward(self, r: Optional[np.ndarray] = None):
+        y = self.system.output
+        if r is None:
+            u_fb = self.control.forward(y)
+        else:
+            u_fb = self.control.forward(y, r)
+        self.system.forward(u=u_fb)
+
+
+class Sequential(MultipleSystem):
+    def __init__(self, obj_list: Union[List[SimObject], Tuple[SimObject]]):
+        super(Sequential, self).__init__()
+        assert len(obj_list) > 0, "(sequential) Invalid obj_list!"
+        self.obj_list = list(obj_list)
+        self.first_obj = obj_list[0]
+        self.other_obj_list = obj_list[1:]
+
+        self.attach_sim_objects(obj_list)
+
+    # implement
+    def _forward(self, **kwargs):
+        self.first_obj.forward(**kwargs)
+        out = self.first_obj.output
+        for obj in self.other_obj_list:
+            if out is None:
+                obj.forward()
+                out = obj.output
+            elif isinstance(out, dict):
+                obj.forward(**out)
+                out = obj.output
+            else:
+                obj.forward(u=out)
+                out = obj.output
+        return out
+
+
+class FlatEarthEnv(object):
+    grav_accel = 9.805
+
+
+class LinSys(DynSystem):
+    def __init__(self, x_0: ArrayType, A: np.ndarray, B: Optional[np.ndarray] = None, C: Optional[np.ndarray] = None):
+        super(LinSys, self).__init__(initial_states={'x': x_0})
+        self.A = A
+        self.B = B
+        if C is None:
+            C = np.eye(self.state_dim, dtype=np.float32)
+        self.C = C
+
+    # implement
+    def _deriv(self, x: np.ndarray, u: Union[None, np.ndarray] = None):
+        if self.B is None or u is None:
+            x_dot = self.A.dot(x)
+        else:
+            x_dot = self.A.dot(x) + self.B.dot(u)
+
+        return {'x': x_dot}
+
+    # implement
+    def _output(self) -> np.ndarray:
+        return self.C.dot(self.state['x'])
+
+
+class FirstOrderLinSys(LinSys):
+    def __init__(self, x_0: ArrayType, tau: float):
+        A = np.array([[-1./tau]])
+        B = np.array([[1./tau]])
+
+        super(FirstOrderLinSys, self).__init__(x_0, A, B)
+        self.tau = tau
+
+    # implement
+    def _output(self) -> Union[None, tuple, np.ndarray]:
+        return self.state['x'][0]
+
+
+class SecondOrderLinSys(LinSys):
+    def __init__(self, x_0: ArrayType, zeta: float, omega: float):
+        A = np.array([
+            [0., 1.],
+            [-omega**2, -2*zeta*omega]
+        ], dtype=np.float32)
+        B = np.array([[0.], [omega**2]], dtype=np.float32)
+
+        super(SecondOrderLinSys, self).__init__(x_0, A, B)
+        self.zeta = zeta
+        self.omega = omega
+
+    # implement
+    def _output(self) -> Union[None, tuple, np.ndarray]:
+        return self.state['x'][0]
+
+
+class Integrator(DynSystem):
+    def __init__(self, initial_state: Union[ArrayType]):
+        def deriv_fun(x: np.ndarray, u: Union[float, np.ndarray]):
+            if isinstance(u, float):
+                u = np.array([u])
+            return {'x': u}
+
+        super(Integrator, self).__init__({'x': initial_state}, deriv_fun)
---
+++ packages/pysimenv/pysimenv-0.0.5-py3.8.egg/pysimenv/missile/engagement.py
@@ -0,0 +1,222 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Tuple
+from pysimenv.core.system import MultipleSystem
+from pysimenv.missile.model import PlanarMissile2dof, PlanarManVehicle2dof
+from pysimenv.missile.guidance import PurePNG2dim, IACBPNG
+from pysimenv.missile.util import RelKin2dim, CloseDistCond, miss_distance, closest_instant, lin_interp
+
+
+class Engagement2dim(MultipleSystem):
+    def __init__(self, missile: PlanarMissile2dof, target: PlanarManVehicle2dof):
+        super(Engagement2dim, self).__init__()
+        self.missile = missile
+        self.target = target
+        self.rel_kin = RelKin2dim()
+        self.close_dist_cond = CloseDistCond(r_threshold=10.0)
+
+        self.attach_sim_objects([self.missile, self.target])
+
+    # override
+    def reset(self):
+        super(Engagement2dim, self).reset()
+        self.close_dist_cond.reset()
+
+    # implement
+    def initialize(self):
+        x_M = self.missile.state['x']
+        x_T = self.target.state['x']
+        self.rel_kin.evaluate(x_M, x_T)
+
+    # implement
+    def forward(self):
+        x_M = self.missile.state['x']
+        x_T = self.target.state['x']
+        self.rel_kin.evaluate(x_M, x_T)
+        self.close_dist_cond.evaluate(self.rel_kin.r)
+        self.target.forward()
+
+    # implement
+    def check_stop_condition(self) -> Tuple[bool, int]:
+        to_stop = False
+
+        missile_stop, _ = self.missile.check_stop_condition()
+        if self.intercepted():  # probable interception
+            to_stop = True
+            self.flag = 1
+
+        if missile_stop:  # stop due to the missile
+            to_stop = True
+            self.flag = 2
+
+        if self.is_out_of_view():  # out of field-of-view limit
+            to_stop = True
+            self.flag = 3
+
+        return to_stop, self.flag
+
+    def intercepted(self) -> bool:
+        return self.close_dist_cond.check()
+
+    def is_out_of_view(self) -> bool:
+        sigma = self.rel_kin.sigma
+        return self.missile.is_out_of_view(sigma)
+
+    def miss_distance(self) -> float:
+        x_M = self.missile.history('x')
+        x_T = self.target.history('x')
+
+        p_M = x_M[:, 0:2]
+        p_T = x_T[:, 0:2]
+        d_miss = miss_distance(p_M, p_T)
+        return d_miss
+
+    def state_on_closest_instant(self):
+        x_M = self.missile.history('x')
+        x_T = self.target.history('x')
+
+        p_M = x_M[:, 0:2]
+        p_T = x_T[:, 0:2]
+        index_close, xi_close = closest_instant(p_M, p_T)
+        x_M_close = lin_interp(x_M[index_close], x_M[index_close + 1], xi_close)
+        x_T_close = lin_interp(x_T[index_close], x_T[index_close + 1], xi_close)
+        return x_M_close, x_T_close
+
+    def report(self):
+        self.missile.report()
+        if self.flag == 1:
+            print("[engagement] The target has been intercepted!")
+        else:
+            print("[engagement] The target has been missed!")
+
+        d_miss = self.miss_distance()
+        x_M_close, x_T_close = self.state_on_closest_instant()
+
+        print("[engagement] Miss distance: {:.6f} (m)".format(d_miss))
+        print("[engagement] Missile state on the closest instant: {:.2f}(m), {:.2f}(m), {:.2f}(m/s), {:.2f}(deg) \n".
+              format(x_M_close[0], x_M_close[1], x_M_close[2], np.rad2deg(x_M_close[3]))
+              )
+        print("[engagement] Target state on the closest instant: {:.2f}(m), {:.2f}(m), {:.2f}(m/s), {:.2f}(deg) \n".
+              format(x_T_close[0], x_T_close[1], x_T_close[2], np.rad2deg(x_T_close[3]))
+              )
+
+    def plot(self):
+        fig_axs = dict()
+
+        fig_ax = self.missile.plot_path()
+        self.target.plot_path(fig_ax)
+
+        fig1, ax1 = fig_ax['fig'], fig_ax['ax']
+        fig1.suptitle("2-dim flight path")
+        fig_axs['path'] = {'fig': fig1, 'ax': ax1}
+
+        # plots for rel. kin.
+        time_list = self.missile.history('t')
+        x_M_list = self.missile.history('x')
+        x_T_list = self.target.history('x')
+
+        relKin = RelKin2dim()
+        r_list = []
+        sigma_list = []
+        lam_list = []
+        omega_list = []
+
+        zem_list = []
+
+        for i in range(x_M_list.shape[0]):
+            x_M = x_M_list[i, :]
+            x_T = x_T_list[i, :]
+
+            relKin.evaluate(x_M, x_T)
+
+            r_list.append(relKin.r)
+            sigma_list.append(relKin.sigma)
+            lam_list.append(relKin.lam)
+            omega_list.append(relKin.omega)
+
+            zem_list.append(relKin.zem)
+
+        r_list = np.array(r_list)
+        sigma_list = np.array(sigma_list)
+        lam_list = np.array(lam_list)
+        omega_list = np.array(omega_list)
+
+        zem_list = np.array(zem_list)
+
+        fig2, ax2 = plt.subplots(2, 2)
+        fig2.tight_layout()
+        ax2_1 = ax2[0, 0]
+        ax2_1.set_title("Rel. dist")
+        ax2_1.plot(time_list[:-1], r_list[:-1], label="Rel. dist")
+        ax2_1.set_xlabel("Time (s)")
+        ax2_1.set_ylabel("Rel. dist (m)")
+        ax2_1.grid()
+        ax2_1.legend()
+
+        ax2_2 = ax2[0, 1]
+        ax2_2.set_title("Look angle")
+        ax2_2.plot(time_list[:-1], np.rad2deg(sigma_list[:-1]), label="look angle")
+        ax2_2.set_xlabel("Time (s)")
+        ax2_2.set_ylabel("Look angle (deg)")
+        ax2_2.grid()
+        ax2_2.legend()
+
+        ax2_3 = ax2[1, 0]
+        ax2_3.set_title("LOS angle")
+        ax2_3.plot(time_list[:-1], np.rad2deg(lam_list[:-1]), label="LOS angle")
+        ax2_3.set_xlabel("Time (s)")
+        ax2_3.set_ylabel("LOS angle (deg)")
+        ax2_3.grid()
+        ax2_3.legend()
+
+        ax2_4 = ax2[1, 1]
+        ax2_4.set_title("LOS rate")
+        ax2_4.plot(time_list[:-1], np.rad2deg(omega_list[:-1]), label="LOS rate")
+        ax2_4.set_xlabel("Time (s)")
+        ax2_4.set_ylabel("LOS rate (deg/s)")
+        ax2_4.grid()
+        ax2_4.legend()
+
+        fig_axs['Rel. Kin.'] = {'fig': fig2, 'ax': ax2}
+
+        fig3, ax3 = plt.subplots()
+        ax3.set_title("ZEM")
+        ax3.plot(time_list[:-1], zem_list[:-1], label="ZEM")
+        ax3.set_xlabel("Time (s)")
+        ax3.set_ylabel("ZEM (m)")
+        ax3.grid()
+        ax3.legend()
+
+        fig_axs['Rel. Kin. add.'] = {'fig': fig3, 'ax': ax3}
+
+        return fig_axs
+
+
+class PurePNG2dimEngagement(Engagement2dim):
+    def __init__(self, missile: PlanarMissile2dof, target: PlanarManVehicle2dof):
+        super(PurePNG2dimEngagement, self).__init__(missile, target)
+
+        self.pure_png = PurePNG2dim(N=3.0)
+        self.attach_sim_objects([self.pure_png])
+
+    # implement
+    def forward(self):
+        super(PurePNG2dimEngagement, self).forward()
+        V_M = self.missile.V
+        omega = self.rel_kin.omega
+
+        a_M = self.pure_png.forward(V_M, omega)
+        self.missile.forward(a_M=np.array([0, a_M]))
+
+
+class IACBPNGEngagement(Engagement2dim):
+    def __init__(self, missile: PlanarMissile2dof, target: PlanarManVehicle2dof, bpng: IACBPNG):
+        super(IACBPNGEngagement, self).__init__(missile, target)
+        self.bpng = bpng
+        self.attach_sim_objects([self.bpng])
+
+    def forward(self):
+        super(IACBPNGEngagement, self).forward()
+        a_y_cmd = self.bpng.forward(self.missile, self.target, self.rel_kin)
+        self.missile.forward(a_M=np.array([0., a_y_cmd]))
+
