59be91626b811a27ce678f67dbb0fef4f498b20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package statisticspy/0.1/statisticspy-0.1.tar.gz

---
+++ packages/statisticspy/statisticspy-0.1.tar.gz/statisticspy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statisticspy',
+      version='0.1',
+      description='Gaussian and Binominal distributions',
+      packages=['statisticspy'],
+      author = 'Albert Guo',
+      author_email = 'sub.jl@icloud.com',
+      zip_safe=False)
