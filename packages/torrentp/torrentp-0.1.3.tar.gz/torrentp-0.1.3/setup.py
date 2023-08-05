e1acc44aab6e0f6ac3f5adae2c8f656514ea1f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:58 +0000

    Add package torrentp/0.1.3/torrentp-0.1.3.tar.gz

---
+++ packages/torrentp/torrentp-0.1.3.tar.gz/torrentp-0.1.3/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+import codecs
+import os.path
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='torrentp',
+    version=get_version("torrentp/__init__.py"),
+    description='Download from torrent with magnet link or .torrent file',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/iw4p/torrentp',
+    author='Nima Akbarzadeh',
+    author_email='iw4p@protonmail.com',
+    license='BSD 2-clause',
+    packages=['torrentp'],
+    install_requires=['libtorrent>=2.0.7',],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
