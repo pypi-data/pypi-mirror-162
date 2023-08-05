a54a026341fac074173b90752dd5bbcb072e5494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package hydromt/0.5.0/hydromt-0.5.0-py3-none-any.whl

---
+++ packages/hydromt/hydromt-0.5.0-py3-none-any.whl/hydromt/models/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""HydroMT models API"""
+import sys
+from .model_api import Model
+from . import model_plugins
+
+
+# dictionary with entry points (not yet loaded!)
+ENTRYPOINTS = model_plugins.discover()
+PLUGINS = {ep.object_name: name for name, ep in ENTRYPOINTS.items()}
+
+# only load when requested
+def __getattr__(name):
+    thismodule = sys.modules[__name__]
+
+    # load a register all models
+    if name == "MODELS":
+        MODELS = {
+            ep.name: model_plugins.load(ep, thismodule) for ep in ENTRYPOINTS.values()
+        }
+        setattr(thismodule, name, MODELS)
+        return MODELS
+
+    # trick to allow import of plugin model class from hydromt core
+    # from hydromt.models import xxxxModel
+    elif name in PLUGINS:
+        model_class = model_plugins.load(ENTRYPOINTS[PLUGINS[name]])
+        return model_class
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
---
+++ packages/hydromt/hydromt-0.5.0-py3-none-any.whl/hydromt/models/model_plugins.py
@@ -0,0 +1,69 @@
+import itertools
+import entrypoints
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def discover(path=None, logger=logger):
+    """Discover hydromt models:
+    - Find 'hydromt.models' entrypoints provided by any Python packages in the
+      environment.
+
+    Parameters
+    ----------
+    path : str or None
+        Default is ``sys.path``.
+
+    Returns
+    -------
+    eps : dict
+        Entrypoints dict
+    """
+    # Discover drivers via entrypoints.
+    group = entrypoints.get_group_named("hydromt.models", path=path)
+    group_all = entrypoints.get_group_all("hydromt.models", path=path)
+    if len(group_all) != len(group):
+        # There are some name collisions. Let's go digging for them.
+        for name, matches in itertools.groupby(group_all, lambda ep: ep.name):
+            matches = list(matches)
+            if len(matches) != 1:
+                winner = group[name]
+                logger.debug(
+                    f"There are {len(matches)} 'hydromt.models' entrypoints for the name {name}: {matches}."
+                    f"The match {winner} is selected."
+                )
+
+    eps = {}
+    for name, ep in group.items():
+        logger.debug(
+            f"Discovered model plugin '{name} = {ep.module_name}.{ep.object_name}' ({ep.distro.version})"
+        )
+        eps[ep.name] = ep
+    return eps
+
+
+def load(ep, module=None, logger=logger):
+    """Load entrypoint and return plugin model class
+
+    Parameters
+    ----------
+    ep : entrypoint
+        discovered entrypoint
+
+    Returns
+    -------
+    model_class : Model
+        plugin model class
+    """
+    try:
+        # plugins[ep.name] = ep.load()
+        model_class = ep.load()
+        if module is not None:
+            setattr(module, model_class.__name__, model_class)
+        logger.debug(
+            f"Loaded model plugin '{ep.name} = {ep.module_name}.{ep.object_name}' ({ep.distro.version})"
+        )
+    except (ModuleNotFoundError, AttributeError) as err:
+        logger.exception(f"Error while loading entrypoint {ep.name}: {str(err)}")
+    return model_class
