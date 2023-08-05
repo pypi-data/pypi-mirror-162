61d8021a33a7fbd92ab25fe22b35e2e6ac9dad0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:58 +0000

    Add package pyfamsa/0.1.0/pyfamsa-0.1.0-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pyfamsa/pyfamsa-0.1.0-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/pyfamsa/__init__.py
@@ -0,0 +1,23 @@
+# noqa: D104
+from ._version import __version__  # isort: skip
+
+from . import _famsa
+from ._famsa import (
+    Aligner,
+    Alignment,
+    GappedSequence,
+    GuideTree,
+    Sequence,
+)
+
+__doc__ = _famsa.__doc__
+__all__ = [
+    "Aligner",
+    "Alignment",
+    "GappedSequence"
+    "GuideTree"
+    "Sequence",
+]
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
