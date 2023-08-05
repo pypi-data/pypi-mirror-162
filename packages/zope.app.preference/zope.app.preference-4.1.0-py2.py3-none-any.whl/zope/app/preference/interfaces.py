42f67f14165b57e77b1d383d2eb94897ff2258d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:56 +0000

    Add package zope.app.preference/4.1.0/zope.app.preference-4.1.0-py2.py3-none-any.whl

---
+++ packages/zope.app.preference/zope.app.preference-4.1.0-py2.py3-none-any.whl/zope/app/preference/interfaces.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""User Preferences Interfaces"""
+
+# BBB
+from zope.preference.interfaces import IDefaultPreferenceProvider
+from zope.preference.interfaces import IPreferenceCategory
+from zope.preference.interfaces import IPreferenceGroup
+from zope.preference.interfaces import IUserPreferences
