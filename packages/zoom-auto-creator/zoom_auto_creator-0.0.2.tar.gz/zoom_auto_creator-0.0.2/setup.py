2bae268a5390602b4c5099d697deb4c7a7a9b9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:31 +0000

    Add package zoom-auto-creator/0.0.2/zoom_auto_creator-0.0.2.tar.gz

---
+++ packages/zoom-auto-creator/zoom_auto_creator-0.0.2.tar.gz/zoom_auto_creator-0.0.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['zoom_auto_creator']
+install_requires = \
+['Pillow>=9.2.0,<10.0.0', 'pyautogui>=0.9.53,<0.10.0']
+
+setup_kwargs = {
+    'name': 'zoom-auto-creator',
+    'version': '0.0.2',
+    'description': 'Creating Zoom Rooms',
+    'long_description': None,
+    'author': 'Bohdan Salamakha',
+    'author_email': 'allen.avanheim@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
