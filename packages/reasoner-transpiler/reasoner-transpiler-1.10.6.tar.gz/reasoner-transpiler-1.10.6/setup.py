c1f47d0a80f69af2d90a3929535d4e0639b01843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package reasoner-transpiler/1.10.6/reasoner-transpiler-1.10.6.tar.gz

---
+++ packages/reasoner-transpiler/reasoner-transpiler-1.10.6.tar.gz/reasoner-transpiler-1.10.6/setup.py
@@ -0,0 +1,19 @@
+"""Setup reasoner-transpiler package."""
+from setuptools import setup
+
+setup(
+    name="reasoner-transpiler",
+    version="1.10.6",
+    author="Patrick Wang",
+    author_email="patrick@covar.com",
+    url="https://github.com/ranking-agent/reasoner-transpiler",
+    description="TRAPI → Cypher transpiler",
+    packages=["reasoner_transpiler"],
+    package_data={"reasoner_transpiler": ["attribute_types.json"]},
+    install_requires=[
+        "bmt-lite-v2.3.0>=2.2.1,<3.0",
+    ],
+    zip_safe=False,
+    license="MIT",
+    python_requires=">=3.6",
+)
