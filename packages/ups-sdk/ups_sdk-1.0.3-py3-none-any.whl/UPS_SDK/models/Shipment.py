5e6955f161df88bf7927d08973f0c3b598d58c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:47 +0000

    Add package ups-sdk/1.0.3/ups_sdk-1.0.3-py3-none-any.whl

---
+++ packages/ups-sdk/ups_sdk-1.0.3-py3-none-any.whl/UPS_SDK/models/Shipment.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from UPS_SDK.models.Package import Package
+from UPS_SDK.models.ShipTo import ShipTo
+from UPS_SDK.models.Shipper import Shipper
+from UPS_SDK.models.ReferenceNumberItem import ReferenceNumberItem
+from UPS_SDK.models.Service import Service
+from UPS_SDK.models.ShipmentWeight import ShipmentWeight
+
+class Shipment(BaseModel):
+    Shipper: Shipper
+    ShipTo: ShipTo
+    ShipmentWeight: ShipmentWeight
+    Service: Service
+    ReferenceNumbers: List[ReferenceNumberItem] = Field(None, alias="ReferenceNumber")
+    ShipmentIdentificationNumber: str
+    PickupDate: str
+    ScheduledDeliveryDate: Optional[str]
+    Package: Package
