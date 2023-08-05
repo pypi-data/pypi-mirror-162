9df3dff5b5d526a014d7ba1263822c8f6f96fac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package mage-ai/0.2.3/mage_ai-0.2.3-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.2.3-py3-none-any.whl/mage_ai/data_preparation/models/constants.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+PIPELINES_FOLDER = 'pipelines'
+PIPELINE_CONFIG_FILE = 'metadata.yaml'
+
+
+DATAFRAME_ANALYSIS_KEYS = frozenset(
+    [
+        'metadata',
+        'statistics',
+        'insights',
+        'suggestions',
+    ]
+)
+DATAFRAME_ANALYSIS_MAX_ROWS = 100_000
+DATAFRAME_SAMPLE_COUNT_PREVIEW = 10
+DATAFRAME_SAMPLE_COUNT = 1000
+VARIABLE_DIR = '.variables'
+
+
+class BlockStatus(str, Enum):
+    EXECUTED = 'executed'
+    FAILED = 'failed'
+    NOT_EXECUTED = 'not_executed'
+    UPDATED = 'updated'
+
+
+class BlockType(str, Enum):
+    CHART = 'chart'
+    DATA_EXPORTER = 'data_exporter'
+    DATA_LOADER = 'data_loader'
+    SCRATCHPAD = 'scratchpad'
+    TRANSFORMER = 'transformer'
+
+
+class PipelineType(str, Enum):
+    PYTHON = 'python'
+    PYSPARK = 'pyspark'
+
+
+CUSTOM_EXECUTION_BLOCK_TYPES = [
+    BlockType.CHART,
+    BlockType.DATA_EXPORTER,
+    BlockType.DATA_LOADER,
+    BlockType.TRANSFORMER,
+]
+
+
+NON_PIPELINE_EXECUTABLE_BLOCK_TYPES = [
+    BlockType.CHART,
+    BlockType.SCRATCHPAD,
+]
