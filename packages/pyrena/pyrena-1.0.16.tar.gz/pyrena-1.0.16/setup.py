321b9926c909454b5762c3bd3ca1d53063252bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:46 +0000

    Add package pyrena/1.0.16/pyrena-1.0.16.tar.gz

---
+++ packages/pyrena/pyrena-1.0.16.tar.gz/pyrena-1.0.16/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name="pyrena",
+	version="1.0.16",
+	description="Python wrapper for Arena QMS API.",
+	url="https://github.com/thecodeforge/pyrena",
+	py_modules=["pyrena"],
+	install_requires=[
+		"mistletoe",
+		"requests"
+	],
+	packages=setuptools.find_packages(),
+	package_dir={
+		"pyrena":"pyrena"
+	}
+)
