5a77d2b359d16097975b1041c400598437e4885a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:55 +0000

    Add package calcSimpleWow/0.0.1/calcSimpleWow-0.0.1.tar.gz

---
+++ packages/calcSimpleWow/calcSimpleWow-0.0.1.tar.gz/calcSimpleWow-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='calcSimpleWow',
+    version='0.0.1',
+    description='Idk',
+    Long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    url='',
+    author='Sixten Bohman Herman Agustus hus hus',
+    author_email='sixten.bohman.08@gmail.com',
+    License=None,
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['']
+)
