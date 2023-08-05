270df1ea1709f6d70431205c1eb551c314fe7c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package ih-acryl-datahub/0.0.1/ih_acryl_datahub-0.0.1-py3-none-any.whl

---
+++ packages/ih-acryl-datahub/ih_acryl_datahub-0.0.1-py3-none-any.whl/datahub/__init__.py
@@ -0,0 +1,8 @@
+raise RuntimeError(
+    """
+    This package exists to prevent accidental installs of malicious packages
+    that are intended to be private to Included Health.
+
+    If you've installed this by accident, reach out to the Data Infrastructure team.
+    """
+)
