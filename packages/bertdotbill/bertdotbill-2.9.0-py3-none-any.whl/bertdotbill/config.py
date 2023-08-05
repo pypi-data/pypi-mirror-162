9db0e91eded2e00b2ca4a3fd3b487f57b5f912fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:46 +0000

    Add package bertdotbill/2.9.0/bertdotbill-2.9.0-py3-none-any.whl

---
+++ packages/bertdotbill/bertdotbill-2.9.0-py3-none-any.whl/bertdotbill/config.py
@@ -0,0 +1,37 @@
+from bertdotbill.defaults import default_settings
+from bertdotbill.defaults import default_config_file_name
+from bertdotbill.logger import Logger
+import os
+from bertdotconfig import Config
+
+logger = Logger().init_logger(__name__)
+
+class AppConfig():
+
+  def __init__(self, **kwargs):
+    pass
+
+  def initialize(self, **kwargs):
+
+    logger.info("Initializing config")
+    args = kwargs.get('args', {})
+    verify_tls = kwargs.get('verify_tls')
+    # Initialize App Config
+    initial_data = {
+    'environment': os.environ
+    }  
+
+    config_file_uri = args.get('config_file') or default_config_file_name
+    logger.info(f"Config file URI is {config_file_uri}")
+    # Initialize App Config
+    config = Config(
+        config_file_uri=config_file_uri,
+        default_value=default_settings,
+        initial_data=initial_data,
+        args=args,
+        verify_tls=verify_tls
+    )
+
+    settings = config.read()
+
+    return settings
---
+++ packages/bertdotbill/bertdotbill-2.9.0-py3-none-any.whl/bertdotbill/defaults.py
@@ -0,0 +1,47 @@
+import os
+
+# SSL Requests
+default_verify_tls = True
+
+# Websocket Settings
+default_webterminal_listen_port = 10001
+default_webterminal_listen_host  = '0.0.0.0'
+default_websocket_address = f'ws://127.0.0.1:{default_webterminal_listen_port}'
+default_footer_websocket_address = default_websocket_address
+default_rightpane_websocket_address = default_websocket_address
+default_webterminal_shell = '/bin/bash'
+default_webterminal_shell_command = '-l'
+default_webterminal_env = {
+  "SHELL": 'bash',
+  "TERM": 'xterm',
+  "WEBTERMINAL": 'True',
+  "TESTING": 'True',
+}
+
+# Flask app settings
+app_name = "Bert's Interactive Lesson Loader (BILL)"
+gui_dirname = 'bill.gui'
+default_app_port = 10000
+default_open_browser_delay = 1.25
+default_config_file_name = 'bill.config.yaml'
+default_app_host_address = '0.0.0.0'
+default_settings = {
+  "terminals": {
+    "default": {
+      "address": default_websocket_address
+    },
+    "footer": {
+      "address": default_footer_websocket_address
+    },
+    "rightpane": {
+      "address": default_rightpane_websocket_address
+    }
+  },
+  "external_configs": [
+    {
+      "name": "someconfig",
+      "uri": "https://raw.githubusercontent.com/someuser/somerepo/main/bill.config.yaml"
+    }
+  ]
+}
+
