548cadac938e5ca59e9bc1a17ae5cea1d06ad96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:31 +0000

    Add package dsnd-probability-fara/1.2/dsnd_probability_fara-1.2.tar.gz

---
+++ packages/dsnd-probability-fara/dsnd_probability_fara-1.2.tar.gz/dsnd_probability_fara-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_fara',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_fara'],
+      author = 'faranak kh',
+      author_email = 'faranak.kheirandish@gmail.com',
+      zip_safe=False)
