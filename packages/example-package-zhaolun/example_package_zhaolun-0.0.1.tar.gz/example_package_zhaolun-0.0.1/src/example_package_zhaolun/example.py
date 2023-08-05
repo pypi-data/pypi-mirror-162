71e8bfdef44910ba8fd942f17f9f1b1771f5da93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:07 +0000

    Add package example-package-zhaolun/0.0.1/example_package_zhaolun-0.0.1.tar.gz

---
+++ packages/example-package-zhaolun/example_package_zhaolun-0.0.1.tar.gz/example_package_zhaolun-0.0.1/src/example_package_zhaolun/example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/5 2:09 PM
+# @Author  : zhaolun@deepwise.com
+# @Site    : http://192.168.10.111:3000/MIG/DW_Research_Platform
+# @File    : example.py
+# @Software: PyCharm
+
+
+def add_one(number):
+    return number + 1
