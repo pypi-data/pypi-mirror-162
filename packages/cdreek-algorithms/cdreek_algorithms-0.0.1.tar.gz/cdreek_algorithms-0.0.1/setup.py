4f43bfd1458dc98309d55bcb3307235faf161364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package cdreek-algorithms/0.0.1/cdreek_algorithms-0.0.1.tar.gz

---
+++ packages/cdreek-algorithms/cdreek_algorithms-0.0.1.tar.gz/cdreek_algorithms-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cdreek_algorithms',
+    version='0.0.1',
+    author='cnspary',
+    author_email='cnspary@outlook.com',
+    description='MC-PackageOne',
+    packages=find_packages(),
+    python_requires='>=3.0'
+)
