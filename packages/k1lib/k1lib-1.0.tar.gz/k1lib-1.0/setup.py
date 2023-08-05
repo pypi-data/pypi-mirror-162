5b4182010f829c6ec0c0ce04a59be52075164a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:17 +0000

    Add package k1lib/1.0/k1lib-1.0.tar.gz

---
+++ packages/k1lib/k1lib-1.0.tar.gz/k1lib-1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+try:
+    with open("readme.md") as f:
+        md = f.read()
+except:
+    md = ""
+
+deps = ["graphviz", "torchvision", "pillow",
+        "scikit-image", "pyperclip", "forbiddenfruit"]
+
+setup(
+    name="k1lib",
+    packages=["k1lib", "k1lib._hidden",
+              "k1lib.cli",
+              "k1lib.callbacks", "k1lib.callbacks.profilers",
+              "k1lib.callbacks.lossFunctions",
+              "k1lib._mo"],
+    version="1.0",
+    python_requires='>=3.7',
+    install_requires=["torch", "numpy>=1.14", "matplotlib>=2.0", "dill"],
+    extras_require={"extras": [
+        *deps, "k1a>=1.0.1,<2"], "extras_windows": deps},
+    description="Some nice ML overhaul",
+    url="https://github.com/157239n/k1lib",
+    author="Quang Ho",
+    author_email="157239q@gmail.com",
+    long_description=md,
+    long_description_content_type='text/markdown',
+    license="MIT",
+)
