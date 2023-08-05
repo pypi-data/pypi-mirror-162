df1e49fc516be697327e12c7fda11d2a49e81133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:47 +0000

    Add package data-engineering-job-market/0.1.4/data_engineering_job_market-0.1.4-py3-none-any.whl

---
+++ packages/data-engineering-job-market/data_engineering_job_market-0.1.4-py3-none-any.whl/data_engineering_job_market_package_FelitaD/elt/transform_and_load.py
@@ -0,0 +1,9 @@
+from data_engineering_job_market_package_FelitaD.elt.transform.transform import transform
+from data_engineering_job_market_package_FelitaD.elt.load.load import Loader
+
+
+def transform_and_load():
+    pivotted_jobs = transform()
+
+    loader = Loader(pivotted_jobs)
+    loader.load()
