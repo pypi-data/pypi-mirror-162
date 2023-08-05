ce08eb8f2715e8a9c1d1ddf02c03e5f4e3c83eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package sqlglot/4.2.8/sqlglot-4.2.8-py3-none-any.whl

---
+++ packages/sqlglot/sqlglot-4.2.8-py3-none-any.whl/sqlglot/dialects/oracle.py
@@ -0,0 +1,22 @@
+from sqlglot import exp
+from sqlglot.dialects.dialect import Dialect
+from sqlglot.generator import Generator
+from sqlglot.tokens import Tokenizer, TokenType
+
+
+class Oracle(Dialect):
+    class Generator(Generator):
+        TYPE_MAPPING = {
+            exp.DataType.Type.TINYINT: "NUMBER",
+            exp.DataType.Type.SMALLINT: "NUMBER",
+            exp.DataType.Type.INT: "NUMBER",
+            exp.DataType.Type.BIGINT: "NUMBER",
+            exp.DataType.Type.DECIMAL: "NUMBER",
+            exp.DataType.Type.VARCHAR: "VARCHAR2",
+        }
+
+    class Tokenizer(Tokenizer):
+        KEYWORDS = {
+            **Tokenizer.KEYWORDS,
+            "TOP": TokenType.TOP,
+        }
