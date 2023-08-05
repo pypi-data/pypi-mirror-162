05d077a729ace4902f25f0d0396e056934f3d82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:39 +0000

    Add package columnarframe/0.6.2/columnarframe-0.6.2.tar.gz

---
+++ packages/columnarframe/columnarframe-0.6.2.tar.gz/columnarframe-0.6.2/columnarframe/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.6.2'
+
+from .frame import ColumnarFrame  # noqa
+from .read_csv import read_csv  # noqa
+from .read_excel import read_excel  # noqa
+from .concat import concat  # noqa
