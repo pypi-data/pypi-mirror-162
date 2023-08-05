93ec5e9b80bc2884616b62b897a5716fe3f6a3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package peaqevcore/4.0.3/peaqevcore-4.0.3.tar.gz

---
+++ packages/peaqevcore/peaqevcore-4.0.3.tar.gz/peaqevcore-4.0.3/peaqevcore/services/locale/locale_model.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from ..locale.free_charge import FreeChargePattern
+from .querytypes.const import (
+HOURLY,
+QUARTER_HOURLY
+)
+from .querytypes.querytypes import(
+LocaleQuery
+)
+
+
+@dataclass(frozen=True)
+class Locale_Type:
+    observed_peak: str
+    charged_peak: str
+    query_model: LocaleQuery
+    free_charge_pattern: FreeChargePattern = None
+    peak_cycle: str = HOURLY
+    converted: bool = False #transition key to remove sql-dependency
+
+    def free_charge(self, mockdt: datetime=datetime.min) -> bool:
+        if self.free_charge_pattern is None:
+            return False
+
+        now = datetime.now() if mockdt is datetime.min else mockdt
+        return self.free_charge_pattern.free_charge(now)
+
+    def is_quarterly(self) -> bool:
+        return self.peak_cycle == QUARTER_HOURLY
+
+
