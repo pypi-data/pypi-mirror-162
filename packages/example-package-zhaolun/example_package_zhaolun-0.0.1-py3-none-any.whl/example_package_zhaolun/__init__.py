55cdbbc5981a30c54073af5dd942bc2d2684be5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:07 +0000

    Add package example-package-zhaolun/0.0.1/example_package_zhaolun-0.0.1-py3-none-any.whl

---
+++ packages/example-package-zhaolun/example_package_zhaolun-0.0.1-py3-none-any.whl/example_package_zhaolun/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/5 2:08 PM
+# @Author  : zhaolun@deepwise.com
+# @Site    : http://192.168.10.111:3000/MIG/DW_Research_Platform
+# @File    : __init__.py
+# @Software: PyCharm
