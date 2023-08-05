7d9aaa7d0b8e342c981c4637100e6694cad79891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:05 +0000

    Add package CRPS/2.0.4/CRPS-2.0.4.tar.gz

---
+++ packages/CRPS/CRPS-2.0.4.tar.gz/CRPS-2.0.4/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '2.0.4'
+PACKAGE_NAME = 'CRPS'
+AUTHOR = 'Naveen Goutham'
+AUTHOR_EMAIL = 'naveen.goutham@outlook.com'
+URL = 'https://github.com/gouthamnaveen/CRPS'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'A package to compute the Continuous Ranked Probability Score (CRPS), the Fair-CRPS, and the Adjusted-CRPS. Read the documentation at https://github.com/gouthamnaveen/CRPS'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy']
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      keywords=['crps','continuous ranked probability score','fair crps','adjusted crps','proper score','probability score','ensemble forecast','python'],
+      packages=find_packages()
+      )
