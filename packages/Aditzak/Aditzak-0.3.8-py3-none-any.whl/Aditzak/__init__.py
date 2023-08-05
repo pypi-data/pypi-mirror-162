4ae4032e96c7dcd1112ab52d0d738b37be31227a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package Aditzak/0.3.8/Aditzak-0.3.8-py3-none-any.whl

---
+++ packages/Aditzak/Aditzak-0.3.8-py3-none-any.whl/Aditzak/__init__.py
@@ -0,0 +1,5 @@
+import builder
+import analyzer
+
+__version__ = "0.3.8"
+__author__ = [{"Henry Cook":"henryscookolaizola@gmail.com"}, {"Alejandro Sempere":"asempere@colegioaleman.net"}, "Pablo Pérez"]
