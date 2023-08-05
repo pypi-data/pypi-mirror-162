c6880c518cd36b7bb65aae867636e19752db8a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package authomize-rest-api-client/1.3.3/authomize-rest-api-client-1.3.3.tar.gz

---
+++ packages/authomize-rest-api-client/authomize-rest-api-client-1.3.3.tar.gz/authomize-rest-api-client-1.3.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_namespace_packages, setup
+
+if __name__ == '__main__':
+    setup(
+        version='1.3.3',
+        name='authomize-rest-api-client',
+        author='Authomize inc.',
+        license='MIT',
+        author_email='info@authomize.com',
+        description='Authomize REST API Python Client',
+        packages=find_namespace_packages(include=['authomize.*']),
+        package_data={},
+        python_requires='>=3.10',
+        install_requires=[
+            'requests~=2.26',
+            'api-client-pydantic~=1.2',
+        ],
+        extras_require={
+            'test': [
+                'coverage~=5.2',
+                'flake8~=4.0',
+                'flake8-isort~=4.0',
+                'pyhamcrest~=2.0',
+                'pytest~=6.2',
+                'pytest-html~=2.1',
+            ],
+            'codegen': [
+                'datamodel-code-generator~=0.11',
+            ],
+        },
+    )
