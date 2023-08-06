51f74fd50f0a96a0d4782deb27893a033ffe820e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:51 +0000

    Add package ytdl-nfo/0.2.3/ytdl_nfo-0.2.3-py3-none-any.whl

---
+++ packages/ytdl-nfo/ytdl_nfo-0.2.3-py3-none-any.whl/ytdl_nfo/Ytdl_nfo.py
@@ -0,0 +1,42 @@
+import os
+import json
+from .nfo import get_config
+
+
+class Ytdl_nfo:
+    def __init__(self, file_path, extractor=None):
+        self.path = file_path
+        self.dir = os.path.dirname(file_path)
+
+        # Read json data
+        with open(self.path, "rt", encoding="utf-8") as f:
+            self.data = json.load(f)
+
+        self.extractor = extractor
+        if extractor is None:
+            self.extractor = self.data['extractor'].lower()
+
+        if file_path.endswith(".info.json"):
+            self.filename = file_path[:-10]
+        else:
+            self.filename = os.path.splitext(self.data['_filename'])[0]
+
+        self.nfo = get_config(self.extractor)
+
+    def process(self):
+        if self.data['_type'] != "video":
+            print(f'{self.filename} is not a video metadata file, Skipping')
+            return False
+
+        self.nfo.generate(self.data)
+        self.write_nfo()
+        return True
+
+    def write_nfo(self):
+        self.nfo.write_nfo(f'{self.filename}.nfo')
+
+    def print_data(self):
+        print(json.dumps(self.data, indent=4, sort_keys=True))
+
+    def get_nfo(self):
+        return self.nfo.get_nfo()
