ee727ce521620f7272f125d2188141c35b336eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:09 +0000

    Add package pass-export/0.1.0/pass-export-0.1.0.tar.gz

---
+++ packages/pass-export/pass-export-0.1.0.tar.gz/pass-export-0.1.0/src/pass_export/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from .main import Exporter, Importer, main_export, main_import, __doc__, __version__
+
+__all__ = ('Exporter', 'Importer', 'main_export', 'main_import', '__doc__', '__version__')
