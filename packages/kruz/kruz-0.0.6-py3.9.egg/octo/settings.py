ad705ea945722fc23659b7377d8c191a90fd8003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package kruz/0.0.6/kruz-0.0.6-py3.9.egg

---
+++ packages/kruz/kruz-0.0.6-py3.9.egg/octo/settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+DEFAULTS = {}
+DEFAULTS['debug'] = False
+DEFAULTS['src_ip'] = '0.0.0.0'
+DEFAULTS['src_port'] = 19302
+DEFAULTS['stun_ip'] = 'stun.l.google.com'
+DEFAULTS['stun_port'] = 19302
+
+CHUNK_SIZE = 4096
