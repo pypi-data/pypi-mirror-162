59ce308d5b934f532e73fbfa3bdef7b4d70e43d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:15 +0000

    Add package mybox/0.0.23/mybox-0.0.23.tar.gz

---
+++ packages/mybox/mybox-0.0.23.tar.gz/mybox-0.0.23/mybox/package/archive.py
@@ -0,0 +1,108 @@
+import tempfile
+from abc import ABCMeta, abstractmethod
+from pathlib import Path
+from typing import Optional, Union
+
+from ..fs import LOCAL, find_executable, is_executable, make_executable, makedirs
+from ..utils import *
+from .manual import ManualPackage
+
+TAR = find_executable("gtar", "tar")
+
+
+class ArchivePackage(ManualPackage, metaclass=ABCMeta):
+    def __init__(
+        self,
+        *,
+        raw: Union[bool, str] = False,
+        raw_executable: bool = False,
+        strip: int = 0,
+        **kwargs,
+    ) -> None:
+        self.raw = raw
+        self.raw_executable = raw_executable
+        self.strip = strip
+        super().__init__(**kwargs)
+
+    @abstractmethod
+    def archive_url(self) -> str:
+        pass
+
+    def package_directory(self) -> Path:
+        result = LOCAL / f"{self.name.replace('/', '--')}.app"
+        makedirs(result)
+        return result
+
+    def untar(self, source: str, *extra: str) -> None:
+        run(
+            TAR,
+            "-x",
+            "--strip",
+            str(self.strip),
+            "-C",
+            str(self.package_directory()),
+            *extra,
+            "-f",
+            source,
+        )
+
+    def extract(self, url: str, source: str) -> None:
+        if self.raw:
+            if isinstance(self.raw, str):
+                filename = self.raw
+            else:
+                filename = url.rsplit("/", 1)[-1]
+            target = self.package_directory() / filename
+            run("cp", str(source), str(target))
+            if self.raw_executable:
+                make_executable(target)
+        elif ".tar" in url:
+            if ".gz" in url:
+                self.untar(source, "-z")
+            elif ".bz2" in url:
+                self.untar(source, "-j")
+            else:
+                self.untar(source)
+        elif ".tgz" in url:
+            self.untar(source, "-z")
+        elif ".txz" in url:
+            self.untar(source, "-J")
+        else:
+            raise ValueError(f"Unknown archive format: {url}")
+
+    def binary_path(self, binary: str) -> Path:
+        paths: list[list[str]] = [[], ["bin"]]
+        for relative_path in paths:
+            candidate = self.package_directory() / Path(*relative_path) / binary
+            if candidate.is_file() and is_executable(candidate):
+                return candidate
+        raise ValueError(f"Cannot find {binary} in {self.package_directory()}.")
+
+    def app_path(self, name: str) -> Path:
+        candidate = (
+            self.package_directory() / "share" / "applications" / f"{name}.desktop"
+        )
+        if candidate.is_file():
+            return candidate
+        raise ValueError(
+            f"Cannot find application '{name}' in {self.package_directory()}."
+        )
+
+    def icon_directory(self) -> Optional[Path]:
+        candidate = self.package_directory() / "share" / "icons"
+        if candidate.is_dir():
+            return candidate
+        return None
+
+    def font_path(self, name: str) -> Path:
+        candidate = self.package_directory() / name
+        if candidate.is_file():
+            return candidate
+        raise ValueError(f"Cannot find font '{name}' in {self.package_directory()}.")
+
+    def install(self):
+        url = self.archive_url()
+        with tempfile.NamedTemporaryFile() as archive_file:
+            run("curl", "-sSL", url, stdout=archive_file)
+            self.extract(url, archive_file.name)
+        super().install()
