3575a6c2e6be0f6299db7934126744e8a924cf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:55 +0000

    Add package discord-py-activity/1.3/discord-py-activity-1.3.tar.gz

---
+++ packages/discord-py-activity/discord-py-activity-1.3.tar.gz/discord-py-activity-1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()[3:]
+
+setup(
+    name="discord-py-activity",
+    version="1.3",
+    license="MIT",
+    author="SpaceX1919",
+    author_email="edgardbu@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="A simple library that gives you access to discord activities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lol123123f/Discord-activities",
+    keywords="Discord activity",
+    install_requires=["discord.py", "requests"],
+)
