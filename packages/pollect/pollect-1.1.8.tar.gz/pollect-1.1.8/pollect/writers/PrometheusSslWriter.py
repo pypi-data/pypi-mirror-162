66b672221b14a7e4cbd3e6bd705539b9d5036e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package pollect/1.1.8/pollect-1.1.8.tar.gz

---
+++ packages/pollect/pollect-1.1.8.tar.gz/pollect-1.1.8/pollect/writers/PrometheusSslWriter.py
@@ -0,0 +1,47 @@
+import logging
+from urllib.parse import parse_qs
+
+from gevent import pywsgi
+from prometheus_client import exposition
+from prometheus_client import registry
+
+from pollect.writers.PrometheusWriter import PrometheusWriter
+
+
+class PrometheusSslWriter(PrometheusWriter):
+    def __init__(self, config):
+        super().__init__(config)
+        self._server = None
+        self._key = config['key']
+        self._cert = config['cert']
+
+        # See if the files exist
+        with open(self._key):
+            pass
+        with open(self._cert):
+            pass
+
+    def start(self):
+        def serve(env, start_response):
+            params = parse_qs(env['QUERY_STRING'])
+            accept_header = env['HTTP_ACCEPT']
+
+            reg = registry.REGISTRY
+            encoder, content_type = exposition.choose_encoder(accept_header)
+            if 'name[]' in params:
+                reg = reg.restricted_registry(params['name[]'])
+            output = encoder(reg)
+            headers = [('Content-Type', content_type)]
+
+            start_response('200 OK', headers)
+            return [output]
+
+        logger = logging.getLogger('prom')
+        logger.setLevel(logging.ERROR)
+        self._server = pywsgi.WSGIServer(('0.0.0.0', self._port), serve, keyfile=self._key, certfile=self._cert,
+                                         log=logger)
+        self._server.start()
+
+    def stop(self):
+        self._server.stop()
+
---
+++ packages/pollect/pollect-1.1.8.tar.gz/pollect-1.1.8/pollect/writers/PrometheusWriter.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from prometheus_client import start_http_server, Gauge, registry
+
+from pollect.core.ValueSet import ValueSet
+from pollect.writers.Writer import Writer
+
+
+class PrometheusWriter(Writer):
+    def __init__(self, config):
+        super().__init__(config)
+        self._port = self.config.get('port', 8080)
+        self._exporters = {}
+
+    def supports_partial_write(self) -> bool:
+        return True
+
+    def start(self):
+        start_http_server(self._port)
+
+    def stop(self):
+        pass
+
+    def write(self, data: List[ValueSet]):
+        for value_set in data:
+            for value_obj in value_set.values:
+                path = value_set.name
+                if value_obj.name is not None:
+                    path += '.' + value_obj.name
+
+                path = path.replace('-', '_').replace('.', '_').replace('!', '')
+
+                if path not in self._exporters:
+                    self._exporters[path] = Gauge(path, path, labelnames=value_set.labels)
+                gauge = self._exporters[path]
+                if len(value_set.labels) > 0:
+                    if len(value_obj.label_values) != len(value_set.labels):
+                        raise ValueError('Incorrect label count for ' + path + ': Got ' +
+                                         str(len(value_set.labels)) + ' labels and ' +
+                                         str(len(value_obj.label_values)) + ' label names')
+
+                    gauge.labels(*value_obj.label_values).set(value_obj.value)
+                    continue
+                gauge.set(value_obj.value)
