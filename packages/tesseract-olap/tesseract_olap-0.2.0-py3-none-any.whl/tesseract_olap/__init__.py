62421b242a7a7bafe0e21f6a8c3f4b76fbabb9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:00 +0000

    Add package tesseract-olap/0.2.0/tesseract_olap-0.2.0-py3-none-any.whl

---
+++ packages/tesseract-olap/tesseract_olap-0.2.0-py3-none-any.whl/tesseract_olap/__init__.py
@@ -0,0 +1,10 @@
+from .query import DataRequest, MembersRequest
+from .server import OlapServer
+
+__version__ = "0.1.0"
+
+__all__ = (
+    "DataRequest",
+    "MembersRequest",
+    "OlapServer",
+)
