307d761272b256ee00057cf4e2579df1f5ccbeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:13 +0000

    Add package cheminformatics/0.0.1/cheminformatics-0.0.1.tar.gz

---
+++ packages/cheminformatics/cheminformatics-0.0.1.tar.gz/cheminformatics-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cheminformatics',
+    version='0.0.1',
+    license='MIT',
+    author='Jacob Gerlach',
+    author_email='jwgerlach00@gmail.com',
+    url='https://github.com/jwgerlach00/cheminformatics',
+    description='Data science and machine learning tools for chemistry applications',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    install_requires=[],
+)
