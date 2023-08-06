c235a00f3f08d2ab191c6f20d7426b9798aca00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:51 +0000

    Add package gausino-probability/0.1/gausino_probability-0.1.tar.gz

---
+++ packages/gausino-probability/gausino_probability-0.1.tar.gz/gausino_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gausino_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gausino_probability'],
+      zip_safe=False)
