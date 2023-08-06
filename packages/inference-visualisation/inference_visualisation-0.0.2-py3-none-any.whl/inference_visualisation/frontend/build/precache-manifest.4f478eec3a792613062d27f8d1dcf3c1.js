4f95d092ef237a2f1481bf79dd2f3fd59884e097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:17 +0000

    Add package inference-visualisation/0.0.2/inference_visualisation-0.0.2-py3-none-any.whl

---
+++ packages/inference-visualisation/inference_visualisation-0.0.2-py3-none-any.whl/inference_visualisation/frontend/build/precache-manifest.4f478eec3a792613062d27f8d1dcf3c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b093d1101d6730eb0c7ecdaeda5c5064",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f51f2b75758cea50cc1",
+    "url": "./static/js/2.d2b4dac6.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.d2b4dac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b792afacd1b6d0b19aba",
+    "url": "./static/js/main.b48b1f99.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
