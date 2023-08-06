f0b59da3e7b6fad05c1e58c66eb397d40206ff92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:48 +0000

    Add package odoo12-addon-femprocomuns/12.0.1.2.2/odoo12-addon-femprocomuns-12.0.1.2.2.tar.gz

---
+++ packages/odoo12-addon-femprocomuns/odoo12-addon-femprocomuns-12.0.1.2.2.tar.gz/odoo12-addon-femprocomuns-12.0.1.2.2/odoo/addons/femprocomuns/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Odoo Femprocomuns Custom Module",
+    "version": "12.0.1.2.2",
+    "depends": ["easy_my_coop"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Discuss",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Odoo Femprocomuns Custom Module
+    """,
+    "data": [
+        "views/subscription_template.xml",
+        "views/subscription_request_view.xml",
+    ],
+    "installable": True,
+}
