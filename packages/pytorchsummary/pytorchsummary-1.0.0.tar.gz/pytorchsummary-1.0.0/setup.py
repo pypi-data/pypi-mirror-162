6f48f3d0cf5c53e826804ba709f2a633220f6d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:28 +0000

    Add package pytorchsummary/1.0.0/pytorchsummary-1.0.0.tar.gz

---
+++ packages/pytorchsummary/pytorchsummary-1.0.0.tar.gz/pytorchsummary-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "pytorchsummary",
+    version="1.0.0",
+    description="Summary of PyTorch Models just like `model.summary() in Keras",
+    url="https://github.com/GSAUC3/pytorch-model-details",
+    author="Rajarshi Banerjee",
+    author_email="raju.banerjee.720@gmail.com",
+    license="MIT",
+    keywords=['python','PyTorch','Pytorch model summary','Pytorch parameter summary'],
+    packages=[],
+)
