ffd7d4a919b4368b5e0f097d61361241f9cc17d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:26 +0000

    Add package cidc-schemas/0.25.50/cidc_schemas-0.25.50-py2.py3-none-any.whl

---
+++ packages/cidc-schemas/cidc_schemas-0.25.50-py2.py3-none-any.whl/cidc_schemas/prism/constants.py
@@ -0,0 +1,55 @@
+"""CIDC schemas-specific constants relevant to prismifying/merging functionality."""
+
+PROTOCOL_ID_FIELD_NAME = "protocol_identifier"
+
+SUPPORTED_ASSAYS = [
+    "atacseq_fastq",
+    "wes_fastq",
+    "wes_bam",
+    "olink",
+    "cytof",
+    "ihc",
+    "elisa",
+    "rna_fastq",
+    "rna_bam",
+    "mif",
+    "tcr_adaptive",
+    "tcr_fastq",
+    "hande",
+    "nanostring",
+    "clinical_data",
+    "misc_data",
+    "ctdna",
+    "microbiome",
+]
+
+SUPPORTED_SHIPPING_MANIFESTS = [
+    "pbmc",
+    "plasma",
+    "tissue_slide",
+    "normal_blood_dna",
+    "normal_tissue_dna",
+    "tumor_tissue_dna",
+    "tumor_tissue_rna",
+    "h_and_e",
+    "microbiome_dna",
+]
+# weird non shipping manifest
+SUPPORTED_WEIRD_MANIFESTS = ["tumor_normal_pairing", "participants_annotations"]
+MANIFESTS_WITH_PARTICIPANT_INFO = SUPPORTED_SHIPPING_MANIFESTS + [
+    "participants_annotations"
+]
+SUPPORTED_MANIFESTS = SUPPORTED_SHIPPING_MANIFESTS + SUPPORTED_WEIRD_MANIFESTS
+
+SUPPORTED_ANALYSES = [
+    "atacseq_analysis",
+    "ctdna_analysis",
+    "cytof_analysis",
+    "rna_level1_analysis",
+    "tcr_analysis",
+    "wes_analysis",
+    "wes_tumor_only_analysis",
+    "microbiome_analysis",
+]
+
+SUPPORTED_TEMPLATES = SUPPORTED_ASSAYS + SUPPORTED_MANIFESTS + SUPPORTED_ANALYSES
