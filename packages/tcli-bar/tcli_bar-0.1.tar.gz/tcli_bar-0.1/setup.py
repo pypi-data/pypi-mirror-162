ba81cb5f6606c40bf6af50f1a220fdb15e9925a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:49 +0000

    Add package tcli-bar/0.1/tcli_bar-0.1.tar.gz

---
+++ packages/tcli-bar/tcli_bar-0.1.tar.gz/tcli_bar-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='tcli_bar',
+    version='0.1',
+    description='Create a CLI progress bar for your python project\nDon’t forget to add a new line after you add the progress bar You can find out why ;)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="SiXFeet",
+    author_email='chukunekunwanwene@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=['tcli_bar'],
+    keywords='cli loading bar',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
