0226062fffa80660482b2204b2521b4c291193ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:31 +0000

    Add package abotserver/2022rc1/abotserver-2022rc1-py3-none-any.whl

---
+++ packages/abotserver/abotserver-2022rc1-py3-none-any.whl/abotserver/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from .routers import index, aliyun, hikvision, jsst
+
+
+app = FastAPI(title="ABOT API Docs", version="v1.0")
+
+app.include_router(index.router, tags=["index"])
+app.include_router(aliyun.router, tags=["aliyun"])
+app.include_router(hikvision.router, tags=["hikvision"])
+app.include_router(jsst.router, tags=["jsst"])
