34be8ea105fb3064e50998a23ada21b466966c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package pure-sql/0.0.1/pure_sql-0.0.1-py3-none-any.whl

---
+++ packages/pure-sql/pure_sql-0.0.1-py3-none-any.whl/raw_sql/raw_sql.py
@@ -0,0 +1,36 @@
+import logging
+from pathlib import Path
+from typing import Union
+
+from raw_sql.exceptions import SQLLoadException
+from raw_sql.query_loader import QueryLoader
+
+log = logging.getLogger(__name__)
+
+
+def from_str(sql: str):
+    """Load queries from a SQL string."""
+    query_loader = QueryLoader()
+    query_data = query_loader.load_query_data_from_sql(sql)
+    return query_data
+
+
+def from_path(sql_path: Union[str, Path]):
+    """Load queries from a `.sql` file, or directory of `.sql` files."""
+    path = Path(sql_path)
+
+    if not path.exists():
+        raise SQLLoadException(f"File does not exist: {path}")
+
+    query_loader = QueryLoader()
+
+    if path.is_file():
+        query_data = query_loader.load_query_data_from_file(path)
+        return query_data
+    elif path.is_dir():
+        query_data_tree = query_loader.load_query_data_from_dir_path(path)
+        return query_data_tree
+    else:  # pragma: no cover
+        raise SQLLoadException(
+            f"The sql_path must be a directory or file, got {sql_path}"
+        )
