578f04863c1b395733e8a60ac52384e8aca382a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:01 +0000

    Add package medetpy/1.1.0/medetpy-1.1.0-py3.7.egg

---
+++ packages/medetpy/medetpy-1.1.0-py3.7.egg/medetpy/__init__.py
@@ -0,0 +1,15 @@
+'''
+Author: your name
+Date: 2022-08-05 11:31:51
+LastEditTime: 2022-08-05 18:00:17
+LastEditors: your name
+Description: 
+FilePath: \medetpy\medetpy0\__init__.py
+'''
+'''
+Need package:
+    requests
+    medpy
+    numpy
+'''
+from . import met
