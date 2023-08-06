4094e5dc56524f2f9483b3f4155d683425e30c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:27 +0000

    Add package doc-printer/0.13.0/doc_printer-0.13.0.tar.gz

---
+++ packages/doc-printer/doc_printer-0.13.0.tar.gz/doc_printer-0.13.0/doc_printer/smart.py
@@ -0,0 +1,58 @@
+import collections.abc
+import contextlib
+import dataclasses
+
+from .doc import *
+from .simple import *
+
+
+class LineWidthExceeded(Exception):
+    pass
+
+
+@dataclasses.dataclass
+class SmartDocRenderer(SimpleDocRenderer):
+    max_line_width: int = 80
+
+    def render(self, doc: Doc) -> TokenStream:
+        if isinstance(doc, Alt) and len(doc.alts) > 1:
+            yield from self.render_with_lookahead(doc.alts)
+        else:
+            yield from self.render_simple(doc)
+
+    ###########################################################################
+    # Strict Mode & Raising Errors when Max Line Width is Exceeded
+    ###########################################################################
+
+    is_strict: bool = dataclasses.field(default=False, init=False)
+
+    def strict_emit(self, token: Token) -> Token:
+        if self.column + len(token) > self.max_line_width:
+            raise LineWidthExceeded()
+        else:
+            return token
+
+    @contextlib.contextmanager
+    def strict(self) -> collections.abc.Iterator[None]:
+        self.on_emit.append(self.strict_emit)
+        try:
+            yield None
+        finally:
+            self.on_emit.remove(self.strict_emit)
+
+    def render_with_lookahead(self, alts: tuple[Doc, ...]) -> TokenStream:
+        fallback, *rest = alts
+        succeeded = False
+        for alt in reversed(rest):
+            with self.strict():
+                try:
+                    token_stream = self.render_simple(alt)
+                    token_buffer, token_stream = self.buffer_line(token_stream)
+                    succeeded = True
+                    yield from map(self.emit, token_buffer)
+                    yield from token_stream
+                    break
+                except LineWidthExceeded:
+                    continue
+        if not succeeded:
+            yield from self.render(fallback)
