4b212b593db58246b2a089f54960d862a83ca1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:01 +0000

    Add package sport-activities-features/0.3.3/sport-activities-features-0.3.3.tar.gz

---
+++ packages/sport-activities-features/sport-activities-features-0.3.3.tar.gz/sport-activities-features-0.3.3/sport_activities_features/classes.py
@@ -0,0 +1,24 @@
+class StoredSegments(object):
+    """
+    Class for stored segments.\n
+    Args:
+        segment ():
+
+        ascent ():
+
+    Note:
+        [WIP]
+        This class is still under developement,
+        therefore its methods may not work as expected.
+    """
+    def __init__(self, segment, ascent) -> None:
+        """
+        Initialisation method for StoredSegments class.\n
+        Args:
+            segment ():
+
+            ascent ():
+
+        """
+        self.segment = segment
+        self.ascent = ascent
