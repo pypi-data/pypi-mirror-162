b3627b7d676d5517aefc01b8d3c6a597b4b481d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:20 +0000

    Add package HABApp/1.0.3/HABApp-1.0.3.tar.gz

---
+++ packages/HABApp/HABApp-1.0.3.tar.gz/HABApp-1.0.3/src/HABApp/core/files/file/file_state.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+
+
+class FileState(Enum):
+    LOADED = auto()
+    FAILED = auto()
+
+    DEPENDENCIES_OK = auto()
+    DEPENDENCIES_MISSING = auto()
+    PROPERTIES_ERROR = auto()
+
+    PENDING = auto()
+    REMOVED = auto()
+
+    def __str__(self):
+        return str(self.name)
