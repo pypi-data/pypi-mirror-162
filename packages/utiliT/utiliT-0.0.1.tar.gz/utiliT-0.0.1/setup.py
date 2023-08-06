45874f29764b42d5f654938cf6f4b76bb7c4820e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:43 +0000

    Add package utiliT/0.0.1/utiliT-0.0.1.tar.gz

---
+++ packages/utiliT/utiliT-0.0.1.tar.gz/utiliT-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python utilities'
+LONG_DESCRIPTION = 'A package to help develop code faster through '
+
+# Setting up
+setup(
+    name="utiliT",
+    version=VERSION,
+    author="Nakul Upadhya",
+    author_email="<nakulupadhya1@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'utilities'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
