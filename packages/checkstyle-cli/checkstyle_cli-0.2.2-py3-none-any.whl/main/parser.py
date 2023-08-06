fc4317167aab5102ce3be9bc69f58c7daa83c5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:06 +0000

    Add package checkstyle-cli/0.2.2/checkstyle_cli-0.2.2-py3-none-any.whl

---
+++ packages/checkstyle-cli/checkstyle_cli-0.2.2-py3-none-any.whl/main/parser.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="/google_checks.xml",
+        help="checkstyle configuration file",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        default="10.3.2",
+        help="checkstyle version",
+    )
+    parser.add_argument(
+        "filenames", nargs="*",
+        help="filenames for checkstyle",
+    )
+    return parser
