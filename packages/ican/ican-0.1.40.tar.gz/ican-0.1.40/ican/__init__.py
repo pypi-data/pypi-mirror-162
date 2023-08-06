52d6ac6a3b743943ba3ee775f805fc5eb9372a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:43 +0000

    Add package ican/0.1.40/ican-0.1.40.tar.gz

---
+++ packages/ican/ican-0.1.40.tar.gz/ican-0.1.40/ican/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+__version__ = "0.1.40+build.289"
+__public_version__ = "0.1.40"
+__author__ = "@jthop"
+__copyright__ = f"Copyright 2022 {__author__}"
+
+
+__all__ = [
+    "__version__",
+    "__public_version__",
+    "__author__",
+    "__copyright__",
+]
