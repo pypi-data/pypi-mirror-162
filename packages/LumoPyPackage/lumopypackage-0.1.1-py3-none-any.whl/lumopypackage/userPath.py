93453c1ea43f1de66ed547cb6b480fed9d557303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:54 +0000

    Add package LumoPyPackage/0.1.1/lumopypackage-0.1.1-py3-none-any.whl

---
+++ packages/LumoPyPackage/lumopypackage-0.1.1-py3-none-any.whl/lumopypackage/userPath.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022年8月6日 21:37
+# @Author  : Lumo
+# @Site    : Home
+# @File    : downloadBingImg.py
+# @Software: PyCharm
+
+
+import os
+import subprocess
+
+
+def userPath():
+    '''
+    次函数用于获取windows平台下当前登录账户的目录路径。
+    '''
+
+    # 判断是windows平台执行函数。
+    if os.name == 'nt':
+
+        # 执行终端明天通过管道获取用户目录路径。
+        obj = subprocess.Popen('echo %USERPROFILE%',
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
+        # 将从管道拿到的内容转成字符串类型，并去除末尾的换行符。
+        Path = obj.stdout.read().decode('utf-8').strip('\n').strip('\r')
+        return Path
+
+    # 判断不是windows平台提示错误。
+    else:
+        print('此函数只能用于windows平台')
+        return '此函数只能用于windows平台'
+
+
+if __name__ == '__main__':
+    pass
