b1450aef70fdb63327af58cdf7463c051c9c104b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:52 +0000

    Add package m3-db-utils/0.3.1/m3_db_utils-0.3.1-py3-none-any.whl

---
+++ packages/m3-db-utils/m3_db_utils-0.3.1-py3-none-any.whl/m3_db_utils/decorators.py
@@ -0,0 +1,34 @@
+from functools import (
+    wraps,
+)
+
+from m3_legacy.middleware import (
+    get_thread_data,
+)
+
+
+def log_query(log_query_name):
+    """
+    Декоратор на экшен.
+
+    Под наименованием log_query_name логирует стактрейс всех мест в экшене, которые создают и отправляют запросы в БД.
+    """
+
+    def inner(action):
+        @wraps(action)
+        def wrapper(self, request, context, *args, **kwargs):
+            thread_locals = get_thread_data()
+
+            setattr(thread_locals, 'log_query', log_query_name)
+
+            try:
+                result = action(self, request, context, *args, **kwargs)
+
+            finally:
+                delattr(thread_locals, 'log_query')
+
+            return result
+
+        return wrapper
+
+    return inner
