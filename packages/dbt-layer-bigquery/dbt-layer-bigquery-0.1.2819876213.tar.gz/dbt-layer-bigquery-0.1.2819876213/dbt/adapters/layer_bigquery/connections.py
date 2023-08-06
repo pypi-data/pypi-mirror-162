40372c93fc5814eb2fa39563144ed6e2d8f51204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:01 +0000

    Add package dbt-layer-bigquery/0.1.2819876213/dbt-layer-bigquery-0.1.2819876213.tar.gz

---
+++ packages/dbt-layer-bigquery/dbt-layer-bigquery-0.1.2819876213.tar.gz/dbt-layer-bigquery-0.1.2819876213/dbt/adapters/layer_bigquery/connections.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+from dbt.adapters.bigquery.connections import (  # type:ignore
+    BigQueryConnectionManager,
+    BigQueryCredentials,
+)
+
+
+@dataclass
+class LayerBigQueryCredentials(BigQueryCredentials):
+    # Layer config json creds
+    layer_configfile: Optional[str] = None
+    layer_configfile_json: Optional[Dict[str, Any]] = None
+    layer_project: Optional[str] = None
+    layer_api_key: Optional[str] = None
+
+    @property
+    def type(self) -> str:
+        return "layer_bigquery"
+
+
+class LayerBigQueryConnectionManager(BigQueryConnectionManager):
+    TYPE = "layer_bigquery"
