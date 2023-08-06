b7010f9191419d70e441e6dffbae102e16025ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:43 +0000

    Add package torch-pso/1.1.1/torch_pso-1.1.1-py3-none-any.whl

---
+++ packages/torch-pso/torch_pso-1.1.1-py3-none-any.whl/torch_pso/__init__.py
@@ -0,0 +1,9 @@
+from .optim.ParticleSwarmOptimizer import ParticleSwarmOptimizer
+from .optim.GenerationalPSO import GenerationalPSO
+from .optim.AutotuningPSO import AutotuningPSO
+from .optim.RingTopologyPSO import RingTopologyPSO
+from .optim.ChaoticPSO import ChaoticPSO
+from .optim.GenericPSO import GenericPSO
+from .optim.AcceleratedPSO import AcceleratedPSO
+
+OPTIMIZERS = list(GenericPSO.subclasses)
