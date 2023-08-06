2b9c60ef9c39afb7a3eb2cf107eeb5a6704cd80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:36 +0000

    Add package FSAE47-Function-Box-Test/0.1.1/FSAE47_Function_Box_Test-0.1.1.tar.gz

---
+++ packages/FSAE47-Function-Box-Test/FSAE47_Function_Box_Test-0.1.1.tar.gz/FSAE47_Function_Box_Test-0.1.1/README.md
@@ -0,0 +1,5 @@
+# description
+Where all the functions are gonna be stored
+
+# Caveats
+Requirements currently stored in the setup.py file, I plan to move them later.
---
+++ packages/FSAE47-Function-Box-Test/FSAE47_Function_Box_Test-0.1.1.tar.gz/FSAE47_Function_Box_Test-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='FSAE47_Function_Box_Test',
+    version='0.1.1',
+    license='MIT',
+    author="Toby Osborne",
+    author_email='toby.osborne@fsae.co.nz',
+    packages=find_packages('src'),
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    url='https://github.com/UOA-FSAE/functionBox',
+    keywords='function box',
+    install_requires=[
+          'scikit-learn',
+      ],
+)
