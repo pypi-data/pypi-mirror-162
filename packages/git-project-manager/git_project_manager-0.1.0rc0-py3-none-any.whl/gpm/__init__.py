2a1652ad22f884cfd02dacdc401b7e3ddca8e901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:50 +0000

    Add package git-project-manager/0.1.0rc0/git_project_manager-0.1.0rc0-py3-none-any.whl

---
+++ packages/git-project-manager/git_project_manager-0.1.0rc0-py3-none-any.whl/gpm/__init__.py
@@ -0,0 +1,10 @@
+__title__ = "gpm"
+__summary__ = "Git Project Manager(GPM)"
+__author__ = "Timothy Pidashev"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present Timothy Pidashev"
+__version__ = "0.1.0-rc.0"
+
+
+def run_as_module():
+    print("GPM")
