89886877aeb3fa3f1ae0134cb2c2efac2ecdacb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:41 +0000

    Add package themispy/0.0.17/themispy-0.0.17-py3-none-any.whl

---
+++ packages/themispy/themispy-0.0.17-py3-none-any.whl/themispy/project/utils.py
@@ -0,0 +1,29 @@
+import datetime
+import os
+
+import pytz
+
+
+def split_filepath(url: str) -> 'tuple[str, str]':
+    """
+    Splits and url and returns the document name and extension
+    as a 2-element tuple.
+    """
+    docname, docext = os.path.splitext(url)
+    docname = docname.rsplit('/')[-1]
+    return docname, docext
+
+
+def get_logpath(tz: str = 'America/Sao_Paulo') -> str:
+    """
+    Returns the current date formatted for logging directories.
+    \nExample:\n
+    * container_fullpath = 'my_container/subdir' + get_logpath() \n
+    Printing 'container_fullpath' will return:
+    * 'my_container/subdir/<THIS_YEAR>/<THIS_MONTH>/<THIS_DAY>'.\n
+    
+    :param str: timezone\n
+    Default timezone is 'America/Sao_Paulo'.
+    """
+    tz = pytz.timezone(tz)
+    return datetime.datetime.now(tz=tz).strftime('/%Y/%m/%d')
