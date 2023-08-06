a56ed934d926a856e3b6aa27b1b69909cdf473c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:55 +0000

    Add package sourav-tesseract/0.1.9/sourav_tesseract-0.1.9.tar.gz

---
+++ packages/sourav-tesseract/sourav_tesseract-0.1.9.tar.gz/sourav_tesseract-0.1.9/tesseract_idx/config.py
@@ -0,0 +1,9 @@
+# EXTENSION_LIST = ['.pdf', '.png', '.jpg', '.tif', '.jpeg']
+# S3_BUCKET_NAME = 'input-adaptor'
+# INPUT_FILE_FOLDER = 'Temp/'
+"""
+configuration file for tessaract ocr
+"""
+
+OUTPUT_PATH = 'json_dir'
+EXTENSION_LIST = ['.png','.tif','.jpg', '.jpeg']
