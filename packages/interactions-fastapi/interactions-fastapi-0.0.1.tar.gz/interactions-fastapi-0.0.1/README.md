206a44cdf1c159dbce9e19a0023fe674f8c733b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package interactions-fastapi/0.0.1/interactions-fastapi-0.0.1.tar.gz

---
+++ packages/interactions-fastapi/interactions-fastapi-0.0.1.tar.gz/interactions-fastapi-0.0.1/README.md
@@ -0,0 +1,28 @@
+# FastAPI wrapper for interactions.py
+
+An Extension library to add Fast API for your interactions.py bot .
+
+## Installation
+
+``pip install git+https://github.com/Damego/interactions-fastapi.git``
+
+
+## Examples
+
+```python
+import interactions
+from interactions.ext.api import setup
+
+client = interactions.Client(...)
+api = setup(client)
+
+@api.route("GET", "/")
+async def index():
+    return {"status": "success"}
+
+client.start()
+```
+
+Server will run on ``127.0.0.1:32512`` (host and port modifiable)
+
+More examples in ``examples`` folder.
---
+++ packages/interactions-fastapi/interactions-fastapi-0.0.1.tar.gz/interactions-fastapi-0.0.1/setup.py
@@ -0,0 +1,48 @@
+from codecs import open
+
+from setuptools import setup
+
+# Package information
+AUTHOR = "Damego"
+AUTHOR_EMAIL = "damego.dev@gmail.com"
+DESCRIPTION = "Build an API for your bot with FastAPI"
+PROJECT_NAME = "interactions-fastapi"
+MAIN_PACKAGE_NAME = "interactions.ext.fastapi"
+URL = "https://github.com/Damego/interactions-fastapi"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read()
+
+
+setup(
+    name=PROJECT_NAME,
+    version="0.0.1",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    include_package_data=True,
+    install_requires=requirements,
+    license="GPL-3.0 License",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=[MAIN_PACKAGE_NAME],
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
