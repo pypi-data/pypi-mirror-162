e49493ca794010ca80466dbed62f00d198ca9b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:43 +0000

    Add package eitaa/1.3.3/eitaa-1.3.3.tar.gz

---
+++ packages/eitaa/eitaa-1.3.3.tar.gz/eitaa-1.3.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup (
+    name='eitaa',
+    version='1.3.3',
+    packages=['eitaa'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bistcuite/eitaapykit",
+    install_requires=[
+        'requests',
+        'bs4'
+    ]
+ )
