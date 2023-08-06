29c198e515cb61f1ada972d573cca7d462c90e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:22 +0000

    Add package game-extraction-toolbox/0.0.6/game-extraction-toolbox-0.0.6.tar.gz

---
+++ packages/game-extraction-toolbox/game-extraction-toolbox-0.0.6.tar.gz/game-extraction-toolbox-0.0.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from importlib.metadata import entry_points
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'python-magic',
+        'python-magic-bin',
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gextoolbox = toolbox:cli'
+        ]
+    }
+)
