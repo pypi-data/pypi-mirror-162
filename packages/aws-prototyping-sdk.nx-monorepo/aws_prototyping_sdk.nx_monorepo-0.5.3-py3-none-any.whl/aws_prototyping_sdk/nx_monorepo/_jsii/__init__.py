ec7fb161baa9ed0963402937264f2ca5538cbd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:42 +0000

    Add package aws-prototyping-sdk.nx-monorepo/0.5.3/aws_prototyping_sdk.nx_monorepo-0.5.3-py3-none-any.whl

---
+++ packages/aws-prototyping-sdk.nx-monorepo/aws_prototyping_sdk.nx_monorepo-0.5.3-py3-none-any.whl/aws_prototyping_sdk/nx_monorepo/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/nx-monorepo",
+    "0.5.3",
+    __name__[0:-6],
+    "nx-monorepo@0.5.3.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
