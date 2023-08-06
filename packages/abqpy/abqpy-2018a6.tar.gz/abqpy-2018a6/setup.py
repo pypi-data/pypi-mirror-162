cc22a68f8fdcd9ddc7705c25e9b325c7e12641b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:10 +0000

    Add package abqpy/2018a6/abqpy-2018a6.tar.gz

---
+++ packages/abqpy/abqpy-2018a6.tar.gz/abqpy-2018a6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="abqpy",
+    version="2018a6",
+    author="WANG Hailin",
+    author_email="hailin.wang@connect.polyu.hk",
+    description="Type hints for Abaqus/Python scripting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haiiliin/abqpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    package_dir={'': 'src'},
+    install_requires=['ipyparams', 'typing-extensions'],
+    packages=setuptools.find_packages('src'),
+    py_modules=['abaqusConstants', 'animation', 'annotationToolset', 'assembly', 'caeModules', 'caePrefsAccess',
+                'calibration', 'connectorBehavior', 'customKernel', 'deleteObjectCallback', 'displayGroupMdbToolset',
+                'displayGroupOdbToolset', 'driverUtils', 'field', 'fields', 'inpParser', 'interaction', 'material',
+                'mesh', 'meshEdit', 'methodCallback', 'monitorManager', 'odbAccess', 'odbConnectorBehavior',
+                'odbMaterial', 'odbSection', 'part', 'redentABQ', 'section', 'symbolicConstants', 'textRepr',
+                'upgradeScript', 'visualization'],
+)
