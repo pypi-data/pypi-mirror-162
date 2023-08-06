8c428e3e363f08125b780e35846a247a830035c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:26 +0000

    Add package GTMX/0.0.4/GTMX-0.0.4.tar.gz

---
+++ packages/GTMX/GTMX-0.0.4.tar.gz/GTMX-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='GTMX',
+    version='0.0.4',
+    description="A Python package for Generative Topographic Mapping (GTM)",
+    url='https://github.com/innovationb1ue/GTMX',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'matplotlib',
+        'bokeh',
+        'pandas'
+    ],
+    long_description=open('README.rst').read()
+)
