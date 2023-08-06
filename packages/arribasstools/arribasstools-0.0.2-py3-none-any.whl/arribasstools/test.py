c162cefe59702b60cfa8bb5ad81426e50f7cec4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:50 +0000

    Add package arribasstools/0.0.2/arribasstools-0.0.2-py3-none-any.whl

---
+++ packages/arribasstools/arribasstools-0.0.2-py3-none-any.whl/arribasstools/test.py
@@ -0,0 +1,12 @@
+
+class Mytest:
+    ''' Clase de test '''
+    def __init__(self):
+        print("mytest class")
+
+    def mytest(self):
+        print("mytest function")
+        
+    def pruebas():
+        print("pruebas function")
+        return
