c2c101eeb6a5c10b335d3d9275f079075e1a18be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:09 +0000

    Add package jaxns/1.1.2/jaxns-1.1.2.tar.gz

---
+++ packages/jaxns/jaxns-1.1.2.tar.gz/jaxns-1.1.2/jaxns/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(format='%(levelname)s[%(asctime)s]: %(message)s', level=logging.INFO)
+
+from jaxns.nested_sampler import *
+from jaxns.modules.optimisation import *
+from jaxns.prior_transforms import *
+from jaxns.nested_sampler.utils import (summary,
+                                        resample,
+                                        marginalise_static,
+                                        marginalise_dynamic,
+                                        analytic_log_evidence,
+                                        analytic_posterior_samples,
+                                        evidence_posterior_samples,
+                                        save_results,
+                                        load_results)
+from jaxns.nested_sampler.plotting import (plot_diagnostics,
+                                           plot_cornerplot)
