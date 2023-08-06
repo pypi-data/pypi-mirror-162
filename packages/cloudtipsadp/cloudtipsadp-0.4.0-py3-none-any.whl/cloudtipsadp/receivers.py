cd95d1e4a08e41a55d4f2442a43c8befb66e8e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:59 +0000

    Add package cloudtipsadp/0.4.0/cloudtipsadp-0.4.0-py3-none-any.whl

---
+++ packages/cloudtipsadp/cloudtipsadp-0.4.0-py3-none-any.whl/cloudtipsadp/receivers.py
@@ -0,0 +1,54 @@
+import json
+import os
+
+import requests as requests
+
+from cloudtipsadp.clients import Connect, SandboxClient
+from cloudtipsadp.constants import M_BASE_IMPLEMENTED
+
+
+class Receiver:
+    receivers = list
+    phone_number: str
+    name: str
+    place_id: str = str(os.getenv('placeId'))
+
+    def __init__(self, name: str, phone_number: str):
+        self.phone_number = phone_number
+        self.name = name
+
+    def get_data(self):
+        try:
+            receivers = [dict(phoneNumber=self.phone_number, name=self.name)]
+            data = dict(placeId=self.place_id, receivers=receivers)
+        except AttributeError:
+            print('No user data.')
+        else:
+            return json.dumps(data)
+
+    def create(self):
+        raise NotImplementedError(M_BASE_IMPLEMENTED)
+
+
+class Receivers(Receiver):
+    """Получатель донатов."""
+    base_path = 'receivers'
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def create(self):
+        """Создать получателя донатов в сервисе."""
+        # URL для запроса к API
+        api_url = Connect.client.api([self.base_path, 'create-many'])
+        response = requests.post(api_url, data=self.get_data(),
+                                 headers=Connect.get_headers())
+        return response.json()
+
+
+if __name__ == '__main__':
+    connect = Connect(SandboxClient())
+    # connect.get_token()
+    ob = Receivers('Bob', '+79125265720').create()
+    if ob.get('succeed'):
+        print(f'Получатель создан. {ob}')
