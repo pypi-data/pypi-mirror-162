dc2d6f387e81589bfba7f5d65c422fa347d813c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:26 +0000

    Add package grocropclient/1.0.3/grocropclient-1.0.3-py3-none-any.whl

---
+++ packages/grocropclient/grocropclient-1.0.3-py3-none-any.whl/grocropclient/crop_budgets_data.py
@@ -0,0 +1,305 @@
+METRIC_MAPPING = {
+  "Gross Revenue": {
+    "Revenues, incl Govt Payments": {
+      "Production Revenue": {
+        "Producer Prices": [],
+        "Yield": [
+          "Yield Assumption"
+        ]
+      },
+      "Government Payments": {
+        "Government Payments, Other": []
+      },
+      "Revenues, Other": {},
+      "Crop Insurance Payments": {},
+      "Production Revenue, Projected": {
+        "Producer Prices, Projected": [],
+        "Yield, Historical Average": []
+      }
+    }
+  },
+  "Total Costs": {
+    "Expenses": {
+      "Operating Expenses": {
+        "Seed Expenses": [
+          "Bt and ECB Seed Expenses",
+          "Bt, ECB, and RIB Seed Expenses",
+          "Bt, ECB, and RW Seed Expenses",
+          "Bt, ECB, RR2, and RIB Seed Expenses",
+          "Bt, ECB, RR2, LL, and RIB Seed Expenses",
+          "Bt, ECB, RW, and RIB Seed Expenses",
+          "Bt, ECB, RW, and RR2 Seed Expenses",
+          "Bt, ECB, RW, RR2, and RIB Seed Expenses",
+          "Bt, ECB, RW, RR2, LL, and RIB Seed Expenses",
+          "Certified and Treated Seed Expenses",
+          "ECB and RR2 Seed Expenses",
+          "Enlist E3 Treated Seed Expenses",
+          "Enlist, Bt, ECB, and RIB Seed Expenses",
+          "Inoculant Seed Expenses",
+          "RR Seed Expenses",
+          "RR2 Seed Expenses",
+          "RR2 Xtend Seed Expenses",
+          "Rye Seed Expenses",
+          "Seed Expenses, B2 Auxin Technology",
+          "Seed Expenses, B2RF",
+          "Seed Expenses, B2RF Technology Fee",
+          "Seed Expenses, B2XF",
+          "Seed Expenses, BIIRF",
+          "Seed Expenses, BIIRRF",
+          "Seed Expenses, Bt",
+          "Seed Expenses, Bt Technology Fee",
+          "Seed Expenses, BtRR",
+          "Seed Expenses, Cotton",
+          "Seed Expenses, Cowpea",
+          "Seed Expenses, GMO",
+          "Seed Expenses, Herbicide Resistant",
+          "Seed Expenses, Herbicide Technology Fee",
+          "Seed Expenses, Herbicide Tolerant",
+          "Seed Expenses, Herbicide Tolerant Technology Fee",
+          "Seed Expenses, Inoculant",
+          "Seed Expenses, Insect Treatment",
+          "Seed Expenses, Roundup Ready",
+          "Seed Expenses, Stacked Gene",
+          "Seed Expenses, Technology Fee",
+          "Seed Expenses, Wheat",
+          "SmartStax RIB Complete Seed Expenses",
+          "Soybeans Seed Expenses",
+          "Wheat Seed Expenses"
+        ],
+        "Labor Expenses": [
+          "Custom Hand Hoeing Expenses",
+          "Labor Expenses, Hand Labor",
+          "Labor Expenses, Hoeing",
+          "Labor Expenses, Salaried",
+          "Labor Expenses, Unallocated"
+        ],
+        "Crop Insurance Premium Expenses": [],
+        "Fertilizer Expenses": [
+          "Lime Fertilizer Expenses",
+          "Nitrogen Fertilizer Expenses",
+          "Phosphorous Fertilizer Expenses",
+          "Potash Fertilizer Expenses",
+          "Urea Fertilizer Expenses",
+          "Ammonium Phosphate Fertilizer Expenses",
+          "Anhydrous Ammonia Fertilizer Expenses",
+          "Fertilizer, Lime, and Soil Conditioner Expenses",
+          "Phosphate Fertilizer Expenses",
+          "Potassium Fertilizer Expenses",
+          "Urea Ammonium Nitrate Fertilizer Expenses",
+          "0-60-0 Fertilizer Expenses",
+          "2-4D Amine Fertilizer Expenses",
+          "24-6-2 Fertilizer Expenses",
+          "24-8-0 Fertilizer Expenses",
+          "32-11-3 6S Fertilizer Expenses",
+          "Ammonia Fertilizer Expenses",
+          "Ammonium Polyphosphate Fertilizer Expenses",
+          "Complete NPK",
+          "Custom Fertilizer Expenses",
+          "Diammonium Phosphate",
+          "Dry Nitrogen Fertilizer Expenses",
+          "Dry Phosphorous Fertilizer Expenses",
+          "Liquid Nitrogen Fertilzer Expenses",
+          "Liquid Phosphorous Fertilizer Expenses",
+          "Manure Fertilizer Expenses",
+          "Monoammonium Phosphate",
+          "Monoammonium Phosphate Fertilizer Expenses",
+          "NPK Fertilizer Expenses",
+          "NPZnS Fertilizer Expenses",
+          "Phosphoric Acid Fertilizer Expenses",
+          "Spray Fertilizer Expenses",
+          "Sulfur Fertilizer Expenses",
+          "Triple Super Phosphate Fertilizer Expenses",
+          "Urea Ammonium Nitrate Fertilizer Expenses, Applied at or before the Flowering Stage",
+          "Urea Ammonium Nitrate Fertilizer Expenses, Applied by Pivot"
+        ],
+        "Hauling Expenses": [],
+        "Chemical Expenses": [
+          "Fungicide Expenses",
+          "Herbicide Expenses",
+          "Insecticide Expenses",
+          "Pesticide Expenses",
+          "2-4D Amine Chemical Expenses",
+          "Chemical Expenses, Acid",
+          "Chemical Expenses, Prep",
+          "Inoculant Expenses",
+          "Surfactant Expenses"
+        ],
+        "Machine Hire and Custom Work Expenses": [],
+        "Operating Expenses, Other": [],
+        "Custom Work Expenses": [],
+        "Drying Expenses": [],
+        "Fungicide Application Expenses": [],
+        "Storage Expenses": [],
+        "USDA Agricultural Risk and Price Loss Coverage Program Payments": [],
+        "Bag/Tie Expenses": [],
+        "Boll Weevil Assessment Expenses": [],
+        "Boll Weevil Eradication Expenses": [],
+        "Boll Weevil Program Expenses": [],
+        "Boll Worm Eradication Expenses": [],
+        "Cart Expenses": [],
+        "Chemical Application": [],
+        "Chemical Application Cost": [],
+        "Crop Consulting Expenses": [],
+        "Crop Growing Expenses": [],
+        "Fertilizer Application": [],
+        "Fertilizer Application Cost": [],
+        "Fertilizer Application Expenses": [
+          "Anhydrous Ammonia Fertilizer Application Expenses",
+          "Ammonia Fertilizer Application Expenses",
+          "Dry Fertilizer Application Expenses",
+          "Fertilizer Application Expenses, Broadcast Blend",
+          "Fertilizer Application Expenses, Broadcast P & K",
+          "Lime Fertilizer Application Expenses",
+          "Liquid Fertilizer Application Expenses, High",
+          "Liquid Fertilizer Application Expenses, Low",
+          "Liquid Fertilizer Rig Expenses",
+          "Spray Fertilizer Application Expenses",
+          "Spread Fertilizer Application Expenses"
+        ],
+        "Field Cultivation Expenses": [],
+        "Field Operations Expenses": [
+          "Field Operations Expenses, One-Pass Tillage"
+        ],
+        "Ginning and Hauling Expenses": [],
+        "Ginning Expenses": [
+          "Custom Ginning Expenses"
+        ],
+        "Harvest Aid Application Expenses": [
+          "Harvest Aid Application Expenses, Drip"
+        ],
+        "Harvest and Hauling Expenses": [],
+        "Harvest Expenses": [],
+        "Herbicide and Application Expenses": [],
+        "Herbicide Application Expenses": [
+          "Herbicide Application Expenses, Aerial Spray",
+          "Herbicide Expenses, First Application",
+          "Herbicide Expenses, Second Application",
+          "Post-Plant Herbicide Application Expenses",
+          "ant h",
+          "Spray Burndown Herbicide Application Expenses",
+          "Spray Herbicide Application Expenses"
+        ],
+        "Insecticide Application Expenses": [
+          "Apply by Air Insecticide Expenses",
+          "Inseticide Expenses, First Application",
+          "Inseticide Expenses, Second Application"
+        ],
+        "Inseticide and Application Expenses": [],
+        "Inseticide Expenses": [],
+        "Irrigation Expenses": [
+          "Ditch Irrigation Expenses",
+          "Irrigation Energy Expenses",
+          "Irrigation Expenses, Energy Cost",
+          "Irrigation Expenses, Labor",
+          "Irrigation Expenses, Water Cost",
+          "Irrigation Labor Expenses",
+          "Pipe Irrigation Expenses",
+          "Pivot Irrigation Expenses"
+        ],
+        "Machinery and Energy Expenses": [],
+        "Marketing Expenses": [],
+        "Picking Expenses": [],
+        "Planting Expenses": [
+          "No-Till Planting Expenses"
+        ],
+        "Scouting Expenses": [
+          "Scouting Expenses, Insects",
+          "Scouting/Consulting Expenses"
+        ],
+        "Spray Expenses": [],
+        "Storage and Marketing Expenses": [],
+        "Sweep Plowing Expenses": [],
+        "Utility Expenses": [],
+        "Variable Expenses, Other": [],
+        "Variable Machine Hire Expenses": [],
+        "Variable Utility Expenses": []
+      },
+      "Overhead Expenses": {
+        "Interest Expenses": [
+          "Operating Interest Expenses"
+        ],
+        "Overhead Expenses, Other": [],
+        "Fuel Expenses": [
+          "Fuel Expenses, incl. Electricity",
+          "Diesel Fuel Expenses, General Use Equipment",
+          "Diesel Fuel Expenses, Tractors",
+          "Fuel Expenses, Drying",
+          "Fuel Expenses, General Use Equipment",
+          "Fuel Expenses, incl Electricity",
+          "Fuel Expenses, Tractors",
+          "Vehicle Expenses"
+        ],
+        "Repairs and Maintenance Expenses": [],
+        "Insurance Premium Expenses": [
+          "Building Insurance Premium Expenses",
+          "Crop Insurance Premium Expenses, Hail",
+          "Crop Insurance Premium Expenses, Herbicide Drift",
+          "Miscellaneous Insurance Premium Expenses, SCO and STAX",
+          "Multiple Peril Crop Insurance",
+          "Revenue Protection Crop Insurance Premium Expenses, 65%",
+          "Revenue Protection Crop Insurance Premium Expenses, 65% YA",
+          "Revenue Protection Crop Insurance Premium Expenses, 70%",
+          "Revenue Protection Crop Insurance Premium Expenses, 70% SE",
+          "Revenue Protection Crop Insurance Premium Expenses, 70% YA",
+          "Revenue Protection Crop Insurance Premium Expenses, 70% YE",
+          "STAX Crop Insurance Premium Expenses",
+          "Supplemental Coverage Option"
+        ],
+        "Rent and Lease Payment Expenses": [],
+        "Machinery Expenses": [
+          "Machine Repair Expenses",
+          "Machinery and Equipment Investment Expense, General Use Equipment",
+          "Machinery and Equipment Investment Expense, Implements",
+          "Machinery and Equipment Investment Expense, Irrigation Equipment",
+          "Machinery and Equipment Investment Expense, Tractors",
+          "Machinery Expenses, Other Labor",
+          "Machinery Expenses, Tractors Labor"
+        ],
+        "Depreciation Expenses": [
+          "Machinery Depreciation Expenses",
+          "Capital Investment and Depreciation Expenses",
+          "Building Depreciation Expenses"
+        ],
+        "General Management Expenses": [],
+        "Land Opportunity Cost": [],
+        "Land Rent and Lease Payment Expenses": [],
+        "Unpaid Labor Opportunity Cost": [],
+        "Building Maintenance Expenses": [],
+        "Capital Opportunity Cost": [],
+        "Electricity Expenses": [],
+        "Establishment Cost, Equipment": [],
+        "Fixed Labor Expenses": [],
+        "Fixed Machine Hire Expenses": [],
+        "Fixed Utility Expenses": [],
+        "Fuel and Machinery Expenses": [],
+        "Land rent": [],
+        "Loan Payment Expenses": [],
+        "Machinery and Equipment Overhead Cost, General Use Equipment": [],
+        "Machinery and Equipment Overhead Cost, Implements": [],
+        "Machinery and Equipment Overhead Cost, Irrigation Equipment": [],
+        "Machinery and Equipment Overhead Cost, Tractors": [],
+        "Professional Fees and Dues": [],
+        "Real Estate Taxes": [],
+        "Seed, Chemicals, Insurance, and Miscellaneous Expenses": [],
+        "Soil Testing Expenses": [],
+        "Taxes and Insurance Expenses": []
+      },
+      "Expenses, excl Land": {},
+      "Expenses, Other": {}
+    },
+    "Expenses (currency/mass)": {
+      "Operating Expenses (currency/mass)": {},
+      "Overhead Expenses (currency/mass)": {}
+    }
+  },
+  "Net Income": {
+    "Net Profit": {},
+    "Operating Profit": {},
+    "Net Profit, excl Land Expenses": {},
+    "Net Profit, excl Labor and Management Expenses": {},
+    "Net Profit, excl Land, Labor and Management Expenses": {},
+    "Net Profit, excl Overhead Costs": {},
+    "Net Profit, Projected": {},
+    "Operating Profit, incl Govt Payments": {}
+  }
+}
