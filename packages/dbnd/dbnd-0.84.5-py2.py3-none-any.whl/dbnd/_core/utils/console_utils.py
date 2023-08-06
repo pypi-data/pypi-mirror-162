afa5e1c585bb8b1ea8bb7e8136f35f5cc73037d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package dbnd/0.84.5/dbnd-0.84.5-py2.py3-none-any.whl

---
+++ packages/dbnd/dbnd-0.84.5-py2.py3-none-any.whl/dbnd/_core/utils/console_utils.py
@@ -0,0 +1,38 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import re
+
+from dbnd._vendor.termcolor import colored
+
+
+# values should be calculated at run time - so colored function could be removed if needed
+def error_separator():
+    return colored("--------------------------------------", color="red")
+
+
+def error_separator_small():
+    return colored("- - -", on_color="on_red")
+
+
+def bold(value):
+    return colored(value, attrs=["bold"])
+
+
+def underline(value):
+    return colored(value, attrs=["underline"])
+
+
+def red(value):
+    return colored(value, color="red")
+
+
+def cyan(value):
+    return colored(value, color="cyan")
+
+
+COLOR_REGEX = re.compile(r"\x1b?\[(\d;)?\d*m")
+
+
+def uncolor(text):
+    """removes the ANSII color formatting from text"""
+    return COLOR_REGEX.sub("", text)
