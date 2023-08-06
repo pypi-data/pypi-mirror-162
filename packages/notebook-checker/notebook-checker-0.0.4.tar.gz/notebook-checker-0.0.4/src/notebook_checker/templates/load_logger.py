849bd6a1fbae32026248cd5056450bb4a247792b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:49 +0000

    Add package notebook-checker/0.0.4/notebook-checker-0.0.4.tar.gz

---
+++ packages/notebook-checker/notebook-checker-0.0.4.tar.gz/notebook-checker-0.0.4/src/notebook_checker/templates/load_logger.py
@@ -0,0 +1,10 @@
+_notebook_check_logger = __import__('logging').getLogger('notebook-checker')
+_notebook_module_type = __import__('types').ModuleType
+_notebook_get_traceback = __import__('traceback')
+_notebook_log_store = set()
+
+def _notebook_log_error(message):
+    if message not in _notebook_log_store:
+        _notebook_log_store.add(message)
+        _notebook_check_logger.error(message)
+
