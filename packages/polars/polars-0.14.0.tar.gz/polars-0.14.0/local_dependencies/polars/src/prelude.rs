8e932f5e45d06b8f0f64ac96ac98f095b5c1178c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:55 +0000

    Add package polars/0.14.0/polars-0.14.0.tar.gz

---
+++ packages/polars/polars-0.14.0.tar.gz/polars-0.14.0/local_dependencies/polars/src/prelude.rs
@@ -0,0 +1,12 @@
+pub use polars_core::frame::groupby::*;
+pub use polars_core::{prelude::*, utils::NoNull};
+#[cfg(feature = "polars-io")]
+pub use polars_io::prelude::*;
+
+#[cfg(feature = "lazy")]
+pub use polars_lazy::prelude::*;
+
+#[cfg(feature = "temporal")]
+pub use polars_time::prelude::*;
+
+pub use polars_ops::prelude::*;
