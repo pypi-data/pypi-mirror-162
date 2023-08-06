ee86c7a716b79b1bad8b6c3d0a3c2df1eb3bb676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:42 +0000

    Add package badsql-tools/2.2/badsql-tools-2.2.tar.gz

---
+++ packages/badsql-tools/badsql-tools-2.2.tar.gz/badsql-tools-2.2/badsql_tools/badsql_tools.py
@@ -0,0 +1,32 @@
+import argparse, badsql
+
+def mkdb():
+    parser = argparse.ArgumentParser(prog ='badsql',
+                                     description ='badsql mkdb')
+  
+    parser.add_argument('headers', metavar ='headers', type=list, nargs='*', help ='headers')
+
+    args = parser.parse_args()
+
+    db = badsql.db([''.join([str(x) for x in y]) for y in args.headers])
+    db.display()
+    db.save('test')
+
+def insertrow():
+    parser = argparse.ArgumentParser(prog ='badsql',
+                                     description ='badsql insertrow')
+
+    parser.add_argument('pos', metavar ='pos', type=int, nargs=1, help= 'pos')
+  
+    parser.add_argument('values', metavar ='values', type=list, nargs='*', help ='values')
+
+    args = parser.parse_args()
+
+    try:
+        db = badsql.load('test')
+        db.insert(args.pos[0], [''.join([str(x) for x in y]) for y in args.values])
+        db.display()
+        db.save('test')
+    except Exception as e:
+        print('Error occurred:', e)
+
