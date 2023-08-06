3e470b578764f2a7a0ed889aecc8d464f2dfaaa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:23 +0000

    Add package forgefrenzy/0.1.14/forgefrenzy-0.1.14-py3-none-any.whl

---
+++ packages/forgefrenzy/forgefrenzy-0.1.14-py3-none-any.whl/forgefrenzy/__init__.py
@@ -0,0 +1,15 @@
+from forgefrenzy.products import Product, Products, Set, Sets, PartList, PartLists, Piece, Pieces
+from forgefrenzy.store import Webstore, Catalog
+
+__all__ = [
+    "Product",
+    "Products",
+    "Set",
+    "Sets",
+    "PartList",
+    "PartLists",
+    "Piece",
+    "Pieces",
+    "Catalog",
+    "Webstore",
+]
