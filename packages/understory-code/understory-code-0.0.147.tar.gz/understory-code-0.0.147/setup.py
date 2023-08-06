838fd5de14c33435b061ee59b886b2adeeb2b4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:03 +0000

    Add package understory-code/0.0.147/understory-code-0.0.147.tar.gz

---
+++ packages/understory-code/understory-code-0.0.147.tar.gz/understory-code-0.0.147/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['understory', 'understory.code', 'understory.code.templates']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['understory>=0.0,<0.1', 'warez>=0.0,<0.1']
+
+entry_points = \
+{'understory': ['code = understory.code:app']}
+
+setup_kwargs = {
+    'name': 'understory-code',
+    'version': '0.0.147',
+    'description': 'Host code in the understory',
+    'long_description': None,
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@ragt.ag',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://ragt.ag/code/understory-code',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
