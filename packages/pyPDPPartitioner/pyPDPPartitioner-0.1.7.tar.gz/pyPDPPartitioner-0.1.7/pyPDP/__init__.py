028399c0093604148866845f276de84c0d39dea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:12 +0000

    Add package pyPDPPartitioner/0.1.7/pyPDPPartitioner-0.1.7.tar.gz

---
+++ packages/pyPDPPartitioner/pyPDPPartitioner-0.1.7.tar.gz/pyPDPPartitioner-0.1.7/pyPDP/__init__.py
@@ -0,0 +1,8 @@
+name = "pyPDPPartitioner"
+package_name = "pyPDP"
+author = "Yannik Mahlau and Dominik Woiwode"
+author_email = "dominik.woiwode@stud.uni-hannover.de"
+description = "A python implementation of 'Explaining Hyperparameter Optimization via Partial Dependence Plots' by Moosbauer et al."
+version = "0.1.7"
+license = "MIT"
+url = "https://github.com/dwoiwode/py-pdp-partitioner"
