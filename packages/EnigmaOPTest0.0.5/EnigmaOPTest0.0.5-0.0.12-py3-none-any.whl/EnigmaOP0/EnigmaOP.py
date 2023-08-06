52e149c97b718df55e6a1c922acd7d456e3e904f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:43 +0000

    Add package EnigmaOPTest0.0.5/0.0.12/EnigmaOPTest0.0.5-0.0.12-py3-none-any.whl

---
+++ packages/EnigmaOPTest0.0.5/EnigmaOPTest0.0.5-0.0.12-py3-none-any.whl/EnigmaOP0/EnigmaOP.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 13:19:38 2021
+@author: ninad
+"""
+from resource import Rotors
+from resource import cipher
+from resource import generate
+from resource import convert
+
+class EnigmaOP:
+    
+    def __init__(self):
+        self.tple=Rotors.Rotor()
+        self.wiring=list(self.tple)
+
+    
+        self.input_string=''
+        self.copyinput=self.input_string
+        self.cipher_text=''
+        self.initialsettings=[]
+        self.plugboard={}
+
+        self.level =3
+
+        self.Rotor_combination,self.Rotor_Setting,self.plugboard=generate.random_settings()
+        self.initialsettings.append(self.Rotor_combination)
+        self.initialsettings.append(tuple(self.Rotor_Setting))
+        self.initialsettings.append(self.plugboard)
+    
+    def encrypt(self,input_string):
+        for i in input_string:
+    
+            self.ascii_num=convert.tonum(i)
+    
+            self.Rotor_combination,self.Rotor_Setting,self.ascii_num=cipher.encrypt(self.Rotor_combination,self.Rotor_Setting,self.plugboard,self.ascii_num,self.level)
+            self.var=self.ascii_num
+    
+            self.cipher_text=self.cipher_text+convert.tochar(self.var)
+        return self.cipher_text
