2104952db3bfbb0c97b0fc99f9927d79efa67cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:41 +0000

    Add package here-map-widget-ext-for-jupyter/0.0.1/here_map_widget_ext_for_jupyter-0.0.1-py2.py3-none-any.whl

---
+++ packages/here-map-widget-ext-for-jupyter/here_map_widget_ext_for_jupyter-0.0.1-py2.py3-none-any.whl/here_map_widget_ext/_version.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2019-2021 HERE Europe B.V.
+# SPDX-License-Identifier: MIT
+
+"""Project version information."""
+
+# Module version
+version_info = (0, 0, 1)
+
+# Module version accessible using here_map_widget_ext.__version__
+__version__ = "%s.%s.%s" % (
+    version_info[0],
+    version_info[1],
+    version_info[2],
+)
+
+EXTENSION_VERSION = "^0.0.1"
