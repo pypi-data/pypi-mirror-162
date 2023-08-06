368f07924a61c45abc043c9ef301f84046d49bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:00 +0000

    Add package glob-custom/1.0.1/glob-custom-1.0.1.tar.gz

---
+++ packages/glob-custom/glob-custom-1.0.1.tar.gz/glob-custom-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from io import open
+from setuptools import setup
+
+
+"""
+:authors: DarkRastafar
+:license: Apache License
+:copyright: (c) 2022 DarkRastafar
+"""
+
+version = '1.0.0'
+
+long_description = '''smth'''
+
+setup(
+    name='glob_custom',
+    version=version,
+    author='DarkRastafar',
+    author_email='alwaysintouch247@gmail.com',
+    description=('test'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/pypa/sampleproject",
+
+)
