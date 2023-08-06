6df07581b28eb07aa952328414441bd1c08a3808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package async-cloud-tasks/0.0.3/async_cloud_tasks-0.0.3-py3-none-any.whl

---
+++ packages/async-cloud-tasks/async_cloud_tasks-0.0.3-py3-none-any.whl/async_cloud_tasks/__init__.py
@@ -0,0 +1,14 @@
+from django.utils.module_loading import autodiscover_modules
+from .base import remote_task
+
+__version__ = '0.0.3'
+
+
+def autodiscover():
+    autodiscover_modules('cloud_tasks')
+
+
+default_app_config = 'async_cloud_tasks.apps.DCTConfig'
+
+create_remote_task = remote_task
+
