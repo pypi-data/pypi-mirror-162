19cbfa0b5922a29658024e532d2c58ba635839ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package bioregistry/0.5.59/bioregistry-0.5.59.tar.gz

---
+++ packages/bioregistry/bioregistry-0.5.59.tar.gz/bioregistry-0.5.59/src/bioregistry/collection_api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""API for collections."""
+
+from typing import Optional
+
+from .schema import Collection, Context
+from .schema_utils import read_collections, read_contexts
+
+__all__ = [
+    "get_collection",
+    "get_context",
+]
+
+
+def get_collection(identifier: str) -> Optional[Collection]:
+    """Get the collection entry for the given identifier."""
+    return read_collections().get(identifier)
+
+
+def get_context(identifier: str) -> Optional[Context]:
+    """Get the context for the given identifier."""
+    return read_contexts().get(identifier)
