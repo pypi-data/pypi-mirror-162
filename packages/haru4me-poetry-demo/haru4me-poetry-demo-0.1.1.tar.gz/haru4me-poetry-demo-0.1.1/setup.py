98261d447e300afdb1e81d3b0f016f6f9b9f5c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:13 +0000

    Add package haru4me-poetry-demo/0.1.1/haru4me-poetry-demo-0.1.1.tar.gz

---
+++ packages/haru4me-poetry-demo/haru4me-poetry-demo-0.1.1.tar.gz/haru4me-poetry-demo-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['haru4me_poetry_demo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.1,<2.0.0', 'pytest-reportlog>=0.1.2,<0.2.0']
+
+setup_kwargs = {
+    'name': 'haru4me-poetry-demo',
+    'version': '0.1.1',
+    'description': 'Poetry demo package',
+    'long_description': None,
+    'author': 'Haru4me',
+    'author_email': 'durdaev2010@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
