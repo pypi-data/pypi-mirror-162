7ee9f13a56d5c737c80286043e7ea5cc4e3af2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:02 +0000

    Add package MolGNN-try-more/0.0.1/MolGNN_try_more-0.0.1.tar.gz

---
+++ packages/MolGNN-try-more/MolGNN_try_more-0.0.1.tar.gz/MolGNN_try_more-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MolGNN_try_more',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Giulio Mattedi',
+    author_email='lxu110@stu.suda.edu.cn',
+    description='',
+)
