60c058419c6521ca09124ab33b17b62a71ba2d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:56 +0000

    Add package metricflow-lite/0.110.2.dev0/metricflow_lite-0.110.2.dev0-py3-none-any.whl

---
+++ packages/metricflow-lite/metricflow_lite-0.110.2.dev0-py3-none-any.whl/metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/BigQuerySqlClient/test_compute_metrics_node_simple_expr__plan0_optimized.sql
@@ -0,0 +1,24 @@
+-- Compute Metrics via Expressions
+SELECT
+  listing
+  , listing__country_latest
+  , booking_value * 0.05 AS booking_fees
+FROM (
+  -- Join Standard Outputs
+  -- Aggregate Measures
+  SELECT
+    bookings_source_src_10000.listing_id AS listing
+    , listings_latest_src_10003.country AS listing__country_latest
+    , SUM(bookings_source_src_10000.booking_value) AS booking_value
+  FROM (
+    -- User Defined SQL Query
+    SELECT * FROM ***************************.fct_bookings
+  ) bookings_source_src_10000
+  LEFT OUTER JOIN
+    ***************************.dim_listings_latest listings_latest_src_10003
+  ON
+    bookings_source_src_10000.listing_id = listings_latest_src_10003.listing_id
+  GROUP BY
+    listing
+    , listing__country_latest
+) subq_11
