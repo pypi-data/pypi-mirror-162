e82f9ca7408c2d5b412acc13071a6a16068ffeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:04 +0000

    Add package pdfcrrr/1.1/pdfcrrr-1.1.tar.gz

---
+++ packages/pdfcrrr/pdfcrrr-1.1.tar.gz/pdfcrrr-1.1/pdfcrrr/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text """
+
+    def convert(self, path):
+        """
+        convert the givern PDF to text.
+
+        Parameters:
+        path(str): the path to a PDF file.
+
+        Returns:
+        str: The content of PDF file as text.
+        """
+        print("PDF2text")
