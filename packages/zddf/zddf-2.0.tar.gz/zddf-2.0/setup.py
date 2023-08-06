a477d3b144a8f9ffee648939f6463427ad6938cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:16 +0000

    Add package zddf/2.0/zddf-2.0.tar.gz

---
+++ packages/zddf/zddf-2.0.tar.gz/zddf-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+setup(
+    name = "zddf",   # 对外的模块名字
+    version = "2.0",    # 版本号
+    description = "自动打分",   # 信息描述
+    author = "keyee",# 作者
+    author_email = "du1872779171@163.com",
+    py_modules=["ASS.ASS"] # 要发布的模块
+)
