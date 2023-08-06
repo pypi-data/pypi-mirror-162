145d25a38f4153c5c3c02dc9abe01846d733765a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:07 +0000

    Add package nicks-dominic-distributions-probability/1.1/nicks_dominic_distributions_probability-1.1.tar.gz

---
+++ packages/nicks-dominic-distributions-probability/nicks_dominic_distributions_probability-1.1.tar.gz/nicks_dominic_distributions_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'nicks_dominic_distributions_probability',
+      version='1.1',
+      description='Guassian and Binomial Distributions',
+      packages=['nicks_dsnd_probability'],
+      author = 'Nicholas Smith',
+      author_email='nicholas122008@hotmail.com',
+      zip_safe=False)
