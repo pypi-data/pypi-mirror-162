969bdaf51124214208d5acbdb10b861972500e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:40 +0000

    Add package airflow-aggua-plugin/1.0.4/airflow_aggua_plugin-1.0.4.tar.gz

---
+++ packages/airflow-aggua-plugin/airflow_aggua_plugin-1.0.4.tar.gz/airflow_aggua_plugin-1.0.4/airflow_aggua_plugin/config.py
@@ -0,0 +1,7 @@
+VIEW_BASE_ROUTE = "airflow_aggua_plugin"
+VIEW_BASE_URL = "/" + VIEW_BASE_ROUTE
+PLUGIN_NAME = "airflow_aggua_plugin"
+PLUGIN_VERSION = "1.0.4"
+AIRFLOW_UI_MENU_ENTRY = "Aggua API"
+AIRFLOW_UI_SUB_MENU_ENTRY = "Reference Docs"
+API_BASE_URL = "/api/experimental/aggua/"
