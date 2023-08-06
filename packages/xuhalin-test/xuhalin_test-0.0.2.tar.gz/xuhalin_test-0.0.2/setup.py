586f856948093024112a14145b189b9dad80987e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:37 +0000

    Add package xuhalin-test/0.0.2/xuhalin_test-0.0.2.tar.gz

---
+++ packages/xuhalin-test/xuhalin_test-0.0.2.tar.gz/xuhalin_test-0.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time: 2018-1-23 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+  name = "xuhalin_test",
+  version = "0.0.2",
+  keywords = ("pip", "test"),
+  description = "test",
+  long_description = "test",
+  license = "MIT Licence",
+
+  url = "http://180.169.109.94:3000/xuhailin/project_mock",
+  author = "xuhailiin",
+  author_email = "117570235@qq.com",
+
+  packages = find_packages(),
+  include_package_data = True,
+  platforms = "any",
+  install_requires = []
+)
---
+++ packages/xuhalin-test/xuhalin_test-0.0.2.tar.gz/xuhalin_test-0.0.2/xuhailin_test/xu_test.py
@@ -0,0 +1,7 @@
+
+
+def  xu_add(a, b):
+    return a+6
+
+def xu_minus(a,b):
+    return a-b
