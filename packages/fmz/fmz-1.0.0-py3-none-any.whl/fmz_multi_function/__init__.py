ce5b4ff02a00045a20df37cc499e13a77b3ee9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:38 +0000

    Add package fmz/1.0.0/fmz-1.0.0-py3-none-any.whl

---
+++ packages/fmz/fmz-1.0.0-py3-none-any.whl/fmz_multi_function/__init__.py
@@ -0,0 +1,6 @@
+#encodeing: utf-8
+
+from .requests_download import download
+
+
+__version__ = "1.0.0"
---
+++ packages/fmz/fmz-1.0.0-py3-none-any.whl/fmz_multi_function/requests_download.py
@@ -0,0 +1,21 @@
+#encodeing: utf-8
+
+
+import requests
+from tqdm import tqdm
+
+def download(url: str, fname: str):
+    resp = requests.get(url, stream=True)
+    total = int(resp.headers.get('content-length',0))
+    with open(fname, 'wb') as file, tqdm(
+        desc=fname,
+        total=total,
+        unit='B',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
+
+
