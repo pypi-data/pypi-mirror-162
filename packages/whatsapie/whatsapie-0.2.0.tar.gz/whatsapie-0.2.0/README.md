f10de41c221ee29225d41385b30d20bd71274bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:09 +0000

    Add package whatsapie/0.2.0/whatsapie-0.2.0.tar.gz

---
+++ packages/whatsapie/whatsapie-0.2.0.tar.gz/whatsapie-0.2.0/README.md
@@ -0,0 +1,27 @@
+# whatsapie
+
+Unofficial wrapper for Meta's [**Whatsapp Cloud API**](https://developers.facebook.com/docs/whatsapp/cloud-api) written in Python
+
+## INSTALL
+
+```console
+$ pip install whatsapie
+```
+
+!!! note
+
+    whatsapie requires python 3.10 or above
+
+## PREREQUISITES
+
+-   Follow Meta's [Whatsapp Cloud API Documentation](https://developers.facebook.com/docs/whatsapp/cloud-api) and obtain, meta business app [ACCESS_TOKEN](#) and [PHONE_NUMBER_ID]()
+
+## Documentation
+
+-   Get started [here](https://advrxh.github.io/whatsapie/get-started/)
+
+## CONTRIBUTION
+
+This repo is open for contribution. I'd love some contribution [here](https://github.com/advrxh/whatsapie)
+
+Maintained by [Aadil Varsh](https://advrxh.github.io)
---
+++ packages/whatsapie/whatsapie-0.2.0.tar.gz/whatsapie-0.2.0/whatsapie/__init__.py
@@ -0,0 +1,77 @@
+import asyncio
+
+import httpx
+from loguru import logger
+
+from whatsapie.constants import (META_GRAPH_API_ENDPOINT,
+                                 META_GRAPH_API_VERSION, REQUEST_HEADERS)
+from whatsapie.errors import ErrorResponse
+from whatsapie.ext.message import *
+from whatsapie.schema_generator import SchemaGenerator
+
+
+class Whatsapie:
+    """
+    Whatsapp Cloud API manager class, this class handles the functions of making
+    httpx to Meta's cloud api.
+    """
+
+    def __init__(self, access_token: str, phone_number_id: str, log:bool=False) -> None:
+        """Initializes the whatsapie manager class.
+
+        Args:
+            access_token: Meta's Whatsapp Cloud API user specific access token.
+            phone_number_id: Meta's Whatsapp Cloud API user specific phone number ID.
+
+        """
+        self.schema_generator = SchemaGenerator()
+        self.access_token = access_token
+        self.phone_number_id = phone_number_id
+        self.log = log
+
+        self.url = META_GRAPH_API_ENDPOINT.format(
+            version=META_GRAPH_API_VERSION, phone_number_id=self.phone_number_id
+        )
+
+    async def send(self, message: Message):
+        """Invoke this method from an instance to send a whatsapp message
+
+        Args:
+            message: Must be a type of Message Instance supports **TextMessage, LocationMessage, MediaMessage**
+
+        Returns:
+            A bool status of the performed api call
+
+        Raises:
+            ErrorResponse: If the Cloud API returns and error response
+        """
+
+
+        async with httpx.AsyncClient(base_url=self.url) as client:
+            await self.post(client=client, message=message)
+
+    async def post(self, client, message):
+        body = self.schema_generator.generate(message, dump_json_str=True)
+
+        headers = REQUEST_HEADERS
+        headers["Authorization"] = f"Bearer {self.access_token}"
+
+        response = await client.post(url="/", data=body, headers=headers)
+
+        if response.status_code == 200:
+            if self.log:
+                logger.info(f"OK | {response.status_code} | {response.text}") 
+            return True
+
+        raise ErrorResponse(response)
+
+    async def send_group(self, message_group:MessageGroup):
+
+        async with httpx.AsyncClient(base_url=self.url) as client:
+            tasks = []
+            for message in message_group.message_list:
+                tasks.append(
+                    asyncio.create_task(self.post(client=client, message=message))
+                )
+
+            await asyncio.gather(*tasks)
