6affd18aabb9a3d0235472ab8621f6866c9e4854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:54 +0000

    Add package zyr-distributions/0.2/zyr-distributions-0.2.tar.gz

---
+++ packages/zyr-distributions/zyr-distributions-0.2.tar.gz/zyr-distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zyr-distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      author = 'Richard',
+      author_email = 'izyrichy@gmail.com',
+      packages=['zyr-distributions'],
+      zip_safe=False)
