0be0929e8a735a8c778fbf3be3b12570652fb33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:40 +0000

    Add package pynetsys/0.0.7/pynetsys-0.0.7-py3-none-any.whl

---
+++ packages/pynetsys/pynetsys-0.0.7-py3-none-any.whl/pynetsys/__init__.py
@@ -0,0 +1,21 @@
+"""pynetsys is a collection of tools and malicious packets."""
+
+from .constants import Protocol, Packets
+from . import _tool, _packet
+
+__version__ = []
+
+tool, packet = _tool, _packet
+
+def isOnline(Address: str):
+    # IMPORT
+    from scapy.layers.inet import IP
+
+    # CREATING PACKET & RUN & RETURN
+    try:
+        IP(dst = Address)
+        return True
+    except:
+        return False
+    
+
