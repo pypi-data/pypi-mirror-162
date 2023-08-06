abfe2d216eea8a3f1fa914e70783c805324796c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package scrapeanything/0.9.104/scrapeanything-0.9.104-py3-none-any.whl

---
+++ packages/scrapeanything/scrapeanything-0.9.104-py3-none-any.whl/scrapeanything/utils/config.py
@@ -0,0 +1,28 @@
+from configparser import ConfigParser
+
+class Config:
+
+    def __init__(self, path: str):
+        self.config = ConfigParser()
+        # read config.ini file
+        self.config.read(path)
+
+    def get(self, section: str, key: str, default: any=None):
+        # get config value
+        if section in self.config and key in self.config[section]:
+            response = self.config[section][key]
+
+            if response == 'True':
+                return True
+            elif response == 'False':
+                return False
+            else:
+                return response
+
+        elif (section in self.config and key not in self.config[section]) or (section not in self.config):
+            if default is not None:
+                return default
+            else:
+                return None
+        else:
+            raise Exception('Configuration not found!')
