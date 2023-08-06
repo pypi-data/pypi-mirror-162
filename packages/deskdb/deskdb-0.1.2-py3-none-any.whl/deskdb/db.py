7cc66d39151d1215db2b342dfa43f31a8a02456e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package deskdb/0.1.2/deskdb-0.1.2-py3-none-any.whl

---
+++ packages/deskdb/deskdb-0.1.2-py3-none-any.whl/deskdb/db.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+class DataStoreItem():
+	def __init__(self, name, db_name):
+		self.name = name
+		self.db_name = db_name
+		self.path = Path(f"{self.db_name}//{self.name}")
+
+		self.path.mkdir(exist_ok = True)
+
+	def read(self, value_name):
+		with open(f"{self.db_name}/{self.name}/{value_name}.data", "r") as file:
+			result = file.readlines()[0]
+
+			if result.isdigit():
+				return int(result)
+			else:
+				return result
+
+	def write(self, value_name, value):
+		with open(f"{self.db_name}/{self.name}/{value_name}.data", "w") as file:
+			file.write(str(value))
+
+	def is_valid(self, value_name):
+		try:
+			with open(f"{self.db_name}/{self.name}/{value_name}.data", "r") as file:
+				return True
+		except:
+			return False
+
+class DataStore():
+	def __init__(self, name):
+		self.name = name
+		self.path = Path(self.name)
+
+		self.path.mkdir(exist_ok = True)
+
+	def get_item(self, item_name):
+		return DataStoreItem(item_name, self.name)
