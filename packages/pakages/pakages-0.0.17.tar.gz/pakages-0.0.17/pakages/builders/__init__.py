efb40445c4c80d5dae74b8872d5e273ae7f59ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:41 +0000

    Add package pakages/0.0.17/pakages-0.0.17.tar.gz

---
+++ packages/pakages/pakages-0.0.17.tar.gz/pakages-0.0.17/pakages/builders/__init__.py
@@ -0,0 +1,15 @@
+from .python import PythonPackage
+from pakages.logger import logger
+import os
+
+
+def get_package(root):
+    """
+    Get a packager based on a context
+    """
+    if isinstance(root, list):
+        root = root[0]
+    if os.path.exists(root):
+        if PythonPackage.matches(root):
+            return PythonPackage(root)
+    logger.exit(f"We currently don't support a builder for content in {root}")
---
+++ packages/pakages/pakages-0.0.17.tar.gz/pakages-0.0.17/pakages/utils/__init__.py
@@ -0,0 +1,27 @@
+from .terminal import (
+    run_command,
+    check_install,
+    get_installdir,
+    get_userhome,
+    get_user,
+    which,
+    stream_command,
+    confirm_action,
+)
+from .command import Command
+from .fileio import (
+    copyfile,
+    get_file_hash,
+    get_tmpdir,
+    get_tmpfile,
+    mkdir_p,
+    mkdirp,
+    print_json,
+    read_file,
+    read_json,
+    read_yaml,
+    recursive_find,
+    workdir,
+    write_file,
+    write_json,
+)
