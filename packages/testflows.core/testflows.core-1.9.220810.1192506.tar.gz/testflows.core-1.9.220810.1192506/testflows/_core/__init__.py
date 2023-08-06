b649268cf5301ccf4f5ca4277af4c1007e8aa349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:36 +0000

    Add package testflows.core/1.9.220810.1192506/testflows.core-1.9.220810.1192506.tar.gz

---
+++ packages/testflows.core/testflows.core-1.9.220810.1192506.tar.gz/testflows.core-1.9.220810.1192506/testflows/_core/__init__.py
@@ -0,0 +1,39 @@
+# Copyright 2019-2021 Katteli Inc.
+# TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+__author__ = "Vitaliy Zakaznikov"
+__version__ = "1.9.220810.1192506"
+__repository__ = "https://github.com/testflows/TestFlows-Core.git"
+__commit__ = "d3d9c9ab103f8278b1fc3e02675c2118d7e0fcd1"
+__branch__ = "master"
+__license__ = f"""
+Copyright 2019-{datetime.now().year} Katteli Inc.
+TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+See testflows/_core/contrib folder in sources for lincenses of each third-party module.
+"""
