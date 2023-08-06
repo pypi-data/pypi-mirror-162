4747e95b8b88e550616ab5f559a69285568c3ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:15 +0000

    Add package ansys-dpf-core/0.5.2/ansys-dpf-core-0.5.2.tar.gz

---
+++ packages/ansys-dpf-core/ansys-dpf-core-0.5.2.tar.gz/ansys-dpf-core-0.5.2/ansys/dpf/core/operators/__init__.py
@@ -0,0 +1,21 @@
+"""
+.. _ref_operators_package:
+
+Operators
+---------
+"""
+
+from . import result
+from . import math
+from . import utility
+from . import min_max
+from . import scoping
+from . import metadata
+from . import logic
+from . import mesh
+from . import filter
+from . import serialization
+from . import geo
+from . import averaging
+from . import invariant
+from . import mapping
