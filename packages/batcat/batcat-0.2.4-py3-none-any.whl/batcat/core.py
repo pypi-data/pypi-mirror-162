21d8e9643dfc3b4ad6179751ab93c52b4b51deb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:57 +0000

    Add package batcat/0.2.4/batcat-0.2.4-py3-none-any.whl

---
+++ packages/batcat/batcat-0.2.4-py3-none-any.whl/batcat/core.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+author:     Ewen Wang
+email:      wolfgangwong2012@gmail.com
+license:    Apache License 2.0
+"""
+import pandas as pd
+
+def get_date_with_delta(delta, format='%Y/%m/%d'):
+    """ Get the date delta days ago.
+
+    Args:
+        delta: the number of days ago, int
+    
+    Returns:
+        date: strftime('%Y/%m/%d')
+    """
+    from datetime import date, timedelta
+    return (date.today() - timedelta(days=delta)).strftime(format)
+
+def read_data_from_bd(query, 
+                      host,
+                      user, 
+                      port,
+                      database,
+                      password) -> pd.DataFrame:
+    """Read data from a database.
+
+    Args:
+        query: sql 
+        username: database username
+        password: database password
+
+    Returns:    
+        df: dataframe
+    """
+    import pymysql
+    
+    connection = pymysql.connect(host=host,
+                                 user=user,
+                                 port=port,
+                                 db=database,
+                                 password=password)
+    
+    df = pd.read_sql(query, connection)
+    return df
+
+if __name__ == '__main__':
+    main()
