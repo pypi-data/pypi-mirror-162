78494a5f285fb579a81d18f0d56d12c38ab001e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package multi-request/0.0.7/multi_request-0.0.7.tar.gz

---
+++ packages/multi-request/multi_request-0.0.7.tar.gz/multi_request-0.0.7/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()  
+    long_description=long_description
+    long_description_content_type="text/markdown"
+
+
+sdict = {
+    'name': 'multi_request',
+    'version': "0.0.7",
+    'packages': ['multi_request'],
+    'zip_safe': False,
+    'install_requires': [
+        'pandas==1.3.4',
+        'requests==2.26.0'
+    ],
+    'author': 'Zhao Xu',
+    'classifiers': [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python'
+    ],
+    'long_description': long_description,
+    'long_description_content_type': long_description_content_type
+}
+
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
