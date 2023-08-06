8579dc90690ad0aa4e9169f0c7fd2a666c79f78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:27 +0000

    Add package cloudtipsadp/0.4.1/cloudtipsadp-0.4.1.tar.gz

---
+++ packages/cloudtipsadp/cloudtipsadp-0.4.1.tar.gz/cloudtipsadp-0.4.1/src/cloudtipsadp/__init__.py
@@ -0,0 +1,11 @@
+
+"""Top-level package for cloudtipsadp API Python Client Library."""
+from cloudtipsadp.accumulations import Accumulations
+from cloudtipsadp.cards import Cards
+from cloudtipsadp.clients import Connect, ProductClient, SandboxClient
+from cloudtipsadp.configuration import Configuration, Token
+from cloudtipsadp.places import Places
+from cloudtipsadp.receivers import Receivers
+
+__author__ = "SETTER"
+__email__ = 'lphp@mail.ru'
