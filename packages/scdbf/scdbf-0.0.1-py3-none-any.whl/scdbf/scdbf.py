7d35e15a03bf1f4c51c36a5ce99418c5f52c3ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:48 +0000

    Add package scdbf/0.0.1/scdbf-0.0.1-py3-none-any.whl

---
+++ packages/scdbf/scdbf-0.0.1-py3-none-any.whl/scdbf/scdbf.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# coding: utf-8
+from __future__ import print_function
+
+import sys
+import csv
+from dbfread import DBF
+
+# for record in DBF('/home/ambagasdowa/Documents/devold/radiobase_old/db/veracruz.dbf'):
+#    print(record)
+
+
+# table = DBF('/home/ambagasdowa/Documents/devold/radiobase_old/db/veracruz.dbf')
+
+
+def show(*words):
+    print('  ' + ' '.join(str(word) for word in words))
+
+
+def show_field(field):
+    print('    {} ({} {})'.format(field.name, field.type, field.length))
+
+
+def info():
+    for filename_info in sys.argv[1:]:
+
+        #        print(filename_info + ':')
+        table_info = DBF(filename_info, ignore_missing_memofile=True)
+#        show('Name:', table_info.name)
+#        show('Memo File:', table_info.memofilename or '')
+#        show('DB Version:', table_info.dbversion)
+#        show('Records:', len(table_info))
+#        show('Deleted Records:', len(table_info.deleted))
+#        show('Last Updated:', table_info.date)
+#        show('Character Encoding:', table_info.encoding)
+#        show('Fields:')
+#        for field in table_info.fields:
+#            show_field(field)
+        if(table_info.encoding == 'ascii'):
+            return 'cp850'
+        else:
+            return table_info.encoding
+
+
+def main():
+    for filename in sys.argv[1:]:
+        table = DBF(filename, ignore_missing_memofile=True,
+                    encoding=info())
+        print(info())
+
+        writer = csv.writer(sys.stdout)
+        writer.writerow(table.field_names)
+        for record in table:
+            writer.writerow(list(record.values()))
+
+
+main()
