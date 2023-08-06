0ec0c9ccf3e3a4635854a244214625f79407534a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:08 +0000

    Add package strawberry-graphql/0.124.0.dev1659690947/strawberry-graphql-0.124.0.dev1659690947.tar.gz

---
+++ packages/strawberry-graphql/strawberry-graphql-0.124.0.dev1659690947.tar.gz/strawberry-graphql-0.124.0.dev1659690947/strawberry/federation/__init__.py
@@ -0,0 +1,21 @@
+from .enum import enum, enum_value
+from .field import field
+from .mutation import mutation
+from .object_type import input, interface, type
+from .scalar import scalar
+from .schema import Schema
+from .union import union
+
+
+__all__ = [
+    "enum",
+    "enum_value",
+    "field",
+    "mutation",
+    "input",
+    "interface",
+    "type",
+    "scalar",
+    "Schema",
+    "union",
+]
