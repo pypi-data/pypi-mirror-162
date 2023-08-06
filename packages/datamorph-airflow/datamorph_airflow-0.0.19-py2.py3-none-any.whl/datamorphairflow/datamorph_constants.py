1375a37e4999a170b392a877b3f8a593cb291911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package datamorph-airflow/0.0.19/datamorph_airflow-0.0.19-py2.py3-none-any.whl

---
+++ packages/datamorph-airflow/datamorph_airflow-0.0.19-py2.py3-none-any.whl/datamorphairflow/datamorph_constants.py
@@ -0,0 +1,20 @@
+"""
+DataMorph constants are maintained in this file
+"""
+DATAMORPH_UI_PROPERTY_ID: str = "id"
+DATAMORPH_PROPERTY_DEPENDSON:str = "dependsOn"
+DATAMORPH_PROPERTY_WORKFLOW: str = "workflow"
+DATAMORPH_PROPERTY_DATAMORPHCONF: str = "datamorphConf"
+DATAMORPH_PROPERTY_SUBSTITUTIONS: str = "substitutions"
+DATAMORPH_PROPERTY_PARAMETERS: str = "parameters"
+DATAMORPH_PROPERTY_CONNECTIONS: str = "connections"
+DATAMORPH_PROPERTIES: str = "properties"
+DATAMORPH_PROPERTY_NAME: str = "name"
+
+
+DATAMORPH_PREFIX = "datamorph_"
+
+NODE_SUCCESS: str = "_Success"
+NODE_FAILURE: str = "_Failure"
+
+DEFAULT_AWS_REGION: str = "us-west-2"
