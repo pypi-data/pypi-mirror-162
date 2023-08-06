91155699c443004b3e42cf994b7601963ba048fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:14 +0000

    Add package razator-utils/0.1.1/razator_utils-0.1.1-py2.py3-none-any.whl

---
+++ packages/razator-utils/razator_utils-0.1.1-py2.py3-none-any.whl/razator_utils/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Razator Utils."""
+
+__author__ = """Ryan Scott"""
+__email__ = 'ryan.t.scott73@gmail.com'
+__version__ = '0.1.1'
+
+from razator_utils.razator_utils import *
+from razator_utils import *
+
+__all__ = ['batchify', 'camel_to_snake', 'log']
---
+++ packages/razator-utils/razator_utils-0.1.1-py2.py3-none-any.whl/razator_utils/razator_utils.py
@@ -0,0 +1,13 @@
+"""Main module."""
+
+
+def batchify(iterable, n=1):
+    iter_len = len(iterable)
+    for ndx in range(0, iter_len, n):
+        yield iterable[ndx:min(ndx + n, iter_len)]
+
+
+def camel_to_snake(camel_str):
+    import re
+    snake_str = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', camel_str)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', snake_str).lower()
