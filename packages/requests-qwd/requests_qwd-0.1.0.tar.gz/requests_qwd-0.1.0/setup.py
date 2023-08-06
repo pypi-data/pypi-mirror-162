918414ec58d8bf17f2c6f6c08c044ae3e1367981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package requests-qwd/0.1.0/requests_qwd-0.1.0.tar.gz

---
+++ packages/requests-qwd/requests_qwd-0.1.0.tar.gz/requests_qwd-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import setuptools
+with open("README.md", "r",encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='requests_qwd',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/pypa/sampleproject',
+    license='MIT',
+    author='zyb',
+    author_email='1052350468@qq.com',
+    description='A small package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
