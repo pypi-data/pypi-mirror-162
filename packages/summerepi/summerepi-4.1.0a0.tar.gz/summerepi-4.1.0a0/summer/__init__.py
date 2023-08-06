085519b56a0ac386ac3ea7ed67a83dd126ca3c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:06 +0000

    Add package summerepi/4.1.0a0/summerepi-4.1.0a0.tar.gz

---
+++ packages/summerepi/summerepi-4.1.0a0.tar.gz/summerepi-4.1.0a0/summer/__init__.py
@@ -0,0 +1,5 @@
+from .adjust import Multiply, Overwrite
+from .compartment import Compartment
+from .model import CompartmentalModel
+from .stratification import AgeStratification, StrainStratification, Stratification
+from .jaxify import get_using_jax, set_using_jax, has_jax
