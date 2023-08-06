1055d3929736d34106b03a049981fd322eeb7ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:41 +0000

    Add package cuaca/0.2.0/cuaca-0.2.0.tar.gz

---
+++ packages/cuaca/cuaca-0.2.0.tar.gz/cuaca-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cuaca']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cuaca',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'sweemeng',
+    'author_email': 'sweester@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
