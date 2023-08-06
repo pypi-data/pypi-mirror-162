2071e7836fe0ba6ee3e5fc0a81f89823c3308b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:29 +0000

    Add package probdistri-gausbino/0.1/probdistri_gausbino-0.1.tar.gz

---
+++ packages/probdistri-gausbino/probdistri_gausbino-0.1.tar.gz/probdistri_gausbino-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probdistri_gausbino',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probdistri_gausbino'],
+      zip_safe=False)
