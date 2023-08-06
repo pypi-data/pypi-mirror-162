d551be683f94ea8985457c68b072fc70eba5ca7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:11 +0000

    Add package abqpy/2017a6/abqpy-2017a6.tar.gz

---
+++ packages/abqpy/abqpy-2017a6.tar.gz/abqpy-2017a6/src/abaqus/PlotOptions/TieConstraint.py
@@ -0,0 +1,24 @@
+class TieConstraint:
+    """The TieConstraint object.
+
+    .. note:: 
+        This object can be accessed by:
+
+        .. code-block:: python
+
+            import visualization
+            session.odbData[name].ties[i]
+    """
+
+    #: A String specifying the tie constraint name. This attribute is read-only.
+    name: str = ""
+
+    def constraintData(self):
+        """This method returns constraint data if any are associated with the object
+
+        Returns
+        -------
+        tuple[float, ...]
+            A tuple containing coordinates of the nodes pertaining to the constraint.
+        """
+        pass
