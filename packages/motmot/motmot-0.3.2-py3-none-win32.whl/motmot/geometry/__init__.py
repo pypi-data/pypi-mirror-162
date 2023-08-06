72c4b7ab25a5ac7a97e13ad760dd623d0f334abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:11 +0000

    Add package motmot/0.3.2/motmot-0.3.2-py3-none-win32.whl

---
+++ packages/motmot/motmot-0.3.2-py3-none-win32.whl/motmot/geometry/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from ._geometry import (
+    area,
+    center_of_mass,
+    closest,
+    furthest,
+    get_components,
+    get_components_zipped,
+    inner_product,
+    magnitude,
+    magnitude_sqr,
+    normalise,
+    normalised,
+    orthogonal_bases,
+    snap_to_plane,
+    unzip,
+    zip,
+)
+from ._geometry import _reduce_last_axis
+from ._unit_vector import UnitVector
