acb5416291336138688ea25f1960a03e60116cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package divewidgets/0.1.5/divewidgets-0.1.5-py2.py3-none-any.whl

---
+++ packages/divewidgets/divewidgets-0.1.5-py2.py3-none-any.whl/divewidgets/_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Chung Chan.
+# Distributed under the terms of the Modified BSD License.
+
+# version_info = (0, 1, 2, 'dev', 0)
+version_info = (0, 1, 4)
+__version__ = ".".join(map(str, version_info))
