a1c790fccf29ae6d9d0546bb3ba1a13dd35fede3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:45 +0000

    Add package pkgcore/0.12.13/pkgcore-0.12.13.tar.gz

---
+++ packages/pkgcore/pkgcore-0.12.13.tar.gz/pkgcore-0.12.13/tests/cache/test_flat_hash.py
@@ -0,0 +1,24 @@
+from pkgcore.cache import flat_hash
+from snakeoil.test.mixins import TempDirMixin
+
+from . import test_base
+from .test_util import GenericCacheMixin
+
+
+class db(flat_hash.database):
+
+    def __setitem__(self, cpv, data):
+        data['_chf_'] = test_base._chf_obj
+        return flat_hash.database.__setitem__(self, cpv, data)
+
+    def __getitem__(self, cpv):
+        d = dict(flat_hash.database.__getitem__(self, cpv).items())
+        d.pop(f'_{self.chf_type}_', None)
+        return d
+
+
+class TestFlatHash(GenericCacheMixin, TempDirMixin):
+
+    def get_db(self, readonly=False):
+        return db(self.dir,
+            auxdbkeys=self.cache_keys, readonly=readonly)
