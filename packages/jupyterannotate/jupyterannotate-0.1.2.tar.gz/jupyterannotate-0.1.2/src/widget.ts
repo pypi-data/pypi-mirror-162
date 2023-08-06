376ef2643deffc001ae32335e78dd3fd1f4f0dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:11 +0000

    Add package jupyterannotate/0.1.2/jupyterannotate-0.1.2.tar.gz

---
+++ packages/jupyterannotate/jupyterannotate-0.1.2.tar.gz/jupyterannotate-0.1.2/src/widget.ts
@@ -0,0 +1,72 @@
+// Copyright (c) Stuart Quin
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  DOMWidgetModel,
+  DOMWidgetView,
+  ISerializers,
+  WidgetModel,
+} from "@jupyter-widgets/base";
+
+import { h, render } from "preact";
+
+import { MODULE_NAME, MODULE_VERSION } from "./version";
+import { Span } from "./annotate";
+import Annotate from "./components/Annotate";
+// Import the CSS
+import "../css/widget.css";
+
+export class AnnotateModel extends DOMWidgetModel {
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+    // Add any extra serializers here
+  };
+
+  static model_name = "AnnotateModel";
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+  static view_name = "AnnotateView"; // Set to null if no view
+  static view_module = MODULE_NAME; // Set to null if no view
+  static view_module_version = MODULE_VERSION;
+}
+
+export class AnnotateView extends DOMWidgetView {
+  render(): void {
+    const docs = this.model.get("docs") || [];
+    const labels = this.model.get("labels");
+    const initialSpans = this.model.get("spans") || [];
+
+    const registerSpanChangeCallback = (
+      callback: (spans: Span[][]) => void
+    ) => {
+      this.model.on(
+        "change:spans",
+        (model: WidgetModel) => {
+          if (callback) {
+            callback(model.changed.spans);
+          }
+        },
+        this
+      );
+    };
+
+    const app = h(
+      "div",
+      { className: "app" },
+      h(Annotate, {
+        docs,
+        registerSpanChangeCallback,
+        labels,
+        initialSpans,
+        onUpdateSpans: (spans: Span[][]) => this.handleChange(spans),
+      })
+    );
+
+    render(app, this.el);
+  }
+
+  handleChange(spans: Span[][]): void {
+    this.model.set("spans", spans);
+    this.model.save_changes();
+  }
+}
