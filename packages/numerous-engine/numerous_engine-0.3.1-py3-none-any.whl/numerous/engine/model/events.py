8911b6e17636c49b7098bec54fc0c59efc27269c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package numerous-engine/0.3.1/numerous_engine-0.3.1-py3-none-any.whl

---
+++ packages/numerous-engine/numerous_engine-0.3.1-py3-none-any.whl/numerous/engine/model/events.py
@@ -0,0 +1,100 @@
+import ast
+import inspect
+
+import numpy.typing as npt
+import numpy as np
+from numba.core.registry import CPUDispatcher
+
+from numerous.engine.model.utils import njit_and_compile_function
+from numerous.engine.numerous_event import NumerousEvent
+
+
+def generate_event_condition_ast(event_functions: list[NumerousEvent],
+                                 from_imports: list[tuple[str, str]]) -> tuple[list[CPUDispatcher], npt.ArrayLike]:
+    array_label = "result"
+    directions_array = []
+    body = [ast.Assign(targets=[ast.Name(id=array_label)], lineno=0,
+                       value=ast.List(elts=[], ctx=ast.Load()))]
+    compiled_functions = {}
+    for event in event_functions:
+        if event.compiled_functions:
+            compiled_functions.update(event.compiled_functions)
+        directions_array.append(event.direction)
+        body.append(event.condition)
+        body.append(ast.Expr(value=ast.Call(
+            func=ast.Attribute(value=ast.Name(id=array_label, ctx=ast.Load()), attr='append', ctx=ast.Load()),
+            args=[ast.Call(func=ast.Name(id=event.condition.name, ctx=ast.Load()),
+                           args=[ast.Name(id='t', ctx=ast.Load()), ast.Name(id='states', ctx=ast.Load())],
+                           keywords=[])], keywords=[])))
+
+    body.append(ast.Return(value=ast.Call(func=ast.Attribute(value=ast.Name(id='np',
+                                                                            ctx=ast.Load()), attr='array',
+                                                             ctx=ast.Load()),
+                                          args=[ast.Name(id=array_label, ctx=ast.Load()),
+                                                ast.Attribute(value=ast.Name(id='np', ctx=ast.Load()), attr='float64',
+                                                              ctx=ast.Load())],
+                                          keywords=[])))
+    body_r = ast.FunctionDef(name='condition_list',
+                             args=ast.arguments(posonlyargs=[], args=[ast.arg(arg='t'), ast.arg(arg='states')],
+                                                kwonlyargs=[], kw_defaults=[], defaults=[]),
+                             body=body, decorator_list=[], lineno=0)
+
+    return [njit_and_compile_function(body_r, from_imports, compiled_functions=compiled_functions)], np.array(
+        directions_array, dtype=np.float)
+
+
+class VariablesVisitor(ast.NodeVisitor):
+    def __init__(self, path_to_root_str, model, idx_type):
+        self.path_to_root_str = path_to_root_str
+        self.model = model
+        self.idx_type = idx_type
+
+    def generic_visit(self, node):
+        if isinstance(node, ast.Subscript) and isinstance(node.slice, ast.Constant):
+            if isinstance(node.slice.value, str):
+                for (var_path, var) in self.model.path_to_variable.items():
+                    if var_path.startswith(self.path_to_root_str):
+                        var_path = var_path[len(self.path_to_root_str):]
+                    if var_path == node.slice.value:
+                        node.slice.value = self.model._get_var_idx(var, self.idx_type)[0]
+            if isinstance(node.slice.value, str):
+                raise KeyError(f'No such variable: {node.slice.value}')
+
+        ast.NodeVisitor.generic_visit(self, node)
+
+
+def _replace_path_strings(model, function, idx_type, path_to_root=[]):
+    if hasattr(function, 'lines'):
+        lines = function.lines
+    else:
+        lines = inspect.getsource(function)
+    path_to_root_str = ".".join(path_to_root) + "."
+    func = ast.parse(lines.strip()).body[0]
+    VariablesVisitor(path_to_root_str, model, idx_type).generic_visit(func)
+    return func
+
+
+def generate_event_action_ast(event_functions: list[NumerousEvent],
+                              from_imports: list[tuple[str, str]]) -> CPUDispatcher:
+    body = []
+    compiled_functions = {}
+    for idx, event in enumerate(event_functions):
+        if event.compiled_functions:
+            compiled_functions.update(event.compiled_functions)
+        body.append(event.action)
+        body.append(ast.If(test=ast.Compare(left=ast.Name(id='a_idx', ctx=ast.Load()), ops=[ast.Eq()],
+                                            comparators=[ast.Constant(value=idx)]),
+                           body=[ast.Expr(value=ast.Call(func=ast.Name(id=event.action.name, ctx=ast.Load()),
+                                                         args=[ast.Name(id='t', ctx=ast.Load()),
+                                                               ast.Name(id='states', ctx=ast.Load())],
+                                                         keywords=[]))], orelse=[]))
+
+    body.append(ast.Return(value=ast.Name(id='states', ctx=ast.Load())))
+
+    body_r = ast.FunctionDef(name='action_list',
+                             args=ast.arguments(posonlyargs=[], args=[ast.arg(arg='t'), ast.arg(arg='states'),
+                                                                      ast.arg(arg='a_idx')],
+                                                kwonlyargs=[], kw_defaults=[], defaults=[]),
+                             body=body, decorator_list=[], lineno=0)
+
+    return njit_and_compile_function(body_r, from_imports, compiled_functions=compiled_functions)
---
+++ packages/numerous-engine/numerous_engine-0.3.1-py3-none-any.whl/numerous/engine/model/lowering/utils.py
@@ -0,0 +1,156 @@
+import logging
+from enum import IntEnum, unique
+
+import ast
+
+
+@unique
+class VarTypes(IntEnum):
+    CONSTANT = 0
+    PARAMETER = 1
+    DERIV = 2
+    STATE = 3
+    LOCAL = 4
+    TMP = 5
+
+
+def wrap_module(body):
+    mod = ast.Module()
+    for i in body:
+        if not hasattr(i, "_fields"):
+            setattr(i, "_fields", [])
+    mod.body = body
+    mod.type_ignores = []
+    setattr(mod, "_fields", [])
+    return mod
+
+
+def generate_code_file(mod_body, file, imports, external_functions_source=False, names="#"):
+    for (module, name) in imports.as_imports:
+        mod_body.insert(0, ast.Import(names=[ast.alias(name=module, asname=name)], level=0))
+    for (module, name) in imports.from_imports:
+        mod_body.insert(0, ast.ImportFrom(module=module, names=[ast.alias(name=name, asname=None)], level=0))
+    if external_functions_source:
+        mod_body.insert(0, ast.ImportFrom(module=external_functions_source, names=[ast.alias(name='*', asname=None)],
+                                          level=0))
+
+    mod = wrap_module(mod_body)
+    logging.info('Generating Source')
+    source = names + ast.unparse(mod)
+    return source
+
+
+# Define helper functions to check the variables and sub lists
+def non_unique_check(listname_, list_):
+    if len(list_) > len(set(list_)):
+        import collections
+        raise ValueError(
+            f'Non unique {listname_}: {[item for item, count in collections.Counter(list_).items() if count > 1]}')
+
+
+def contains_dot(str_list):
+    for s in str_list:
+        if '.' in s:
+            raise ValueError(f'. in {s}')
+
+
+def are_all_scalars(str_list, scalar_variables):
+    for s in str_list:
+        if not s in scalar_variables:
+            raise ValueError(f'Not a scalar: {s}')
+
+
+def are_all_set_variables(str_list, set_variables):
+    for s in str_list:
+        if not s in set_variables:
+            raise ValueError(f'Not a set variable: {s}')
+
+
+class Vardef:
+    def __init__(self, llvm=True):
+        self.vars_inds_map = []
+        self.targets = []
+        self.args = []
+        self.llvm = llvm
+        self.args_order = []
+        self.trgs_order = []
+
+    def format(self, var, read=True):
+        if read:
+            _ctx = ast.Load()
+        else:
+            _ctx = ast.Store()
+        return ast.Name(id=var.replace('scope.', 's_'), lineno=0, col_offset=0, ctx=_ctx)
+
+    def format_target(self, var, read):
+        if read:
+            _ctx = ast.Load()
+        else:
+            _ctx = ast.Store()
+        if self.llvm:
+            return ast.Subscript(
+                slice=ast.Index(value=ast.Constant(value=0, lineno=0, col_offset=0), lineno=0, col_offset=0),
+                value=ast.Call(
+                    args=[ast.Name(id=var.replace('scope.', 's_'), lineno=0, col_offset=0, ctx=ast.Load()),
+                          ast.Tuple(ctx=ast.Load(), elts=[ast.Constant(value=1, lineno=0, col_offset=0)], lineno=0,
+                                    col_offset=0)],
+                    func=ast.Name(id='carray', lineno=0, col_offset=0, ctx=ast.Load()),
+                    keywords=[], lineno=0, col_offset=0), lineno=0, col_offset=0, ctx=_ctx)
+        else:
+            return ast.Name(id=var.replace('scope.', 's_'), lineno=0, col_offset=0, ctx=_ctx)
+
+    def order_variables(self, order_data):
+        for (var, var_id, used) in order_data:
+            if used:
+                self.args_order.append("scope." + var)
+            else:
+                self.args_order.append(var_id)
+
+        for (var, var_id, used) in order_data:
+            tmp_v = "scope." + var
+            if tmp_v in self.targets:
+                self.trgs_order.append(tmp_v)
+
+    def var_def(self, var, ctxread, read=True):
+        if not var in self.vars_inds_map:
+            self.vars_inds_map.append(var)
+        if read and 'scope.' in var:
+            if var not in self.targets and var not in self.args:
+                self.args.append(var)
+        elif 'scope.' in var:
+
+            if var not in self.targets:
+                self.targets.append(var)
+
+        if var in self.targets:
+            return self.format_target(var, ctxread)
+        else:
+            return self.format(var, ctxread)
+
+    def get_order_args(self, form=True):
+        if form:
+            result = [self.format(a, False) for a in self.args_order]
+        else:
+            result = self.args
+        result_2 = []
+        for name in result:
+            result_2.append(ast.arg(arg=name.id, lineno=0, col_offset=0))
+        return result_2
+
+    def get_order_trgs(self, form=True):
+        if form:
+            return [self.format(a, False) for a in self.trgs_order]
+        else:
+            return self.args
+
+    def get_args(self, form=True):
+        if form:
+            return [self.format(a, False) for a in self.args]
+        else:
+            return self.args
+
+    def get_targets(self, form=True):
+        if form:
+            return [self.format(a, False) for a in self.targets]
+        else:
+            return self.targets
