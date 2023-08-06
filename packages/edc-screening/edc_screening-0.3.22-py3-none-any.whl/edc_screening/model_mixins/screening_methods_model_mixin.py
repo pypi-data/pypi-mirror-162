5fdda408318ca76fc9feac6d75cfb79375dbb154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:20 +0000

    Add package edc-screening/0.3.22/edc_screening-0.3.22-py3-none-any.whl

---
+++ packages/edc-screening/edc_screening-0.3.22-py3-none-any.whl/edc_screening/model_mixins/screening_methods_model_mixin.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+from django.db import models
+
+from ..stubs import SubjectScreeningModelStub
+
+
+class ScreeningMethodsModeMixin(models.Model):
+    def __str__(self: SubjectScreeningModelStub):
+        return f"{self.screening_identifier} {self.gender} {self.age_in_years}"
+
+    def natural_key(self: SubjectScreeningModelStub):
+        return (self.screening_identifier,)  # noqa
+
+    @staticmethod
+    def get_search_slug_fields():
+        return ["screening_identifier", "subject_identifier", "reference"]
+
+    @property
+    def estimated_dob(self: SubjectScreeningModelStub) -> date:
+        return self.report_datetime.date() - relativedelta(years=self.age_in_years)
+
+    class Meta:
+        abstract = True
