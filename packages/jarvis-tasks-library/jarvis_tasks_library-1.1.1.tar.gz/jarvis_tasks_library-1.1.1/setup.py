8b5c5884405c8de59b005bbe666d9f48b1f8221e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:27 +0000

    Add package jarvis-tasks-library/1.1.1/jarvis_tasks_library-1.1.1.tar.gz

---
+++ packages/jarvis-tasks-library/jarvis_tasks_library-1.1.1.tar.gz/jarvis_tasks_library-1.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jarvis_tasks_library']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'jarvis-tasks-library',
+    'version': '1.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Juan Rodriguez',
+    'author_email': 'angel.neb@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
