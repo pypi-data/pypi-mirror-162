d0d3f604cb06e46125d6e3b95071d35b6e8935f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:40 +0000

    Add package sitemapa/0.1.3/sitemapa-0.1.3.tar.gz

---
+++ packages/sitemapa/sitemapa-0.1.3.tar.gz/sitemapa-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '0.1.3'
+DESCRIPTION = 'Create advanced sitemaps easily'
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+# Setting up
+setup(
+    name="sitemapa",
+    version=VERSION,
+    author="Dmitry S.",
+    author_email="<dmitry@abstractkitchen.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/abstractkitchen/sitemapa',
+    keywords=['python', 'sitemap', ''],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ]
+)
