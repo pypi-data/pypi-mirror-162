b24d0c83137924810472967815ea981042b6ab1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:06 +0000

    Add package texcli/0.0.9/texcli-0.0.9.tar.gz

---
+++ packages/texcli/texcli-0.0.9.tar.gz/texcli-0.0.9/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os.path import exists
+from setuptools import setup, find_packages
+
+author = 'Matthias Baer'
+email = 'matthias.r.baer@googlemail.com'
+description = 'Convert TeX to images on the command line.'
+name = 'texcli'
+year = '2022'
+url = 'https://github.com/maroba'
+version = '0.0.9'
+
+setup(
+    name=name,
+    author=author,
+    author_email=email,
+    url=url,
+    version=version,
+    packages=find_packages(),
+    package_dir={name: name},
+    include_package_data=True,
+    license='None',
+    description=description,
+    long_description=open('README.md').read() if exists('README.md') else '',
+    long_description_content_type="text/markdown",
+    install_requires=['sphinx', 'Click', 'matplotlib', 'scikit-image'
+                      ],
+    python_requires=">=3.6",
+    classifiers=['Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3',
+                 ],
+    platforms=['ALL'],
+    py_modules=['imagetool'],
+    entry_points={
+        'console_scripts': [
+            'texcli = texcli.cli:cli',
+        ]}
+)
