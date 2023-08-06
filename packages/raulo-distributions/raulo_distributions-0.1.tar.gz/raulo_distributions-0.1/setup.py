c57b8b9d4ec0e5510b66502d394575587d5a124d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:11 +0000

    Add package raulo-distributions/0.1/raulo_distributions-0.1.tar.gz

---
+++ packages/raulo-distributions/raulo_distributions-0.1.tar.gz/raulo_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='raulo_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['raulo_distributions'],
+      author='Raul Serrano Mena',
+      zip_safe=False)
