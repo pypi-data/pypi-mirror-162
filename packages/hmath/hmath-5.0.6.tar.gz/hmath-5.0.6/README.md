061fc8b938a73513054e8fa7c94520dbea50abee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:33 +0000

    Add package hmath/5.0.6/hmath-5.0.6.tar.gz

---
+++ packages/hmath/hmath-5.0.6.tar.gz/hmath-5.0.6/README.md
@@ -0,0 +1,16 @@
+# [hmath](https://caleb7023.hmath.pro)
+
+![hmath](https://caleb7023.hmath.pro)
+
+hmath was developed to use advanced math functions in python.
+You can use complex numbers for most functions.
+
+# Version
+[v.5.0.0](https://caleb7023.hmath.pro/version/v-5-0-0)
+
+hmath is investigating whether the calculation of the function is correct and whether the calculation is efficient. 
+Please help us provide information on efficient calculation methods and whether the calculations are correct.
+[Click on this to answer.](https://caleb7023.hmath.pro/Feedback)
+
+
+[Feedback on hmath modules](https://caleb7023.hmath.pro/Feedback)
---
+++ packages/hmath/hmath-5.0.6.tar.gz/hmath-5.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hmath",
+    version="5.0.6",
+    author="caleb7023",
+    description="hmath that can use advanced mathematical functions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://caleb7023.hmath.pro/home",
+    keywords=["hmath","math","python","caleb7023"],
+    project_urls={
+        "Bug Tracker": "https://github.com/caleb7023/hmath",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "hmath"},
+    packages=setuptools.find_packages(where="hmath"),
+    python_requires=">=3.0",
+)
+
+# Author: caleb7023
+# Copyright (c)
+# License: BSD 3 clause
