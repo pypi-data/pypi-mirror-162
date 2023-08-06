d9d0d3965665d35aa65ca7ac49d61e1cadff2f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:12 +0000

    Add package alien-ink/0.0.2/alien-ink-0.0.2.tar.gz

---
+++ packages/alien-ink/alien-ink-0.0.2.tar.gz/alien-ink-0.0.2/README.md
@@ -0,0 +1,20 @@
+## alien-ink
+
+ `alien-ink` is a personal machine learning toolkit
+
+
+#### Install
+[![PyPI version](https://badge.fury.io/py/alien-ink.svg)](https://badge.fury.io/py/alien-ink)
+
+```bash
+$ pip install alien-ink
+```
+
+#### Usage
+
+```python
+
+import alien_ink as ink
+
+print(ink.stars)
+```
---
+++ packages/alien-ink/alien-ink-0.0.2.tar.gz/alien-ink-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+exec(open('alien_ink/version.py').read())
+
+setup(
+  name = 'alien-ink',
+  packages = find_packages(exclude=[]),
+  version = __version__,
+  license='MIT',
+  description = 'Alien Ink - a personal machine learning toolkit',
+  author = 'Cody Collier',
+  author_email = 'cody@telnet.org',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/codycollier/alien-ink',
+  keywords = [
+    'artificial intelligence',
+    'machine learning',
+  ],
+  install_requires=[
+    'kaggle',
+    'numpy',
+    'packaging',
+    'pandas',
+    'sklearn',
+    'tensorflow',
+    'torch>=1.6',
+    'transformers',
+    'tqdm',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
