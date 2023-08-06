e5f11dbd87321686b44600b02803c9053d963d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:59 +0000

    Add package mfdls/0.0.2/mfdls-0.0.2.tar.gz

---
+++ packages/mfdls/mfdls-0.0.2.tar.gz/mfdls-0.0.2/README.md
@@ -0,0 +1,16 @@
+# medford-language-server
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Linting with Pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint)
+[![Checked with mypy](http://www.mypy-lang.org/static/mypy_badge.svg)](http://mypy-lang.org/)
+
+A language server, implementing the Language Server Protocol, for the MEDFORD metadata markup language.
+
+
+## Editors:
+* [VS Code](https://marketplace.visualstudio.com/items?itemName=TuftsBCB.medford-vscode)
+
+## Development
+This project is configured to use black, mypy, and pylint. Please run them and deal with any errors or warnings before commiting to master.
+* run `black` with `black mfdls`
+* run `mypy` with `mypy mfdls`
+* run `pylint` with `pylint mfdls`
---
+++ packages/mfdls/mfdls-0.0.2.tar.gz/mfdls-0.0.2/tests/unit/test_features.py
@@ -0,0 +1,68 @@
+import pytest
+from mock import Mock
+from pygls.lsp.types import (
+    DidChangeConfigurationParams,
+    DidOpenTextDocumentParams,
+    TextDocumentIdentifier,
+    TextDocumentItem,
+)
+from pygls.workspace import Document, Workspace
+
+from mfdls.server import did_change, did_open
+
+# We want this empty class instance because its helpful.
+# pylint: disable-next=R0903
+class FakeServer:
+    """We don't need real server to unit test features."""
+    publish_diagnostics = None
+    show_message = None
+    show_message_log = None
+
+    def __init__(self):
+        self.workspace = Workspace('', None)
+
+
+FAKE_DOCUMENT_URI = "file://fake_doc.txt"
+FAKE_DOCUMENT_CONTENT = "text"
+fake_document = Document(FAKE_DOCUMENT_URI, FAKE_DOCUMENT_CONTENT)
+
+
+server = FakeServer()
+server.publish_diagnostics = Mock()
+server.show_message = Mock()
+server.show_message_log = Mock()
+server.workspace.get_document = Mock(return_value=fake_document)
+
+
+def _reset_mocks():
+    server.publish_diagnostics.reset_mock()
+    server.show_message.reset_mock()
+    server.show_message_log.reset_mock()
+
+
+def test_did_change():
+    _reset_mocks()
+    params = DidChangeConfigurationParams(
+        text_document=TextDocumentIdentifier(uri=FAKE_DOCUMENT_URI)
+    )
+
+    did_change(server, params)
+
+
+@pytest.mark.asyncio
+async def test_did_open():
+    _reset_mocks()
+
+    params = DidOpenTextDocumentParams(
+        text_document=TextDocumentItem(
+            uri=FAKE_DOCUMENT_URI,
+            language_id="json",
+            version=1,
+            text=FAKE_DOCUMENT_CONTENT,
+        )
+    )
+
+    await did_open(server, params)
+
+    # Check publish diagnostics is called
+    server.publish_diagnostics.assert_called_once()
