f092655fc7db96f5f64236971216734774aa30d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:39 +0000

    Add package texcli/0.0.6/texcli-0.0.6.tar.gz

---
+++ packages/texcli/texcli-0.0.6.tar.gz/texcli-0.0.6/texcli/cli.py
@@ -0,0 +1,21 @@
+import sys
+
+
+import click
+import texcli.commands as cmd
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('output_filename')
+@click.option('--dpi', type=int)
+def latex(output_filename, dpi):
+    tex = ''.join(sys.stdin).replace('\n', ' ')
+    if not dpi:
+        dpi = 300
+    cmd.latex(tex, output_filename, dpi)
+
+
