2d7034259fd8214ed52b9d6c3e7231c5ee2c1785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:18 +0000

    Add package bpc/0.1.1/bpc-0.1.1.tar.gz

---
+++ packages/bpc/bpc-0.1.1.tar.gz/bpc-0.1.1/bpc/__init__.py
@@ -0,0 +1,5 @@
+from burmese_phoneme.cleaner.clean import Cleaner
+from burmese_phoneme.cleaner.normalizer import Normalizer
+from burmese_phoneme.cleaner.word_break import WordBreak
+from burmese_phoneme.cleaner.preprocessor import Preprocessor
+from burmese_phoneme.phonemizer.phonemize import BurmesePhonemizer
