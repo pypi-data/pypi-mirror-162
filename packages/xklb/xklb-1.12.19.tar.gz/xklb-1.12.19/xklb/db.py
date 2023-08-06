90f09f79089915ce7a21d9f0249ab13a4228a9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:17 +0000

    Add package xklb/1.12.19/xklb-1.12.19.tar.gz

---
+++ packages/xklb/xklb-1.12.19.tar.gz/xklb-1.12.19/xklb/db.py
@@ -0,0 +1,18 @@
+import os
+import sqlite3
+
+from xklb.utils import stop
+
+
+def sqlite_con(db):
+    if not os.path.exists(db):
+        print("Database file does not exist. Create one with lb extract.")
+        stop()
+
+    con = sqlite3.connect(db)
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def fetchall_dict(con, *args):
+    return [dict(r) for r in con.execute(*args).fetchall()]
