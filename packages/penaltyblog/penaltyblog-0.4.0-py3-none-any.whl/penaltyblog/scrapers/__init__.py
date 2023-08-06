f2e3c82a5549b699fd5249d3815899ed101843ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:36 +0000

    Add package penaltyblog/0.4.0/penaltyblog-0.4.0-py3-none-any.whl

---
+++ packages/penaltyblog/penaltyblog-0.4.0-py3-none-any.whl/penaltyblog/scrapers/__init__.py
@@ -0,0 +1,6 @@
+# from .fbref import FbRef
+from .clubelo import ClubElo  # noqa
+from .espn import ESPN  # noqa
+from .footballdata import FootballData  # noqa
+from .team_mappings import get_example_team_name_mappings  # noqa
+from .understat import Understat  # noqa
