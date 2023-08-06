6fec8283b4878690e32b9b00985d2931c05e870c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:31 +0000

    Add package torchdata/0.4.1/torchdata-0.4.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torchdata/torchdata-0.4.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torchdata/datapipes/iter/util/header.py
@@ -0,0 +1,62 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Iterator, TypeVar
+from warnings import warn
+
+from torchdata.datapipes import functional_datapipe
+from torchdata.datapipes.iter import IterDataPipe
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+@functional_datapipe("header")
+class HeaderIterDataPipe(IterDataPipe[T_co]):
+    r"""
+    Yields elements from the source DataPipe from the start, up to the specfied limit (functional name: ``header``).
+    This DataPipe can also be used to manually set the length of a DataPipe to a certain value; you
+    can do so by calling ``dp.header(desired_len)``.
+
+    Args:
+        source_datapipe: the DataPipe from which elements will be yielded
+        limit: the number of elements to yield before stopping
+
+    Example:
+        >>> from torchdata.datapipes.iter import IterableWrapper
+        >>> dp = IterableWrapper(range(10))
+        >>> header_dp = dp.header(3)
+        >>> list(header_dp)
+        [0, 1, 2]
+    """
+
+    def __init__(self, source_datapipe: IterDataPipe[T_co], limit: int = 10) -> None:
+        self.source_datapipe: IterDataPipe[T_co] = source_datapipe
+        self.limit: int = limit
+        self.length: int = -1
+
+    def __iter__(self) -> Iterator[T_co]:
+        i: int = 0
+        for value in self.source_datapipe:
+            i += 1
+            if i <= self.limit:
+                yield value
+            else:
+                break
+        self.length = min(i, self.limit)  # We know length with certainty when we reach here
+
+    def __len__(self) -> int:
+        if self.length != -1:
+            return self.length
+        try:
+            source_len = len(self.source_datapipe)
+            self.length = min(source_len, self.limit)
+            return self.length
+        except TypeError:
+            warn(
+                "The length of this HeaderIterDataPipe is inferred to be equal to its limit."
+                "The actual value may be smaller if the actual length of source_datapipe is smaller than the limit."
+            )
+            return self.limit
