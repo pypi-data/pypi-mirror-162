290280124502c62e83eaf9612f0a7836791a7359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:03 +0000

    Add package openseries/0.6.3/openseries-0.6.3.tar.gz

---
+++ packages/openseries/openseries-0.6.3.tar.gz/openseries-0.6.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as read_me:
+    long_description = read_me.read()
+
+setup(long_description=long_description, long_description_content_type="text/markdown")
