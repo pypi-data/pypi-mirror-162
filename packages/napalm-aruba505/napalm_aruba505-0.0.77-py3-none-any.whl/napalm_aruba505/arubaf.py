a9bd5b1dfe9db37d00d52bb1df7e9d664080bff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:21 +0000

    Add package napalm-aruba505/0.0.77/napalm_aruba505-0.0.77-py3-none-any.whl

---
+++ packages/napalm-aruba505/napalm_aruba505-0.0.77-py3-none-any.whl/napalm_aruba505/arubaf.py
@@ -0,0 +1,356 @@
+# -*- coding: utf-8 -*-
+"""NAPALM ArubaOS Five zero five Handler."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+import socket
+import json
+from napalm.base import constants as c
+from netmiko import ConnectHandler
+from napalm.base.base import NetworkDriver
+
+
+# Easier to store these as constants
+SECONDS = 1
+MINUTE_SECONDS = 60
+HOUR_SECONDS = 3600
+DAY_SECONDS = 24 * HOUR_SECONDS
+WEEK_SECONDS = 7 * DAY_SECONDS
+YEAR_SECONDS = 365 * DAY_SECONDS
+
+
+
+class ArubaFDriver(NetworkDriver):
+    """NAPALM ArubaOS Five zero five Handler."""
+
+    def __init__(self, hostname, username, password, timeout=60, optional_args=None):
+        """NAPALM Cisco IOS Handler."""
+        if optional_args is None:
+            optional_args = {}
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.timeout = timeout
+
+        self.transport = optional_args.get('transport', 'ssh')
+
+        # Netmiko possible arguments
+        netmiko_argument_map = {
+            'port': None,
+            'secret': '',
+            'verbose': False,
+            'keepalive': 30,
+            'global_delay_factor': 1,
+            'use_keys': False,
+            'key_file': None,
+            'ssh_strict': False,
+            'system_host_keys': False,
+            'alt_host_keys': False,
+            'alt_key_file': '',
+            'ssh_config_file': None,
+            'allow_agent': False,
+        }
+
+        # Build dict of any optional Netmiko args
+        self.netmiko_optional_args = {}
+        for k, v in netmiko_argument_map.items():
+            try:
+                self.netmiko_optional_args[k] = optional_args[k]
+            except KeyError:
+                pass
+
+        default_port = {
+            'ssh': 22,
+            'telnet': 23
+        }
+        self.port = optional_args.get('port', default_port[self.transport])
+
+        self.device = None
+        self.config_replace = False
+        self.interface_map = {}
+        self.profile = ["ArubaOS"]
+
+    def open(self):
+        """Open a connection to the device."""
+        device_type = 'aruba_os'
+        if self.transport == 'ssh':
+            device_type = 'aruba_os'
+        self.device = ConnectHandler(device_type=device_type,
+                                     host=self.hostname,
+                                     username=self.username,
+                                     password=self.password,
+                                     **self.netmiko_optional_args)
+        # ensure in enable mode
+        ## self.device.enable()
+
+    def close(self):
+        """Close the connection to the device."""
+        self.device.disconnect()
+
+    def is_alive(self):
+        """Returns a flag with the state of the connection."""
+        null = chr(0)
+        if self.device is None:
+            return {'is_alive': False}
+        else:
+            # SSH
+            try:
+                # Try sending ASCII null byte to maintain the connection alive
+                self.device.write_channel(null)
+                return {'is_alive': self.device.remote_conn.transport.is_active()}
+            except (socket.error, EOFError):
+                # If unable to send, we can tell for sure that the connection is unusable
+                return {'is_alive': False}
+        ## return {'is_alive': False}
+
+    def get_config(self, retrieve="all", full=False, sanitized=False):
+        """
+        Get config from device.
+        Returns the running configuration as dictionary.
+        The candidate and startup are always empty string for now,
+        """
+
+        configs = {
+            "running": "",
+            "startup": "No Startup",
+            "candidate": "No Candidate"
+        }
+
+        if retrieve.lower() in ('running', 'all'):
+            command = "show running-config"
+            output = self.device.send_command(command)
+            if output:
+                configs['running'] = output
+                data = str(configs['running']).split("\n")
+                non_empty_lines = [line for line in data if line.strip() != ""]
+
+                string_without_empty_lines = ""
+                for line in non_empty_lines:
+                    string_without_empty_lines += line + "\n"
+                configs['running'] = string_without_empty_lines
+
+        if retrieve.lower() in ('startup', 'all'):
+            pass
+        return configs
+
+
+    def show_summary_sanitizer(self, data):
+        """ Collects the fqdn and the serial number from the 'show summary'
+        :returns a tuple with two values (hostname, fqdn, serial_number)
+        """
+
+        fqdn = ""
+        serial_number = ""
+        hostname_ = ""
+
+        if data:
+            data_l = data.strip().splitlines()
+
+            for l in data_l:
+                if "Name" in l and not hostname_:
+                    hostname_ = f"{l.split(':')[1].lower()}"
+                if "DNSDomain" in l and hostname_:
+                    fqdn = f"{hostname_}.{l.split(':')[1]}"
+                if "Serial Number" in l :
+                    serial_number = l.split(':')[1]
+        return hostname_, fqdn, serial_number
+
+
+    def show_version_sanitizer(self, data):
+        """ Collects the vendor, model, os version and uptime from the 'show version'
+        :returns a tuple with two values (vendor, model, os version, uptime)
+        """
+
+        # Initialize to zero
+        (years, weeks, days, hours, minutes, seconds) = (0, 0, 0, 0, 0, 0)
+
+        vendor = "Hewlett Packard"
+        model = ""
+        os_version = ""
+        uptime = ""
+
+        if data:
+            data_l = data.strip().splitlines()
+            for l in data_l:
+                if "MODEL" in l:
+                    model, os_version = l.split(',')
+                if "AP uptime is" in l:
+                    tmp_uptime = l.replace("AP uptime is", "").split()
+                    uptimes_records = [int(i) for i in tmp_uptime if i.isnumeric()]
+
+                    if uptimes_records and len(uptimes_records) >= 5:
+                        weeks, days, hours, minutes, seconds = uptimes_records
+                        uptime = float(sum([(years * YEAR_SECONDS), (weeks * WEEK_SECONDS), (days * DAY_SECONDS),
+                                            (hours * HOUR_SECONDS), (minutes * MINUTE_SECONDS), (seconds * SECONDS), ]))
+                    if uptimes_records and len(uptimes_records) == 4:
+                        weeks, days, hours, minutes = uptimes_records
+                        uptime = float(sum([(years * YEAR_SECONDS), (weeks * WEEK_SECONDS), (days * DAY_SECONDS),
+                                            (hours * HOUR_SECONDS), (minutes * MINUTE_SECONDS), (seconds * SECONDS), ]))
+
+        return vendor, model, os_version, uptime
+
+
+
+    def get_facts(self):
+        """Return a set of facts from the devices"""
+
+        configs = {}
+        show_version_output = self.device.send_command("show version")
+        show_summary_output = self.device.send_command("show summary")
+
+        # processing 'show version' output
+        configs['show_version'] = show_version_output
+        show_version_data = str(configs['show_version']).split("\n")
+        show_version_non_empty_lines = [line for line in show_version_data if line.strip() != ""]
+
+        show_version_string_ = ""
+        for line in show_version_non_empty_lines:
+            show_version_string_ += line + "\n"
+        vendor, model, os_version, uptime = self.show_version_sanitizer(show_version_string_)
+
+        # processing 'show summary' output
+        configs['running_'] = show_summary_output
+        data = str(configs['running_']).split("\n")
+        non_empty_lines = [line for line in data if line.strip() != ""]
+
+        show_summary_string_ = ""
+        for line in non_empty_lines:
+            show_summary_string_ += line + "\n"
+        hostname_, fqdn_, serial_number_ = self.show_summary_sanitizer(show_summary_string_)
+
+        return {
+            "hostname": str(hostname_),
+            "fqdn": fqdn_,
+            "vendor": str(vendor),
+            "model": str(model),
+            "serial_number": str(serial_number_),
+            "os_version": str(os_version).strip(),
+            "uptime": uptime,
+        }
+
+
+    def _run_commands(self, commands, **kwargs):
+        if self.transport == "ssh":
+            ret = []
+            for command in commands:
+                if kwargs.get("encoding") == "text":
+                    cmd_output = self._netmiko_device.send_command(command).replace(
+                        "% Invalid input", ""
+                    )
+                    ret.append({"output": cmd_output})
+                    continue
+
+                cmd_pipe = command + " | json"
+                cmd_txt = self._netmiko_device.send_command(cmd_pipe)
+                try:
+                    cmd_json = json.loads(cmd_txt)
+                except json.decoder.JSONDecodeError:
+                    cmd_json = {}
+                ret.append(cmd_json)
+            return ret
+        else:
+            return self.device.run_commands(commands, **kwargs)
+
+
+    def get_ping(
+            self,
+            destination,
+            source=c.PING_SOURCE,
+            ttl=c.PING_TTL,
+            timeout=c.PING_TIMEOUT,
+            size=c.PING_SIZE,
+            count=c.PING_COUNT,
+            vrf=c.PING_VRF,
+            source_interface=c.PING_SOURCE_INTERFACE,
+    ):
+        """
+        Execute ping on the device and returns a dictionary with the result.
+        Output dictionary has one of following keys:
+            * success
+            * error
+        In case of success, inner dictionary will have the followin keys:
+            * probes_sent (int)
+            * packet_loss (int)
+            * rtt_min (float)
+            * rtt_max (float)
+            * rtt_avg (float)
+            * rtt_stddev (float)
+            * results (list)
+        'results' is a list of dictionaries with the following keys:
+            * ip_address (str)
+            * rtt (float)
+        """
+        ping_dict = {}
+        commands = []
+
+        if vrf:
+            commands.append("routing-context vrf {vrf}".format(vrf=vrf))
+
+        command = "ping {}".format(self.hostname)
+        command += " timeout {}".format(timeout)
+        command += " size {}".format(size)
+        command += " repeat {}".format(count)
+        if source != "":
+            command += " source {}".format(source)
+        if source_interface != "":
+            command += " interface {}".format(source_interface)
+
+        commands.append(command)
+        output = self._run_commands(commands, encoding="text")[-1]["output"]
+
+        if "connect:" in output:
+            ping_dict["error"] = output
+        elif "PING" in output:
+            ping_dict["success"] = {
+                "probes_sent": 0,
+                "packet_loss": 0,
+                "rtt_min": 0.0,
+                "rtt_max": 0.0,
+                "rtt_avg": 0.0,
+                "rtt_stddev": 0.0,
+                "results": [],
+            }
+            results_array = []
+            for line in output.splitlines():
+                fields = line.split()
+                if "icmp" in line:
+                    if "Unreachable" in line:
+                        if "(" in fields[2]:
+                            results_array.append(
+                                {"ip_address": str(fields[2][1:-1]), "rtt": 0.0}
+                            )
+                        else:
+                            results_array.append(
+                                {"ip_address": str(fields[1]), "rtt": 0.0}
+                            )
+                    elif "truncated" in line:
+                        if "(" in fields[4]:
+                            results_array.append(
+                                {"ip_address": str(fields[4][1:-2]), "rtt": 0.0}
+                            )
+                        else:
+                            results_array.append(
+                                {"ip_address": str(fields[3][:-1]), "rtt": 0.0}
+                            )
+                    elif fields[1] == "bytes":
+                        m = fields[6][5:]
+                        results_array.append(
+                            {"ip_address": str(fields[3][:-1]), "rtt": float(m)}
+                        )
+                elif "packets transmitted" in line:
+                    ping_dict["success"]["probes_sent"] = int(fields[0])
+                    ping_dict["success"]["packet_loss"] = int(fields[0]) - int(
+                        fields[3]
+                    )
+                elif "min/avg/max" in line:
+                    m = fields[3].split("/")
+                    ping_dict["success"].update(
+                        {
+                            "rtt_min": float(m[0]),
+                            "rtt_avg": float(m[1]),
+                            "rtt_max": float(m[2]),
+                            "rtt_stddev": float(m[3]),
+                        }
+                    )
+            ping_dict["success"].update({"results": results_array})
+        return ping_dict
