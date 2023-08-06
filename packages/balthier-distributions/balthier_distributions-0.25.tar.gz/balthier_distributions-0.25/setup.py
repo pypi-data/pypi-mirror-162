afb038a7cf61be71b1801b530d89c2f6152bcfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:31 +0000

    Add package balthier-distributions/0.25/balthier_distributions-0.25.tar.gz

---
+++ packages/balthier-distributions/balthier_distributions-0.25.tar.gz/balthier_distributions-0.25/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='balthier_distributions',
+      version='0.25',
+      description='Gaussian + Binomial distributions',
+      packages=['balthier_distributions'],
+      zip_safe=False)
