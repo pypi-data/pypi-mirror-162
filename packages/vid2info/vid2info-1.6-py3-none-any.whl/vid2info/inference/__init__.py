d8ebc4ba1d997c5a594a9e31aa31c2ec58204e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:54 +0000

    Add package vid2info/1.6/vid2info-1.6-py3-none-any.whl

---
+++ packages/vid2info/vid2info-1.6-py3-none-any.whl/vid2info/inference/__init__.py
@@ -0,0 +1,3 @@
+from inference import detection, segmentation, tracking, utils
+from inference.config import XMIN, YMIN, XMAX, YMAX, CONFIDENCE, CLASS_ID, \
+    CLASS_NAME, CLASS_NAMES_LIST, BBOX_XYXY
