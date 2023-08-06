21493c807cb3fe5206b4b6554ae9072dbab638e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:55 +0000

    Add package not-sklearn/0.0.1/not_sklearn-0.0.1.tar.gz

---
+++ packages/not-sklearn/not_sklearn-0.0.1.tar.gz/not_sklearn-0.0.1/not_sklearn/regression/LinearRegression.py
@@ -0,0 +1,19 @@
+from sklearn.base import BaseEstimator,ClassifierMixin
+
+
+class CustomLinearRegression(BaseEstimator,ClassifierMixin):
+    def __init__(self):
+        pass 
+    
+    def fit(self, x, y):
+        pass
+    
+    def predict(self, x):
+        pass 
+    
+    
+    def get_params(self, deep=True):
+        return super().get_params(deep)
+    
+    def set_params(self, **params):
+        return super().set_params(**params)
---
+++ packages/not-sklearn/not_sklearn-0.0.1.tar.gz/not_sklearn-0.0.1/test/test_first.py
@@ -0,0 +1,2 @@
+def test_naif():
+    assert True
