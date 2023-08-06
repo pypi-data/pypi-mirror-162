4ab9e096354794a7cf1b166486c5cf558f73cfc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:59 +0000

    Add package enedisgatewaypy/1.1.0/enedisgatewaypy-1.1.0.tar.gz

---
+++ packages/enedisgatewaypy/enedisgatewaypy-1.1.0.tar.gz/enedisgatewaypy-1.1.0/enedisgatewaypy/auth.py
@@ -0,0 +1,60 @@
+"""Class for Enedis Authentification (http://enedisgateway.tech)."""
+from __future__ import annotations
+
+import logging
+
+from aiohttp import ClientSession, ClientError, ClientResponse
+from .exceptions import EnedisException, EnedisGatewayException, EnedisLimitReached
+
+URL = "https://enedisgateway.tech"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class EnedisAuth:
+    """Class for Enedis Auth API."""
+
+    def __init__(self, token, session=None):
+        """Init."""
+        self.token = token
+        self.session = session if session else ClientSession()
+
+    async def async_close(self):
+        """Close session."""
+        await self.session.close()
+
+    async def request(self, method: str = "POST", path: str = "api", **kwargs) -> ClientResponse:
+        """Request session."""
+        headers = kwargs.get("headers")
+
+        if headers is None:
+            headers = {}
+        else:
+            headers = dict(headers)
+
+        if kwargs.get("json"):
+            headers["Content-Type"] = "application/json"
+
+        headers["Authorization"] = self.token
+
+        try:
+            _LOGGER.debug("Request %s", kwargs)
+            resp = await self.session.request(
+                method, f"{URL}/{path}", **kwargs, headers=headers, timeout=5
+            )
+
+            response = await resp.json()
+            _LOGGER.debug("Response %s", response)
+
+            if "tag" in response and response["tag"] in [
+                "limit_reached",
+                "enedis_return_ko",
+            ]:
+                raise EnedisLimitReached(response.get("description"))
+
+            if "error" in response:
+                raise EnedisGatewayException(response.get("error"))
+
+            return response
+        except ClientError as error:
+            raise EnedisException from error
