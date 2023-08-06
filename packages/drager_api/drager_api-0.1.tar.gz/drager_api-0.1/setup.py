ea0b59940ec5d557c2de0014f473f2c4d4b22811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:31 +0000

    Add package drager_api/0.1/drager_api-0.1.tar.gz

---
+++ packages/drager_api/drager_api-0.1.tar.gz/drager_api-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'drager_api',
+  packages = ['drager_api'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A tutorial lib',
+  author = 'Khannasu',
+  author_email = 'sukratk@gmail.com',
+  url = 'https://github.com/sajorn-warrior/drager_api',
+  download_url = 'https://github.com/sajorn-warrior/drager_api/tarball/0.1',
+  keywords = ['add', 'sub', 'tests'], 
+  classifiers = [],
+)
