0866d3180dbea6010715e573275366c314f9aff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:24 +0000

    Add package hagelkorn/1.2.3/hagelkorn-1.2.3.tar.gz

---
+++ packages/hagelkorn/hagelkorn-1.2.3.tar.gz/hagelkorn-1.2.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+__packagename__ = "hagelkorn"
+
+
+def get_version():
+    import os
+    import re
+
+    VERSIONFILE = os.path.join(__packagename__, "__init__.py")
+    initfile_lines = open(VERSIONFILE, "rt").readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError(f"Unable to find version string in {VERSIONFILE}.")
+
+
+__version__ = get_version()
+
+
+setup(
+    name=__packagename__,
+    packages=[__packagename__],  # this must be the same as the name above
+    version=__version__,
+    description="Generates alphanumeric IDs that increase monotonically with time.",
+    url="https://github.com/michaelosthege/hagelkorn",
+    download_url="https://github.com/michaelosthege/hagelkorn/tarball/%s" % __version__,
+    author="Michael Osthege",
+    author_email="m.osthege@fz-juelich.de",
+    copyright="(c) 2018 Forschungszentrum Jülich GmbH",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+    ],
+    package_data={"hagelkorn": ["py.typed"]},
+    install_requires=[],
+)
