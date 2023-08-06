7fce339657953e6984db973cabc4effc2a1f001b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:21 +0000

    Add package platonic-solids-Carlos/0.1/platonic_solids_Carlos-0.1.tar.gz

---
+++ packages/platonic-solids-Carlos/platonic_solids_Carlos-0.1.tar.gz/platonic_solids_Carlos-0.1/platonic_solids_Carlos/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 11:50:51 2022
+
+@author: calonso
+"""
+
---
+++ packages/platonic-solids-Carlos/platonic_solids_Carlos-0.1.tar.gz/platonic_solids_Carlos-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='platonic_solids_Carlos',
+version='0.1',
+description='Solidos platonicos de Gonzalo',
+packages=['platonic_solids_Carlos'],
+author_email='carlosalonsobonilla@gmail.com',
+zip_safe=False)
