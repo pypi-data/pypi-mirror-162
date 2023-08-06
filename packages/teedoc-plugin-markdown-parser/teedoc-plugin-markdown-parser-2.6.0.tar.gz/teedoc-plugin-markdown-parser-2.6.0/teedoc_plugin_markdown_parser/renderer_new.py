6f64b40661a6255a12b879e8ab5bd81206dfa972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:55 +0000

    Add package teedoc-plugin-markdown-parser/2.6.0/teedoc-plugin-markdown-parser-2.6.0.tar.gz

---
+++ packages/teedoc-plugin-markdown-parser/teedoc-plugin-markdown-parser-2.6.0.tar.gz/teedoc-plugin-markdown-parser-2.6.0/teedoc_plugin_markdown_parser/renderer_new.py
@@ -0,0 +1,31 @@
+import mistune
+from .renderer_base import Block_Quote_Renderer, Header_Renderer, HighlightRenderer
+from .renderer_math import MathRendererMixin
+
+plugins = [
+    # "abbr",
+    'strikethrough',
+    'footnotes',
+    'table',
+    'url',
+    'task_lists',
+    'def_list',
+]
+
+
+class MDRenderer(
+                 # HighlightMixin,
+                #  MathRendererMixin,
+                #  TasklistRenderMixin,
+                 Block_Quote_Renderer,
+                 Header_Renderer,
+                 HighlightRenderer,
+                 mistune.Renderer):
+    def __init__(self):
+        mistune.Renderer.__init__(self, escape = False, hard_wrap = True)
+
+
+def create_markdown_parser():
+    parser = mistune.create_markdown(renderer=MDRenderer(), plugins=plugins)
+    return parser
+
