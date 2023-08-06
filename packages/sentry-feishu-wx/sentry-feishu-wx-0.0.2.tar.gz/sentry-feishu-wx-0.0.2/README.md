131a50d5b4847756d787537cbb38a55faaa91143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:37 +0000

    Add package sentry-feishu-wx/0.0.2/sentry-feishu-wx-0.0.2.tar.gz

---
+++ packages/sentry-feishu-wx/sentry-feishu-wx-0.0.2.tar.gz/sentry-feishu-wx-0.0.2/README.md
@@ -0,0 +1,8 @@
+# sentry_feishu
+飞书 sentry 插件
+
+## 上传pypi
+```
+python3 setup.py sdits bdist_wheel
+twine upload dist/*
+```
---
+++ packages/sentry-feishu-wx/sentry-feishu-wx-0.0.2.tar.gz/sentry-feishu-wx-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sentry-feishu-wx",
+    version='0.0.2',
+    author='chengpeng',
+    author_email='chengandpeng@gmail.com',
+    url='https://github.com/chengandpeng/sentry_feishu',
+    description='A Sentry extension which send errors stats to Feishu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry feishu',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'sentry_feishu = sentry_feishu.plugin:FeiShuPlugin'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
+)
