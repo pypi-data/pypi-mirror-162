e4a0c4560907347a51e8f8743792f863b1b2d98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package featurebox/0.0.992/featurebox-0.0.992.tar.gz

---
+++ packages/featurebox/featurebox-0.0.992.tar.gz/featurebox-0.0.992/featurebox/cli/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# @Time     : 2021/10/8 14:27
+# @Software : PyCharm
+# @License  : GNU General Public License v3.0
+# @Author   : xxx
+
+__doc__ = """
+The ``cli`` part include:
+
+    "bandgap", "dbc", "bader", "cohp", "dos", "general", "diff", "converge"
+
+which run in command line mode. All message (help) could get by ``'-h'`` .
+
+1.Run in featurebox: featurebox bandgap -h
+
+2.Copy the script and run the single file: python bandgap -h
+
+Example:
+
+>>> $ featurebox bandgap -p /home/parent_dir
+
+>>> $ python bandgap.py -p /home/parent_dir
+
+"""
