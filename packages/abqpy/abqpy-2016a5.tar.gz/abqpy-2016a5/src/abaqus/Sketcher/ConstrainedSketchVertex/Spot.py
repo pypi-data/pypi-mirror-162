9a8a0e6bb357903e3969714ab5ed7e7b41a0813c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:42 +0000

    Add package abqpy/2016a5/abqpy-2016a5.tar.gz

---
+++ packages/abqpy/abqpy-2016a5.tar.gz/abqpy-2016a5/src/abaqus/Sketcher/ConstrainedSketchVertex/Spot.py
@@ -0,0 +1,25 @@
+from .ConstrainedSketchVertex import ConstrainedSketchVertex
+
+
+class Spot(ConstrainedSketchVertex):
+    def __init__(self, point: tuple[float]):
+        """This method creates a spot (construction point) located at the specified coordinates.
+
+        .. note:: 
+            This function can be accessed by:
+
+            .. code-block:: python
+
+                mdb.models[name].sketches[name].Spot
+
+        Parameters
+        ----------
+        point
+            A pair of Floats specifying the coordinates of the construction point.
+
+        Returns
+        -------
+        ConstrainedSketchVertex
+            A :py:class:`~abaqus.Sketcher.ConstrainedSketchVertex.ConstrainedSketchVertex.ConstrainedSketchVertex` object (None if the spot cannot be created).
+        """
+        pass
