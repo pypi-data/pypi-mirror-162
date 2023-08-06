084d1d804e241b955c127bc5bc81d6ad7f6b1150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:19 +0000

    Add package neqsim/2.3.27/neqsim-2.3.27.tar.gz

---
+++ packages/neqsim/neqsim-2.3.27.tar.gz/neqsim-2.3.27/neqsim/__init__.py
@@ -0,0 +1,18 @@
+from .neqsimpython import * 
+
+def methods(checkClass):
+    methods = checkClass.getClass().getMethods()
+    for method in methods:
+        print(method.getName())
+
+
+def has_matplotlib():
+    from importlib.util import find_spec
+    return find_spec("matplotlib")
+
+
+def setDatabase(connectionString):
+    from neqsim.neqsimpython import jNeqSim
+
+    jNeqSim.util.database.NeqSimDataBase.setConnectionString(connectionString)
+    jNeqSim.util.database.NeqSimDataBase.setCreateTemporaryTables(True)
