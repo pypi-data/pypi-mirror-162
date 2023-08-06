54577c1da4eab7da638e59b4b822623c5c2ecf36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:01 +0000

    Add package ampel-core/0.8.3a20/ampel_core-0.8.3a20-py3-none-any.whl

---
+++ packages/ampel-core/ampel_core-0.8.3a20-py3-none-any.whl/ampel/test/dummy.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                Ampel-core/ampel/test/dummy.py
+# License:             BSD-3-Clause
+# Author:              jvs
+# Date:                Unspecified
+# Last Modified Date:  11.02.2021
+# Last Modified By:    jvs
+
+import time
+from collections.abc import Sequence
+
+from ampel.struct.UnitResult import UnitResult
+from ampel.types import StockId, UBson
+from ampel.abstract.AbsEventUnit import AbsEventUnit
+from ampel.abstract.AbsStockT2Unit import AbsStockT2Unit
+from ampel.abstract.AbsPointT2Unit import AbsPointT2Unit
+from ampel.abstract.AbsStateT2Unit import AbsStateT2Unit
+from ampel.abstract.AbsTiedStateT2Unit import AbsTiedStateT2Unit
+
+from ampel.content.DataPoint import DataPoint
+from ampel.content.T1Document import T1Document
+from ampel.view.T2DocView import T2DocView
+from ampel.model.StateT2Dependency import StateT2Dependency
+from ampel.abstract.AbsT0Muxer import AbsT0Muxer
+
+
+class Sleepy(AbsEventUnit):
+    """
+    A processor that does nothing (especially not touching the db, which is not
+    mocked in subprocesses)
+    """
+
+    process_name: str = "ZZZzzzz"
+
+    def proceed(self, event_hdlr):
+        time.sleep(1)
+
+
+class DummyMuxer(AbsT0Muxer):
+
+    #: number of points to add to provided list
+    points_to_insert: int = 5
+
+    def process(
+        self, dps: list[DataPoint], stock_id: None | StockId = None
+    ) -> tuple[None | list[DataPoint], None | list[DataPoint]]:
+        """
+        :returns: tuple[datapoints to insert, datapoints to combine]
+            <datapoints to insert> will be provided to a T0 ingester
+            <datapoints to combine> will potentially be provided to an underlying T1 combiner
+        """
+
+        new_dps: list[DataPoint] = [
+            {"id": i, "stock": stock_id or 0} # type: ignore[typeddict-item]
+            for i in range(dps[-1]["id"] + 1, dps[-1]["id"] + 1 + self.points_to_insert)
+        ]
+        assert self.points_to_insert == 5
+        assert len(new_dps) == self.points_to_insert
+        return new_dps + dps, new_dps + dps
+
+
+class DummyStockT2Unit(AbsStockT2Unit):
+    def process(self, stock_doc):
+        return {"id": stock_doc["stock"]}
+
+
+class DummyPointT2Unit(AbsPointT2Unit):
+    def process(self, datapoint):
+        return {"thing": datapoint["body"]["thing"]}
+
+
+class DummyStateT2Unit(AbsStateT2Unit):
+
+    foo: int = 42
+
+    def process(self, compound, datapoints):
+        return {"len": len(datapoints)}
+
+
+class DummyTiedStateT2Unit(AbsTiedStateT2Unit):
+
+    t2_dependency = [StateT2Dependency(unit="DummyStateT2Unit")] # type: ignore
+
+    def process(
+        self,
+        compound: T1Document,
+        datapoints: Sequence[DataPoint],
+        t2views: Sequence[T2DocView],
+    ) -> UBson | UnitResult:
+        assert t2views, "dependencies were found"
+        assert len(t2views[-1].body or []) == 1
+        data = t2views[-1].get_payload() or {}
+        assert isinstance(data, dict)
+        return {k: v * 2 for k, v in data.items()}
---
+++ packages/ampel-core/ampel_core-0.8.3a20-py3-none-any.whl/ampel/test/test_T2Processor.py
@@ -0,0 +1,87 @@
+from ampel.content.T2Document import T2Document
+from ampel.dev.DevAmpelContext import DevAmpelContext
+from ampel.enum.DocumentCode import DocumentCode
+from contextlib import contextmanager
+
+from ampel.metrics.AmpelMetricsRegistry import AmpelMetricsRegistry
+from ampel.t2.T2Worker import T2Worker
+from ampel.test.dummy import DummyPointT2Unit
+
+
+@contextmanager
+def collect_diff(store):
+    store.clear()
+    before = {}
+    for metric in AmpelMetricsRegistry.registry().collect():
+        for sample in metric.samples:
+            key = (sample.name, tuple(sample.labels.items()))
+            before[key] = sample.value
+
+    delta = {}
+    yield
+    for metric in AmpelMetricsRegistry.registry().collect():
+        for sample in metric.samples:
+            key = (sample.name, tuple(sample.labels.items()))
+            delta[key] = sample.value - (before.get(key) or 0)
+    store.update(delta)
+
+
+def test_metrics(dev_context, ingest_stock_t2):
+    t2 = T2Worker(context=dev_context, raise_exc=True, process_name="t2")
+
+    stats = {}
+    with collect_diff(stats):
+        assert t2.run() == 1
+
+    assert stats[("ampel_t2_docs_processed_total", (("unit", "DummyStockT2Unit"),))] == 1
+    assert stats[("ampel_t2_latency_seconds_sum", (("unit", "DummyStockT2Unit"),))] > 0
+
+
+def test_error_reporting(dev_context: DevAmpelContext):
+    dev_context.register_unit(DummyPointT2Unit)
+    # DummyPointT2Unit will raise an error on the malformed T0 doc
+    dev_context.db.get_collection("t0").insert_one({
+        "id": 42
+    })
+    channels = ["channel_a", "channel_b"]
+    doc: T2Document = {
+        "unit": "DummyPointT2Unit",
+        "code": DocumentCode.NEW,
+        "config": None,
+        "col": "t0",
+        "stock": 42,
+        "link": 42,
+        "channel": channels,
+        "meta": [
+            {"ts": 0, "tier": 2}
+        ],
+        "body": []
+    }
+    dev_context.db.get_collection("t2").insert_one(doc) # type: ignore[arg-type]
+    t2 = T2Worker(context=dev_context, raise_exc=False, process_name="t2", run_dependent_t2s=True)
+    assert t2.run() == 1
+    assert (doc := dev_context.db.get_collection("t2").find_one({})) # type: ignore[assignment]
+    assert doc["code"] == DocumentCode.EXCEPTION
+    assert (trouble := dev_context.db.get_collection("troubles").find_one({}))
+    assert trouble["channel"] == channels
+
+def test_tied_t2s(dev_context, ingest_tied_t2):
+    assert (num_dps := dev_context.db.get_collection("t0").count_documents({}))
+    t2 = T2Worker(context=dev_context, raise_exc=True, process_name="t2", run_dependent_t2s=True)
+
+    num_docs = t2.run()
+    t2 = dev_context.db.get_collection("t2")
+    assert t2.count_documents({}) == num_docs
+
+    if "point" in ingest_tied_t2.param.lower():
+        assert num_docs == 1+num_dps
+        assert next(t2.find({"unit": "DummyPointT2Unit"}).sort([("link", -1)]).limit(1))["body"][0]["thing"] == 3
+        assert next(t2.find({"unit": "DummyTiedStateT2Unit"}).sort([("link", -1)]).limit(1))["body"][0]["thing"] == 2*3
+    elif "stock" in ingest_tied_t2.param.lower():
+        assert num_docs == 2
+        assert t2.find_one({"unit": "DummyStockT2Unit"})["body"][0]["id"] == "stockystock"
+        assert t2.find_one({"unit": "DummyTiedStateT2Unit"})["body"][0]["id"] == 2*"stockystock"
+    else:
+        assert num_docs == 2
+        assert t2.find_one({"unit": "DummyStateT2Unit"})["body"][0]["len"] == num_dps
+        assert t2.find_one({"unit": "DummyTiedStateT2Unit"})["body"][0]["len"] == 2*num_dps
