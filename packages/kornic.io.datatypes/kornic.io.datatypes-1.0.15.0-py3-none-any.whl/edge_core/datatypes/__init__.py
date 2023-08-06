4128a3ec6621c32e566c2b063a4f8cb91371860f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:59 +0000

    Add package kornic.io.datatypes/1.0.15.0/kornic.io.datatypes-1.0.15.0-py3-none-any.whl

---
+++ packages/kornic.io.datatypes/kornic.io.datatypes-1.0.15.0-py3-none-any.whl/edge_core/datatypes/__init__.py
@@ -0,0 +1,21 @@
+
+from edge_core.datatypes import data_source_property_pb2
+from edge_core.datatypes import io_common_pb2
+from edge_core.datatypes import io_control_pb2
+from edge_core.datatypes import io_control_pb2_grpc
+from edge_core.datatypes import io_property_pb2
+from edge_core.datatypes import service_io_control_pb2
+from edge_core.datatypes import service_io_control_pb2_grpc
+from edge_core.datatypes import sinker_payload_pb2
+
+
+__all__ = (
+    'data_source_property_pb2',
+    'io_common_pb2',
+    'io_control_pb2',
+    'io_control_pb2_grpc',
+    'io_property_pb2',
+    'service_io_control_pb2',
+    'service_io_control_pb2_grpc',
+    'sinker_payload_pb2',
+)
