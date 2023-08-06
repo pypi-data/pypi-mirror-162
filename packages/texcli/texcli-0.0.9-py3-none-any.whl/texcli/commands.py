ca9d6ecbd653bf5f2073c66a6414399cc3fe84a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:06 +0000

    Add package texcli/0.0.9/texcli-0.0.9-py3-none-any.whl

---
+++ packages/texcli/texcli-0.0.9-py3-none-any.whl/texcli/commands.py
@@ -0,0 +1,35 @@
+import sys
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def latex(tex, output_file, dpi=300, bgcolor='white', fgcolor='black'):
+    tex = '$ ' + tex + ' $'
+    mpl.rcParams['text.usetex'] = True
+    mpl.rcParams['text.latex.preamble'] = r'\usepackage{{amsmath}}'
+    plt.rcParams.update({'mathtext.fontset': 'cm'})
+    fig = plt.figure(figsize=(10, 10), dpi=100)
+
+    t = fig.text(0, 0, tex,
+                horizontalalignment='left', verticalalignment='bottom',
+                fontsize=30
+                )
+
+    r = fig.canvas.get_renderer()
+
+    bbox = t.get_tightbbox(r)
+    w, h = (bbox.width / r.dpi, bbox.height / r.dpi)
+
+    fig = plt.figure(figsize=(1.1 * w, 1.1 * h), dpi=dpi)
+    t = fig.text(0, 0, tex, fontsize=30,
+                 verticalalignment="bottom", horizontalalignment="left",
+                 bbox={'facecolor': bgcolor, 'edgecolor': bgcolor},
+                 color=fgcolor
+                 )
+
+    fig.savefig(output_file, transparent=False)
+
+
+if __name__ == '__main__':
+    latex(r'\frac{\partial}{\partial z}', 'test.png')
