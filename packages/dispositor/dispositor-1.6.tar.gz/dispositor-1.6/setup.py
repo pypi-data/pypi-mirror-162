db66cfc37b387de00f10180b11bd2de86e95bdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:14 +0000

    Add package dispositor/1.6/dispositor-1.6.tar.gz

---
+++ packages/dispositor/dispositor-1.6.tar.gz/dispositor-1.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='dispositor',
+      version='1.6',
+      description='Added the ability to save to the database',
+      packages=find_packages(),
+      author_email='astro.slfd@gmail.com',
+      zip_safe=False)
