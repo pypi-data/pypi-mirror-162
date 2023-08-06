e3927dea1a2b06762a06d185dffb7bbfe273cdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:41 +0000

    Add package af-metrics/0.0.0/af_metrics-0.0.0-py3-none-any.whl

---
+++ packages/af-metrics/af_metrics-0.0.0-py3-none-any.whl/af_metrics/__init__.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) 2022 Durbar Chakraborty. All rights reserved.
+
+af-metrics: Source code for analysis of metrics on Coffea-Casa Analysis Facility
+"""
+
+
+from __future__ import annotations
+from .monitoring_module import CustomCollector
+
+
+__version__="0.1.0"
+__author__="Durbar Chakraborty"
