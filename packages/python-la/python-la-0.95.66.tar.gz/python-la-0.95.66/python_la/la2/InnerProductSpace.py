e21f1fb16a3e3c3f098d401ad18dccb8435d1bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:17 +0000

    Add package python-la/0.95.66/python-la-0.95.66.tar.gz

---
+++ packages/python-la/python-la-0.95.66.tar.gz/python-la-0.95.66/python_la/la2/InnerProductSpace.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import Union
+from ..utils import areinstances, check_foreach
+from ..la1 import VectorSpace, Field, Complex, Vector
+from .InnerProduct import InnerProduct
+
+
+class InnerProductSpace(VectorSpace):
+    def __init__(self, field: Field, inner_product: InnerProduct) -> None:
+        super().__init__(field)
+        self.inner_product = inner_product
+
+    def apply(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        return self(v, u)
+
+    def inner_product(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        return self(v, u)
+
+    def __call__(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        if not areinstances([v, u], Vector):
+            raise TypeError("v and u must be of type Vector")
+        return self.inner_product(v, u)
+
+    def arePerpendicular(self, v: Vector, u: Vector) -> bool:
+        if not areinstances([v, u], Vector):
+            raise TypeError("v and u must be of type Vector")
+        if not check_foreach([v, u], lambda x: x in self):
+            raise ValueError("v and u must be in the VectorSpace")
+        return self(v, u) == 0
