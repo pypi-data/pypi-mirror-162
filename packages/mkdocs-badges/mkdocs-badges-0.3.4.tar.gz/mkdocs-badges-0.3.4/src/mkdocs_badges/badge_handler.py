8fcbbb6f85209940421acafc10580c49a513f52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:01 +0000

    Add package mkdocs-badges/0.3.4/mkdocs-badges-0.3.4.tar.gz

---
+++ packages/mkdocs-badges/mkdocs-badges-0.3.4.tar.gz/mkdocs-badges-0.3.4/src/mkdocs_badges/badge_handler.py
@@ -0,0 +1,101 @@
+from urllib.parse import urlparse
+
+# local files
+from . import warning
+from .parser import ParsedBadge, BadgeException, FileParser
+from .badge_html import generate_badge_html, generate_single_element_badge_html
+from .install_badge import InstallBadgeManager
+from .tag_badge import TagBadgeManager
+
+LINK_BADGE_EMPTY_FIELDS = ["copy_text", "link", "reflink"]
+REFLINK_BADGE_EMPTY_FIELDS = LINK_BADGE_EMPTY_FIELDS
+INSTALL_BADGE_EMPTY_FIELDS = LINK_BADGE_EMPTY_FIELDS
+COPY_BADGE_EMPTY_FIELDS = LINK_BADGE_EMPTY_FIELDS
+TAG_BADGE_EMPTY_FIELDS = LINK_BADGE_EMPTY_FIELDS
+STRIP_SUBDOMAINS = [
+    "www", # Pretty standard prefix for websites
+    "m", # Pretty standard for mobile sites
+]
+
+def replace_badges(file_name: str, markdown: str, *args) -> str:
+    lines = markdown.split("\n")
+    for parser_result_entry in FileParser(file_name, lines).process():
+        index = parser_result_entry.line_index
+        badge = parser_result_entry.parsed_badge
+        try:
+            lines[index] = format_badge(badge, *args)
+        except BadgeException as error:
+            warning(f"[{file_name}:{index+1}] Processing error: {error}")
+    
+    return "\n".join(lines)
+
+
+def format_badge(badge: ParsedBadge, install_badge_manager: InstallBadgeManager, tag_badge_manager: TagBadgeManager) -> str:
+    badge.check_fields()
+    typ = badge.badge_type
+
+    if not typ:
+        # TODO: Make it possible to use copy_text and reflink at the same time
+        if badge.reflink:
+            badge.assert_empty("copy_text")
+
+        classes = ["badge-normal", *badge.html_classes]
+        if badge.reflink:
+            classes.append("badge-reflink")
+        badge_html = generate_badge_html(badge.title, badge.value, copy_text=badge.copy_text, link=badge.link, extra_classes=classes)
+        if badge.reflink:
+            badge_html = f"[{badge_html}][{badge.reflink}]"
+        return badge_html
+    
+    elif typ == "L":
+        badge.assert_all_empty(LINK_BADGE_EMPTY_FIELDS)
+        simplified_host_name = get_simplified_hostname(badge.value)
+        classes = ["badge-link", *badge.html_classes]
+        return generate_badge_html(badge.title, simplified_host_name, link=badge.value, extra_classes=classes)
+
+    elif typ == "R":
+        badge.assert_all_empty(REFLINK_BADGE_EMPTY_FIELDS)
+        classes = ["badge-reflink", *badge.html_classes]
+        badge_html = generate_badge_html(badge.title, badge.value, extra_classes=classes)
+        return f"[{badge_html}][{badge.value}]"
+
+    elif typ == "I":
+        return install_badge_manager.format_badge(badge)
+    
+    elif typ == "C":
+        badge.assert_all_empty(COPY_BADGE_EMPTY_FIELDS)
+        classes = ["badge-copy", *badge.html_classes]
+        return generate_badge_html(badge.title, badge.value, copy_text=badge.value, extra_classes=classes)
+
+    elif typ == "T":
+        badge.assert_all_empty(TAG_BADGE_EMPTY_FIELDS)
+        return tag_badge_manager.format_badge(badge)
+
+    elif typ == "S":
+        classes = ["badge-single", *badge.html_classes]
+        badge_html = generate_single_element_badge_html(badge.title, copy_text=badge.copy_text, link=badge.link, extra_classes=classes)
+        if badge.reflink:
+            badge_html = f"[{badge_html}][{badge.reflink}]"
+        return badge_html
+
+    else:
+        raise BadgeException(f"Unknown badge type '{typ}'")
+
+
+def get_simplified_hostname(url: str) -> str:
+    url = url.strip()
+    simplified_host_name = urlparse(url).netloc
+    if not simplified_host_name:
+        raise Exception(f"No hostname in URL: '{url}'")
+    
+    # Remove unnecessary subdomains: for example "www.example.com" -> "example.com"
+    parts = simplified_host_name.split(".")
+    if len(parts) >= 2: # only handle www.example.com, but not www.com
+        if parts[0] in STRIP_SUBDOMAINS:
+            # remove the first subdomain
+            parts = parts[1:]
+            # store our change in the original variable
+            simplified_host_name = ".".join(parts)
+    
+    return simplified_host_name
+
