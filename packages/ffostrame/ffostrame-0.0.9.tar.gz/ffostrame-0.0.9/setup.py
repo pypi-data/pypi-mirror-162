b845340e16ae80662a81eba18f4754ccadf33587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:56 +0000

    Add package ffostrame/0.0.9/ffostrame-0.0.9.tar.gz

---
+++ packages/ffostrame/ffostrame-0.0.9.tar.gz/ffostrame-0.0.9/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def read_requirements():
+    with open("requirements.txt", "r") as req:
+        content = req.read()
+        requirements = content.split("\n")
+
+    return requirements
+
+
+def get_version():
+    version_file = open("version.txt", "r")
+    version = version_file.read()
+    version_file.close()
+
+    return version
+
+
+setup(
+    name="ffostrame",
+    version="0.0.9",
+    install_requires = ['click-shell==2.1', 'rich-click==1.5.1', 'click==7.1.2' ],
+    # install_requires=read_requirements(),
+    include_package_data=True,
+    scripts=[],
+    entry_points="""
+        [console_scripts]
+        ff=cli:cli
+    """,
+)
