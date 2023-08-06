24929956e9140ffea365691cb3f4cdd228b8b0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:54 +0000

    Add package otter-submit/0.5/otter_submit-0.5.tar.gz

---
+++ packages/otter-submit/otter_submit-0.5.tar.gz/otter_submit-0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="otter_submit",
+    description="python package for nbextension to be used with Gofer or Otter Grader",
+    version='0.5',
+    author="V. Su(Sean Morris - changed 8/8/2022)",
+    author_email="sean.smorris@berkeley.edu",
+    url="https://github.com/data-8/gofer_submit",
+    include_package_data=True,
+    data_files=[
+        # like `jupyter nbextension install --sys-prefix`
+        ("share/jupyter/nbextensions/gofer_submit", [
+            "gofer_submit/static/index.js",
+        ]),
+        # like `jupyter nbextension enable --sys-prefix`
+        ("etc/jupyter/nbconfig/notebook.d", [
+            "jupyter-config/nbconfig/notebook.d/gofer_submit.json"
+        ])
+    ],
+    zip_safe=False
+)
