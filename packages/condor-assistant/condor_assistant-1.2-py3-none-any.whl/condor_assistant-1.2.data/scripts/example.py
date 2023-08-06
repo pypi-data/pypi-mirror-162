a0dd4dbcecafbe13547410dfb3d01c88293dd86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:25 +0000

    Add package condor-assistant/1.2/condor_assistant-1.2-py3-none-any.whl

---
+++ packages/condor-assistant/condor_assistant-1.2-py3-none-any.whl/condor_assistant-1.2.data/scripts/example.py
@@ -0,0 +1,28 @@
+#!python
+
+from condor_assistant import condor_booklist, createScript
+
+# create executable
+createScript("example.sh", """#!/bin/bash
+string=$1
+echo "the string is ${string}"
+""")
+
+# setup the condor bookklist with executable, jobtype(optional) and jobname(optional)
+condor_list = condor_booklist('example.sh', 'example', 'test')
+
+# add an argument to the booklist
+condor_list.add_Argument('Hello World')
+
+# add multiple arguments to the booklist
+Arguments = ['Monkey', 'Badgers']
+condor_list.add_Argument(Arguments)
+
+# setup requested running time (default is 'tomrrow' => one day)
+condor_list.set_JobFlavour('longlunch')
+
+# print out the condor setting
+condor_list.summary('Basic')
+
+# submit the scheduled jobs
+condor_list.submit()
