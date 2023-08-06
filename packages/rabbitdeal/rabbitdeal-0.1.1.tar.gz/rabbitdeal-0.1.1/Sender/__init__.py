d62ca8bdfe973df93cb3f55406a619cb4d172664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:43 +0000

    Add package rabbitdeal/0.1.1/rabbitdeal-0.1.1.tar.gz

---
+++ packages/rabbitdeal/rabbitdeal-0.1.1.tar.gz/rabbitdeal-0.1.1/Sender/__init__.py
@@ -0,0 +1,1 @@
+from Sender.Sender import Sender
---
+++ packages/rabbitdeal/rabbitdeal-0.1.1.tar.gz/rabbitdeal-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['Receiver', 'Sender']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pika>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'rabbitdeal',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'germangerken',
+    'author_email': 'germangerken@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MattiooFR/package_name',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
