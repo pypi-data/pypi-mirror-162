8a268eaae0feadbf0074524bd20ddab6ec0e69e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:53 +0000

    Add package makepackage/0.1.2/makepackage-0.1.2.tar.gz

---
+++ packages/makepackage/makepackage-0.1.2.tar.gz/makepackage-0.1.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extras_requirements = {
+    "dev": ["wheel", "black", "pytest", "mypy", ],
+}
+
+setuptools.setup(
+    name="makepackage",
+    version="0.1.2",
+    author="Nyggus",
+    author_email="nyggus@gmail.com",
+    description="Creating a structure of a simple Python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/nyggus/makepackage",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "easycheck",
+    ],
+    python_requires=">=3.6",
+    extras_require=extras_requirements,
+    entry_points={
+        "console_scripts": ["makepackage = makepackage.__main__:main"]
+    },
+)
