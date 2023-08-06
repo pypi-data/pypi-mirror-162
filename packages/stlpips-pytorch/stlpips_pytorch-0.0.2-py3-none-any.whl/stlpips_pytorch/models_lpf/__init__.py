a1f716dc7e5318c931101862a384249f470078c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:38 +0000

    Add package stlpips-pytorch/0.0.2/stlpips_pytorch-0.0.2-py3-none-any.whl

---
+++ packages/stlpips-pytorch/stlpips_pytorch-0.0.2-py3-none-any.whl/stlpips_pytorch/models_lpf/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2019, Adobe Inc. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
+# 4.0 International Public License. To view a copy of this license, visit
+# https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
+
+from .downsample import *
+from .resnet import *
+from .vgg import *
+from .squeezenet import *
+from .vgg_w_skip import *
