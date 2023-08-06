f8c159abcba6f0adb19f048764681b5d3da03192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:30 +0000

    Add package gensbo/0.1.1/gensbo-0.1.1.tar.gz

---
+++ packages/gensbo/gensbo-0.1.1.tar.gz/gensbo-0.1.1/gensbo/__version__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ======================================================================
+#
+#  gensbo - a GENeral Simulation Based Optimizer
+#
+#           一个基于仿真的优化器
+#
+# ======================================================================
+#
+#  author        :    Mingtao Li
+#  date          :    2019.03.26
+#  contributors  :    Xiaohai Zhang
+#  email         :    mingtao.li@gmail.com
+#  url           :    https://www.mingtaoli.cn
+#
+# ======================================================================
+
+__version__ = '0.0.3'
