7ef2568fcf9bc84f6db14e54ba87412830bc58ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:22 +0000

    Add package distributions-amiref/0.1/distributions_amiref-0.1.tar.gz

---
+++ packages/distributions-amiref/distributions_amiref-0.1.tar.gz/distributions_amiref-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_amiref',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_amiref'],
+      zip_safe=False)
