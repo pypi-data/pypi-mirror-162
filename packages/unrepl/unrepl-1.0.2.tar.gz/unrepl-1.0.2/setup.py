5b779a9e6ed4eb99114f288774a8464490b1e5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:38 +0000

    Add package unrepl/1.0.2/unrepl-1.0.2.tar.gz

---
+++ packages/unrepl/unrepl-1.0.2.tar.gz/unrepl-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="unrepl",
+    packages=["unrepl"],
+    version="1.0.2",
+    include_package_data=True,
+    long_description="unrepl",
+    description="translated REPL code fragments to proper Python code",
+    author="Ruud van der Ham",
+    author_email="info@salabim.org",
+    url="https://github.com/salabim/unrepl",
+    download_url="https://github.com/salabim/unrepl",
+    keywords=["repl"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+)
