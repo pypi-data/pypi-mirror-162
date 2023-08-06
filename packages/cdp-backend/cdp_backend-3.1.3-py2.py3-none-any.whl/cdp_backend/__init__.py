0a98818ebeb5dc7b45200376c3f60ed53135b0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:07 +0000

    Add package cdp-backend/3.1.3/cdp_backend-3.1.3-py2.py3-none-any.whl

---
+++ packages/cdp-backend/cdp_backend-3.1.3-py2.py3-none-any.whl/cdp_backend/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cdp-backend."""
+
+__author__ = "Council Data Project Contributors"
+__email__ = "evamaxfieldbrown@gmail.com"
+
+from .version import __version__  # noqa: F401
