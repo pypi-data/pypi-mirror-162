6437090a4d1d2268ed3575b56d053c35193bf67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package policytester/0.1.4/policytester-0.1.4.tar.gz

---
+++ packages/policytester/policytester-0.1.4.tar.gz/policytester-0.1.4/setup.py
@@ -0,0 +1,7 @@
+
+# Workaround to enable editable install “Project file has a ‘pyproject.toml’ and its
+# build backend is missing the ‘build_editable’ hook.”
+
+from setuptools import setup
+
+setup()
