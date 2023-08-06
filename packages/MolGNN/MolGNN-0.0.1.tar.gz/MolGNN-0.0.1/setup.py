8f5469315d7ca2d3930245967f63bc2f31009f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:34 +0000

    Add package MolGNN/0.0.1/MolGNN-0.0.1.tar.gz

---
+++ packages/MolGNN/MolGNN-0.0.1.tar.gz/MolGNN-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MolGNN',
+    version='0.0.2',
+    packages=find_packages(),
+    url='',
+    license='MIT License',
+    author='Giulio Mattedi',
+    author_email='lxu110@stu.suda.edu.cn',
+    description='',
+    python_requires='>=3.5, <=3.8',
+    
+    #install_requires=['rdkit', 'torch', 'numpy', 'torch-geometric', 'torch-scatter', 'torch-sparse']
+)
