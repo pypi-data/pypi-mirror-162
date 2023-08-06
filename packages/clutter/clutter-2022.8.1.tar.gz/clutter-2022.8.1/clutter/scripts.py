b45de65056483ba8035ec2cbff55aa548fd7cde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:23 +0000

    Add package clutter/2022.8.1/clutter-2022.8.1.tar.gz

---
+++ packages/clutter/clutter-2022.8.1.tar.gz/clutter-2022.8.1/clutter/scripts.py
@@ -0,0 +1,26 @@
+import click
+
+from .docker import load_secrets, SECRETS_DIR, SECRETS_DELIM
+from .aws import list_secrets
+
+
+@click.group()
+def clutter():
+    pass
+
+
+@clutter.command()
+@click.argument("patterns", nargs=-1)
+@click.option("--profile", default=None)
+def list_aws_secrets(patterns, profile):
+    patterns = patterns if patterns else "*"
+    list_secrets(patterns=patterns, profile_name=profile)
+
+
+# [NOTE]
+# Child Process Cannot Load Parent's Environment Variables!
+# @clutter.command()
+# @click.option("--secrets-dir", type=click.Path(exists=True, file_okay=False), default=SECRETS_DIR, show_default=True)
+# @click.option("--secrets-delim", type=str, default=SECRETS_DELIM, show_default=True)
+# def load_envs_from_docker_secrets(secrets_dir, secrets_delim):
+#     load_secrets(secrets_dir=secrets_dir, secrets_delim=secrets_delim)
