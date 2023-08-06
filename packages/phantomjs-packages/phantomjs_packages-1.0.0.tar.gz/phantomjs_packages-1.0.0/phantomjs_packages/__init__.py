6569c5795a5aa11cd01aad2b05084d71ff2cf1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:16 +0000

    Add package phantomjs-packages/1.0.0/phantomjs_packages-1.0.0.tar.gz

---
+++ packages/phantomjs-packages/phantomjs_packages-1.0.0.tar.gz/phantomjs_packages-1.0.0/phantomjs_packages/__init__.py
@@ -0,0 +1,15 @@
+from .main import phantomjs_mac, phantomjs_linux, phantomjs_windows
+
+__all__=[
+    'phantomjs_mac', 'phantomjs_linux', 'phantomjs_windows'
+]
+
+
+__version__='1.0.0'
+__license__='GPL-2.0 License'
+__author__='Tiancheng Jiao'
+__url__='https://github.com/jtc1246'
+__author_email__='jtc1246@outlook.com'
+__description__='Phantomjs packages for different systems.'
+
+
---
+++ packages/phantomjs-packages/phantomjs_packages-1.0.0.tar.gz/phantomjs_packages-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='phantomjs_packages',
+    version='1.0.0',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246',
+    description='Phantomjs packages for different systems.',
+    packages=['phantomjs_packages'],
+    install_requires=[],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License'
+)
+
