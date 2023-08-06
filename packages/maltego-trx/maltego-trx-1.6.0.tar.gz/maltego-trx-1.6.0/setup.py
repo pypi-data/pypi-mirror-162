f8df8be976927ca2668b95645312d27c3aafc85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:38 +0000

    Add package maltego-trx/1.6.0/maltego-trx-1.6.0.tar.gz

---
+++ packages/maltego-trx/maltego-trx-1.6.0.tar.gz/maltego-trx-1.6.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from maltego_trx import VERSION
+
+with open("README.md", "r") as readme_md:
+    long_description = readme_md.read()
+
+setup(
+    name='maltego-trx',
+    version=VERSION,
+    description='Python library used to develop Maltego transforms',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/paterva/maltego-trx/',
+    author='Maltego Staff',
+    author_email='support@maltego.com',
+    license='MIT',
+    install_requires=[
+        'flask>=1',
+        'six>=1',
+        'cryptography==3.3.2'  # pinned for now as newer versions require setuptools_rust
+    ],
+    extras_require={
+        ':python_version == "3.6"': [
+            'dataclasses',
+        ],
+    },
+    packages=[
+        'maltego_trx',
+        'maltego_trx/template_dir',
+        'maltego_trx/template_dir/transforms',
+    ],
+    package_data={
+        'maltego_trx/template_dir': [
+            'settings.csv',
+            'transforms.csv',
+            'docker-compose.yml',
+            'Dockerfile',
+            'requirements.txt',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'maltego-trx = maltego_trx.commands:execute_from_command_line',
+        ]
+    },
+    zip_safe=False
+)
