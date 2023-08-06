132b62cb164cdec899904805e62e7568cb1fb9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:10 +0000

    Add package Data-Adaptor-sourav/0.1.9/Data_Adaptor_sourav-0.1.9.tar.gz

---
+++ packages/Data-Adaptor-sourav/Data_Adaptor_sourav-0.1.9.tar.gz/Data_Adaptor_sourav-0.1.9/setup.py
@@ -0,0 +1,28 @@
+"""
+setup file for python package
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="Data_Adaptor_sourav",
+    version="0.1.9",
+    author="Crossml",
+    author_email="sourav@crossml.com",
+    packages=setuptools.find_packages(),
+    description="Input Adaptor to verify file extension",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords=['Input-adaptor', 'Adaptor', 'DataAdaptor'],
+    python_requires='>=3',
+    install_requires=['boto3', 'requests', ]
+)
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
