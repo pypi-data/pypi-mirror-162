0b70229bac095d906c3b29cfe83237d52c40dc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:33 +0000

    Add package padacioso/0.1.2/padacioso-0.1.2.tar.gz

---
+++ packages/padacioso/padacioso-0.1.2.tar.gz/padacioso-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='padacioso',
+    version='0.1.2',
+    packages=['padacioso'],
+    url='https://github.com/OpenJarbas/padacioso',
+    license='apache-2.0',
+    author='jarbasai',
+    author_email='jarbasai@mailfence.com',
+    install_requires=["simplematch"],
+    description='dead simple intent parser'
+)
