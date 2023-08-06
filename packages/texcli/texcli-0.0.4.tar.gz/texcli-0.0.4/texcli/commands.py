066d3eaac7f7898a6eafde45df1073ddea51728f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:37 +0000

    Add package texcli/0.0.4/texcli-0.0.4.tar.gz

---
+++ packages/texcli/texcli-0.0.4.tar.gz/texcli-0.0.4/texcli/commands.py
@@ -0,0 +1,31 @@
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def latex(tex, output_file, dpi=300):
+    tex = '$ ' + tex + ' $'
+
+    plt.rcParams.update({'mathtext.fontset': 'cm'})
+    fig = plt.figure(figsize=(10, 10), dpi=100)
+
+    t = fig.text(0, 0, tex,
+                horizontalalignment='left', verticalalignment='bottom',
+                fontsize=30,
+                )
+
+    r = fig.canvas.get_renderer()
+
+    bbox = t.get_tightbbox(r)
+    w, h = (bbox.width / r.dpi, bbox.height / r.dpi)
+
+    fig = plt.figure(figsize=(1.1 * w, 1.1 * h), dpi=dpi)
+    t = fig.text(0, 0, tex, fontsize=30,
+                 verticalalignment="bottom", horizontalalignment="left",
+                 )
+
+    fig.savefig(output_file)
+
+
+if __name__ == '__main__':
+    latex(r'\frac{\partial}{\partial z}', 'test.png')
