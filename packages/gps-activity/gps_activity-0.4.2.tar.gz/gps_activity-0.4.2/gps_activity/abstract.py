415b0391eb4fd56c4c96c5dcd22283312c843121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:56 +0000

    Add package gps-activity/0.4.2/gps_activity-0.4.2.tar.gz

---
+++ packages/gps-activity/gps_activity-0.4.2.tar.gz/gps_activity-0.4.2/gps_activity/abstract.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+
+
+class AbstractNode(ABC, BaseEstimator, TransformerMixin):
+    @abstractmethod
+    def fit(self, X: pd.DataFrame, y=None):
+        pass
+
+    @abstractmethod
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def fit_transform(self, X: pd.DataFrame, y=None):
+        self.fit(X, y)
+        return self.transform(X)
+
+
+class AbstractPredictor(ABC, BaseEstimator, TransformerMixin):
+    @abstractmethod
+    def fit(self, X: pd.DataFrame, y=None):
+        pass
+
+    @abstractmethod
+    def predict(self, X: pd.DataFrame) -> Union[List[Union[int, float, str]], np.array]:
+        pass
+
+    def fit_predict(self, X: pd.DataFrame, y=None) -> Union[List[Union[int, float, str]], np.array]:
+        self.fit(X, y)
+        return self.predict(X)
+
+
+class AbstractPipelineFactory(ABC):
+    @staticmethod
+    @abstractmethod
+    def factory_pipeline(*args, **kwargs) -> Pipeline:
+        pass
---
+++ packages/gps-activity/gps_activity-0.4.2.tar.gz/gps_activity-0.4.2/gps_activity/extraction/nodes/__init__.py
@@ -0,0 +1,8 @@
+from .fdbscan import FDBSCAN
+from .velocity_fragmentator import VelocityFragmentator
+
+
+__all__ = [
+    "FDBSCAN",
+    "VelocityFragmentator",
+]
