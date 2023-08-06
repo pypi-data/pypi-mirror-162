c8bc1dbe390491bd9a2fd969043037f3ca8f935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:37 +0000

    Add package no-spark-in-my-home/0.1.0/no_spark_in_my_home-0.1.1.tar.gz

---
+++ packages/no-spark-in-my-home/no_spark_in_my_home-0.1.1.tar.gz/no_spark_in_my_home-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='no_spark_in_my_home',
+    version='0.1.0',
+    author=['scripthound, nomilkinmyhome'],
+    setup_requires=['wheel', 'faker', 'pyspark'],
+    packages=find_packages(include=['src', 'src.*'])
+)
