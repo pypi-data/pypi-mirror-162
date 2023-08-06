c9dd3f17864d326744e0d47cdb3ede855231b12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package slack-transfer/0.1.1a1/slack-transfer-0.1.1a1.tar.gz

---
+++ packages/slack-transfer/slack-transfer-0.1.1a1.tar.gz/slack-transfer-0.1.1a1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['slack_transfer', 'slack_transfer.functions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8.2,<3.0.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'slack-sdk>=3.18.1,<4.0.0',
+ 'tqdm>=4.64.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'slack-transfer',
+    'version': '0.1.1a1',
+    'description': '',
+    'long_description': '# slack_transfer',
+    'author': 'Masanori HIRANO',
+    'author_email': 'masa.hirano.1996@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
