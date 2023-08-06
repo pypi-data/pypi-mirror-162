f1754bcb4a81626782de61f0931ba6819bcf4aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:49 +0000

    Add package tcli-bar/0.1/tcli_bar-0.1-py3-none-any.whl

---
+++ packages/tcli-bar/tcli_bar-0.1-py3-none-any.whl/tcli_bar.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+
+def bar(progress, total, icon='█', begin='Downloading', show_percent=True):
+    '''
+    Be sure to print a ('\\n') after running the bar function!
+    You can find out why ;)
+    '''
+    percent = 50 * (progress / float(total))
+    some = percent - 50
+    bar = icon * int(percent) + '-' * (50 - int(percent))
+    if show_percent == True:
+        print(f"\r{begin} |{bar}| {50 + percent:.2f}%", end="")
+    else:
+        print(f"\r{begin} |{bar}|", end="")
+
+def rotate(begin = 'Downloading', sleep_value = 0.1):
+    list = ['|', '/', '-', '\\']
+    ting = '|'
+    for i in list:
+        sleep(sleep_value)
+        ting = i
+        print(f'\r{begin} {ting}', end='\r')
+
+import math
+numbers = [x * 5 for x in range(100, 2000)]
+list = []
+for i, a in enumerate(numbers):
+    list.append(math.factorial(i))
+    bar(i + 1, len(numbers))
