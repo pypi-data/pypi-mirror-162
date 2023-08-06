2b6492cffa9798644a69bcf38c29718d30558529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:37 +0000

    Add package mac-cleanup/2.2.1/mac_cleanup-2.2.1-py3-none-any.whl

---
+++ packages/mac-cleanup/mac_cleanup-2.2.1-py3-none-any.whl/mac_cleanup/console.py
@@ -0,0 +1,50 @@
+from mac_cleanup import __version__
+from argparse import ArgumentParser, RawTextHelpFormatter
+from rich.console import Console
+from rich.theme import Theme
+
+parser = ArgumentParser(
+    description=
+    f"""\
+    A Mac Cleanup Utility in Python
+    {__version__}
+    https://github.com/mac-cleanup/mac-cleanup-py\
+    """,
+    formatter_class=RawTextHelpFormatter,
+)
+
+parser.add_argument(
+    "-d", "--dry-run",
+    help="Shows approx space to be cleaned",
+    action="store_true"
+)
+
+parser.add_argument(
+    "-u", "--update",
+    help="Script will update brew while cleaning",
+    action="store_true"
+)
+
+parser.add_argument(
+    "-c", "--configure",
+    help="Launch modules configuration",
+    action="store_true"
+)
+
+parser.add_argument(
+    "-m", "--modules",
+    help="Specify custom modules' path",
+    action="store_true"
+)
+
+args = parser.parse_args()
+# args.dry_run = True  # debug
+# args.configure = True  # debug
+# args.modules = True  # debug
+
+custom_theme = Theme({
+    "info": "dim cyan",
+    "warning": "magenta",
+    "danger": "bold red"
+})  # WIP
+console = Console(theme=custom_theme)
