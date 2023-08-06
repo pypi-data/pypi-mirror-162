313157aa975d148935820dae497985e0afa77f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:12 +0000

    Add package json-queries/0.1.14/json-queries-0.1.14.tar.gz

---
+++ packages/json-queries/json-queries-0.1.14.tar.gz/json-queries-0.1.14/jsonutils/config/__init__.py
@@ -0,0 +1,9 @@
+from .completion import AUTOCOMPLETE_ONLY_NODES
+from .locals import DECIMAL_SEPARATOR, THOUSANDS_SEPARATOR
+from .queries import (
+    CLEVER_PARSING,
+    INCLUDE_PARENTS,
+    NATIVE_TYPES,
+    QUERY_EXCEPTIONS,
+    RECURSIVE_QUERIES,
+)
