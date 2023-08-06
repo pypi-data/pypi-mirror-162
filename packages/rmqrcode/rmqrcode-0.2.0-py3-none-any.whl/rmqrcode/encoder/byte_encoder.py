2aeeaf15694f09f4552e68c8b386fe5f5380798e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:41 +0000

    Add package rmqrcode/0.2.0/rmqrcode-0.2.0-py3-none-any.whl

---
+++ packages/rmqrcode/rmqrcode-0.2.0-py3-none-any.whl/rmqrcode/encoder/byte_encoder.py
@@ -0,0 +1,21 @@
+class ByteEncoder:
+    MODE_INDICATOR = "011"
+
+    @staticmethod
+    def _encoded_bits(s):
+        res = ""
+        encoded = s.encode("utf-8")
+        for byte in encoded:
+            res += bin(byte)[2:].zfill(8)
+        return res
+
+    @staticmethod
+    def encode(data, character_count_length):
+        res = ByteEncoder.MODE_INDICATOR
+        res += bin(len(data))[2:].zfill(character_count_length)
+        res += ByteEncoder._encoded_bits(data)
+        return res
+
+    @staticmethod
+    def length(data):
+        return len(data.encode("utf-8"))
