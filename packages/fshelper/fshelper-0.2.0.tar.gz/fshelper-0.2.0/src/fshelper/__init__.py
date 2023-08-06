051003dee74618d5dea42e8185647bae37f38bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:06 +0000

    Add package fshelper/0.2.0/fshelper-0.2.0.tar.gz

---
+++ packages/fshelper/fshelper-0.2.0.tar.gz/fshelper-0.2.0/src/fshelper/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for fshelper."""
+
+__author__ = """Matthew Larsen"""
+__email__ = "matt.larsen@connorgp.com"
+__version__ = "0.2.0"
+
+from .api import Credential, RequestService
+from .v2 import (
+    ServiceItemsEndPoint,
+    TicketFormFieldsEndPoint,
+    TicketsEndPoint,
+    AssetsEndPoint,
+)
---
+++ packages/fshelper/fshelper-0.2.0.tar.gz/fshelper-0.2.0/src/fshelper/v2/__init__.py
@@ -0,0 +1,4 @@
+from .service_items_endpoint import ServiceItemsEndPoint
+from .ticket_form_fields import TicketFormFieldsEndPoint
+from .tickets_endpoint import TicketsEndPoint
+from .assets import AssetsEndPoint
