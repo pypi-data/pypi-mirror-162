25586dce8c223f9d4eea2459ba211a533f2d1a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:04 +0000

    Add package eb-aws-sso/0.0.8/eb-aws-sso-0.0.8.tar.gz

---
+++ packages/eb-aws-sso/eb-aws-sso-0.0.8.tar.gz/eb-aws-sso-0.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+
+
+root_path = os.path.join(os.path.dirname(__file__))
+file = open(os.path.join(root_path, "README.md"))
+readme_file = file.read()
+file.close()
+
+install_package_requires = [
+    "boto3>=1.24,<2",
+]
+
+setup(
+    name="eb-aws-sso",
+    version="0.0.8",
+    python_requires=">=3.8",
+    scripts=["awssso"],
+    install_requires=install_package_requires,
+    long_description=readme_file,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebanalyse/awssso",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+)
