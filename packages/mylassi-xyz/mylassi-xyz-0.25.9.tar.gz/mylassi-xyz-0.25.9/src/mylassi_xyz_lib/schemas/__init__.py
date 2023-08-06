2ed54caceeaf74bd98474d90b146c315acb825b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:32 +0000

    Add package mylassi-xyz/0.25.9/mylassi-xyz-0.25.9.tar.gz

---
+++ packages/mylassi-xyz/mylassi-xyz-0.25.9.tar.gz/mylassi-xyz-0.25.9/src/mylassi_xyz_lib/schemas/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'LoginRequestData', 'LoginRequestSchema',
+    'LoginResponseData', 'LoginResponseSchema',
+
+    'DocumentData', 'DocumentSchema',
+    'UserData', 'UserSchema'
+]
+
+from .apiv2 import *
+from .documents import *
+from .documents import *
+from .users import *
