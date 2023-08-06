ce579f07cc1c692cd8c4335df4469e950c11c8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package vid2info/1.19/vid2info-1.19.tar.gz

---
+++ packages/vid2info/vid2info-1.19.tar.gz/vid2info-1.19/vid2info/inference/__init__.py
@@ -0,0 +1,4 @@
+from . import detection, segmentation, tracking, utils
+from .config import XMIN, YMIN, XMAX, YMAX, CONFIDENCE, CLASS_ID, \
+    CLASS_NAME, CLASS_NAMES_LIST, BBOX_XYXY
+from .pipeline import Pipeline
