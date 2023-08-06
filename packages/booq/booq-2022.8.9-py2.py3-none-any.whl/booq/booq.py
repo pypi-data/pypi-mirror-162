9b861662b9d90eb1b4203d3da9911ddeb88b4287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:48 +0000

    Add package booq/2022.8.9/booq-2022.8.9-py2.py3-none-any.whl

---
+++ packages/booq/booq-2022.8.9-py2.py3-none-any.whl/booq/booq.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""API of interface operations for calculating binary year to date metrics with monthly cumulative slices.."""
+# import os
+import sys
+from typing import List, Union
+
+
+def main(argv: Union[List[str], None] = None) -> int:
+    argv = sys.argv[1:] if argv is None else argv
+    return 0
