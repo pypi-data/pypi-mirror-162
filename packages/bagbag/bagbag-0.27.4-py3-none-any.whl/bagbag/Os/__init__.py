21b9fe9bd0bb719ca732e81350cb36dfa5c136b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:57 +0000

    Add package bagbag/0.27.4/bagbag-0.27.4-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.27.4-py3-none-any.whl/bagbag/Os/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sys 
+from . import Path
+
+def Exit(num:int=0):
+    sys.exit(num)
+
+System = os.system 
+
+def Mkdir(path:str):
+    os.makedirs(path, exist_ok=True)
+
+def ListDir(path:str) -> list[str]:
+    return os.listdir(path)
+
+def Args() -> list[str]:
+    return sys.argv
