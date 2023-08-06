cb13f4ec9b343feaa449ab8c03f6f376549c0af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:18 +0000

    Add package pyxsim/4.0b4/pyxsim-4.0b4-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/pyxsim/pyxsim-4.0b4-cp38-cp38-macosx_10_9_x86_64.whl/pyxsim/__init__.py
@@ -0,0 +1,28 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyxsim")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+
+from pyxsim.event_list import \
+    EventList
+
+from pyxsim.photon_list import \
+    make_photons, \
+    project_photons, \
+    project_photons_allsky
+
+from pyxsim.source_models import \
+   SourceModel, \
+   CIESourceModel, \
+   NEISourceModel, \
+   LineSourceModel, \
+   PowerLawSourceModel, \
+   IGMSourceModel
+
+from pyxsim.utils import merge_files, \
+    compute_elem_mass_fraction, \
+    create_metal_fields
