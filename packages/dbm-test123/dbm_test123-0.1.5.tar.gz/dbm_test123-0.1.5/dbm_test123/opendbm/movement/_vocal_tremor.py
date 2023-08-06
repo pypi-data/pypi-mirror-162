fda8492224a00e99cb7a63e62572437b8a5e1e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:29 +0000

    Add package dbm-test123/0.1.5/dbm_test123-0.1.5.tar.gz

---
+++ packages/dbm-test123/dbm_test123-0.1.5.tar.gz/dbm_test123-0.1.5/dbm_test123/opendbm/movement/_vocal_tremor.py
@@ -0,0 +1,18 @@
+from dbm_test123.dbm_lib.dbm_features.raw_features.movement.voice_tremor import run_vtremor
+from dbm_test123.opendbm.model import VideoModel
+
+
+class VocalTremor(VideoModel):
+    def __init__(self):
+        super().__init__()
+        self._params = [
+            "mov_freqtremfreq",
+            "mov_freqtremindex",
+            "mov_freqtrempindex",
+            "mov_amptremfreq",
+            "mov_amptremindex",
+            "mov_amptrempindex",
+        ]
+
+    def _fit_transform(self, path):
+        return run_vtremor(path, ".", self.r_config, save=False)
