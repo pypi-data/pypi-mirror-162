b5ceebdc1efa2721ddbe75316731736f84913861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:02 +0000

    Add package markup-markdown/1.1.2/markup-markdown-1.1.2.tar.gz

---
+++ packages/markup-markdown/markup-markdown-1.1.2.tar.gz/markup-markdown-1.1.2/markup/Markers.py
@@ -0,0 +1,5 @@
+markup_markdown_end = "<!-- markup:markdown-end -->"
+markup_markdown_begin = "<!-- markup:markdown-begin -->"
+markup_markdown_skipline = "<!-- markup:skip-line -->"
+markup_markdown_blank= "<!-- markup:blank-line -->"
+markup_markdown_pagebreak= "<!-- markup:page-break-xml -->"
