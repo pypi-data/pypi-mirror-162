e902662ac9daa8cd67eee1c559521122813a7097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:26 +0000

    Add package example-publish-pypi-qytest/0.0.2/example_publish_pypi_qytest-0.0.2.tar.gz

---
+++ packages/example-publish-pypi-qytest/example_publish_pypi_qytest-0.0.2.tar.gz/example_publish_pypi_qytest-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="example_publish_pypi_qytest",
+    version="0.0.2",
+    license='MIT',
+    author="Qiuyu Lian",
+    author_email="qiuyu.lian@sjtu.edu.cn",
+    description="example of publishing pypi package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    #url="https://tomas.readthedocs.io/en/latest/",
+    #python_requires='>=3.8',
+    install_requires=[
+        "scikit-learn",
+
+    ]
+)
