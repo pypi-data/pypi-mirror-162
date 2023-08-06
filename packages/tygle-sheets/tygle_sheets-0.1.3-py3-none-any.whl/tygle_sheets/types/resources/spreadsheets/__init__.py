9928611476f43b2a37dd1d453fc097e64784f609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:36 +0000

    Add package tygle-sheets/0.1.3/tygle_sheets-0.1.3-py3-none-any.whl

---
+++ packages/tygle-sheets/tygle_sheets-0.1.3-py3-none-any.whl/tygle_sheets/types/resources/spreadsheets/__init__.py
@@ -0,0 +1,12 @@
+# isort: skip_file
+
+from .error_value import ErrorValue
+from .extended_value import ExtendedValue
+from .cell_data import CellData
+from .row_data import RowData
+from .grid_data import GridData
+from .grid_range import GridRange
+from .sheet_properties import SheetProperties
+from .sheet import Sheet
+from .spreadsheet_properties import SpreadsheetProperties
+from .spreadsheet import Spreadsheet, SpreadsheetRESTs
