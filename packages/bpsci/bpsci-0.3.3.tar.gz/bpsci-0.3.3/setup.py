5214824aee38525d8df96bfe445b9c967fda0d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:44 +0000

    Add package bpsci/0.3.3/bpsci-0.3.3.tar.gz

---
+++ packages/bpsci/bpsci-0.3.3.tar.gz/bpsci-0.3.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bpsci']
+
+package_data = \
+{'': ['*'], 'bpsci': ['assets/objects/*']}
+
+install_requires = \
+['numpy>=1.18.4,<2.0.0', 'pandas>=1.3.4,<2.0.0', 'scipy>=1.7.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'bpsci',
+    'version': '0.3.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Jerry Varghese',
+    'author_email': 'varghese.jerryj@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
