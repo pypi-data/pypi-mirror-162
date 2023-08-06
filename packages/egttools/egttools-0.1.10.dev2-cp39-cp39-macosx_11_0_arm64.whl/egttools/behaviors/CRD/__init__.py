84b8005d92871012677f33e99542bfd8b09fcb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package egttools/0.1.10.dev2/egttools-0.1.10.dev2-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/egttools/egttools-0.1.10.dev2-cp39-cp39-macosx_11_0_arm64.whl/egttools/behaviors/CRD/__init__.py
@@ -0,0 +1,7 @@
+"""
+API reference documentation for `behaviors.CRD` submodule.
+"""
+
+from egttools.numerical.behaviors.CRD import AbstractCRDStrategy, CRDMemoryOnePlayer
+from .moving_average import MovingAverageCRDStrategy
+from .goal_based import GoalBasedCRDStrategy
