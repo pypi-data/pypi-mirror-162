c235f150b70e9a43c226d1392e6598ef48b40301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:03 +0000

    Add package datedays/2.0.0/datedays-2.0.0.tar.gz

---
+++ packages/datedays/datedays-2.0.0.tar.gz/datedays-2.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datedays",
+    version="2.0.0",
+    author="liang1024",
+    author_email="11751155@qq.com",
+    description="Welcome to install and use datedays",
+    long_description="Get the list of days in the format '%y-%m-%d'，For example: ['2022-08-05','2022-08-06','2022-08-07', ...]。Welcome to install and use",
+    long_description_content_type="text/markdown",
+    url="https://github.com/liang1024/datedays",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
