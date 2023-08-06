da6ec9dc24a295fb9d434362c2914cb6508abab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:46 +0000

    Add package nonebot-plugin-bangumi-search/1.0.1/nonebot-plugin-bangumi-search-1.0.1.tar.gz

---
+++ packages/nonebot-plugin-bangumi-search/nonebot-plugin-bangumi-search-1.0.1.tar.gz/nonebot-plugin-bangumi-search-1.0.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(name="nonebot-plugin-bangumi-search",
+    version="1.0.1",
+    description="a nonebot2 plugin to help you search anime on bgm.tv and return some information to you",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["nonebot-plugin-bangumi-search"],
+    author="Ankhyty",
+    author_email="ankhyty@gmail.com",
+    url="https://github.com/Ankhyty/nonebot-plugin-bangumi-search",
+    python_requires=">=3.8.10"
+)
