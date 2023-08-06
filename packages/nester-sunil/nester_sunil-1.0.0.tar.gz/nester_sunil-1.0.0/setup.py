a00eca6451339fe109c47777ccd7e05dffddffa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:06 +0000

    Add package nester-sunil/1.0.0/nester_sunil-1.0.0.tar.gz

---
+++ packages/nester-sunil/nester_sunil-1.0.0.tar.gz/nester_sunil-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+ name = 'nester_sunil',
+ version = '1.0.0',
+ py_modules = ['nester_sunil'],
+ author = 'sunil',
+ author_email = 'nothingelse007@gmail.com',
+ url = 'http://127.0.0.1',
+ description = 'A simple printer of nested lists',
+ )
