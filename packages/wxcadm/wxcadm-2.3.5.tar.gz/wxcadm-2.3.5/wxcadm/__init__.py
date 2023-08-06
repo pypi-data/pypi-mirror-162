d77cedefb1ae0e231273287bb6e6d65b667a076b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:28 +0000

    Add package wxcadm/2.3.5/wxcadm-2.3.5.tar.gz

---
+++ packages/wxcadm/wxcadm-2.3.5.tar.gz/wxcadm-2.3.5/wxcadm/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+from .wxcadm import Webex, RedSky, XSIEvents, XSICallQueue, Call
+from .exceptions import (
+    OrgError, LicenseError, APIError, TokenError,
+    PutError, XSIError, NotAllowed, CSDMError
+)
+
+from .common import decode_spark_id
+from .wxcadm import console_logging
+
+# Set up NullHandler for logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
