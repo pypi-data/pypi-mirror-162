2d1a4e7a5c162453799eee54df100c43eec88248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:38 +0000

    Add package types-sqlalchemy-utils/1.0.1/types_sqlalchemy_utils-1.0.1-py3-none-any.whl

---
+++ packages/types-sqlalchemy-utils/types_sqlalchemy_utils-1.0.1-py3-none-any.whl/sqlalchemy_utils-stubs/primitives/weekdays.pyi
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Hashable, Generator, Union
+
+from ..utils import str_coercible as str_coercible
+from .weekday import WeekDay as WeekDay
+
+@str_coercible
+class WeekDays:
+    def __init__(self, bit_string_or_week_days: Union[str, WeekDays, Hashable]) -> None:
+        pass
+    def __iter__(self) -> Generator[WeekDay, None, None]:
+        pass
+    def as_bit_string(self) -> str:
+        pass
+
