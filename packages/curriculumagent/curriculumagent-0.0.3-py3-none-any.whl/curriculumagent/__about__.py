36a454fe650e55f2ade13a6af6f0382710d9df9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:12 +0000

    Add package curriculumagent/0.0.3/curriculumagent-0.0.3-py3-none-any.whl

---
+++ packages/curriculumagent/curriculumagent-0.0.3-py3-none-any.whl/curriculumagent/__about__.py
@@ -0,0 +1,9 @@
+# Provide version-number etc.
+# This file is automatically edited by git!
+# This file must not have any sideeffects or do anything but define meta variables!
+__copyright__ = '2018, Fraunhofer IEE'
+__title__ = 'CurriculumAgent'
+__description__ =  "CurriculumAgent is a Reinforcement Learning Agent designed to learn from and act within the Grid2Op Environments."
+__author__ = 'OE224'
+__authors__ = ["Malte Lehna"]
+__version__ = "v0.0.1"
