2e3e6a0cff1c69a291f4d077ab4bd511c1a28235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:39 +0000

    Add package pexpect-executor/2.1.1/pexpect_executor-2.1.1-py3-none-any.whl

---
+++ packages/pexpect-executor/pexpect_executor-2.1.1-py3-none-any.whl/pexpect_executor/package/bundle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Bundle class, pylint: disable=too-few-public-methods
+class Bundle:
+
+    # Names
+    NAME = 'pexpect-executor'
+
+    # Environment
+    ENV_ENGINE = 'EXECUTOR_ENGINE'
+    ENV_HOST = 'EXECUTOR_HOST'
+    ENV_TOOL = 'EXECUTOR_TOOL'
