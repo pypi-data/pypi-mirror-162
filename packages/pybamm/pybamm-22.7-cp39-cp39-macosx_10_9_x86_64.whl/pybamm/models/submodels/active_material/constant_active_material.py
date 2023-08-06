2e79d18ff513a77187087a2f76628ce19a4beeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package pybamm/22.7/pybamm-22.7-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pybamm/pybamm-22.7-cp39-cp39-macosx_10_9_x86_64.whl/pybamm/models/submodels/active_material/constant_active_material.py
@@ -0,0 +1,35 @@
+#
+# Class for constant active material
+#
+import pybamm
+
+from .base_active_material import BaseModel
+
+
+class Constant(BaseModel):
+    """Submodel for constant active material
+
+    Parameters
+    ----------
+    param : parameter class
+        The parameters to use for this submodel
+    domain : str
+        The domain of the model either 'Negative' or 'Positive'
+    options : dict
+        Additional options to pass to the model
+
+    **Extends:** :class:`pybamm.active_material.BaseModel`
+    """
+
+    def get_fundamental_variables(self):
+        eps_solid = self.domain_param.epsilon_s
+        deps_solid_dt = pybamm.FullBroadcast(
+            0, f"{self.domain.lower()} electrode", "current collector"
+        )
+
+        variables = self._get_standard_active_material_variables(eps_solid)
+        variables.update(
+            self._get_standard_active_material_change_variables(deps_solid_dt)
+        )
+
+        return variables
