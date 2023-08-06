b97f8960fd07d0284835fdbd46bfcfe6ec614214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:01 +0000

    Add package binpan/0.0.74/binpan-0.0.74.tar.gz

---
+++ packages/binpan/binpan-0.0.74.tar.gz/binpan-0.0.74/handlers/messages.py
@@ -0,0 +1,278 @@
+from .starters import AesCipher
+import requests
+from .logs import Logs
+from .time_helper import convert_milliseconds_to_str
+# from .time_helper import convert_utc_ms_column_to_time_zone
+from .wallet import get_spot_trades_list, get_spot_balances_df, get_spot_balances_total_value
+from pandas import DataFrame
+
+msg_logger = Logs(filename='./logs/msg_logger.log', name='msg_logger', info_level='INFO')
+
+cipher_object = AesCipher()
+
+try:
+    from secret import encoded_chat_id, encoded_telegram_bot_id
+
+except Exception as exc:
+    msg = """
+Not found telegram bot key or chat key for the telegram message module.
+
+Example adding bot key and chat id:
+    
+    from binpan import handlers
+
+    # from @BotFather, get the bot api key
+    handlers.files.add_any_key(key="xxxxxxxxxx", key_name="encoded_telegram_bot_id")
+
+    # write to your bot and then get your chat id from https://api.telegram.org/bot<YourBOTToken>/getUpdates
+    handlers.files.add_any_key(key="xxxxxxxxxx", key_name="encoded_chat_id")
+"""
+    msg_logger.warning(msg)
+    encoded_telegram_bot_id = ''
+    encoded_chat_id = ''
+
+
+def telegram_bot_send_text(msg: dict or str,
+                           parse_mode='Markdown&text',
+                           disable_notification=False,
+                           alt_enc_bot_id: str = None,
+                           alt_chat_id: str = None) -> dict:
+    """
+    Sends a telegram message.
+
+    It takes bot key and chat id from secret encrypted file. It is required to create previously the bot and chat keys with the
+    variable names *encoded_chat_id*, *encoded_telegram_bot_id*.
+
+    Example adding bot key and chat id:
+
+    .. code-block:: python
+
+        from binpan import handlers
+
+        # from @BotFather, get the bot api key
+        handlers.files.add_any_key(key="xxxxxxxxxx", key_name="encoded_telegram_bot_id")
+
+        # write to your bot and then get your chat id from https://api.telegram.org/bot<YourBOTToken>/getUpdates
+        handlers.files.add_any_key(key="xxxxxxxxxx", key_name="encoded_chat_id")
+
+
+    :param str msg: A message
+    :param str parse_mode: Parsing telegram message. Default is 'Markdown&text'.
+    :param disable_notification: Avoids getting notified by the telegram message. Only working in group chats.
+        https://stackoverflow.com/questions/64360221/disable-notification-in-python-telegram-bot
+    :param str alt_enc_bot_id: Optional alternative bot api key.
+    :param str alt_chat_id: Optional alternative chat id.
+    :return: Telegrams API response.
+    """
+
+    if type(msg) == dict:
+        bot_message = str(msg.copy())
+    else:
+        bot_message = str(msg)
+
+    if not alt_enc_bot_id:
+        alt_enc_bot_id = cipher_object.decrypt(encoded_telegram_bot_id)
+    if not alt_chat_id:
+        alt_chat_id = cipher_object.decrypt(encoded_chat_id)
+
+    send_text = f'https://api.telegram.org/bot{alt_enc_bot_id}/sendMessage?chat_id={alt_chat_id}&' \
+                f'parse_mode={parse_mode}={bot_message}'
+
+    if disable_notification:
+        send_text += "&disable_notification=true"
+
+    response = requests.get(send_text)
+
+    msg_logger.info("TELEGRAM " + " ".join(bot_message.replace('\n', ' ').split()))
+
+    return response.json()
+
+
+def telegram_parse_dict(msg_data: dict, timezone='UTC'):
+    """
+    Parses a dict and downcast types.
+
+    :param dict msg_data: Dict to parse as message.
+    :param str timezone: A time zone to parse more human readable any field containing "time" in the name.
+    :return str: A markdown v1 parsed telegram string.
+    """
+    msg_dict = msg_data.copy()
+    parsed_msg = ""
+    for k, v in msg_dict.items():
+        try:
+            fv1 = float(v)
+        except:
+            fv1 = v
+        try:
+            fv2 = int(fv1)
+            assert fv2 == fv1
+        except:
+            fv2 = fv1
+        if 'pct' in k:
+            fv2 = fv2 * 100
+            row = f"*{k}* : `{fv2:.2f}`\n"
+        elif type(fv2) == float:
+            row = f"*{k}* : `{fv2:.8f}`\n"
+        elif type(fv2) == int:
+            row = f"*{k}* : `{fv2}`\n"
+        else:
+            if 'time' in k:
+                date = convert_milliseconds_to_str(ms=fv2, timezoned=timezone)
+                row = f"*{k}* : {date}\n"
+            else:
+                row = f"*{k}* : {fv2}\n"
+        parsed_msg += row
+    return parsed_msg
+
+
+# futuro bot #
+
+
+def tab_str(text: str, indentation=8) -> str:
+    """
+    Indentation for telegram messages.
+    :param str text: A message with spaces to insert tabulations in.
+    :param int indentation: The spaces quantity to insert.
+    :return str: Tabulated message.
+    """
+    sp = text.split()
+    ret = ""
+    for i in sp:
+        length = len(i)
+        ind = (indentation - length) % indentation
+        ret += i + " " * ind
+    return ret
+
+
+def telegram_parse_dataframe_markdown(data: DataFrame,
+                                      indentation: int = 6,
+                                      title: str = "Balances") -> str:
+    """
+    Parses a Dataframe in telegrams message format.
+
+    :param DataFrame data: A dataframe
+    :param int indentation: Spaces to insert each tabulation.
+    :param str title: a header or title to the result.
+    :return str: String parsed with tabulations.
+    """
+    ret = f"*{title}*: \n"
+    cols = data.columns
+    ret += "```\n"
+    for idx, row in data.iterrows():
+        data_row = row.tolist()
+        str_row = f" {idx} " + ''.join([f"{cols[i]}: {d:.8f} " for i, d in enumerate(data_row)])
+        ret += str(tab_str(str_row, indentation=indentation)) + "\n"
+    return ret + "```"
+
+
+def get_fills_price(original_order_dict: dict) -> float:
+    """
+    Obtain averaged price from order API response.
+
+    :param dict original_order_dict: API order dict response.
+    :return float: Price averaged.
+    """
+    ordered = original_order_dict.copy()
+
+    symbol = ordered['symbol']
+
+    if 'fills' in ordered.keys():
+        if type(ordered['fills']) == dict:
+            return float(ordered['fills']['price'])
+        elif type(ordered['fills']) == list:
+            n = 0
+            m = 0
+            for fill in ordered['fills']:
+                price = float(fill['price'])
+                qty = float(fill['qty'])
+                n += price * qty
+                m += qty
+            return n / m
+        else:
+            raise Exception(f"BinPan Error: Error en parseo de orden ejecutada.\n{ordered}")
+    else:
+        # if it was a buy market order, it has no price, price = 0.0, then get from API trades
+        price_ret = float(ordered['price'])
+        if price_ret != 0:
+            return price_ret
+        else:  # https://dev.binance.vision/t/how-to-get-the-price-of-a-market-order-that-is-already-filled/4046
+            msg_logger.info(f"Get fills from API trades!")
+            orderId = ordered['orderId']
+            trades = get_spot_trades_list(symbol=symbol, limit=25)
+            trade = [i for i in trades if i['orderId'] == orderId]
+            if trade:
+                msg_logger.debug(f"fill_price: Trade: {trade}")
+                return trade[-1]['price']
+            else:
+                msg_logger.error("fill_price: Trade not found, increase get trades limit")
+                raise Exception(f"BinPan Error: Trade not found, increase get trades limit")
+
+
+def telegram_parse_order_markdown(original_order: dict, timezoned='Europe/Madrid'):
+    """
+    Parses API order response to telegram string.
+
+    :param dict original_order: API order response.
+    :param str timezoned: a time zone like 'Europe/Madrid' for human readable timestamps.
+    :return str: Parsed telegram style  string.
+    """
+    order_ = original_order.copy()
+    parsed = ""
+
+    if 'fills' in order_.keys():
+        order_['fills'] = get_fills_price(order_)
+        order_['price'] = order_['fills']
+    try:
+        order_.pop('clientOrderId', None)
+    except Exception as exc:
+        msg_logger.debug(f"Error popping key clientOrderId {exc}")
+    try:
+        order_.pop('orderListId', None)
+    except Exception as exc:
+        msg_logger.debug(f"Error popping key orderListId {exc}")
+    order_['symbol'] = f"*{order_['symbol']}*\n------------------------------------------------"
+    if 'time' in order_.keys():
+        date = convert_milliseconds_to_str(int(order_['time']), timezoned=timezoned)
+        order_.update({'time': date})
+    if 'updateTime' in order_.keys():
+        date = convert_milliseconds_to_str(int(order_['updateTime']), timezoned=timezoned)
+        order_.update({'updateTime': date})
+    if 'quantity' in order_.keys():
+        order_.update({'quantity': f"`{order_['quantity']}`"})
+    if 'price' in order_.keys():
+        order_.update({'price': f"`{order_['price']}`"})
+    if 'origQty' in order_.keys():
+        order_.update({'origQty': f"`{order_['origQty']}`"})
+    if 'stopPrice' in order_.keys():
+        order_.update({'stopPrice': f"`{order_['stopPrice']}`"})
+    if 'fills' in order_.keys():
+        order_.update({'fills': f"`{order_['fills']}`"})
+
+    for key, val in order_.items():
+        if val:
+            line = rf"*{key}*: {val}" + "\n"
+            parsed += line
+
+    return parsed
+
+
+def send_balances(convert_to: str = 'BUSD') -> float:
+    """
+    Sends telegram message with total value of spot wallet in selected coin.
+
+    It returns free and locked assets value added.
+
+    :param str convert_to: A Binance coin.
+    :return float: total value of wallet.
+
+    """
+    balances_df = get_spot_balances_df()
+    parsed_balances = telegram_parse_dataframe_markdown(balances_df)
+
+    # add total value in usdt
+    total_value = get_spot_balances_total_value(balances_df=balances_df,
+                                                convert_to=convert_to)
+
+    parsed_balances += f"\n*Total value*: `{total_value}` \n*Quote*: `{convert_to}`"
+    telegram_bot_send_text(parsed_balances)
+    return total_value
---
+++ packages/binpan/binpan-0.0.74.tar.gz/binpan-0.0.74/handlers/wallet.py
@@ -0,0 +1,370 @@
+import pandas as pd
+
+from .logs import Logs
+from .quest import api_raw_signed_get, api_raw_signed_post
+from .time_helper import convert_milliseconds_to_str
+from .time_helper import convert_string_to_milliseconds
+from .market import get_prices_dic, convert_coin
+
+wallet_logger = Logs(filename='./logs/wallet_logger.log', name='wallet_logger', info_level='INFO')
+
+
+##########
+# Helper #
+##########
+
+def convert_str_date_to_ms(date: str or int,
+                           time_zone: str):
+    """
+    Converts dates strings formatted as "2022-05-11 06:45:42" to timestamp in milliseconds.
+
+    :param str or int date: Date to check format.
+    :param time_zone: A time zone like 'Europe/Madrid'
+    :return int: Milliseconds of timestamp.
+    """
+    if type(date) == str:
+        date = convert_string_to_milliseconds(date, timezoned=time_zone)
+    return date
+
+
+##########
+# WALLET #
+##########
+
+
+def daily_account_snapshot(account_type: str = 'SPOT',
+                           startTime: int or str = None,
+                           endTime: int = None,
+                           limit=30,
+                           time_zone=None) -> pd.DataFrame:
+    """
+    The query time period must be inside the previous 30 days.
+    Support query within the last month, one month only.
+    If startTime and endTime not sent, return records of the last days by default.
+
+    Weight(IP): 2400
+
+    :param str account_type: SPOT or MARGIN (cross)
+    :param int limit: Days limit. Default 30.
+    :param int or str startTime: Can be integer timestamp in milliseconds or formatted string: 2022-05-11 06:45:42
+    :param int or str endTime: Can be integer timestamp in milliseconds or formatted string: 2022-05-11 06:45:42
+    :param str time_zone: A time zone to parse index like 'Europe/Madrid'
+    :return pd.DataFrame: Pandas DataFrame
+
+    """
+    startTime = convert_str_date_to_ms(date=startTime, time_zone=time_zone)
+    endTime = convert_str_date_to_ms(date=endTime, time_zone=time_zone)
+
+    available = ['SPOT', 'MARGIN']
+    if account_type not in available:
+        exc = f"BinPan error: {account_type} not in {available}"
+        wallet_logger.error(exc)
+        raise Exception(exc)
+    elif limit > 30 or limit < 7:
+        exc = f"BinPan error: {limit} is over the maximum 30 or under the minimum 7."
+        wallet_logger.error(exc)
+        raise Exception(exc)
+
+    ret = api_raw_signed_get(endpoint='/sapi/v1/accountSnapshot',
+                             params={'type': account_type,
+                                     'startTime': startTime,
+                                     'endTime': endTime,
+                                     'limit': limit},
+                             weight=2400)
+    rows = []
+    for update in ret['snapshotVos']:
+        updateTime = update['updateTime']
+        data = update['data']
+        totalAssetOfBtc = float(data['totalAssetOfBtc'])
+        if account_type == 'MARGIN':
+            assets_field = 'userAssets'
+        else:
+            assets_field = 'balances'
+        for balance in data[assets_field]:
+            free = float(balance['free'])
+            locked = float(balance['locked'])
+            if free > 0 or locked > 0:
+                row = {'updateTime': updateTime,
+                       'totalAssetOfBtc': totalAssetOfBtc,
+                       'asset': balance['asset'],
+                       'free': free,
+                       'locked': locked}
+                if time_zone:
+                    row['datetime'] = convert_milliseconds_to_str(ms=updateTime, timezoned=time_zone)
+                rows.append(row)
+
+    if time_zone:
+        ret = pd.DataFrame(rows)
+        if not ret.empty:
+            ret = ret.set_index('datetime').sort_index()
+            ret.index.name = f"{account_type} {time_zone}"
+    else:
+        ret = pd.DataFrame(rows)
+        if not ret.empty:
+            ret = ret.set_index('updateTime').sort_index()
+            ret.index.name = f"{account_type} timestamp"
+    return ret
+
+
+def assets_convertible_dust() -> dict:
+    """
+    Assets dust that can be converted to BNB.
+    Weight(IP): 1
+
+    :return dict: A dictionary.
+
+    .. code-block::
+
+       {
+            "details": [
+                {
+                    "asset": "ADA",
+                    "assetFullName": "ADA",
+                    "amountFree": "6.21",   //Convertible amount
+                    "toBTC": "0.00016848",  //BTC amount
+                    "toBNB": "0.01777302",  //BNB amount（Not deducted commission fee）
+                    "toBNBOffExchange": "0.01741756", //BNB amount（Deducted commission fee）
+                    "exchange": "0.00035546" //Commission fee
+                }
+            ],
+            "totalTransferBtc": "0.00016848",
+            "totalTransferBNB": "0.01777302",
+            "dribbletPercentage": "0.02"     //Commission fee
+        }
+
+    """
+    yn = input(f"This command may change or convert assets from your wallet!!! are you sure? (y/n)")
+    if yn.upper().startswith('Y'):
+        return api_raw_signed_post(endpoint='/sapi/v1/asset/dust-btc', weight=1)
+    else:
+        wallet_logger.warning("Canceled!")
+
+
+##########
+# trades #
+##########
+
+
+def get_spot_trades_list(symbol: str,
+                         limit: int = 1000,
+                         orderId: int = None,
+                         startTime: int = None,
+                         endTime: int = None,
+                         fromId: int = None,
+                         recvWindow: int = 10000) -> list:
+    """
+    Get trades for a specific account and symbol.
+
+    Weight(IP): 10
+
+    :param str symbol: Symbol's trades.
+    :param int limit: Default 500; max 1000.
+    :param fromId: TradeId to fetch from. Default gets most recent trades. If fromId is set, it will get id >= that fromId. Otherwise,
+       most recent trades are returned.
+    :param int endTime: Optional.
+    :param int startTime: Optional.
+    :param int orderId: This can only be used in combination with symbol.
+    :param int recvWindow: The value cannot be greater than 60000
+    :return list: A list.
+
+    Example:
+
+           .. code-block::
+
+                     [
+                        {
+                            "symbol": "BNBBTC",
+                            "id": 28457,
+                            "orderId": 100234,
+                            "orderListId": -1, //Unless OCO, the value will always be -1
+                            "price": "4.00000100",
+                            "qty": "12.00000000",
+                            "quoteQty": "48.000012",
+                            "commission": "10.10000000",
+                            "commissionAsset": "BNB",
+                            "time": 1499865549590,
+                            "isBuyer": true,
+                            "isMaker": false,
+                            "isBestMatch": true
+                          }
+                       ]
+    """
+    endpoint = '/api/v3/myTrades'
+    return api_raw_signed_get(endpoint=endpoint,
+                              params={'limit': limit,
+                                      'symbol': symbol,
+                                      'orderId': orderId,
+                                      'startTime': startTime,
+                                      'endTime': endTime,
+                                      'fromId': fromId,
+                                      'recvWindow': recvWindow},
+                              weight=10)
+
+
+############
+# balances #
+############
+
+
+def get_spot_account_info(recvWindow: int = 10000) -> dict:
+    """
+    Get current account information.
+
+    Weight(IP): 10
+
+    :param int recvWindow: The value cannot be greater than 60000
+    :return dict: A dictionary with data.
+
+    Example:
+
+        .. code-block::
+
+           {
+              "makerCommission": 15,
+              "takerCommission": 15,
+              "buyerCommission": 0,
+              "sellerCommission": 0,
+              "canTrade": true,
+              "canWithdraw": true,
+              "canDeposit": true,
+              "updateTime": 123456789,
+              "accountType": "SPOT",
+              "balances": [
+                {"asset": "BTC", "free": "4723846.89208129","locked": "0.00000000"},
+                {"asset": "LTC","free": "4763368.68006011","locked": "0.00000000"}
+              ],
+              "permissions": ["SPOT"]
+           }
+
+    """
+    endpoint = '/api/v3/account'
+    return api_raw_signed_get(endpoint=endpoint,
+                              params={'recvWindow': recvWindow},
+                              weight=10)
+
+
+def spot_free_balances_parsed(data_dic: dict = None) -> dict:
+    """
+    Parses available balances from account info.
+
+    :param dict data_dic: If available, account info can be passed as data_dic parameter to avoid API calling.
+    :return dict: Free balances.
+    """
+    ret = {}
+    if not data_dic:
+        data_dic = get_spot_account_info()
+    wallet_logger.debug(f"spot_free_balances_parsed len(get_spot_account_info()):{len(data_dic)}")
+    for asset in data_dic['balances']:
+        qty = float(asset['free'])
+        if qty:
+            ret.update({asset['asset']: qty})
+    return ret
+
+
+def spot_locked_balances_parsed(data_dic: dict = None) -> dict:
+    """
+    Parses locked in order balances from account info.
+
+    :param dict data_dic: If available, account info can be passed as data_dic parameter to avoid API calling.
+    :return dict: Locked balances.
+    """
+    ret = {}
+    if not data_dic:
+        data_dic = get_spot_account_info()
+    wallet_logger.debug(f"spot_locked_balances_parsed get_spot_account_info: {len(data_dic)}")
+
+    for asset in data_dic['balances']:
+        qty = float(asset['locked'])
+        if qty:
+            ret.update({asset['asset']: qty})
+    return ret
+
+
+def get_coins_with_balance() -> list:
+    """
+    Get the non-zero balances of an account, free or locked ones. Useful getting wallet value.
+
+    :return list:
+    """
+    data_dic = get_spot_account_info()
+    free = spot_free_balances_parsed(data_dic=data_dic)
+    locked = spot_locked_balances_parsed(data_dic=data_dic)
+    free = [symbol for symbol, balance in free.items() if float(balance) > 0]
+    locked = [symbol for symbol, balance in locked.items() if float(balance) > 0]
+    symbols = free + locked
+    return list(set(symbols))
+
+
+def get_spot_balances_df(filter_empty: bool = True) -> pd.DataFrame:
+    """
+    Create a dataframe with the free or blocked amounts of the spot wallet. The index is the assets list.
+
+    Example:
+
+    .. code-block:: python
+
+       from handlers.messages import get_spot_balances_df
+
+       df = get_spot_balances_df()
+
+       print(df)
+
+                   free      locked
+            asset
+            BNB    0.128359     0.0
+            BUSD   0.000001     0.0
+
+
+    :param bool filter_empty: Discards empty quantities.
+    :return pd.DataFrame: A dataframe with assets locked or free.
+    """
+    balances = get_spot_account_info()['balances']
+    df_ = pd.DataFrame(balances)
+    df_['free'] = df_['free'].apply(pd.to_numeric)
+    df_['locked'] = df_['locked'].apply(pd.to_numeric)
+    df_.set_index('asset', drop=True, inplace=True)
+    if filter_empty:
+        return df_[(df_['free'] != 0) | (df_['locked'] != 0)]
+    else:
+        return df_
+
+
+def get_spot_balances_total_value(balances_df: pd.DataFrame = None,
+                                  convert_to: str = 'BUSD') -> float:
+    """
+    Returns total value expressed in a quote coin. Counts free and locked assets.
+    
+    :param pd.DataFrame balances_df: A BinPan balances dataframe. 
+    :param str convert_to: A Binance coin. 
+    :return float: Total quantity expressed in quote. 
+    """
+    if type(balances_df) != pd.DataFrame:
+        balances_df = get_spot_balances_df()
+
+    prices = get_prices_dic()
+
+    symbols = list(balances_df.index)
+    free_values = balances_df['free'].tolist()
+    locked_values = balances_df['locked'].tolist()
+    total = 0
+
+    for i in range(len(symbols)):
+        coin = symbols[i]
+        free = float(free_values[i])
+        locked = float(locked_values[i])
+
+        if free:
+            free = convert_coin(coin=coin,
+                                prices=prices,
+                                convert_to=convert_to,
+                                coin_qty=free)
+
+        if locked:
+            locked = convert_coin(coin=coin,
+                                  prices=prices,
+                                  convert_to=convert_to,
+                                  coin_qty=locked)
+        total += float(free)
+        total += float(locked)
+
+    return total
