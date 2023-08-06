50ec564837bf34f3eaec6b5250a2beb31fd285a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package ptorru-matmul/0.1.6a2/ptorru_matmul-0.1.6a2-py3-none-any.whl

---
+++ packages/ptorru-matmul/ptorru_matmul-0.1.6a2-py3-none-any.whl/ptorru_matmul/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.1.6a2'
+
+import numpy as np
+
+def matmul(a,b):
+    assert a.shape == b.shape
+    assert a.shape[0] == a.shape[1]
+    sides = a.shape[0]
+    c = np.zeros(a.shape)
+    for i in range(sides):
+        for j in range(sides):
+            for k in range(sides):
+                c[i][j] += a[i][k] * b[k][j]
+    return c
