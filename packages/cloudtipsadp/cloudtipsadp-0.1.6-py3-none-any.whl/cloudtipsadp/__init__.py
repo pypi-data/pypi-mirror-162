503d8a0dd2dc2035d9f8e2dd652ecfee8af340cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:02 +0000

    Add package cloudtipsadp/0.1.6/cloudtipsadp-0.1.6-py3-none-any.whl

---
+++ packages/cloudtipsadp/cloudtipsadp-0.1.6-py3-none-any.whl/cloudtipsadp/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for cloudtipsadp API Python Client Library."""
+
+from cloudtipsadp.configuration import (Configuration, Token)
+from cloudtipsadp.clients import (ProductClient, SandboxClient, Connect)
+from cloudtipsadp.receivers import (Receiver, )
+
+__author__ = "SETTER"
+__email__ = 'lphp@mail.ru'
