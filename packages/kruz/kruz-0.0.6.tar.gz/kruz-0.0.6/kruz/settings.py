0220b41f3eab7fd3687955ed470a600a39eab921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package kruz/0.0.6/kruz-0.0.6.tar.gz

---
+++ packages/kruz/kruz-0.0.6.tar.gz/kruz-0.0.6/kruz/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+DEFAULTS = {}
+DEFAULTS['debug'] = False
+DEFAULTS['src_ip'] = '0.0.0.0'
+DEFAULTS['src_port'] = 19302
+DEFAULTS['stun_ip'] = 'stun.l.google.com'
+DEFAULTS['stun_port'] = 19302
+DEFAULTS['disable-ipv6'] = False
+DEFAULTS['check-all'] = False
+DEFAULTS['short-link'] = False
+
+CHUNK_SIZE = 4096
