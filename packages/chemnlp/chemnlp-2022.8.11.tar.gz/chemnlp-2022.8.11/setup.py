68bcc223b9ecffd7988c4de31cc1e9d70ed793a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:41 +0000

    Add package chemnlp/2022.8.11/chemnlp-2022.8.11.tar.gz

---
+++ packages/chemnlp/chemnlp-2022.8.11.tar.gz/chemnlp-2022.8.11/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chemnlp",  
+    version="2022.8.11",
+    author="Kamal Choudhary",
+    author_email="kamal.choudhary@nist.gov",
+    description="chemnlp",
+    install_requires=[
+        "numpy>=1.22.0",
+        "scipy>=1.6.3",
+        "jarvis-tools>=2021.07.19",
+        "torch>=1.7.1",
+        # "torchvision>=0.10.0+cu111",
+        "scikit-learn>=0.24.1",
+        "matplotlib>=3.4.1",
+        "seaborn>=0.11.2",
+        "tqdm>=4.60.0",
+        "pandas>=1.2.3",
+        "pytorch-ignite>=0.5.0.dev20210429",
+        "pydantic>=1.8.1",
+        "flake8>=3.9.1",
+        "pycodestyle>=2.7.0",
+        "pydocstyle>=6.0.0",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usnistgov/chemnlp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
