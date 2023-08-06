d56fe9f8e50e098a74f870220313895e80be0d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:56 +0000

    Add package metricflow-lite/0.110.2.dev0/metricflow-lite-0.110.2.dev0.tar.gz

---
+++ packages/metricflow-lite/metricflow-lite-0.110.2.dev0.tar.gz/metricflow-lite-0.110.2.dev0/metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/PostgresSqlClient/test_composite_identifier_with_join__plan0_optimized.sql
@@ -0,0 +1,33 @@
+-- Join Standard Outputs
+-- Pass Only Elements:
+--   ['messages', 'user_team__country', 'user_team']
+-- Aggregate Measures
+-- Compute Metrics via Expressions
+SELECT
+  subq_10.user_team___team_id AS user_team___team_id
+  , subq_10.user_team___user_id AS user_team___user_id
+  , users_source_src_10016.country AS user_team__country
+  , SUM(subq_10.messages) AS messages
+FROM (
+  -- Read Elements From Data Source 'messages_source'
+  -- Metric Time Dimension 'ds'
+  -- Pass Only Elements:
+  --   ['messages', 'user_team', 'user_team']
+  SELECT
+    team_id AS user_team___team_id
+    , user_id AS user_team___user_id
+    , 1 AS messages
+  FROM ***************************.fct_messages messages_source_src_10014
+) subq_10
+LEFT OUTER JOIN
+  ***************************.fct_users users_source_src_10016
+ON
+  (
+    subq_10.user_team___team_id = users_source_src_10016.team_id
+  ) AND (
+    subq_10.user_team___user_id = users_source_src_10016.id
+  )
+GROUP BY
+  subq_10.user_team___team_id
+  , subq_10.user_team___user_id
+  , users_source_src_10016.country
