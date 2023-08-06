1336187c767aae441cae35b4f9d49873f546aa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:21 +0000

    Add package battle-cats-save-editor/2.1.0b3/battle-cats-save-editor-2.1.0b3.tar.gz

---
+++ packages/battle-cats-save-editor/battle-cats-save-editor-2.1.0b3.tar.gz/battle-cats-save-editor-2.1.0b3/src/BCSFE_Python/edits/levels/clear_tutorial.py
@@ -0,0 +1,13 @@
+"""Handler for clearing the tutorial"""
+from typing import Any
+
+def clear_tutorial(save_stats: dict[str, Any]) -> dict[str, Any]:
+    """Handler for clearing the tutorial"""
+
+    save_stats["tutorial_cleared"]["Value"] = 1
+    if save_stats["story_chapters"]["Chapter Progress"][0] == 0:
+        save_stats["story_chapters"]["Chapter Progress"][0] = 1
+    save_stats["story_chapters"]["Times Cleared"][0][0] = 1
+    print("Successfully cleared the tutorial")
+
+    return save_stats
