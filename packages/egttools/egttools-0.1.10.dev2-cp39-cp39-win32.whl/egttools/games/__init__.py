68cfe1f0af0d0d705783cef6a2028f05af171cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package egttools/0.1.10.dev2/egttools-0.1.10.dev2-cp39-cp39-win32.whl

---
+++ packages/egttools/egttools-0.1.10.dev2-cp39-cp39-win32.whl/egttools/games/__init__.py
@@ -0,0 +1,10 @@
+"""
+API reference documentation for the `games` submodule.
+"""
+
+from egttools.numerical.games import AbstractGame, NormalFormGame, CRDGame, CRDGameTU, OneShotCRD
+
+from .pgg import PGG
+from .informal_risk import InformalRiskGame
+from .abstract_games import AbstractNPlayerGame, AbstractTwoPLayerGame
+from .nonlinear_games import NPlayerStagHunt
