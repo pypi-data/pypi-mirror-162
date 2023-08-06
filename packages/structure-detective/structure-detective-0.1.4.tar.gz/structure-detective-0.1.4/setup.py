1989c83e91e06eceb01412c479e93e18187f66c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:08 +0000

    Add package structure-detective/0.1.4/structure-detective-0.1.4.tar.gz

---
+++ packages/structure-detective/structure-detective-0.1.4.tar.gz/structure-detective-0.1.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['structure_detective',
+ 'structure_detective.de',
+ 'structure_detective.en',
+ 'structure_detective.es']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['spacy>=3.4.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'structure-detective',
+    'version': '0.1.4',
+    'description': 'Parse sentence',
+    'long_description': None,
+    'author': 'Phoenix.Grey',
+    'author_email': 'phoenix.grey0108@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
