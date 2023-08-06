186cc92ed56e9a184b217e84450346b1cbaaff66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:25 +0000

    Add package condor-assistant/1.2/condor_assistant-1.2.tar.gz

---
+++ packages/condor-assistant/condor_assistant-1.2.tar.gz/condor_assistant-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='condor_assistant',
+   version='1.2',
+   description='Handy tool for condor submission',
+   license="MIT",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author='Jay Chan',
+   author_email='jay.chan@cern.ch',
+   url="https://github.com/qwerasd903/condor_assistant",
+   packages=['condor_assistant'],
+   install_requires=[],
+   scripts=["example.py"]
+)
