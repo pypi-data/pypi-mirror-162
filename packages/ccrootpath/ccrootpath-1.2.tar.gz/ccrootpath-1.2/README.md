5874daff514479926a7f438b55ee4241c03e108e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package ccrootpath/1.2/ccrootpath-1.2.tar.gz

---
+++ packages/ccrootpath/ccrootpath-1.2.tar.gz/ccrootpath-1.2/README.md
@@ -0,0 +1,29 @@
+Import project root path to `sys.path` for import local files.  
+
+# Intro
+
+If you have a project with the following structure:  
+
+```
+| - my_proj
+|
+| - my_proj / config / models / account_model.py (class AccountModel)
+|
+| - my_proj / transaction / checker.py (class Checker)
+
+```
+
+Use ccrootpath to simplize your `import` for local file importing:  
+
+```
+import ccrootpath
+PROJECT_DIR = ccrootpath.set_project_root_path(__file__, 'my_proj')
+from config.models.account_model import AccountModel
+from transaction.checker import Checker
+```
+
+# How to install
+
+```
+pip3 install ccrootpath --upgrade --user
+```
---
+++ packages/ccrootpath/ccrootpath-1.2.tar.gz/ccrootpath-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+
+
+class READMarkDown:
+
+    @staticmethod
+    def read_string(file_name='./README.md'):
+        if os.path.isfile(file_name):
+            with open(file_name) as f:
+                lst = f.read()
+                return lst
+        else:
+            return None
+
+
+setup(name='ccrootpath',
+      version='1.2',
+      description='Set project root path for importing local modules',
+      long_description=READMarkDown.read_string(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/OpenFibers/CCRootPath',
+      author='OpenFibers',
+      author_email='openfibers@gmail.com',
+      license='MIT',
+      packages=['ccrootpath'],
+      zip_safe=False)
