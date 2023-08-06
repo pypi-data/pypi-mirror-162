76ddd1e4875a1258529e3b0b223164a3b88f843b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package patch-antenna/0.1.0/patch_antenna-0.1.0.tar.gz

---
+++ packages/patch-antenna/patch_antenna-0.1.0.tar.gz/patch_antenna-0.1.0/patch_antenna/__init__.py
@@ -0,0 +1,9 @@
+__name__ = 'patch_antenna'
+__version__ = '0.1.0'
+__owner__ = 'bhanuchander210'
+
+from .designer import design
+from .designer import design_result
+from .designer import design_string
+from .designer import write_gerber
+from .designer import write_gerber_design
