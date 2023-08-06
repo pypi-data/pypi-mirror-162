975e097d9e08b235e0cd80917ed7f6ba3db14869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:37 +0000

    Add package parallel-integral/0.0.1/parallel_integral-0.0.1.tar.gz

---
+++ packages/parallel-integral/parallel_integral-0.0.1.tar.gz/parallel_integral-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="parallel_integral",
+    version="0.0.1",
+    author="God, the one who wrote everything",
+    description="A package to make integrals in parallel",
+    packages=["parallel_code"]
+)
