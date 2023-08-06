75f0433b1b024ef7e1384c56bf34d036c81e8017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:04 +0000

    Add package esi-utils-vectors/1.0.1/esi-utils-vectors-1.0.1.tar.gz

---
+++ packages/esi-utils-vectors/esi-utils-vectors-1.0.1.tar.gz/esi-utils-vectors-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="esi-utils-vectors",
+    version="1.0.1",
+    description="USGS Earthquake Impact Utilities",
+    author="Mike Hearne",
+    author_email="mhearne@usgs.gov",
+    url="https://code.usgs.gov/ghsc/esi/esi-utils-vectors",
+    packages=[
+        "esi_utils_vectors",
+    ],
+)
