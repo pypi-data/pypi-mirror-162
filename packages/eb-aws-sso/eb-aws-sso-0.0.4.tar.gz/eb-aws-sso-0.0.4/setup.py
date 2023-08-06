54591c62b379c94c9cfa6e4af45204c586641182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:37 +0000

    Add package eb-aws-sso/0.0.4/eb-aws-sso-0.0.4.tar.gz

---
+++ packages/eb-aws-sso/eb-aws-sso-0.0.4.tar.gz/eb-aws-sso-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+install_package_requires = [
+    "boto3>=1.24,<2",
+]
+
+setup(
+    name="eb-aws-sso",
+    version="0.0.4",
+    python_requires=">=3.8",
+    scripts=["awssso"],
+    install_requires=install_package_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+)
