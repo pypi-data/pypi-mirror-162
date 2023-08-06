434a9ec2ee69f3a20ce7760c85483143494e04fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:05 +0000

    Add package similarnames/0.1.5/similarnames-0.1.5.tar.gz

---
+++ packages/similarnames/similarnames-0.1.5.tar.gz/similarnames-0.1.5/similarnames/__init__.py
@@ -0,0 +1,25 @@
+__version__ = '0.1.5'
+
+from .similarnames import SimilarNames
+
+def closeMatches(obj, names, sep = ',', connectors = ['and','e','y'], languages = ['english', 'portuguese', 'spanish'], customWords = None):
+    if connectors == None:
+        connectors = []
+    else:
+        if not isinstance(connectors, list):
+            connectors = [connectors]
+
+    if languages == None:
+        languages = []
+    else:
+        if not isinstance(languages, list):
+            languages = [languages]
+
+    if customWords == None:
+        customWords = []
+    else:
+        if not isinstance(customWords, list):
+            customWords = [customWords]
+
+    sn = SimilarNames()
+    return sn.closeMatches(obj, names, sep, connectors, languages, customWords)
