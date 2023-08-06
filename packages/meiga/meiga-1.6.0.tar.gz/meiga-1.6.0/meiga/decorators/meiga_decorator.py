56a392558719cda283a589de73178f5614a5e272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:22 +0000

    Add package meiga/1.6.0/meiga-1.6.0.tar.gz

---
+++ packages/meiga/meiga-1.6.0.tar.gz/meiga-1.6.0/meiga/decorators/meiga_decorator.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from typing import Callable, TypeVar, cast
+
+from typing_extensions import ParamSpec
+
+from meiga.alias import Failure
+from meiga.decorators.unexpected_decoration_order_error import (
+    UnexpectedDecorationOrderError,
+)
+from meiga.error import Error
+from meiga.on_failure_exception import OnFailureException
+from meiga.result import Result
+
+P = ParamSpec("P")
+R = TypeVar("R", bound=Result)
+
+
+def meiga(func: Callable[P, R]) -> Callable[P, R]:
+    @wraps(func)
+    def _meiga(*args: P.args, **kwargs: P.kwargs) -> R:
+        try:
+            if isinstance(func, staticmethod):
+                return Failure(UnexpectedDecorationOrderError())
+            elif isinstance(func, classmethod):
+                return Failure(UnexpectedDecorationOrderError())
+            else:
+                return func(*args, **kwargs)
+        except OnFailureException as exc:
+            return exc.result
+        except Error as error:
+            return cast(R, Failure(error))
+
+    return _meiga
