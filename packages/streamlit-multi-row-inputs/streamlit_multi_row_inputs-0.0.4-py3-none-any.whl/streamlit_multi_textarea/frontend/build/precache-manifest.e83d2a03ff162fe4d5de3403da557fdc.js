6d52a81783d74c2b33f7bbdb7dbe73aae80aecbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:19 +0000

    Add package streamlit-multi-row-inputs/0.0.4/streamlit_multi_row_inputs-0.0.4-py3-none-any.whl

---
+++ packages/streamlit-multi-row-inputs/streamlit_multi_row_inputs-0.0.4-py3-none-any.whl/streamlit_multi_textarea/frontend/build/precache-manifest.e83d2a03ff162fe4d5de3403da557fdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdf8ad7441c2611032f6474769956cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01f5be7ae2f905a606d3",
+    "url": "./static/js/2.e4606493.chunk.js"
+  },
+  {
+    "revision": "2bcc9d4f081836c48aa376ea1ea823d1",
+    "url": "./static/js/2.e4606493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d2c1c568af8bd6914b",
+    "url": "./static/js/main.e8672309.chunk.js"
+  },
+  {
+    "revision": "cb0daba11ac97ebb5e44",
+    "url": "./static/js/runtime-main.1765f872.js"
+  }
+]);
