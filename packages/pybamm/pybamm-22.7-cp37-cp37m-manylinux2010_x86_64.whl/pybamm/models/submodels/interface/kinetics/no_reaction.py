954025549e2d1a97f50c90be5731492f07b16134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:36 +0000

    Add package pybamm/22.7/pybamm-22.7-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/pybamm/pybamm-22.7-cp37-cp37m-manylinux2010_x86_64.whl/pybamm/models/submodels/interface/kinetics/no_reaction.py
@@ -0,0 +1,33 @@
+#
+# Bulter volmer class
+#
+
+import pybamm
+from .base_kinetics import BaseKinetics
+
+
+class NoReaction(BaseKinetics):
+    """
+    Base submodel for when no reaction occurs
+
+    Parameters
+    ----------
+    param :
+        model parameters
+    domain : str
+        The domain to implement the model, either: 'Negative' or 'Positive'.
+    reaction : str
+        The name of the reaction being implemented
+
+    **Extends:** :class:`pybamm.interface.kinetics.BaseKinetics`
+    """
+
+    def __init__(self, param, domain, reaction):
+        options = {
+            "SEI film resistance": "none",
+            "total interfacial current density as a state": "false",
+        }
+        super().__init__(param, domain, reaction, options)
+
+    def _get_kinetics(self, j0, ne, eta_r, T, u):
+        return pybamm.Scalar(0)
