0e022538b4993b1f9b07a7008d565e6c849bbad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:15 +0000

    Add package fastberry/0.0.25/fastberry-0.0.25-py2.py3-none-any.whl

---
+++ packages/fastberry/fastberry-0.0.25-py2.py3-none-any.whl/fastberry/config/commands/__init__.py
@@ -0,0 +1,16 @@
+"""
+    Fastberry Command-Line-Interface
+"""
+
+import click
+
+# Import Commands Here
+from .app_manager import start_app
+from .graphql import graphql
+from .run import run
+from .sql_alembic import cli
+
+# Add Commands Here
+cli.add_command(run)
+cli.add_command(graphql)
+cli.add_command(start_app)
