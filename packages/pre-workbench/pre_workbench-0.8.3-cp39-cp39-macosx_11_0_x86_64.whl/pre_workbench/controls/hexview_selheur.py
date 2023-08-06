3210030b8fbbdbe1e619181e2d4ad38912f75871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:46 +0000

    Add package pre-workbench/0.8.3/pre_workbench-0.8.3-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/pre-workbench/pre_workbench-0.8.3-cp39-cp39-macosx_11_0_x86_64.whl/pre_workbench/controls/hexview_selheur.py
@@ -0,0 +1,166 @@
+
+# PRE Workbench
+# Copyright (C) 2022 Mira Weller
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+import struct
+
+from PyQt5.QtGui import QColor, QPen, QPainter
+
+from pre_workbench.configs import SettingsField
+from pre_workbench.objects import ByteBuffer
+from pre_workbench.typeregistry import TypeRegistry
+
+SelectionHelpers = TypeRegistry()
+
+
+def extendRange(bbuf, range, amount=16):
+	(bufIdx, selMin, selMax) = range
+	return bufIdx, max(0, selMin - amount), min(bbuf.length, selMax + amount)
+
+
+def rangeBefore(bbuf, range, amount=16):
+	(bufIdx, selMin, selMax) = range
+	return bufIdx, max(0, selMin - amount), selMin
+
+
+def intToVarious(*values):
+	for value in values:
+		if value < 0:
+			yield from intToFmts(value, [">q","<q",">i","<i",">h","<h",">b","<b"])
+		else:
+			yield from intToFmts(value, [">Q","<Q",">I","<I",">H","<H",">B","<B"])
+		yield str(value).encode("utf-8"), "d "+str(value)
+		yield ("%x"%value).encode("utf-8"), "x "+str(value)
+		yield ("%X"%value).encode("utf-8"), "X "+str(value)
+
+
+def intToFmts(value, fmts):
+	for fmt in fmts:
+		try:
+			yield struct.pack(fmt, value), "%s %d"%(fmt, value)
+		except struct.error:
+			pass
+
+
+def findInRange(bbuf, ranges, values):
+	for (bufIdx, start, end) in ranges:
+		for val, desc in values:
+			l = len(val)
+			if l == 0: raise Exception("")
+			i = start
+			while i <= end-l:
+				if bbuf.buffer[i:i+l] == val:
+					yield (bufIdx, i, i+l), desc
+					i += l
+				else:
+					i += 1
+
+
+def highlightMatch(editor, qp: QPainter, matchrange, desc, color):
+	(bufIdx,start,end)=matchrange
+	for i in range(start,end):
+		(xHex, xAscii, y, dy) = editor.offsetToClientPos(bufIdx, i)
+		if dy is None: break
+		p = QPen(color)
+		p.setWidth(3)
+		qp.setPen(p)
+		qp.drawLine(xHex+3, y+dy, xHex+editor.dxHex-3, y+dy)
+		qp.drawLine(xAscii+1, y+dy, xAscii+editor.dxAscii-2, y+dy)
+
+
+@SelectionHelpers.register(color="#ff00ff", defaultEnabled=True, options=[
+	SettingsField("minLength", "Minimum Selection Length", "int", {"default":2}),
+])
+def selectionLengthMatcher(editor, qp, bbuf, sel, options):
+	"""
+	Searches for the length of the selection in various formats in the 16 bytes preceding, the selection itself and the 16 bytes following the selection.
+
+	Formats are int8 to int64, uint8 to uint64 (big and little endian), decimal string and hex string (lower and upper case).
+	"""
+	(bufIdx, start, end) = sel
+	sellen = end - start + 1
+	if sellen < options.get("minLength", 2): return
+	for match, desc in findInRange(bbuf, [extendRange(bbuf, (bufIdx, start,start))], intToVarious(sellen)):
+		highlightMatch(editor, qp, match,desc,QColor("#ff00ff"))
+
+
+@SelectionHelpers.register(color="#993399", defaultEnabled=False)
+def fuzzySelectionLengthMatcher(editor, qp, bbuf, sel, options):
+	"""
+	Same as selectionLengthMatcher, but searches for the length +1, +2 and +4.
+	"""
+	(bufIdx, start, end) = sel
+	sellen = end - start + 1
+	if sellen == 0: return
+	if sellen >= 5:
+		for match, desc in findInRange(bbuf, [extendRange(bbuf, (bufIdx, start,start))], intToVarious(sellen+1, sellen+2, sellen+4)):
+			highlightMatch(editor, qp, match,desc,QColor("#993399"))
+
+
+@SelectionHelpers.register(color="#775511", defaultEnabled=False)
+def debug_highlightMatchRange(editor, qp, bbuf, sel, options):
+	"""
+	Highlights the 16 bytes preceding the selection, because they are searched for other matches
+
+	Internal debug tool
+
+	"""
+	highlightMatch(editor, qp, rangeBefore(bbuf, sel), "", QColor("#775511"))
+
+
+@SelectionHelpers.register(color="#555555", defaultEnabled=True)
+def highlightSelectionAsLength(editor, qp, bbuf:ByteBuffer, sel, options):
+	"""
+	Parses the current selection as integer and uses the value as a length, to highlight the range with this length
+	following the selection.
+
+	First, parsing as big endian is attempted, if the value is too large, little endian is attempted.
+
+	"""
+	(bufIdx, start, end)=sel
+	end=end+1
+	if end-start>8: return
+	val = bbuf.getInt(start,end,endianness=">",signed=False)
+	if val > 0 and end+val <= len(bbuf):
+		highlightMatch(editor, qp, (bufIdx, end, end+val), "", QColor("#555555"))
+		return
+	val = bbuf.getInt(start,end,endianness="<",signed=False)
+	if val > 0 and end+val <= len(bbuf):
+		highlightMatch(editor, qp, (bufIdx, end, end+val), "", QColor("#555555"))
+		return
+
+
+@SelectionHelpers.register(color="#009999", defaultEnabled=True, options=[
+	SettingsField("minLength", "Minimum Selection Length", "int", {"default":2}),
+])
+def highlightRepetitions(editor, qp, bbuf, sel, options):
+	"""
+	Searches for the byte values of the selection in the whole visible buffer, highlighting all occurrences.
+	"""
+	(bufIdx, selstart, selend) = sel
+	sellen = selend - selstart + 1
+	if sellen < options.get("minLength", 2): return
+	selbytes = bbuf.getBytes(selstart, sellen)
+	logging.debug("highlightRepetitions: searching for %r", selbytes)
+	(firstBuf, firstOffset), (lastBuf, lastOffset) = editor.visibleRange()
+	for i in range(firstBuf, lastBuf + 1):
+		start = firstOffset if i == firstBuf else 0
+		end = lastOffset if i == lastBuf else len(editor.buffers[i])
+		logging.debug("highlightRepetitions: scanning #%d bytes %d-%d",i,start,end)
+		for match, desc in findInRange(editor.buffers[i], [(i, start, end)], [(selbytes, "")]):
+			if match == (bufIdx, selstart, selend + 1): continue
+			highlightMatch(editor, qp, match, desc, QColor("#009999"))
+
---
+++ packages/pre-workbench/pre_workbench-0.8.3-cp39-cp39-macosx_11_0_x86_64.whl/pre_workbench/wdgen/lua/lua_types.py
@@ -0,0 +1,102 @@
+from collections import namedtuple
+from io import StringIO
+
+from pre_workbench.structinfo.format_info import DYN_LEN, EXPR_LEN, PREFIX_LEN
+from pre_workbench.wdgen.lua.lua_expr import to_lua_expr
+
+ws_field_def = namedtuple('ws_field_def', ('long_id', 'short_id', 'format_type', 'show', 'charset'))
+
+class WDGenVisitor:
+	def __init__(self, context, proto_name):
+		self.context = context
+		self.ws_field_defs = list()
+		self.proto_name = proto_name
+		self.output = StringIO()
+
+	def out(self, s):
+		self.output.write(s + '\n')
+
+	def get_result(self):
+		return self.output.getvalue()
+
+	def structfi(self, desc):
+		self.out("  -- struct " + self.context.get_path("_"))
+		for name, child, comment in desc.fi.children:
+			self.context.id = name
+			child.visit(self.context, self)
+
+	def variantstructfi(self, desc):
+		self.out("  -- variant " + self.context.get_path("_"))
+		raise NotImplementedError("Type 'variant' not implemented yet for code generation")
+
+	def repeatstructfi(self, desc):
+		self.out("  -- repeat " + self.context.get_path("_"))
+		if desc.fi.times_expr:
+			self.out("  for i_" + self.context.get_path("_") + " = 1," + to_lua_expr(desc.fi.times_expr) + " do")
+			desc.fi.children.visit(self.context, self)
+			self.out("  end")
+		else:
+			self.out("  while offset < buffer:len() do")
+			desc.fi.children.visit(self.context, self)
+			self.out("  if " + to_lua_expr(desc.fi.until_expr) + " then break end")
+			self.out("  end")
+
+	def switchfi(self, desc):
+		self.out("  -- switch " + self.context.get_path("_"))
+		raise NotImplementedError("Type 'switch' not implemented yet for code generation")
+
+	def namedfi(self, desc):
+		self.out("  -- named " + self.context.get_path("_"))
+		self.out("  offset = offset + parse_"+desc.fi.ref_name+"(buffer(offset), pinfo, subtree, '" + self.context.top_id() + ": ', fval)")
+
+	def fieldfi(self, desc):
+		self.out("  -- field " + self.context.get_path("_")+" "+desc.fi.format_type)
+		long_id = self.context.get_path("_")
+		short_id = self.context.top_id()
+		if "STRING" in desc.fi.format_type:
+			encoding = 'ENC_UTF_8' if 'utf' in self.context.get_param('charset') else 'ENC_ASCII'
+		else:
+			encoding = "ENC_LITTLE_ENDIAN" if self.context.get_param("endianness") == "<" else "ENC_BIG_ENDIAN"
+			le_prefix = "le_" if self.context.get_param("endianness") == "<" else ""
+		if desc.fi.size == DYN_LEN:
+			raise NotImplementedError('DYN_LEN not implemented')
+		elif desc.fi.size == PREFIX_LEN:
+			raise NotImplementedError('PREFIX_LEN not implemented')
+		elif desc.fi.size == EXPR_LEN:
+			self.out("  len = " + to_lua_expr(desc.fi.size_expr) + "  -- expression-based length")
+		else:
+			self.out("  len = " + str(desc.fi.size) + "  -- static length")
+		if desc.fi.size <= 0: self.out("  if len > 0 then")
+		self.out(f"    field_item, fval['{short_id}'] = subtree:add_packet_field(f_{long_id}, buffer(offset, len), {encoding})")
+		if desc.fi.format_type in ('INT8', 'INT16', 'INT24', 'INT32'):
+			self.out(f"    fval['{short_id}'] = buffer(offset, len):{le_prefix}int()")
+		elif desc.fi.format_type in ('CHAR', 'UINT8', 'UINT16', 'UINT24', 'UINT32'):
+			self.out(f"    fval['{short_id}'] = buffer(offset, len):{le_prefix}uint()")
+		magic = self.context.get_param("magic", raise_if_missing=False)
+		if magic is not None:
+			self.out(f"    if fval['{short_id}'] ~= " + repr(magic) + " then")
+			self.out(f'      field_item:add_expert_info(PI_MALFORMED, PI_ERROR, "magic value mismatch, expected {magic}, got " .. fval["{short_id}"])')
+			self.out(f"    end")
+		if desc.fi.size <= 0: self.out("  end")
+		self.out("  offset = offset + len")
+		show = desc.params.get("show", "")
+		self.ws_field_defs.append(ws_field_def(
+			long_id=self.context.get_path("."),
+			short_id=short_id,
+			format_type=desc.fi.format_type,
+			show=self.context.get_param('show',raise_if_missing=False),
+			charset=self.context.get_param('charset',raise_if_missing=False)))
+		self.out("")
+
+
+	def unionfi(self, desc):
+		self.out("  -- union " + self.context.get_path("_"))
+		raise NotImplementedError("Type 'union' not implemented yet for code generation")
+
+	def bitstructfi(self, desc):
+		self.out("  -- bits " + self.context.get_path("_"))
+		raise NotImplementedError("Type 'bits' not implemented yet for code generation")
+		for key, len in desc.fi.children:
+			self.out("  -- "+key+" "+str(len))
+			self.ws_field_defs.append((self.context.get_path("."), "UINT32"))
+
