7dd04367b9d3f1a2e61fa948bb9de55c22a866c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:16 +0000

    Add package grab-fork-from-libgen/2.8.7/grab-fork-from-libgen-2.8.7.tar.gz

---
+++ packages/grab-fork-from-libgen/grab-fork-from-libgen-2.8.7.tar.gz/grab-fork-from-libgen-2.8.7/grab_fork_from_libgen/__init__.py
@@ -0,0 +1,6 @@
+from .search import LibgenSearch
+from .metadata import Metadata
+from .aio_search import AIOLibgenSearch
+from .aio_metadata import AIOMetadata
+from .models.search_models import ValidTopics, SearchEntry
+from .models.metadata_models import MetadataResponse
