4303811ebcb98018f56a86e131100b2f232c79ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:17 +0000

    Add package testdump/0.1.0/testdump-0.1.0.tar.gz

---
+++ packages/testdump/testdump-0.1.0.tar.gz/testdump-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+VERSION = '0.1.0'
+
+setup(
+    name="testdump",
+    version='0.1.0',
+    author="Durbar Chakraborty",
+    author_email="durbardibyo@gmail.com",
+    install_requires=['prometheus_client >= 0.14.1']
+)
