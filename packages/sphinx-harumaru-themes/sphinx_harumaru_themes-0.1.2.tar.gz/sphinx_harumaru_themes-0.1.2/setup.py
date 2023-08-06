e127dac08d26d5bbec02e17705b54454ac546b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:54 +0000

    Add package sphinx-harumaru-themes/0.1.2/sphinx_harumaru_themes-0.1.2.tar.gz

---
+++ packages/sphinx-harumaru-themes/sphinx_harumaru_themes-0.1.2.tar.gz/sphinx_harumaru_themes-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setup(
+    name="sphinx_harumaru_themes",
+    version="0.1.2",
+    author="DanielSDVG",
+    author_email="danielsdvg@gmail.com",
+    url="",
+    description="A package with cute Sphinx documentation themes",
+    long_description=readme,
+    license="MIT",
+    packages=["sphinx_harumaru_themes"],
+    package_data={
+        "sphinx_harumaru_themes": [
+            "haruki_hw_theme/*",
+            "haruki_hw_theme/static/*",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "sphinx.html_themes": [
+            "haruki_hw = sphinx_harumaru_themes",
+        ]
+    },
+)
