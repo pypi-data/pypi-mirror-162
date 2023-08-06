cef5ec3e7d3681553301336f8dc342db4982bf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:15 +0000

    Add package electrumsv-bsor/0.9/electrumsv-bsor-0.9.tar.gz

---
+++ packages/electrumsv-bsor/electrumsv-bsor-0.9.tar.gz/electrumsv-bsor-0.9/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='electrumsv-bsor',
+      version='0.9',
+      description='A BSOR encoding/decoding library.',
+      author='Roger Taylor',
+      author_email='roger.taylor.email@gmail.com',
+      url='https://github.com/electrumsv/electrumsv-bsor',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      license='Open BSV License')
