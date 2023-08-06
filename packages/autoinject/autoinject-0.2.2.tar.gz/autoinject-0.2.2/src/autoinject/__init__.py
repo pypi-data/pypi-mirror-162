0d5bc529856f54666e7547371d5b8aaf89631f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:28 +0000

    Add package autoinject/0.2.2/autoinject-0.2.2.tar.gz

---
+++ packages/autoinject/autoinject-0.2.2.tar.gz/autoinject-0.2.2/src/autoinject/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+
+.. moduleauthor:: Erin Turnbull <erin.a.turnbull@gmail.com>
+
+"""
+from .injection import InjectionManager, MissingArgumentError, ExtraKeywordArgumentsError, ExtraPositionalArgumentsError
+from .class_registry import ClassRegistry, ClassNotFoundException, CacheStrategy
+from .context_manager import ContextManager
+
+__version__ = '0.2.2'
+
+injector = InjectionManager()
+
+from .informants import ContextInformant, NamedContextInformant
