a9d21d25e779bd7f13802a79da54c8c8d3c6a39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:21 +0000

    Add package iwopy/0.0.14/iwopy-0.0.14.tar.gz

---
+++ packages/iwopy/iwopy-0.0.14.tar.gz/iwopy-0.0.14/iwopy/__init__.py
@@ -0,0 +1,16 @@
+from .core import Problem, Objective, Constraint, Memory
+from .wrappers import (
+    ProblemWrapper,
+    DiscretizeRegGrid,
+    SimpleProblem,
+    SimpleObjective,
+    SimpleConstraint,
+)
+
+from . import utils
+from . import interfaces
+from . import benchmarks
+
+from importlib.resources import read_text
+
+__version__ = read_text(__package__, "VERSION")
