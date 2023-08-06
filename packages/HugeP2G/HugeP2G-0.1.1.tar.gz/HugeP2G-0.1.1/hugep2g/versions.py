34c6c17dbdaf61ed3c86cbf71c223a86ee8560b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package HugeP2G/0.1.1/HugeP2G-0.1.1.tar.gz

---
+++ packages/HugeP2G/HugeP2G-0.1.1.tar.gz/HugeP2G-0.1.1/hugep2g/versions.py
@@ -0,0 +1,18 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.1.1",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. Separate WPGmapper from ToolBiox and rename as HugeP2G",
+        ],
+    },
+]
