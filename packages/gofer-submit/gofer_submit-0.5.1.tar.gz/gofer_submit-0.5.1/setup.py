0fe05998ecdbf4ee8031702be104efd3c4ecdf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:39 +0000

    Add package gofer-submit/0.5.1/gofer_submit-0.5.1.tar.gz

---
+++ packages/gofer-submit/gofer_submit-0.5.1.tar.gz/gofer_submit-0.5.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="gofer_submit",
+    description="python package for nbextension to be used with Gofer Grader",
+    version='0.5.1',
+    author="V. Su(Sean Morris - changed 8/8/2022)",
+    author_email="sean.smorris@berkeley.edu",
+    url="https://github.com/data-8/gofer_submit",
+    include_package_data=True,
+    data_files=[
+        # like `jupyter nbextension install --sys-prefix`
+        ("share/jupyter/nbextensions/gofer_submit", [
+            "gofer_submit/static/index.js",
+        ]),
+        # like `jupyter nbextension enable --sys-prefix`
+        ("etc/jupyter/nbconfig/notebook.d", [
+            "jupyter-config/nbconfig/notebook.d/gofer_submit.json"
+        ])
+    ],
+    zip_safe=False
+)
