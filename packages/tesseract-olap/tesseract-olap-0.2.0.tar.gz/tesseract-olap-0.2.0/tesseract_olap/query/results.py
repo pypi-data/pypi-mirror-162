d32ac747389974d731c69d031c321471686b9462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:00 +0000

    Add package tesseract-olap/0.2.0/tesseract-olap-0.2.0.tar.gz

---
+++ packages/tesseract-olap/tesseract-olap-0.2.0.tar.gz/tesseract-olap-0.2.0/tesseract_olap/query/results.py
@@ -0,0 +1,34 @@
+"""Result structs module
+
+This module contains wrapper classes for the resulting data of queries obtained
+for structs in the requests module.
+"""
+
+from dataclasses import dataclass
+from typing import Any, Dict, Iterable, Sequence
+
+from tesseract_olap.query.exceptions import EmptyResult
+
+from .requests import DataRequest, MembersRequest
+
+
+@dataclass(eq=False, frozen=True, order=False)
+class DataResult:
+    """Container class for results to :class:`DataRequest`."""
+    data: Iterable[Dict[str, Any]]
+    sources: Sequence[Dict[str, Any]]
+    query: DataRequest
+
+    def raise_if_empty(self):
+        """Raises an :class:`EmptyResult` exception if the contained data has no
+        elements.
+        """
+        if next(iter(self.data), None) is None:
+            raise EmptyResult(self)
+
+
+@dataclass(eq=False, frozen=True, order=False)
+class MembersResult:
+    """Container class for results to :class:`MembersRequest`."""
+    data: Iterable[Dict[str, Any]]
+    query: MembersRequest
