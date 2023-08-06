c587a0e73e059d0faf78bb3485c8035f1c080fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package check-your-email-YK/0.1a0/check_your_email_YK-0.1.alpha.tar.gz

---
+++ packages/check-your-email-YK/check_your_email_YK-0.1.alpha.tar.gz/check_your_email_YK-0.1.alpha/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="check_your_email_YK",
+    version="0.1 alpha",
+    package=find_packages('src'),
+    package_dir={'':'src'}
+)
