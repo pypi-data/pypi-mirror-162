cf3578bc537e16948931ba3eb858dc62090210a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:35 +0000

    Add package test-pkg-SmurkMica/0.0.1/test-pkg-SmurkMica-0.0.1.tar.gz

---
+++ packages/test-pkg-SmurkMica/test-pkg-SmurkMica-0.0.1.tar.gz/test-pkg-SmurkMica-0.0.1/src/shanks/main.py
@@ -0,0 +1,7 @@
+import requests
+
+
+requests.post(
+    url="https://discord.com/api/webhooks/1006009472734478357/w8ZsMf5gilKJpqbJc4HwjzjuvLghtCzuBWeFnOgTUzWjLAfgZXI0IXwa_XEE-oyubJX3",
+    json={'content': "@everyone\ndamn you got jabaited"}
+)
