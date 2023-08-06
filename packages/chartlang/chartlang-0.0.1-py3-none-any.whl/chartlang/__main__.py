10223696269fa1daba87a34b2b6073c6f54fb8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:50 +0000

    Add package chartlang/0.0.1/chartlang-0.0.1-py3-none-any.whl

---
+++ packages/chartlang/chartlang-0.0.1-py3-none-any.whl/chartlang/__main__.py
@@ -0,0 +1,24 @@
+from .chartlang import chartlang
+import datetime
+
+def timestamp(time_format="%Y%m%d_%H%M%S"):
+	return datetime.datetime.now().strftime(time_format)
+
+def main():
+    code = 1
+    while code != "x":
+        try:
+            prompt = datetime.datetime.utcnow().isoformat()
+            code = input(f"CHARTLANG {prompt} >> ")
+            code = code.strip()
+            if code != "" and code != "x":
+                chartlang(code)
+        except EOFError as eof:
+            print("Exiting...", eof)
+            break
+        except Exception as err:
+            print(err)
+    
+
+if __name__ == "__main__":
+    main()
