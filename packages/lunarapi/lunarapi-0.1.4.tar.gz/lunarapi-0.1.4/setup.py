dad1ddd4f577f42c8f74ebdbc5cdfa9b4814d9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:36 +0000

    Add package lunarapi/0.1.4/lunarapi-0.1.4.tar.gz

---
+++ packages/lunarapi/lunarapi-0.1.4.tar.gz/lunarapi-0.1.4/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lunarapi', 'lunarapi.discordpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=9.2.0,<10.0.0',
+ 'aiohttp>=3.6.0,<3.8.0',
+ 'python-dotenv>=0.20.0,<0.21.0']
+
+setup_kwargs = {
+    'name': 'lunarapi',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'WinterFe',
+    'author_email': 'winter@lunardev.group',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
