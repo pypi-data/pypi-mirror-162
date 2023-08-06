506bd7a1d15f34cadb5a29e67daedb1f0a49cb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:27 +0000

    Add package covalent/0.171.0rc0/covalent-0.171.0rc0.tar.gz

---
+++ packages/covalent/covalent-0.171.0rc0.tar.gz/covalent-0.171.0rc0/covalent_dispatcher/_service/app.py
@@ -0,0 +1,148 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+import codecs
+from concurrent.futures import ThreadPoolExecutor
+
+import cloudpickle as pickle
+from flask import Blueprint, Response, jsonify, make_response, request
+
+import covalent_dispatcher as dispatcher
+from covalent._data_store.datastore import workflow_db
+from covalent._data_store.models import Lattice
+from covalent._results_manager.result import Result
+from covalent._results_manager.results_manager import result_from
+from covalent._shared_files import logger
+
+from .._db.dispatchdb import DispatchDB
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+bp = Blueprint("dispatcher", __name__, url_prefix="/api")
+
+
+@bp.before_app_first_request
+def start_pools():
+    global workflow_pool
+    global tasks_pool
+
+    workflow_pool = ThreadPoolExecutor()
+    tasks_pool = ThreadPoolExecutor()
+
+
+@bp.route("/submit", methods=["POST"])
+def submit() -> Response:
+    """
+    Function to accept the submit request of
+    new dispatch and return the dispatch id
+    back to the client.
+
+    Args:
+        None
+
+    Returns:
+        dispatch_id: The dispatch id in a json format
+                     returned as a Flask Response object.
+    """
+
+    json_lattice = request.get_data()
+
+    dispatch_id = dispatcher.run_dispatcher(json_lattice, workflow_pool, tasks_pool)
+
+    return jsonify(dispatch_id)
+
+
+@bp.route("/cancel", methods=["POST"])
+def cancel() -> Response:
+    """
+    Function to accept the cancel request of
+    a dispatch.
+
+    Args:
+        None
+
+    Returns:
+        Flask Response object confirming that the dispatch
+        has been cancelled.
+    """
+    dispatch_id = request.get_data().decode("utf-8")
+
+    dispatcher.cancel_running_dispatch(dispatch_id)
+
+    return jsonify(f"Dispatch {dispatch_id} cancelled.")
+
+
+@bp.route("/db-path", methods=["GET"])
+def db_path() -> Response:
+    db_path = DispatchDB()._dbpath
+    return jsonify(db_path)
+
+
+@bp.route("/result/<dispatch_id>", methods=["GET"])
+def get_result(dispatch_id) -> Response:
+    app_log.warning("get result")
+    args = request.args
+    wait = args.get("wait", default=False, type=lambda v: v.lower() == "true")
+    app_log.warning("wait is " + str(wait))
+    status_only = args.get("status_only", default=False, type=lambda v: v.lower() == "true")
+    while True:
+        with workflow_db.session() as session:
+            lattice_record = (
+                session.query(Lattice).where(Lattice.dispatch_id == dispatch_id).first()
+            )
+            status = lattice_record.status if lattice_record else None
+            try:
+                if not lattice_record:
+                    return (
+                        jsonify(
+                            {"message": f"The requested dispatch ID {dispatch_id} was not found."}
+                        ),
+                        404,
+                    )
+                elif not wait or status in [
+                    str(Result.COMPLETED),
+                    str(Result.FAILED),
+                    str(Result.CANCELLED),
+                    str(Result.POSTPROCESSING_FAILED),
+                    str(Result.PENDING_POSTPROCESSING),
+                ]:
+                    output = {
+                        "id": dispatch_id,
+                        "status": lattice_record.status,
+                    }
+                    if not status_only:
+                        output["result"] = codecs.encode(
+                            pickle.dumps(result_from(lattice_record)), "base64"
+                        ).decode()
+                    return jsonify(output)
+
+            except (FileNotFoundError, EOFError):
+                if wait:
+                    continue
+                response = make_response(
+                    jsonify(
+                        {
+                            "message": "Result not ready to read yet. Please wait for a couple of seconds."
+                        }
+                    ),
+                    503,
+                )
+                response.retry_after = 2
+                return response
