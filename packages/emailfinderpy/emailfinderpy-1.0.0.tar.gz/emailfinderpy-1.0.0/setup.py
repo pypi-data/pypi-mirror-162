d6aae2a3344768d10414d674ad740838f5e6a3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:51 +0000

    Add package emailfinderpy/1.0.0/emailfinderpy-1.0.0.tar.gz

---
+++ packages/emailfinderpy/emailfinderpy-1.0.0.tar.gz/emailfinderpy-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Find and verify work emails.'
+
+# Setting up
+setup(
+    name="emailfinderpy",
+    version=VERSION,
+    author="Sean Michael Suntoso",
+    author_email="<contact@ssuntoso.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'dnspython'],
+    keywords=['python', 'email', 'email finder', 'work email', 'company domain', 'start up'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
