6d7d4f78ed61a6dc537069d14d1b4a37428f6f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package ebookmeta/1.2.8/ebookmeta-1.2.8.tar.gz

---
+++ packages/ebookmeta/ebookmeta-1.2.8.tar.gz/ebookmeta-1.2.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='ebookmeta',
+    version='1.2.8',
+    author='Dmitrii Korpushov',
+    author_email='dnkorpushov@gmail.com',
+    packages=['ebookmeta'],
+    url='https://github.com/dnkorpushov/ebookmeta',
+    license='MIT',
+    description='Read/write ebook metadata for fb2, epub2 and epub3 files',
+    keywords=['ebook', 'epub', 'metadata', 'fb2'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    install_requires=['lxml']
+)
