04f57c64fedc3a1b32b0f450cf348559e78015ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:00 +0000

    Add package mls-api/1.0.0/mls-api-1.0.0.tar.gz

---
+++ packages/mls-api/mls-api-1.0.0.tar.gz/mls-api-1.0.0/mlsapi_client/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mlsapi_client.api.assists_api import AssistsApi
+from mlsapi_client.api.authentication_api import AuthenticationApi
+from mlsapi_client.api.fixtures_api import FixturesApi
+from mlsapi_client.api.historical_data_api import HistoricalDataApi
+from mlsapi_client.api.latest_news_api import LatestNewsApi
+from mlsapi_client.api.offence_api import OffenceApi
+from mlsapi_client.api.players_api import PlayersApi
+from mlsapi_client.api.real_time_data___live_scores_api import RealTimeDataLiveScoresApi
+from mlsapi_client.api.standings_api import StandingsApi
+from mlsapi_client.api.teams_api import TeamsApi
+from mlsapi_client.api.top_scorer_api import TopScorerApi
