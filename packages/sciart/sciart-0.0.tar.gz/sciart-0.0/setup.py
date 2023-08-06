4a2fc1a0e353d7c4c2a90879fc74a828a7e2f921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:59 +0000

    Add package sciart/0.0/sciart-0.0.tar.gz

---
+++ packages/sciart/sciart-0.0.tar.gz/sciart-0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sciart',
+    version='0.0',
+    license='MIT',
+    author="Soran Ghaderi",
+    author_email='soran.gdr.cs@gmail.com',
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    keywords='transformer',
+)
