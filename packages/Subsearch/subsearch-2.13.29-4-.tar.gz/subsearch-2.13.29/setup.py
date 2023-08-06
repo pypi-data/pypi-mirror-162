9a6138851cdb8b5e468e045102aae03bde159844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:04 +0000

    Add package Subsearch/2.13.29/subsearch-2.13.29-4-.tar.gz

---
+++ packages/Subsearch/subsearch-2.13.29-4-.tar.gz/subsearch-2.13.29/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        return f.read()
+
+
+requirements = read_file("requirements.txt").strip().split()
+
+
+setup(
+    install_requires=requirements,
+)
