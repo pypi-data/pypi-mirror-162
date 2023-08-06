3c4b6810bda03d3201bbbb1cb5e616a261c7aec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:34 +0000

    Add package irisml-tasks-training/0.0.8/irisml-tasks-training-0.0.8.tar.gz

---
+++ packages/irisml-tasks-training/irisml-tasks-training-0.0.8.tar.gz/irisml-tasks-training-0.0.8/test/test_build_zero_shot_classifier.py
@@ -0,0 +1,19 @@
+import unittest
+import torch
+from irisml.tasks.build_zero_shot_classifier import Task
+
+
+class TestBuildZeroShotClassifier(unittest.TestCase):
+    def test_simple(self):
+        zero_feature = torch.ones(32)
+        zero_feature[0] = 10
+        one_feature = torch.ones(32)
+        one_feature[1] = 10
+
+        text_features = [one_feature, zero_feature, one_feature, zero_feature]
+        text_classes = [1, 0, 1, 0]
+        inputs = Task.Inputs(text_features, text_classes)
+        outputs = Task(Task.Config(num_classes=2)).execute(inputs)
+
+        self.assertEqual(torch.argmax(outputs.classifier(one_feature)), 1)
+        self.assertEqual(torch.argmax(outputs.classifier(zero_feature)), 0)
