70fd7521ffc7a76c63ca11e7c074a2d77d4df17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:39 +0000

    Add package web3-security-engine/1.0/web3-security-engine-1.0.tar.gz

---
+++ packages/web3-security-engine/web3-security-engine-1.0.tar.gz/web3-security-engine-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="web3-security-engine",
+    version="1.0",
+    author="Debank",
+    author_email="0xjeffer@gmail.com",
+    description="Web3 Security Engine",
+    url="https://rabby.io", 
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'gitdb==4.0.9',
+        'GitPython==3.1.27',
+        'PyYAML==6.0',
+        'smmap==5.0.0'
+    ]
+)
