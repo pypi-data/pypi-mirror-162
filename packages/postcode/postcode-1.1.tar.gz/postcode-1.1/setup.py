fe8013d7231a7bee6f2f40b295fb443ab62640c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:42 +0000

    Add package postcode/1.1/postcode-1.1.tar.gz

---
+++ packages/postcode/postcode-1.1.tar.gz/postcode-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='postcode',
+    version='1.1',
+    license='MIT',
+    author="Quentin PETIT",
+    author_email='contact@quentinptt.fr',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/quentinptt/postcode',
+    keywords='postcode',
+    install_requires=[
+          'requests',
+      ],
+
+)
