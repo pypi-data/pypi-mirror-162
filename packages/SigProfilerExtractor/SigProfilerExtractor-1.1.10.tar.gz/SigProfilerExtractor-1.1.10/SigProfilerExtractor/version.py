f0aebf6059b8aa4671a738db40fb59bfd57b74ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:35 +0000

    Add package SigProfilerExtractor/1.1.10/SigProfilerExtractor-1.1.10.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.10.tar.gz/SigProfilerExtractor-1.1.10/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.10'
+version = '1.1.10'
+Update = '1. Reset reference genome to GRCh37 if a not supported genome is selected. 2. Add support for COSMIC exome reference signatures'
+    
+    
