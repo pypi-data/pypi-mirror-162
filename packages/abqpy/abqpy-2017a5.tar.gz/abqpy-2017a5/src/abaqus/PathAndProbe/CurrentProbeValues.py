52e9bf19a34735a90b1c957a707b4ae2cfae9255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:44 +0000

    Add package abqpy/2017a5/abqpy-2017a5.tar.gz

---
+++ packages/abqpy/abqpy-2017a5.tar.gz/abqpy-2017a5/src/abaqus/PathAndProbe/CurrentProbeValues.py
@@ -0,0 +1,16 @@
+class CurrentProbeValues:
+    """The CurrentProbeValues object has no constructor. The CurrentProbeValues object is
+    created when you import the Visualization module.
+
+    .. note:: 
+        This object can be accessed by:
+
+        .. code-block:: python
+
+            import visualization
+            session.currentProbeValues
+    """
+
+    #: A tuple of Floats specifying the values obtained while probing. These values are updated
+    #: constantly as the user moves the mouse over the object being probed.
+    values: tuple[float] = None
