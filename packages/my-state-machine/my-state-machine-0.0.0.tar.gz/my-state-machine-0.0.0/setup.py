d5afa6550328466e2061acee428dca2ea785c6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:48 +0000

    Add package my-state-machine/0.0.0/my-state-machine-0.0.0.tar.gz

---
+++ packages/my-state-machine/my-state-machine-0.0.0.tar.gz/my-state-machine-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding = utf-8
+
+from setuptools import setup
+
+setup(
+    name='my-state-machine',
+    version='0.0.0',
+    description='Simple State Machine',
+    long_description='''You can use this library to build a state machine.
+    ''',
+    author='Wenhan Zhang',
+    license='MIT License',
+    platforms='any',
+    packages=['my_state_machine']
+)
