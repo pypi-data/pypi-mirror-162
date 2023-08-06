a8ca222fbb8c89f15b173f786be60ca649bc808c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:56 +0000

    Add package SymQInfopy/0.0.1/SymQInfopy-0.0.1.tar.gz

---
+++ packages/SymQInfopy/SymQInfopy-0.0.1.tar.gz/SymQInfopy-0.0.1/README.txt
@@ -0,0 +1,2 @@
+This is a package for Symbolic Algebraic calculations in Quantum Information Theory
+built on top of Sympy.
---
+++ packages/SymQInfopy/SymQInfopy-0.0.1.tar.gz/SymQInfopy-0.0.1/SymQInfopy/__init__.py
@@ -0,0 +1,5 @@
+from sympy.matrices import Matrix
+from sympy.physics.quantum import TensorProduct, qapply, Dagger, OrthogonalKet, OrthogonalBra
+from sympy.interactive import printing
+printing.init_printing(use_latex = True)
+from sympy import *
