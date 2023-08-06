52acebe74f8a7cbeb49bcb2f2b11dbad561d86aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:37 +0000

    Add package gaussian-binom-dist/0.1/gaussian_binom_dist-0.1.tar.gz

---
+++ packages/gaussian-binom-dist/gaussian_binom_dist-0.1.tar.gz/gaussian_binom_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binom_dist',
+      version='0.1',
+      description='Gaussian distributions and binomial distributions',
+      packages=['gaussian_binom_dist'],
+      author="Cihan Evren",
+      author_email="cihan.evren2@outlook.com",
+      zip_safe=False)
