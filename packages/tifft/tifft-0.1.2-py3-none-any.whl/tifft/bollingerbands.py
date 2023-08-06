75283e56476194a614db54bce693b2815d347e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:49 +0000

    Add package tifft/0.1.2/tifft-0.1.2-py3-none-any.whl

---
+++ packages/tifft/tifft-0.1.2-py3-none-any.whl/tifft/bollingerbands.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import logging
+import os
+from pprint import pformat
+
+import numpy as np
+import pandas as pd
+
+
+class BollingerBandsCalculator(object):
+    def __init__(self, window_size=20, sd_multiplier=2, **kwargs):
+        self.__logger = logging.getLogger(__name__)
+        self.sd_multiplier = sd_multiplier
+        self.rolling_kwargs = {'window': window_size, **kwargs}
+        self.__logger.debug(f'vars(self):{os.linesep}' + pformat(vars(self)))
+
+    def calculate(self, values):
+        return (
+            values.to_frame(name='value') if isinstance(values, pd.Series)
+            else pd.DataFrame({'value': values})
+        ).assign(
+            value_ff=lambda d: d['value'].fillna(method='ffill')
+        ).assign(
+            ma=lambda d: d['value_ff'].rolling(**self.rolling_kwargs).mean(),
+            sd=lambda d: d['value_ff'].rolling(**self.rolling_kwargs).std()
+        ).assign(
+            lower_bb=lambda d: (d['ma'] - d['sd'] * self.sd_multiplier),
+            upper_bb=lambda d: (d['ma'] + d['sd'] * self.sd_multiplier),
+            residual=lambda d: ((d['value_ff'] - d['ma']) / d['sd']).fillna(0)
+        ).assign(
+            signal=lambda d: np.where(
+                d['residual'] > 0, np.floor(d['residual']),
+                np.ceil(d['residual'])
+            ).astype(int)
+        ).drop(columns=['value_ff', 'residual'])
---
+++ packages/tifft/tifft-0.1.2-py3-none-any.whl/tifft/macd.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import logging
+import os
+from pprint import pformat
+
+import pandas as pd
+
+
+class MacdCalculator(object):
+    def __init__(self, fast_ema_span=12, slow_ema_span=26, macd_ema_span=9,
+                 **kwargs):
+        assert fast_ema_span < slow_ema_span, 'invalid spans'
+        self.__logger = logging.getLogger(__name__)
+        self.fast_ema_span = fast_ema_span
+        self.slow_ema_span = slow_ema_span
+        self.macd_ema_span = macd_ema_span
+        self.ewm_kwargs = {'adjust': False, **kwargs}
+        self.__logger.debug(f'vars(self):{os.linesep}' + pformat(vars(self)))
+
+    def calculate(self, values):
+        return (
+            values.to_frame(name='value') if isinstance(values, pd.Series)
+            else pd.DataFrame({'value': values})
+        ).assign(
+            value_ff=lambda d: d['value'].fillna(method='ffill')
+        ).assign(
+            macd=lambda d: (
+                d['value_ff'].ewm(
+                    span=self.fast_ema_span, **self.ewm_kwargs
+                ).mean()
+                - d['value_ff'].ewm(
+                    span=self.slow_ema_span, **self.ewm_kwargs
+                ).mean()
+            )
+        ).assign(
+            macd_ema=lambda d: d['macd'].ewm(
+                span=self.macd_ema_span, **self.ewm_kwargs
+            ).mean()
+        ).assign(
+            macd_ema_delta=lambda d: (d['macd'] - d['macd_ema'])
+        ).assign(
+            signal=lambda d: d['macd_ema_delta'].mask(
+                (d['macd_ema_delta'] > 0) & (d['macd'] > 0), 2
+            ).mask(
+                d['macd_ema_delta'] > 0, 1
+            ).mask(
+                (d['macd_ema_delta'] < 0) & (d['macd'] < 0), -2
+            ).mask(
+                d['macd_ema_delta'] < 0, -1
+            ).astype(int)
+        ).drop(columns=['value_ff', 'macd_ema_delta'])
