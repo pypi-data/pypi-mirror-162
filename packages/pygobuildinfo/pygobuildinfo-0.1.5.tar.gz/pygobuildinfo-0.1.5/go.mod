65ae3da13141d1899a020ec25ccad9d3aada87a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:59 +0000

    Add package pygobuildinfo/0.1.5/pygobuildinfo-0.1.5.tar.gz

---
+++ packages/pygobuildinfo/pygobuildinfo-0.1.5.tar.gz/pygobuildinfo-0.1.5/go.mod
@@ -0,0 +1,3 @@
+module github.com/MikeMoore63/pybuildInfo
+
+go 1.18
---
+++ packages/pygobuildinfo/pygobuildinfo-0.1.5.tar.gz/pygobuildinfo-0.1.5/setup.py
@@ -0,0 +1,8 @@
+
+from setuptools import Extension, setup
+
+if __name__ == '__main__':
+    setup(
+        build_golang={'root': 'github.com/MikeMoore63/pybuildInfo'},
+        ext_modules=[Extension('_pyGoBuildinfo', ['pyGoBuildInfo.go'])]
+          )
