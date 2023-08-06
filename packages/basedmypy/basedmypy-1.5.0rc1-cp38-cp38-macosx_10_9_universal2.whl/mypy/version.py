b896596a74862d95b0e83020e9234ea5d9883053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package basedmypy/1.5.0rc1/basedmypy-1.5.0rc1-cp38-cp38-macosx_10_9_universal2.whl

---
+++ packages/basedmypy/basedmypy-1.5.0rc1-cp38-cp38-macosx_10_9_universal2.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "0.971"
+based_version_info = VersionInfo(major=1, minor=5, patch=0, release_level='rc', iteration=1, mypy_version='0.971', mypy_release_level='final')
+__based_version__ = "1.5.0rc1"
