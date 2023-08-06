ad1222737d057ab8e8ee8f660a308dc430a52162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:04 +0000

    Add package NumberScript/1.11.7/NumberScript-1.11.7.tar.gz

---
+++ packages/NumberScript/NumberScript-1.11.7.tar.gz/NumberScript-1.11.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open('README.md') as f:
+    longest_description = f.read()
+
+setuptools.setup(
+    name="NumberScript",
+    version="1.11.7",
+    description="possibly the world's most simplest and restricted language.",
+    author="Sasen Perera",
+    long_description=longest_description,
+    long_description_content_type="text/markdown",
+    packages=["NumberScript"],
+    github_url="https://github.com/Sas2k/NumberScript",
+    license="MIT"
+)
