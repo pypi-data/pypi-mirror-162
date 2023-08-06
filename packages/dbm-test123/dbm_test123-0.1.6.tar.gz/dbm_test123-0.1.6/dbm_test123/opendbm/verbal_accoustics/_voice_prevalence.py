b12079a576cc38587597692170470334fd53e84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:53 +0000

    Add package dbm-test123/0.1.6/dbm_test123-0.1.6.tar.gz

---
+++ packages/dbm-test123/dbm_test123-0.1.6.tar.gz/dbm_test123-0.1.6/dbm_test123/opendbm/verbal_accoustics/_voice_prevalence.py
@@ -0,0 +1,14 @@
+
+from dbm_test123.opendbm.model import AudioModel 
+from dbm_test123.dbm_lib.dbm_features.raw_features.audio.voice_frame_score import run_vfs
+import pandas as pd
+
+class VoicePrevalence(AudioModel):
+    def __init__(self):
+        super().__init__()
+        self._params = ['aco_voiceframe', 'aco_totvoiceframe', 'aco_voicepct']
+
+    @AudioModel.prep_func
+    def _fit_transform(self, path, **kwargs):
+        return run_vfs(path, '.', self.r_config, save=False)
+        
