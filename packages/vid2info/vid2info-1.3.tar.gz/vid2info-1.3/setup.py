fc6968c78e77c95f4f410b72b9222e39d5bb655f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:35 +0000

    Add package vid2info/1.3/vid2info-1.3.tar.gz

---
+++ packages/vid2info/vid2info-1.3.tar.gz/vid2info-1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vid2info',
+    version='1.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Eric-Canas/vid2info',
+    license='MIT',
+    author='Eric Canas',
+    author_email='elcorreodeharu@gmail.com',
+    description='Vid2Info is an easy-to-use Computer Vision based pipeline that implements Detection, Tracking and, optionally, Segmentation.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'opencv-python',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+    ]
+)
