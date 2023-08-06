3e604609121659d3a46b9ae92532f820ca25a042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package apafib/0.0.5/apafib-0.0.5.tar.gz

---
+++ packages/apafib/apafib-0.0.5.tar.gz/apafib-0.0.5/apafib/__init__.py
@@ -0,0 +1,39 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+*************
+
+:Description: __init__.py
+
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 06/05/2022 10:53 
+
+"""
+
+from apafib.datasets import fetch_apa_data, load_BCN_IBEX, load_BCN_UK, load_BCN_vuelos, \
+     load_medical_costs, load_stroke, load_wind_prediction, load_titanic, load_crabs, \
+          load_life_expextancy 
+
+from apafib.classifiers import BlackBoxClassifier, BlackBoxRegressor
+
+
+__all__ = ['fetch_apa_data', 
+           'BlackBoxClassifier', 
+           'BlackBoxRegressor',
+           'load_BCN_IBEX',
+           'load_BCN_UK',
+           'load_BCN_vuelos',
+           'load_medical_costs',
+           'load_wind_prediction',
+           'load_stroke',
+           'load_titanic',
+           'load_crabs',
+           'load_life_expectancy'
+
+           ]
