dc698c12272ad0b3d0b173776a5e3af0eb1495d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:51 +0000

    Add package error-alerts/2.5/error-alerts-2.5.tar.gz

---
+++ packages/error-alerts/error-alerts-2.5.tar.gz/error-alerts-2.5/error_alerts/__init__.py
@@ -0,0 +1,26 @@
+import traceback, telegram
+
+class setup:
+    def __init__(self, telegram_token, telegram_channel, full_error=False, raise_error=False):
+        self.bot = telegram.Bot(token=telegram_token)
+        self.channel = telegram_channel
+        self.full_error = full_error
+        self.raise_error = raise_error
+    def send(self, title, exception):
+        error = str(exception)
+        if self.full_error:
+            error = traceback.format_exc()
+        message = f'{title}: {error}'
+        self.bot.send_message(self.channel, message[:4096])
+        print(message)
+        print()
+        if self.raise_error:
+            raise Exception('Raiser') from exception
+    def send_message(self, *messages):
+        final_message = ''
+        for message in messages:
+            final_message += message
+            final_message += ' '
+        self.bot.send_message(self.channel, final_message)
+        print(final_message)
+        print()
