7e098a92d3f7021cfe7a92caad4b287eec24743f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:31 +0000

    Add package javapython/0.0.1/javapython-0.0.1.tar.gz

---
+++ packages/javapython/javapython-0.0.1.tar.gz/javapython-0.0.1/README.md
@@ -0,0 +1,10 @@
+![Our banner](banner.png)
+# JavaPython:
+## 1.0 Added only JFrame
+#
+# Use on importing:
+# ______________________
+## | from javapython import * |
+# ______________________
+#
+## See the documentation for more
---
+++ packages/javapython/javapython-0.0.1.tar.gz/javapython-0.0.1/src/javapython/__init__.py
@@ -0,0 +1,21 @@
+import tkinter
+
+title = tkinter.Tk(useTk=False)
+
+class javax:
+    class swing:
+        class JFrame:
+            class JFrame():
+                JFrame = tkinter.Tk()
+                def addtitle(self, titl):
+                    """Add a title (not on java but the java technic dosen't work)"""
+                    javax.swing.JFrame.JFrame.JFrame.wm_title(titl)
+                def showVisible(self, boolean):
+                    '''If "true" the window show, If "false" the window dosen't show'''
+                    if boolean == "true":
+                        import __init__
+                        __init__.javax.swing.JFrame.JFrame.JFrame.mainloop()
+                    elif boolean == "false":
+                        ...
+                    else:
+                        print("The method showVisible(String) is undefined for the type JFrame (Java(67108964))")
