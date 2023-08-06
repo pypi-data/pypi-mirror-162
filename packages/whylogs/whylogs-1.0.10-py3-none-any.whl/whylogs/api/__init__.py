04e34cfdee14d6ed42a4734d8badc868460e91cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:55 +0000

    Add package whylogs/1.0.10/whylogs-1.0.10-py3-none-any.whl

---
+++ packages/whylogs/whylogs-1.0.10-py3-none-any.whl/whylogs/api/__init__.py
@@ -0,0 +1,11 @@
+from .annotations import profiling
+from .logger import ResultSet, log, logger, read, write
+
+__ALL__ = [
+    log,
+    logger,
+    read,
+    write,
+    profiling,
+    ResultSet,
+]
