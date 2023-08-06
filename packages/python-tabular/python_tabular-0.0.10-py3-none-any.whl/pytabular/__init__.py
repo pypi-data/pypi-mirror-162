32c908ecce4689577ad31857161b5a4ea1016f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:23 +0000

    Add package python-tabular/0.0.10/python_tabular-0.0.10-py3-none-any.whl

---
+++ packages/python-tabular/python_tabular-0.0.10-py3-none-any.whl/pytabular/__init__.py
@@ -0,0 +1,9 @@
+
+import os
+import sys
+dll = os.path.join(os.path.dirname(__file__),"dll")
+sys.path.append(dll)
+sys.path.append(os.path.dirname(__file__))
+from . pytabular import Tabular, BPA, TE2
+from . basic_checks import Return_Zero_Row_Tables, Table_Last_Refresh_Times, BPA_Violations_To_DF
+from . logic_utils import pd_dataframe_to_m_expression, pandas_datatype_to_tabular_datatype
