63dea45ae90bd6706dc98c4070511ffd9e999942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package dayan-contextcapture/1.0.0/dayan_contextcapture-1.0.0-py3-none-any.whl

---
+++ packages/dayan-contextcapture/dayan_contextcapture-1.0.0-py3-none-any.whl/dayan_contextcapture/constants.py
@@ -0,0 +1,61 @@
+"""The constants of the rayvision_contextcapture."""
+
+PACKAGE_NAME = "rayvision_contextcapture"
+
+TASK_INFO = {
+    'task_info': {
+        "locationoutput": "",
+        "task_stop_time": "259200",
+        "job_id_alias": "",
+        "channel": "9",
+        "frames_per_task": "1",
+        "task_id": "",
+        "project_name": "",
+        "platform": "24",
+        "tiles": "",
+        "is_picture": "1",
+        "distribute_node": "1",
+        "project_id": "0",
+        "job_stop_time": "259200",
+        "display_common_tiles": "",
+        "stop_after_test": "1",
+        "ram": "32",
+        "render_layer_type": "0",
+        "config_name": "",
+        "graphics_cards_num": "2",
+        "edit_name": "",
+        "pre_frames": "",
+        "input_project_path": "",
+        "is_distribute_render": "0",
+        "tiles_type": "strip",
+        "time_out": "43200",
+        "cg_id": "",
+        "job_id": "",
+        "input_cg_file": ""
+    },
+    'software_config': {},
+    'scene_info': {},
+    'scene_info_render': {},
+    'client_setting': {},
+    'plugin_setting': {},
+    'render_setting': {},
+    'user_setting': {}
+}
+
+UPLOAD_INFO = {
+    'asset': [],
+    'scene': [],
+    'control_point': [],
+    'pos_file': [],
+    'kml_file': [],
+    'block_files': []
+}
+
+ASSET_INFO = {
+    'asset': [],
+    "scene": [],
+    "ctrl_point_file": [],
+    "pos_file": [],
+    "kml_file": [],
+    "block_files": []
+}
