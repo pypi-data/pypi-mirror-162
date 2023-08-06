70146fe99275626a05267a911a182eaa554b573a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package swissql/0.2.2/swissql-0.2.2.tar.gz

---
+++ packages/swissql/swissql-0.2.2.tar.gz/swissql-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from swissql.manifest import Manifest
+ 
+ 
+
+ 
+setup(name=Manifest.APP_NAME,
+      version=Manifest.APP_VERSION,
+      url=Manifest.GIT_URL,
+      license=Manifest.LICENSE,
+      author=Manifest.AUTHORS_GIT,
+      author_email=Manifest.EMAIL,
+      packages= find_packages(),
+      install_requires = Manifest.REQUIREMENTS ,
+      description=Manifest.APP_DESCRIPTION,
+      long_description=open('README.md').read(),
+      python_requires=">=3.7.*",
+      entry_points={
+        'console_scripts': [
+            'swissql = swissql.main:main_start',
+        ]}
+      )
