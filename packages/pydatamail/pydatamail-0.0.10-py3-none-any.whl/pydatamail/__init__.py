4d868086ac712a5fb5b1c874558495a7d838af38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:30 +0000

    Add package pydatamail/0.0.10/pydatamail-0.0.10-py3-none-any.whl

---
+++ packages/pydatamail/pydatamail-0.0.10-py3-none-any.whl/pydatamail/__init__.py
@@ -0,0 +1,7 @@
+from pydatamail.database import get_email_database, DatabaseTemplate
+from pydatamail.plots import (
+    get_from_pie_plot,
+    get_labels_pie_plot,
+    get_number_of_email_plot,
+)
+from pydatamail.message import Message, email_date_converter
