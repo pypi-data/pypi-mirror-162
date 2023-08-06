47e1a3bb34aa423c16e1690219e6b1b0de927500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:58 +0000

    Add package pysystemair/0.3.9/pysystemair-0.3.9.tar.gz

---
+++ packages/pysystemair/pysystemair-0.3.9.tar.gz/pysystemair-0.3.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pysystemair']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.0,<2.0.0', 'pymodbus>=2.5.3,<3.0.0', 'python-box>=6.0.2,<7.0.0']
+
+setup_kwargs = {
+    'name': 'pysystemair',
+    'version': '0.3.9',
+    'description': 'Python package to interface a SystamAir SAVE VTR ventilation through modbus. Heavily insipired by pyflexit (https://github.com/Sabesto/pyflexit). Made with the purpose of working with a home assistant component',
+    'long_description': None,
+    'author': 'mvheimburg',
+    'author_email': 'heimburg@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
