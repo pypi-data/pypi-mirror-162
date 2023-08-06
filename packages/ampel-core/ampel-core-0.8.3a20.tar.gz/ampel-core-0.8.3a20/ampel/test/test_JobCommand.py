7e20869e0204625cd66c0805019833b83c48a95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:01 +0000

    Add package ampel-core/0.8.3a20/ampel-core-0.8.3a20.tar.gz

---
+++ packages/ampel-core/ampel-core-0.8.3a20.tar.gz/ampel-core-0.8.3a20/ampel/test/test_JobCommand.py
@@ -0,0 +1,70 @@
+import sys, pytest, yaml
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Optional
+from pytest_mock import MockerFixture
+
+from ampel.cli.main import main
+from ampel.secret.AmpelVault import AmpelVault
+
+
+@contextmanager
+def argv_context(args: list[str]):
+    argv = sys.argv
+    try:
+        sys.argv = args
+        yield
+    finally:
+        sys.argv = argv
+
+
+def run(args: list[str]) -> Optional[int]:
+    try:
+        with argv_context(args):
+            main()
+        return None
+    except SystemExit as exit:
+        return exit.code
+
+
+def dump(payload, tmpdir, name):
+    f = Path(tmpdir) / name
+    yaml.dump(payload, f.open("w"))
+    return f
+
+
+@pytest.fixture
+def vault(tmpdir):
+    return dump({"foo": "bar"}, tmpdir, "secrets.yml")
+
+
+@pytest.fixture
+def schema(tmpdir):
+    return dump({"name": "job", "task": [{"unit": "Nonesuch"}]}, tmpdir, "schema.yml")
+
+
+def test_secrets(testing_config, vault: Path, schema: Path, mocker: MockerFixture):
+    new_context_unit = mocker.patch("ampel.core.UnitLoader.UnitLoader.new_context_unit")
+    assert (
+        run(
+            [
+                "ampel",
+                "job",
+                "--config",
+                str(testing_config),
+                "--secrets",
+                str(vault),
+                "--schema",
+                str(schema),
+            ]
+        )
+        == None
+    )
+    assert new_context_unit.call_count == 1
+    loader_vault: Optional[AmpelVault] = new_context_unit.call_args.kwargs[
+        "context"
+    ].loader.vault
+    assert loader_vault is not None
+    secret = loader_vault.get_named_secret("foo")
+    assert secret is not None
+    assert secret.get() == "bar"
---
+++ packages/ampel-core/ampel-core-0.8.3a20.tar.gz/ampel-core-0.8.3a20/ampel/test/test_UnitLoader.py
@@ -0,0 +1,146 @@
+from typing import Any
+from ampel.base.LogicalUnit import LogicalUnit
+from ampel.secret.NamedSecret import NamedSecret
+from ampel.dev.DevAmpelContext import DevAmpelContext
+import pytest
+
+from ampel.secret.Secret import Secret
+from ampel.secret.DictSecretProvider import DictSecretProvider
+from ampel.secret.AmpelVault import AmpelVault
+from ampel.model.UnitModel import UnitModel
+
+
+@pytest.fixture
+def secrets():
+    return DictSecretProvider(
+        {
+            "dict": {"a": 1, "b": "foo"},
+            "str": "blahblablah",
+            "tuple": (1, 1),
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    "expected_type,key", [(dict, "dict"), (str, "str"), (tuple, "tuple")]
+)
+def test_resolve_secrets_correct_type(
+    dev_context: DevAmpelContext,
+    secrets: DictSecretProvider,
+    monkeypatch,
+    expected_type,
+    key,
+    ampel_logger,
+):
+    class Modelo(LogicalUnit):
+        seekrit: NamedSecret[expected_type] = NamedSecret(label=key)  # type: ignore[valid-type]
+
+    dev_context.register_unit(Modelo)
+    monkeypatch.setattr(dev_context.loader, "vault", AmpelVault(providers=[secrets]))
+
+    unit = dev_context.loader.new(
+        UnitModel(unit="Modelo"), logger=ampel_logger, unit_type=Modelo
+    )
+    assert unit.seekrit.get() == secrets.store[key]
+
+
+@pytest.mark.parametrize(
+    "expected_type,key", [(dict, "str"), (str, "tuple"), (tuple, "dict")]
+)
+def test_resolve_secrets_wrong_type(
+    secrets, dev_context: DevAmpelContext, monkeypatch, expected_type, key, ampel_logger
+):
+    class Modelo(LogicalUnit):
+        seekrit: NamedSecret[expected_type] = NamedSecret(label=key)  # type: ignore[valid-type]
+
+    dev_context.register_unit(Modelo)
+    monkeypatch.setattr(dev_context.loader, "vault", AmpelVault(providers=[secrets]))
+    with pytest.raises(ValueError):
+        dev_context.loader.new(
+            UnitModel(unit="Modelo"), logger=ampel_logger, unit_type=Modelo
+        )
+
+
+def test_resolve_secret_from_config(secrets, dev_context: DevAmpelContext, monkeypatch, ampel_logger):
+    monkeypatch.setattr(dev_context.loader, "vault", AmpelVault(providers=[secrets]))
+
+    class NiceAndConcrete(LogicalUnit):
+        seekrit: NamedSecret[dict]
+
+    dev_context.register_unit(NiceAndConcrete)
+    # unit with concrete secret field can be instantiated
+    dev_context.loader.new(
+        UnitModel(unit="NiceAndConcrete", config={"seekrit": {"label": "dict"}}),
+        logger=ampel_logger,
+        unit_type=NiceAndConcrete
+    )
+
+    # and also validated without instantiating
+    with dev_context.loader.validate_unit_models():
+        UnitModel(unit="NiceAndConcrete", config={"seekrit": {"label": "dict"}})
+        with pytest.raises(TypeError):
+            UnitModel(unit="NiceAndConcrete")
+
+    # unit with abstract secret field cannot be instantiated
+    class BadAndAbstract(LogicalUnit):
+        seekrit: Secret[dict]
+
+    dev_context.register_unit(BadAndAbstract)
+    with pytest.raises(TypeError):
+        dev_context.loader.new(
+            UnitModel(unit="BadAndAbstract", config={"seekrit": {"label": "dict"}}),
+            logger=ampel_logger,
+            unit_type=BadAndAbstract
+        )
+
+
+def test_unit_validation(dev_context: DevAmpelContext):
+    class Dummy(LogicalUnit):
+        param: int = 42
+
+    dev_context.register_unit(Dummy)
+
+    with dev_context.loader.validate_unit_models():
+        # simple, one-level validation
+        UnitModel(unit="Dummy")
+        UnitModel(unit="Dummy", config={"param": 37})
+        with pytest.raises(TypeError):
+            UnitModel(unit="Dummy", config={"param": "fish"})
+        with pytest.raises(TypeError):
+            UnitModel(unit="Dummy", config={"nonexistant_param": True})
+
+        t3_config: dict[str, Any] = {
+            "process_name": "test",
+            "execute": [
+                {
+                    "unit": "T3ReviewUnitExecutor",
+                    "config": {
+                        "supply": {
+                            "unit": "T3DefaultBufferSupplier",
+                            "config": {
+                                "select": {"unit": "T3StockSelector"},
+                                "load": {
+                                    "unit": "T3SimpleDataLoader",
+                                    "config": {
+                                        "directives": [{"col": "stock"}]
+                                    }
+                                }
+                            }
+                        },
+                        "stage": {
+                            "unit": "T3SimpleStager",
+                            "config": {"execute": [{"unit": "DemoReviewT3Unit"}]}
+                        }
+                    }
+                }
+            ]
+        }
+
+        # recursive validation
+        UnitModel(unit="T3Processor", config=t3_config)
+
+        #with pytest.raises(TypeError):
+        #    t3_config["execute"][0]["config"]["supply"]["config"]["select"]["unit"] = "NotActuallyAUnit"
+        #    UnitModel(unit="T3Processor", config=t3_config)
+        #t3_config["execute"][0]["config"]["supply"]["config"]["select"]["unit"] = "NotActuallyAUnit"
+        UnitModel(unit="T3Processor", config=t3_config)
