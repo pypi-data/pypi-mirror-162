901e7134134703c43c43b29bcc83422e23c93d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:06 +0000

    Add package beautiful-repr/0.0/beautiful_repr-0.0.tar.gz

---
+++ packages/beautiful-repr/beautiful_repr-0.0.tar.gz/beautiful_repr-0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="beautiful_repr",
+    version="0.0",
+    url="https://github.com/TheArtur128/Beautiful-repr",
+    download_url="https://github.com/TheArtur128/Beautiful-repr/archive/refs/heads/master.zip",
+    author="Arthur",
+    author_email="s9339307190@gmail.com",
+    description="Library for beautiful object formatting",
+    python_requires='>=3.10',
+    packages=["beautiful_repr"]
+)
