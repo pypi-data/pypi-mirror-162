04dd9b38245f8baed3f766aa6a66b7a69b9a43fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:14 +0000

    Add package disquest-utils/1.1.0/disquest_utils-1.1.0-py3-none-any.whl

---
+++ packages/disquest-utils/disquest_utils-1.1.0-py3-none-any.whl/disquest_utils/levels.py
@@ -0,0 +1,11 @@
+import math
+
+class lvl:
+    def near(xp):
+        return round(xp / 100)
+
+    def next(xp):
+        return math.ceil(xp / 100)
+
+    def cur(xp):
+        return int(xp / 100)
