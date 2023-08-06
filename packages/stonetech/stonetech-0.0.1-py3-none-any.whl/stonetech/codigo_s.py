2bf1460f448572907e17c40c1e77cb29b124d4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:02 +0000

    Add package stonetech/0.0.1/stonetech-0.0.1-py3-none-any.whl

---
+++ packages/stonetech/stonetech-0.0.1-py3-none-any.whl/stonetech/codigo_s.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+"""Script to Make a Code for the StoneTech.
+
+Filename: codigo_s.py
+Created Date: Aug, 10th 2022
+"""
+
+
+class Hackathon:
+    """Simple Class to make easter eggs in python.
+
+    Example:
+    >>> hackathon = Hackathon()
+    >>> hackathon.start()
+    """
+
+    def __init__(self) -> None:
+        """Is a special method that is called when you create a new object.
+
+        Example:
+        >>> hackathon = Hackathon()
+        """
+        print("Hackathon initialized! We are ready to go!")
+
+    def start(self):
+        """Is a method that is part of the Hackathon class.
+
+        Example:
+        >>> hackathon.start()
+        """
+        print("Hackathon started! Good Lock for All!")
