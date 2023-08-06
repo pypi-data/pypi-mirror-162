fdd1859c921ff74cad40837cc09527c61ebbdf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:35 +0000

    Add package essearch/0.6/essearch-0.6.tar.gz

---
+++ packages/essearch/essearch-0.6.tar.gz/essearch-0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(name='essearch',
+      version='0.6',
+      description='an expand search function of elasticsearch',
+      author='SayaGugu',
+      author_email='2708475759@qq.com',
+      requires=['elasticsearch', 'ijson', 'pprint', 'tqdm'],  # 定义依赖哪些模块
+      packages=find_packages(),  # 系统自动从当前目录开始找包
+      # 如果有的文件不用打包，则只能指定需要打包的文件
+      # packages=['esopt', '__init__'],  # 指定目录中需要打包的py文件，注意不要.py后缀
+      license="apache 3.0"
+      )
