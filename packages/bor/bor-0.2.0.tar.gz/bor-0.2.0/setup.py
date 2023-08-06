6102a9eb77f81574b92d215671d3190ce4782953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:31 +0000

    Add package bor/0.2.0/bor-0.2.0.tar.gz

---
+++ packages/bor/bor-0.2.0.tar.gz/bor-0.2.0/setup.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from setuptools import setup
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_long_description():
+    readme_md = CURRENT_DIR / "README.md"
+    with open(readme_md, encoding="utf8") as ld_file:
+        return ld_file.read()
+
+
+setup(
+    name="bor",
+    version="0.2.0",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    description="User-friendly, tiny source code searcher written by pure Python.",
+    keywords=["code searcher"],
+    author="Furkan Onder",
+    author_email="furkanonder@protonmail.com",
+    url="https://github.com/furkanonder/bor/",
+    license="MIT",
+    python_requires=">=3.8",
+    packages=["bor"],
+    install_requires=[],
+    extras_require={},
+    zip_safe=False,
+    include_package_data=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    entry_points={
+        "console_scripts": [
+            "bor = bor.bor:main",
+        ]
+    },
+)
