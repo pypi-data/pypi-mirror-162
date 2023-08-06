94f546d90f5218cb5dc1593eee353c36026d1e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:58 +0000

    Add package jmf-distribution/0.1/jmf_distribution-0.1.tar.gz

---
+++ packages/jmf-distribution/jmf_distribution-0.1.tar.gz/jmf_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jmf_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jmf_distribution'],
+      author='James Michael Fritz',
+      author_email='jamesmichael0444@gmail.com',
+      zip_safe=False)
