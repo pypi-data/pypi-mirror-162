07ddea6031fdd9318b7df815fbd40ec1d4d92c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:52 +0000

    Add package floodmodeller-api/0.3.14.post1/floodmodeller_api-0.3.14.post1.tar.gz

---
+++ packages/floodmodeller-api/floodmodeller_api-0.3.14.post1.tar.gz/floodmodeller_api-0.3.14.post1/floodmodeller_api/__init__.py
@@ -0,0 +1,10 @@
+from .version import __version__
+
+# Import files
+from .zzn import ZZN
+from .ief import IEF
+from .ied import IED
+from .dat import DAT
+from .inp import INP
+
+from .util import open_docs
