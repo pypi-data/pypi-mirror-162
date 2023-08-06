668d5c4cb5c859db0b8011f064041cb32f3cb3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:23 +0000

    Add package healthcheck-helper/0.1.1/healthcheck-helper-0.1.1.tar.gz

---
+++ packages/healthcheck-helper/healthcheck-helper-0.1.1.tar.gz/healthcheck-helper-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from importlib.metadata import entry_points
+import setuptools
+
+setuptools.setup(
+    name="healthcheck-helper",
+    version='0.1.1',
+    packages=setuptools.find_packages(exclude=["tests"]),
+    author='M. Hakim Adiprasetya',
+    author_email='m.hakim.adiprasetya@gmail.com',
+    install_requires=[
+        'fastapi==0.79.0',
+        'uvicorn==0.18.2',
+        'aiohttp==3.8.1',
+        'typer==0.6.1'
+    ],
+    extras_require={
+        'tests': ['pytest', 'pytest-mock', 'pytest-asyncio']
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'healthcheck-helper = healthcheck_helper.main:main'
+        ]
+    }
+)
