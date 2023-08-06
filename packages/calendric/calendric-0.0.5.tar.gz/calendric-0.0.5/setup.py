6cde938e2a2927bb25b07a0ee01be988ab752b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:01 +0000

    Add package calendric/0.0.5/calendric-0.0.5.tar.gz

---
+++ packages/calendric/calendric-0.0.5.tar.gz/calendric-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from calendric import __version__
+
+setup(
+    name='calendric',
+    version=__version__,
+    description = "Calendric-lib is a creative package for calendrical conversion operations in Python.",
+    long_description="""# Calendric
+
+[![GitHub issues](https://img.shields.io/github/issues/saadbinmunir/Calendric-lib)](https://github.com/saadbinmunir/Calendric-lib/issues)
+[![GitHub forks](https://img.shields.io/github/forks/saadbinmunir/Calendric-lib)](https://github.com/saadbinmunir/Calendric-lib/network)
+[![GitHub stars](https://img.shields.io/github/stars/saadbinmunir/Calendric-lib)](https://github.com/saadbinmunir/Calendric-lib/stargazers)
+[![GitHub license](https://img.shields.io/github/license/saadbinmunir/Calendric-lib)](https://github.com/saadbinmunir/Calendric-lib/blob/main/LICENSE)
+
+#  About
+Calendric-lib is a creative package for calendrical conversion operations in Python.
+
+""",
+    long_description_content_type='text/markdown',
+    url='https://github.com/saadbinmunir/Calendric-lib.git',
+    author='Saad Bin Munir',
+    author_email='saadmunir24@gmail.com',
+
+    packages=find_packages(),
+)
