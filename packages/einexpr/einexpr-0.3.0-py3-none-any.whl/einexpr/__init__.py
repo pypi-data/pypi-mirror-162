4a56f30be4de6e646a636b63cf328ed6b2d86b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:55 +0000

    Add package einexpr/0.3.0/einexpr-0.3.0-py3-none-any.whl

---
+++ packages/einexpr/einexpr-0.3.0-py3-none-any.whl/einexpr/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.3'
+from .array_api import _types as types
+from . import dimension_utils
+from . import backends
+from . import utils
+from .utils import exceptions
+from .array_api import *
