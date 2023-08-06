e7e3d8d95c532ac8fb8a3c7acef609c5d96c4d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:14 +0000

    Add package spyder-line-profiler/0.3.1/spyder_line_profiler-0.3.1-py3-none-any.whl

---
+++ packages/spyder-line-profiler/spyder_line_profiler-0.3.1-py3-none-any.whl/spyder_line_profiler/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © 2022, Spyder Line Profiler contributors
+#
+# Licensed under the terms of the MIT license
+# ----------------------------------------------------------------------------
+"""
+Spyder Line Profiler.
+"""
+
+__version__ = "0.3.1"
+
+
+
