d86ea872b0ef272fcff166bd463de6031d0aa7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:21 +0000

    Add package django-zenblog-ds/0.0.2/django-zenblog_ds-0.0.2.tar.gz

---
+++ packages/django-zenblog-ds/django-zenblog_ds-0.0.2.tar.gz/django-zenblog_ds-0.0.2/zenblog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'zenblog'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('category/<int:category_int>', views.Category.as_view(), name='category'),
+    path('search-result/', views.SearchResult.as_view(), name='search_result'),
+    path('search-tag/<str:tag>', views.SearchTag.as_view(), name='search_tag'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('hitcount/', include(('hitcount.urls', 'hitcount'), namespace='hitcount')),
+]
