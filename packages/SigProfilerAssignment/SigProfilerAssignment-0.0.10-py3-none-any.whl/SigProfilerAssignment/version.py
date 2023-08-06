e712443b61af05792b93eb55788992588e1bb3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:26 +0000

    Add package SigProfilerAssignment/0.0.10/SigProfilerAssignment-0.0.10-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.10-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.10'
+version = '0.0.10'
+Update = '1. Reset reference genome to GRCh37 if a not supported genome is selected. 2. Add support for COSMIC exome reference signatures'
+    
+    
