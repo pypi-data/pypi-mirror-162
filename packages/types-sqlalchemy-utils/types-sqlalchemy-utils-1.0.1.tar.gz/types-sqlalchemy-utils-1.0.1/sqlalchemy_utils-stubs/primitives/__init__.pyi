13499ec2fb172d9a8f63b094dce32d7c44c2bef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:38 +0000

    Add package types-sqlalchemy-utils/1.0.1/types-sqlalchemy-utils-1.0.1.tar.gz

---
+++ packages/types-sqlalchemy-utils/types-sqlalchemy-utils-1.0.1.tar.gz/types-sqlalchemy-utils-1.0.1/sqlalchemy_utils-stubs/primitives/__init__.pyi
@@ -0,0 +1,6 @@
+from .country import Country as Country
+from .currency import Currency as Currency
+from .ltree import Ltree as Ltree
+from .weekday import WeekDay as WeekDay
+from .weekdays import WeekDays as WeekDays
+
