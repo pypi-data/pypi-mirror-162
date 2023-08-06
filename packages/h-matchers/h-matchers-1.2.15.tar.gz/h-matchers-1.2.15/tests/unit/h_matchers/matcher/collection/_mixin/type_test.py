462ebe7f112878ce06eeec0050fc7549b79ca001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:58 +0000

    Add package h-matchers/1.2.15/h-matchers-1.2.15.tar.gz

---
+++ packages/h-matchers/h-matchers-1.2.15.tar.gz/h-matchers-1.2.15/tests/unit/h_matchers/matcher/collection/_mixin/type_test.py
@@ -0,0 +1,23 @@
+from h_matchers.exception import NoMatch
+from h_matchers.matcher.collection._mixin.type import TypeMixin
+
+
+class HostClass(TypeMixin):
+    def __eq__(self, other):
+        try:
+            self._check_type(None, original=other)
+
+        except NoMatch:
+            return False
+
+        return True
+
+
+class TestTypeMixin:
+    def test_it_matches_specific_class(self):
+        matcher = HostClass.of_type(list)
+
+        assert matcher == []
+        assert [] == matcher
+        assert matcher != set()
+        assert set() != matcher
