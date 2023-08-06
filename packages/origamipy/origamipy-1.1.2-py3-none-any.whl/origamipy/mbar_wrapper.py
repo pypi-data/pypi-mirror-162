1d3426fff87c69718035c723f49437681041c60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package origamipy/1.1.2/origamipy-1.1.2-py3-none-any.whl

---
+++ packages/origamipy/origamipy-1.1.2-py3-none-any.whl/origamipy/mbar_wrapper.py
@@ -0,0 +1,329 @@
+"""Wrapper for the mbar module of the pymbar package.
+
+It applied to collections of decorrelated simulations, and uses the conditions
+information to perform the mbar analysis and then calculate free energies and
+thermodynamic averages for desired thermodynamic states.
+
+There are also functions for calculating barriers from LFEs.
+"""
+
+import numpy as np
+from pymbar import mbar
+from scipy.signal import argrelextrema
+from scipy.optimize import minimize
+
+from origamipy import files
+from origamipy import utility
+
+
+class MBARWrapper:
+    def __init__(self, decor_outs):
+        self._decor_outs = decor_outs
+
+    def perform_mbar(self):
+        rpots_matrix = self._calc_decorrelated_rpots_for_all_conditions()
+        num_steps_per_condition = self._decor_outs.num_steps_per_condition
+        self._mbar = mbar.MBAR(rpots_matrix, num_steps_per_condition)
+
+    def calc_expectation(self, se_tag, conds):
+        """Calculate expectation and std for given op and conditions."""
+        series = self._decor_outs.get_concatenated_series(se_tag)
+        decor_enes = self._decor_outs.get_concatenated_datatype("enes")
+        decor_ops = self._decor_outs.get_concatenated_datatype("ops")
+        decor_staples = self._decor_outs.get_concatenated_datatype("staples")
+        rpots = utility.calc_reduced_potentials(
+            decor_enes, decor_ops, decor_staples, conds
+        )
+        ave, std = self._mbar.computeExpectations(series, rpots)
+
+        return ave[0].astype(float), std[0].astype(float)
+
+    def calc_all_expectations(self, filebase, se_tags, all_conds):
+        """Calculate expectation and std for given ops and conditions.
+
+        Write both expectation values and standard deviations to file.
+        """
+        all_aves = []
+        all_stds = []
+        for se_tag in se_tags:
+            aves = []
+            stds = []
+            for conds in all_conds:
+                ave, std = self.calc_expectation(se_tag, conds)
+                aves.append(ave)
+                stds.append(std)
+
+            all_aves.append(aves)
+            all_stds.append(stds)
+
+        if type(all_conds) == list:
+            conds_tags = all_conds[0].condition_tags
+            all_conds_values = []
+            for conds in all_conds:
+                values = conds.characteristic_values
+                all_conds_values.append(values)
+        else:
+            conds_tags = all_conds.condition_tags
+            all_conds_values = all_conds.conditions_to_characteristic_values
+
+        all_tags = np.concatenate([conds_tags, se_tags])
+        all_conds_values = np.array(all_conds_values, dtype=float)
+
+        conds_aves = np.concatenate([all_conds_values, np.array(all_aves).T], axis=1)
+        aves_file = files.TagOutFile(f"{filebase}.aves")
+        aves_file.write(all_tags, conds_aves)
+
+        conds_stds = np.concatenate([all_conds_values, np.array(all_stds).T], axis=1)
+        stds_file = files.TagOutFile(f"{filebase}.stds")
+        stds_file.write(all_tags, conds_stds)
+
+    def calc_1d_lfes(self, se_tag, conds, filebase=None, xtag="temp"):
+        """Calculate 1D LFEs and stds for given op and conditions.
+
+        Write both LFEs and standard deviations to file.
+        """
+        series = self._decor_outs.get_concatenated_series(se_tag)
+        bins = list(set(series))
+        bins.sort()
+        lfe, std = self._calc_lfes(bins, series, conds)
+
+        if filebase != None:
+            header = [se_tag, xtag]
+            lfes_filebase = f"{filebase}-{se_tag}"
+            lfes_file = files.TagOutFile(f"{lfes_filebase}.aves")
+            self._write_lfe_series_to_file(lfes_file, header, lfe, bins, 1)
+
+            stds_file = files.TagOutFile(f"{lfes_filebase}.stds")
+            self._write_lfe_series_to_file(stds_file, header, std, bins, 1)
+
+        return lfe, std, bins
+
+    def calc_all_1d_lfes(self, filebase, se_tags, all_conds, xtag="temp"):
+        """Calculate 1D LFEs for given ops and conditions.
+
+        Write both LFEs and standard deviations to file.
+        """
+        xvars = [c[xtag] for c in all_conds]
+        for se_tag in se_tags:
+            lfes = []
+            stds = []
+            series = self._decor_outs.get_concatenated_series(se_tag)
+            bins = list(set(series))
+            bins.sort()
+            for conds in all_conds:
+                lfe, std = self._calc_lfes(bins, series, conds)
+                lfes.append(lfe)
+                stds.append(std)
+
+            header = np.concatenate([[se_tag], xvars])
+            lfes_filebase = f"{filebase}-{se_tag}"
+
+            lfes_file = files.TagOutFile(f"{lfes_filebase}.aves")
+            self._write_lfe_series_to_file(lfes_file, header, lfes, bins, 1)
+
+            stds_file = files.TagOutFile(f"{lfes_filebase}.stds")
+            self._write_lfe_series_to_file(stds_file, header, stds, bins, 1)
+
+    def calc_2d_lfes(self, se_tag_pair, conds):
+        """Calculate 2D LFEs and stds for given op and conditions.
+
+        Write both LFEs and standard deviations to file.
+
+        Needs fixing.
+        """
+        se_tag_1 = se_tag_pair[0]
+        se_tag_2 = se_tag_pair[1]
+        series_1 = self._decor_outs.get_concatenated_series(se_tag_1)
+        series_2 = self._decor_outs.get_concatenated_series(se_tag_2)
+        series_pairs = list(zip(series_1, series_2))
+        bins = list(set(series_pairs))
+        lfe, std = self._calc_lfes(bins, series_pairs, conds)
+
+        header = [se_tag, "lfes"]
+
+        lfes_filebase = f"{filebase}-{se_tag}"
+        lfes_file = files.TagOutFile(f"{lfes_filebase}.aves")
+        self._write_lfe_series_to_file(lfes_file, header, lfe, bins, 2)
+
+        stds_file = files.TagOutFile(f"{lfes_filebase}.stds")
+        self._write_lfe_series_to_file(stds_file, header, std, bins, 2)
+
+        return lfe, std, bins
+
+    def calc_all_2d_lfes(self, filebase, se_tag_pairs, all_conds, xtag="temp"):
+        """Calculate 2D LFEs for given ops and conditions.
+
+        Write both LFEs and standard deviations to file.
+        
+        Needs fixing.
+        """
+        conds_tags = reduced_conditions.condition_tags
+        xvar_i = conds_tags.index(xtag)
+        xvars = [c[xvar_i] for c in all_conds]
+
+        for se_tag_pair in se_tag_pairs:
+            se_tag_1 = se_tag_pair[0]
+            se_tag_2 = se_tag_pair[1]
+            lfes = []
+            stds = []
+            series_1 = self._decor_outs.get_concatenated_series(se_tag_1)
+            series_2 = self._decor_outs.get_concatenated_series(se_tag_2)
+            series_pairs = list(zip(series_1, series_2))
+            bins = list(set(series_pairs))
+            for conds in all_conds:
+                lfes.append(lfe)
+                stds.append(std)
+                lfe, std = self._calc_lfes(bins, series_pairs, conds)
+
+            header = np.concatenate([[se_tag1, se_tag2], xvars])
+            lfes_filebase = f"{filebase}-{se_tag_1}-{se_tag_2}"
+
+            lfes_file = files.TagOutFile(f"{lfes_filebase}.aves")
+            self._write_lfe_series_to_file(lfes_file, header, lfes, bins, 1)
+
+            stds_file = files.TagOutFile(f"{lfes_filebase}.stds")
+            self._write_lfe_series_to_file(stds_file, header, stds, bins, 1)
+
+    def estimate_melting_temp(self, conds, guess_temp):
+        """Estimate the melting temperature assuming barrier exists.
+
+        Find the global maximum that is not at the edge of the domain and then
+        find minima on either side and minimize difference between them.
+        """
+        series = self._decor_outs.get_concatenated_series("numfullyboundstaples")
+        bins = list(set(series))
+        bins.sort()
+        # Fix
+        melting_temp = minimize(
+            self._squared_barrier_diff, guess_temp, args=(bins, series, conds)
+        ).x[0]
+
+        return melting_temp
+
+    def estimate_melting_temp_endpoints(self, conds, guess_temp):
+        """Estimate the melting temperature with the endpoints.
+
+        Minimize the difference between the LFEs at the edges of the range.
+        """
+        series = self._decor_outs.get_concatenated_series("numfullyboundstaples")
+        bins = list(set(series))
+        bins.sort()
+        melting_temp = minimize(
+            self._squared_endpoints_diff, guess_temp, args=(bins, series, conds)
+        ).x[0]
+
+        return melting_temp
+
+    def _calc_decorrelated_rpots_for_all_conditions(self):
+        conditions_rpots = []
+        enes = self._decor_outs.get_concatenated_datatype("enes")
+        ops = self._decor_outs.get_concatenated_datatype("ops")
+        staples = self._decor_outs.get_concatenated_datatype("staples")
+        for conds in self._decor_outs.all_conditions:
+
+            # What if I just want to do one rep?
+            rpots = utility.calc_reduced_potentials(enes, ops, staples, conds)
+            conditions_rpots.append(rpots)
+
+        return np.array(conditions_rpots)
+
+    def _calc_lfes(self, bins, series, conds):
+        enes = self._decor_outs.get_concatenated_datatype("enes")
+        ops = self._decor_outs.get_concatenated_datatype("ops")
+        staples = self._decor_outs.get_concatenated_datatype("staples")
+        rpots = utility.calc_reduced_potentials(enes, ops, staples, conds)
+
+        value_to_bin = {value: i for i, value in enumerate(bins)}
+        bin_index_series = [value_to_bin[i] for i in series]
+        bin_index_series = np.array(bin_index_series)
+        lfes, stds = self._mbar.computePMF(rpots, bin_index_series, len(bins))
+
+        return lfes, stds
+
+    def _write_lfe_series_to_file(self, file, header, series, bins, dim):
+        bins = np.array(bins).reshape(len(bins), dim)
+        data = np.concatenate([bins, np.array(series).T], axis=1)
+        file.write(header, data)
+
+    def _squared_endpoints_diff(self, temp, bins, series, conds):
+        conds._conditions["temp"] = temp
+        lfes, stds = self._calc_lfes(bins, series, conds)
+
+        return (lfes[0] - lfes[-1]) ** 2
+
+
+# Maybe these should be somewhere else?
+def find_barrier(lfes):
+    maxima_i = argrelextrema(lfes, np.greater)[0]
+    if len(maxima_i) == 0:
+        print("No barrier detected")
+        raise Exception
+
+    maxima = lfes[maxima_i]
+    maximum_i = maxima_i[maxima.argmax()]
+
+    return maximum_i
+
+
+def find_minima(lfes, maximum_i):
+    lower_lfes = lfes[:maximum_i]
+    upper_lfes = lfes[maximum_i:]
+
+    return (lower_lfes.min(), upper_lfes.min())
+
+
+def find_minima_locs(lfes, maximum_i):
+    lower_lfes = lfes[:maximum_i]
+    upper_lfes = lfes[maximum_i:]
+
+    return (lower_lfes.argmin(), upper_lfes.argmin())
+
+
+def calc_forward_barrier_height(lfes):
+    try:
+        barrier_i = find_barrier(lfes)
+    except:
+        return np.nan
+
+    minima = find_minima(lfes, barrier_i)
+
+    return lfes[barrier_i] - minima[0]
+
+
+def calc_forward_barrier_error(lfes, stds):
+    try:
+        barrier_i = find_barrier(lfes)
+    except:
+        return np.nan, np.nan
+
+    minima = find_minima(lfes, barrier_i)
+    minima_locs = find_minima_locs(lfes, barrier_i)
+    barrier = lfes[barrier_i] - minima[0]
+    error = np.sqrt(stds[barrier_i]**2 + stds[minima_locs[0]]**2)
+
+    return barrier, error
+
+
+def calc_reverse_barrier_height(lfes):
+    try:
+        barrier_i = find_barrier(lfes)
+    except:
+        return np.nan
+
+    minima = find_minima(lfes, barrier_i)
+
+    return lfes[barrier_i] - minima[1]
+
+
+def calc_reverse_barrier_error(lfes, stds):
+    try:
+        barrier_i = find_barrier(lfes)
+    except:
+        return np.nan, np.nan
+
+    minima = find_minima(lfes, barrier_i)
+    minima_locs = find_minima_locs(lfes, barrier_i)
+    barrier = lfes[barrier_i] - minima[1]
+    error = np.sqrt(stds[barrier_i]**2 + stds[minima_locs[1]]**2)
+
+    return barrier, error
---
+++ packages/origamipy/origamipy-1.1.2-py3-none-any.whl/origamipy/utility.py
@@ -0,0 +1,170 @@
+"""Misc. constants and functions for analysis of simulations."""
+
+import scipy.constants
+from scipy import interpolate
+
+import numpy as np
+
+# Units vectors for euclidean space
+XHAT = np.array([1, 0, 0])
+YHAT = np.array([0, 1, 0])
+ZHAT = np.array([0, 0, 1])
+
+# All possible unit vectors
+dimensions = [XHAT, YHAT, ZHAT]
+directions = [-1, 1]
+VECTORS = [np.array(j * i) for i in dimensions for j in directions]
+
+# Avogadro's number
+AN = scipy.constants.N_A
+
+
+def value_is_multiple(value, multiple):
+    """Test if given value is a multiple of second value."""
+    is_multiple = False
+    try:
+        if value % multiple == 0:
+            is_multiple = True
+        else:
+            pass
+
+    except ZeroDivisionError:
+        is_multiple = False
+
+    return is_multiple
+
+
+def molarity_to_lattice_volume(molarity, lattice_site_volume):
+    """Given a molarity, calculate the volume that cancels the fugacity.
+
+    Volume is in units of number of lattice sites.
+    """
+    # Number of lattice sites per L (1 L * (1000) cm^3 / L * m^3 / (10^2)^3 cm^3)
+    sites_per_litre = 1e-3 / lattice_site_volume
+
+    # u = KB*T*ln(p), where p is the number of particles per lattice site
+    # g = exp(1/(-KB*T)*u) = exp(ln(p)) = p
+    # V * p = 1, V = 1 / p
+    # So just convert molarity to number of particles per lattice site
+    V = 1 / (molarity * AN / sites_per_litre)
+    return V
+
+
+def rotate_vectors_half(vector, rotation_axis):
+    vector = np.copy(vector)
+    if all(np.abs(rotation_axis) == XHAT):
+        vector[:, 1] = -vector[:, 1]
+        vector[:, 2] = -vector[:, 2]
+
+    if all(np.abs(rotation_axis) == YHAT):
+        vector[:, 0] = -vector[:, 0]
+        vector[:, 2] = -vector[:, 2]
+
+    if all(np.abs(rotation_axis) == ZHAT):
+        vector[:, 0] = -vector[:, 0]
+        vector[:, 1] = -vector[:, 1]
+
+    return vector
+
+
+def rotate_vectors_quarter(vectors, rotation_axis, direction):
+    """Rotate given vectors pi/2 about given axis in given direction."""
+    rotated_vectors = np.copy(vectors)
+    if all(rotation_axis == XHAT):
+        y = vectors[:, 1]
+        z = vectors[:, 2]
+        rotated_vectors[:, 1] = direction * -z
+        rotated_vectors[:, 2] = direction * y
+
+    if all(rotation_axis == -XHAT):
+        y = vectors[:, 1]
+        z = vectors[:, 2]
+        rotated_vectors[:, 1] = direction * z
+        rotated_vectors[:, 2] = direction * -y
+
+    elif all(rotation_axis == YHAT):
+        x = vectors[:, 0]
+        z = vectors[:, 2]
+        rotated_vectors[:, 2] = direction * -x
+        rotated_vectors[:, 0] = direction * z
+
+    elif all(rotation_axis == -YHAT):
+        x = vectors[:, 0]
+        z = vectors[:, 2]
+        rotated_vectors[:, 2] = direction * x
+        rotated_vectors[:, 0] = direction * -z
+
+    elif all(rotation_axis == ZHAT):
+        x = vectors[:, 0]
+        y = vectors[:, 1]
+        rotated_vectors[:, 0] = direction * -y
+        rotated_vectors[:, 1] = direction * x
+
+    elif all(rotation_axis == -ZHAT):
+        x = vectors[:, 0]
+        y = vectors[:, 1]
+        rotated_vectors[:, 0] = direction * y
+        rotated_vectors[:, 1] = direction * -x
+
+    return rotated_vectors
+
+
+def calc_reduced_potentials(enes, ops, num_staples, conditions):
+    """Reduced potentials as defined in shirts2008."""
+    temp = float(conditions.temp)
+    bias_collection = conditions.total_bias(temp, ops)
+    e = enes.enthalpies + enes.stacking_energies + bias_collection
+    G = (conditions.reduced_staple_us * num_staples._data[1:].T).sum(axis=1)
+    renes = e / temp
+
+    return renes - enes.entropies + G
+
+
+def calc_staple_lengths(system_file):
+    return np.array([len(ident) for ident in system_file.identities[1:]])
+
+
+def calc_num_staple_types(system_file):
+    return len(system_file.identities[1:])
+
+
+def calc_num_scaffold_domains(system_file):
+    return len(system_file.identities[0])
+
+
+def estimate_melting_points(unittypes, aves, temps, tagbase):
+    melting_points = []
+    offset = 0
+    if "staple" in tagbase:
+        offset = 1
+
+    for unit_i in range(offset, unittypes + offset):
+        tag = f"{tagbase}{unit_i}"
+        ave_states = aves[tag]
+        if ave_states.iloc[-1] >= 0.5:
+            mp = np.NaN
+        else:
+            mp = float(interpolate.interp1d(ave_states, temps)(0.5))
+
+        melting_points.append(mp)
+
+    return np.array(melting_points)
+
+
+def estimate_staple_melting_points(stapletypes, aves, temps):
+    return estimate_melting_points(stapletypes, aves, temps, "staplestates")
+
+
+def estimate_domain_melting_points(scaffolddomains, aves, temps):
+    return estimate_melting_points(scaffolddomains, aves, temps, "domainstate")
+
+
+def fill_assembled_shape_array(flat_array, index_to_unittype):
+    rows = index_to_unittype.shape[0]
+    cols = index_to_unittype.shape[1]
+    assembled_array = np.zeros([rows, cols])
+    for row, unittypes in enumerate(index_to_unittype):
+        for col, unittype in enumerate(unittypes):
+            assembled_array[row, col] = flat_array[unittype - 1]
+
+    return assembled_array
