553a358d7a25e61ce00ae25760083247281f79ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:44 +0000

    Add package jupyterlab-google-analytics/0.0.2/jupyterlab_google_analytics-0.0.2.tar.gz

---
+++ packages/jupyterlab-google-analytics/jupyterlab_google_analytics-0.0.2.tar.gz/jupyterlab_google_analytics-0.0.2/src/index.ts
@@ -0,0 +1,38 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from "@jupyterlab/application";
+
+import { ISettingRegistry } from "@jupyterlab/settingregistry";
+
+/**
+ * Initialization data for the jupyterlab-google-analytics extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: "jupyterlab-google-analytics:plugin",
+  autoStart: true,
+  requires: [ISettingRegistry],
+  activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry) => {
+    const setting = await settingRegistry.load(plugin.id);
+    const trackingId = setting.get("trackingId");
+
+    var ga_url = "https://www.googletagmanager.com/gtag/js?id=" + trackingId;
+    const a = document.createElement("script");
+    const m = document.getElementsByTagName("script")[0];
+    a.async = true;
+    a.src = ga_url;
+    m.parentNode?.insertBefore(a, m);
+
+    // Activate the Global Site Tag
+    const windowAnalytics = window as any;
+    windowAnalytics.dataLayer = windowAnalytics.dataLayer || [];
+    function gtag(a: any, b: any) {
+      windowAnalytics.dataLayer.push([a, b]);
+    }
+
+    gtag("js", new Date());
+    gtag("config", trackingId);
+  },
+};
+
+export default plugin;
