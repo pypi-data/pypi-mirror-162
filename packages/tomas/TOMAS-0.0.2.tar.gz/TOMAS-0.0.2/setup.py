267416aae001b557c2db6393626de2daf6750f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:12 +0000

    Add package tomas/0.0.2/TOMAS-0.0.2.tar.gz

---
+++ packages/tomas/TOMAS-0.0.2.tar.gz/TOMAS-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="TOMAS",
+    version="0.0.2",
+    license='MIT',
+    author="Qiuyu Lian",
+    author_email="qiuyu.lian@sjtu.edu.cn",
+    description="A tool for TOtal-MRNA-Aware Single-cell RNA-seq data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages('tomas'),
+    package_dir={'': 'tomas'},
+    url="https://tomas.readthedocs.io/en/latest/",
+    python_requires='>=3.8',
+    install_requires=[
+        "pyDIMM==0.0.2",
+        "numpy",
+        "scipy",
+        "tqdm",
+        "multiprocessing",
+        "scanpy",
+        "pickle",
+        "statsmodels"
+    ]
+)
